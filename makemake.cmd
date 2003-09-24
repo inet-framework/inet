1a214adacbcb67827ed613410eba478d1ee2a617
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2003-09-24 03:45:00 +0000

    *** empty log message ***
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@38 539eec35-cc16-0410-b457-356aab9780e1

--- makemake.cmd
+++ makemake.cmd
@@ -1,123 +1,69 @@
-@echo off
-set ROOT=C:\home\ipsuite-rearranged
-set ICONV=C:/home/tools/iconv-1.8.win32
-set LIBXML=C:/home/tools/libxml2-2.5.4.win32
+set root=C:\home\opp-rsvp-te\ipsuite-rearranged
+set MAKEMAKE=call %root%\opp_nmakemake
 
-cd %ROOT%
-cd && call opp_nmakemake -f -n -c ipsuiteconfig.vc Network Applications Transport NetworkInterfaces PHY Util Nodes
+set LIBXML_INCLUDES='-I/usr/include/libxml2'
+set LIBXML_LIBS='-lxml2'
+set ALL_IPSUITE_INCLUDES=-I$(ROOT)/Network/IPv4/Core -I$(ROOT)/Network/IPv4/QoS -I$(ROOT)/Transport/TCPv4 -I$(ROOT)/Transport/UDPv4 -I$(ROOT)/Transport/Socketv4 -I$(ROOT)/NetworkInterfaces -I$(ROOT)/NetworkInterfaces/PPP-old -I$(ROOT)/NetworkInterfaces/Queues -I$(ROOT)/Applications/Generic -I$(ROOT)/Applications/TCPApp -I$(ROOT)/Applications/PingApp -I$(ROOT)/Util -I$(ROOT)/Nodes/IPSuite
+set ALL_MPLS_INCLUDES=$ALL_IPSUITE_INCLUDES -I$(ROOT)/Network/MPLS -I$(ROOT)/Network/LDP -I$(ROOT)/Network/RSVP_TE -I$(ROOT)/Nodes/MPLS
 
-cd %ROOT%\Applications
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+%MAKEMAKE% -f -n -c ipsuiteconfig
 
-cd %ROOT%\Examples
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Applications && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\Examples && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\Network && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\NetworkInterfaces && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\Nodes && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\PHY && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\Transport && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
+cd %root%\Util && %MAKEMAKE% -f -n -c ..\ipsuiteconfig
 
-cd %ROOT%\Network
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Applications\Generic && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Applications\PingApp && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Applications\TCPApp && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Network\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Util
+cd %root%\Applications\TCPExample && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\TCPApp -I..\..\Network\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Network\MPLS -I..\..\Network\LDP -I..\..\Util
 
-cd %ROOT%\NetworkInterfaces
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
- 
-cd %ROOT%\Nodes
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Examples\IPSuite && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig
+cd %root%\Examples\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig
 
-cd %ROOT%\PHY
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Examples\IPSuite\KIDSNw1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\McNetwork2 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\OldNetwork && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\PerformAnalysis && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\SocketTester && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\TCPClientServerNet && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\TCPFlavours && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\TCPTester && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\TCPUDPDirectNet && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
+cd %root%\Examples\IPSuite\UDPSockets && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_IPSUITE_INCLUDES
 
-cd %ROOT%\Transport
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Examples\MPLS\Tester && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig $ALL_MPLS_INCLUDES
+cd %root%\Examples\MPLS\demo2 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\ldp-mpls1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\samples && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig
+cd %root%\Examples\MPLS\TestTE1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\TestTE2 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\TestTE3 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\TestTE4 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\TestTE4Old && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\TestTE5 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
+cd %root%\Examples\MPLS\TestTE6 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig -I..\Tester $ALL_MPLS_INCLUDES $LIBXML_LIBS
 
-cd %ROOT%\Util
-cd && call opp_nmakemake -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Network\IPv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig 
+cd %root%\Network\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Util
+cd %root%\Network\LDP && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\MPLS -I..\..\Util
+cd %root%\Network\RSVP_TE && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\MPLS -I..\..\Util $LIBXML_INCLUDES
+cd %root%\Network\IPv4\Core && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig -I..\QoSStub -I..\..\..\NetworkInterfaces -I..\..\..\NetworkInterfaces\Queues -I..\..\..\Util
+cd %root%\Network\IPv4\QoS && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig -I..\QoSStub -I..\Core -I..\..\..\Transport\TCPv4 -I..\..\..\Util
+cd %root%\Network\IPv4\QoSStub && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig -I..\Core -I..\..\..\Util
 
-cd %ROOT%\Applications\Generic
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Util
+cd %root%\NetworkInterfaces\PPP-old && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Util -I..\..\Network\IPv4\Core
+cd %root%\NetworkInterfaces\Queues && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Util -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS
 
-cd %ROOT%\Applications\PingApp
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Util
+cd %root%\Nodes\IPSuite && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\..\NetworkInterfaces\PPP-old -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Queues  -I..\..\Applications\Generic -I..\..\Applications\TCPApp -I..\..\Applications\PingApp -I..\..\Util
+cd %root%\Nodes\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS -I..\..\Network\MPLS -I..\..\Network\LDP -I..\..\Network\RSVP_TE -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\..\NetworkInterfaces\PPP-old -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Queues  -I..\..\Applications\Generic -I..\..\Applications\TCPApp -I..\..\Applications\PingApp -I..\IPSuite -I..\..\Util
 
-cd %ROOT%\Applications\TCPApp
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Transport/TCPv4 -I../../Util
+cd %root%\Transport\Socketv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\TCPv4 -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Transport\TCPv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\..\Network\IPv4\Core -I..\..\Applications\TCPApp -I..\..\Util
+cd %root%\Transport\UDPv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig -I..\TCPv4 -I..\..\Network\IPv4\Core -I..\..\Util
 
-cd %ROOT%\Applications\TCPExample
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Transport/TCPv4 -I../../Network/MPLS -I../../Network/LDP -I../../Util
-
-cd %ROOT%\Examples\IPSuite
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc
-
-cd %ROOT%\Examples\IPSuite\KIDSNw1
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\McNetwork2
-:cd && call opp_nmakemake -f -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-cd && call opp_nmakemake -f -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite ../../../Nodes/IPSuite -I../../../Network/IPv4/Core ../../../Network/IPv4/Core -I../../../Network/IPv4/QoS ../../../Network/IPv4/QoS -I../../../Transport/TCPv4 ../../../Transport/TCPv4 -I../../../Transport/UDPv4 ../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old ../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces ../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues ../../../NetworkInterfaces/Queues  -I../../../Applications/Generic ../../../Applications/Generic -I../../../Applications/PingApp ../../../Applications/PingApp -I../../../Util ../../../Util
-
-cd %ROOT%\Examples\IPSuite\OldNetwork
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\PerformAnalysis
-:cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-cd && call opp_nmakemake -f -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite ../../../Nodes/IPSuite -I../../../Network/IPv4/Core ../../../Network/IPv4/Core -I../../../Network/IPv4/QoS ../../../Network/IPv4/QoS -I../../../Transport/TCPv4 ../../../Transport/TCPv4 -I../../../Transport/UDPv4 ../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old ../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces ../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues ../../../NetworkInterfaces/Queues  -I../../../Applications/Generic ../../../Applications/Generic -I../../../Applications/PingApp ../../../Applications/PingApp -I../../../Util ../../../Util
-
-cd %ROOT%\Examples\IPSuite\SocketTester
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\TCPClientServerNet
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\TCPFlavours
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\TCPTester
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\TCPUDPDirectNet
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Examples\IPSuite\UDPSockets
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../../../Nodes/IPSuite -I../../../Network/IPv4/Core -I../../../Network/IPv4/QoS -I../../../Transport/TCPv4 -I../../../Transport/UDPv4 -I../../../Transport/Socketv4 -I../../../NetworkInterfaces/PPP-old -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues  -I../../../Applications/Generic -I../../../Applications/PingApp -I../../../Util
-
-cd %ROOT%\Network\IPv4
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc 
-
-cd %ROOT%\Network\MPLS
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../IPv4/Core -I../../Transport/TCPv4 -I../../Util
-
-cd %ROOT%\Network\LDP
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../IPv4/Core -I../../Transport/TCPv4 -I../../Transport/UDPv4 -I../MPLS -I../../Util
-
-cd %ROOT%\Network\RSVP_TE
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../IPv4/Core -I../../Transport/TCPv4 -I../MPLS -I../../Util -I%LIBXML%/include -I%ICONV%/include
-
-cd %ROOT%\Network\IPv4\Core
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../QoSStub -I../../../NetworkInterfaces -I../../../NetworkInterfaces/Queues -I../../../Util
-
-cd %ROOT%\Network\IPv4\QoS
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../QoSStub -I../Core -I../../../Transport/TCPv4 -I../../../Util
-
-cd %ROOT%\Network\IPv4\QoSStub
-cd && call opp_nmakemake -f -n -c ..\..\..\ipsuiteconfig.vc -I../Core -I../../../Util
-
-cd %ROOT%\NetworkInterfaces\PPP-old
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Util -I../../Network/IPv4/Core
-
-cd %ROOT%\NetworkInterfaces\Queues
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Util -I../../Network/IPv4/Core -I../../Network/IPv4/QoS
-
-cd %ROOT%\Nodes\IPSuite
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Network/IPv4/QoS -I../../Transport/TCPv4 -I../../Transport/UDPv4 -I../../NetworkInterfaces/PPP-old -I../../NetworkInterfaces -I../../NetworkInterfaces/Queues  -I../../Applications/Generic -I../../Applications/PingApp -I../../Util
-
-cd %ROOT%\Nodes\MPLS
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Network/IPv4/QoS -I../../Network/MPLS -I../../Network/LDP -I../../Network/RSVP_TE -I../../Transport/TCPv4 -I../../Transport/UDPv4 -I../../NetworkInterfaces/PPP-old -I../../NetworkInterfaces -I../../NetworkInterfaces/Queues  -I../../Applications/Generic -I../../Applications/PingApp -I../../Util
-
-cd %ROOT%\Transport\Socketv4
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../TCPv4 -I../../Network/IPv4/Core -I../../Util
-
-cd %ROOT%\Transport\TCPv4
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../../Network/IPv4/Core -I../../Util
-
-cd %ROOT%\Transport\UDPv4
-cd && call opp_nmakemake -f -n -c ..\..\ipsuiteconfig.vc -I../TCPv4 -I../../Network/IPv4/Core -I../../Util
-
-cd %ROOT%
 
