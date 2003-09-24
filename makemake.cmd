d31796b0f708fa02db2b7c6feb006b2f78adfdcc
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2003-09-24 07:28:44 +0000

    *** empty log message ***
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@47 539eec35-cc16-0410-b457-356aab9780e1

--- makemake.cmd
+++ makemake.cmd
@@ -12,25 +12,25 @@ set ALL_MPLS_INCLUDES=%ALL_IPSUITE_INCLUDES% -I$(ROOT)/Network/MPLS -I$(ROOT)/Ne
 
 :#--------------------------------------
 
-%MAKEMAKE% -f -n -c ipsuiteconfig.vc
+%MAKEMAKE% -f -n -r -c ipsuiteconfig.vc
 
-cd %root%\Applications && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\Examples && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\Tests && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\Network && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\NetworkInterfaces && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\Nodes && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\PHY && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\Transport && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
-cd %root%\Util && %MAKEMAKE% -f -n -c ..\ipsuiteconfig.vc
+cd %root%\Applications && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\Examples && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\Tests && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\Network && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\NetworkInterfaces && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\Nodes && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\PHY && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\Transport && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
+cd %root%\Util && %MAKEMAKE% -f -n -r -c ..\ipsuiteconfig.vc
 
-cd %root%\Applications\Generic && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Util
-cd %root%\Applications\PingApp && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Util
-cd %root%\Applications\TCPApp && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Util
-cd %root%\Applications\TCPExample && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\TCPApp -I..\..\Network\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Network\MPLS -I..\..\Network\LDP -I..\..\Util
+cd %root%\Applications\Generic && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Applications\PingApp && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Applications\TCPApp && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Util
+cd %root%\Applications\TCPExample && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\TCPApp -I..\..\Network\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Network\MPLS -I..\..\Network\LDP -I..\..\Util
 
-cd %root%\Examples\IPSuite && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc
-cd %root%\Examples\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc
+cd %root%\Examples\IPSuite && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc
+cd %root%\Examples\MPLS && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc
 
 cd %root%\Examples\IPSuite\KIDSNw1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_IPSUITE_INCLUDES%
 cd %root%\Examples\IPSuite\McNetwork2 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_IPSUITE_INCLUDES%
@@ -43,10 +43,10 @@ cd %root%\Examples\IPSuite\TCPTester && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconf
 cd %root%\Examples\IPSuite\TCPUDPDirectNet && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_IPSUITE_INCLUDES%
 cd %root%\Examples\IPSuite\UDPSockets && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_IPSUITE_INCLUDES%
 
-cd %root%\Examples\MPLS\Tester && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig.vc %ALL_MPLS_INCLUDES%
+cd %root%\Examples\MPLS\Tester && %MAKEMAKE% -f -n -r -c ..\..\..\ipsuiteconfig.vc %ALL_MPLS_INCLUDES%
 cd %root%\Examples\MPLS\demo2 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
 cd %root%\Examples\MPLS\ldp-mpls1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
-cd %root%\Examples\MPLS\samples && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig.vc
+cd %root%\Examples\MPLS\samples && %MAKEMAKE% -f -n -r -c ..\..\..\ipsuiteconfig.vc
 cd %root%\Examples\MPLS\TestTE1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc -I..\Tester %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
 cd %root%\Examples\MPLS\TestTE2 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc -I..\Tester %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
 cd %root%\Examples\MPLS\TestTE3 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc -I..\Tester %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
@@ -55,24 +55,24 @@ cd %root%\Examples\MPLS\TestTE4Old && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig
 cd %root%\Examples\MPLS\TestTE5 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc -I..\Tester %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
 cd %root%\Examples\MPLS\TestTE6 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc -I..\Tester %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
 
-cd %root%\Tests\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc
+cd %root%\Tests\MPLS && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc
 cd %root%\Tests\MPLS\LDP1 && %MAKEMAKE% -f -w -c ..\..\..\ipsuiteconfig.vc %ALL_MPLS_INCLUDES% %LIBXML_LIBS%
 
-cd %root%\Network\IPv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc 
-cd %root%\Network\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Util
-cd %root%\Network\LDP && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\MPLS -I..\..\Util
-cd %root%\Network\RSVP_TE && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\MPLS -I..\..\Util %LIBXML_INCLUDES%
-cd %root%\Network\IPv4\Core && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig.vc -I..\QoSStub -I..\..\..\NetworkInterfaces -I..\..\..\NetworkInterfaces\Queues -I..\..\..\Util
-cd %root%\Network\IPv4\QoS && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig.vc -I..\QoSStub -I..\Core -I..\..\..\Transport\TCPv4 -I..\..\..\Util
-cd %root%\Network\IPv4\QoSStub && %MAKEMAKE% -f -n -c ..\..\..\ipsuiteconfig.vc -I..\Core -I..\..\..\Util
+cd %root%\Network\IPv4 && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc 
+cd %root%\Network\MPLS && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Util
+cd %root%\Network\LDP && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\MPLS -I..\..\Util
+cd %root%\Network\RSVP_TE && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\IPv4\Core -I..\..\Transport\TCPv4 -I..\MPLS -I..\..\Util %LIBXML_INCLUDES%
+cd %root%\Network\IPv4\Core && %MAKEMAKE% -f -n -r -c ..\..\..\ipsuiteconfig.vc -I..\QoSStub -I..\..\..\NetworkInterfaces -I..\..\..\NetworkInterfaces\Queues -I..\..\..\Util
+cd %root%\Network\IPv4\QoS && %MAKEMAKE% -f -n -r -c ..\..\..\ipsuiteconfig.vc -I..\QoSStub -I..\Core -I..\..\..\Transport\TCPv4 -I..\..\..\Util
+cd %root%\Network\IPv4\QoSStub && %MAKEMAKE% -f -n -r -c ..\..\..\ipsuiteconfig.vc -I..\Core -I..\..\..\Util
 
-cd %root%\NetworkInterfaces\PPP-old && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Util -I..\..\Network\IPv4\Core
-cd %root%\NetworkInterfaces\Queues && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Util -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS
+cd %root%\NetworkInterfaces\PPP-old && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Util -I..\..\Network\IPv4\Core
+cd %root%\NetworkInterfaces\Queues && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Util -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS
 
-cd %root%\Nodes\IPSuite && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\..\NetworkInterfaces\PPP-old -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Queues  -I..\..\Applications\Generic -I..\..\Applications\TCPApp -I..\..\Applications\PingApp -I..\..\Util
-cd %root%\Nodes\MPLS && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS -I..\..\Network\MPLS -I..\..\Network\LDP -I..\..\Network\RSVP_TE -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\..\NetworkInterfaces\PPP-old -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Queues  -I..\..\Applications\Generic -I..\..\Applications\TCPApp -I..\..\Applications\PingApp -I..\IPSuite -I..\..\Util
+cd %root%\Nodes\IPSuite && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\..\NetworkInterfaces\PPP-old -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Queues  -I..\..\Applications\Generic -I..\..\Applications\TCPApp -I..\..\Applications\PingApp -I..\..\Util
+cd %root%\Nodes\MPLS && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Network\IPv4\QoS -I..\..\Network\MPLS -I..\..\Network\LDP -I..\..\Network\RSVP_TE -I..\..\Transport\TCPv4 -I..\..\Transport\UDPv4 -I..\..\NetworkInterfaces\PPP-old -I..\..\NetworkInterfaces -I..\..\NetworkInterfaces\Queues  -I..\..\Applications\Generic -I..\..\Applications\TCPApp -I..\..\Applications\PingApp -I..\IPSuite -I..\..\Util
 
-cd %root%\Transport\Socketv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\TCPv4 -I..\..\Network\IPv4\Core -I..\..\Util
-cd %root%\Transport\TCPv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Applications\TCPApp -I..\..\Util
-cd %root%\Transport\UDPv4 && %MAKEMAKE% -f -n -c ..\..\ipsuiteconfig.vc -I..\TCPv4 -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Transport\Socketv4 && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\TCPv4 -I..\..\Network\IPv4\Core -I..\..\Util
+cd %root%\Transport\TCPv4 && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\..\Network\IPv4\Core -I..\..\Applications\TCPApp -I..\..\Util
+cd %root%\Transport\UDPv4 && %MAKEMAKE% -f -n -r -c ..\..\ipsuiteconfig.vc -I..\TCPv4 -I..\..\Network\IPv4\Core -I..\..\Util
 
