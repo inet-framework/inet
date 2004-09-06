50cd472ef59795b4a7b122b2b109fb77e35ec50f
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2004-09-06 21:24:41 +0000

    removed tons of shit
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@396 539eec35-cc16-0410-b457-356aab9780e1

--- Network/MPLS/MPLSPacket.cc
+++ Network/MPLS/MPLSPacket.cc
@@ -13,24 +13,12 @@
 ***************************************************************************/
 
 
-
-/*
-*    File Name MPLSPacket.cc
-*    MPLS library
-*    This file implements MPLSPacket class
-**/
-
 #include <omnetpp.h>
-#include <string.h>
-
 #include "MPLSPacket.h"
 
 // constructors
 MPLSPacket::MPLSPacket(const char *name) : cPacket(name)
 {
-    setLength(8 * MPLS_HEADER_LENGTH);
-
-    _protocol = MPLS_PROT_UNDEF;
 }
 
 MPLSPacket::MPLSPacket(const MPLSPacket & p)
@@ -43,18 +31,7 @@ MPLSPacket::MPLSPacket(const MPLSPacket & p)
 MPLSPacket & MPLSPacket::operator=(const MPLSPacket & p)
 {
     cPacket::operator=(p);
-    _protocol = p._protocol;
     return *this;
 }
 
-// information functions
-void MPLSPacket::info(char *buf)
-{
-    cPacket::info(buf);
-    sprintf(buf + strlen(buf), " Protocol: %x", _protocol);
-}
 
-void MPLSPacket::writeContents(ostream & os)
-{
-    os << "MPLSPacket: " << "\nProtocol " << (int) _protocol << "\n";
-}
--- Network/MPLS/MPLSPacket.h
+++ Network/MPLS/MPLSPacket.h
@@ -13,67 +13,23 @@
 ***************************************************************************/
 
 
-/*
-*    File Name MPLSPacket.h
-*    LDP library
-*    This file defines the MPLSPacket class
-**/
-
 #ifndef __MPLSPacket_H
 #define __MPLSPacket_H
 
 #include <omnetpp.h>
-#include <string>
 #include <list>
 #include <queue>
 
 using namespace std;
 
-//const char* NATIVE_IP_PACKET = "IP";
-const int MPLS_HEADER_LENGTH = 32;
+// label stack
+typedef queue<int,list<int> > IQueue;
 
-/* Different layer 2 technologies*/
-enum MPLS_ProtocolFieldId
-{
-    MPLS_PROT_UNDEF,
-    MPLS_PROT_IP,
-    MPLS_PROT_ATM
-
-};
-
-typedef queue<int, list<int>  >    IQueue;
-
-/*Packet header*/
-struct mpls_hdr
-{
-  string    label;    //mpls label
-  int        cos;    //class of service - reserved bits
-  int        ttl;    //time to live
-  int        s;        //stack bit
-
-};
-
-/*Packet routing info*/
-struct PktInfo
-{
-  int  dstaddr;
-  int  srcnode;
-  int  dstnode;
-  int  phb;
-};
-
-
-
+// FIXME length should be adjusted when length of label stack changes
 class MPLSPacket: public cPacket
 {
   private:
-    MPLS_ProtocolFieldId _protocol;
     IQueue label;
-    int cos;
-    int ttl;
-    bool s;
-    cMessage* data;
-
 
   public:
     /* constructors*/
@@ -83,45 +39,10 @@ class MPLSPacket: public cPacket
     /* assignment operator*/
     virtual MPLSPacket& operator=(const MPLSPacket& p);
 
-    /*dup: clonning function
-     *@param: none
-     **/
+    /**
+     * cloning function
+     */
     virtual cObject *dup() const { return new MPLSPacket(*this); }
-
-    /*className: get class name
-     *@param: none
-     **/
-    virtual const char *className() const { return "MPLSPacket"; }
-
-    /*info: set packet info
-     *@param: buf - The information
-     **/
-    virtual void info(char *buf);
-
-    /*writeContents: Write out the packet content
-     *@param: os - The ostream
-     **/
-    virtual void writeContents(ostream& os);
-
-    /*heaaderBitLength: Get the header lenght in bit
-     *@param:    none
-     **/
-    int headerBitLength() { return 8 * MPLS_HEADER_LENGTH; }
-
-    /*headerByteLength:    Get the header length in byte
-     *@param: none
-     **/
-    int headerByteLength() { return MPLS_HEADER_LENGTH; }
-
-    /*protocol:    Get the protocol field
-     *@param:    none
-     **/
-    MPLS_ProtocolFieldId protocol() { return _protocol; }
-
-    /*setProtocol:    Set the protocol field
-     *@param: none
-     **/
-    void setProtocol( MPLS_ProtocolFieldId p ) { _protocol = p; }
 
     /*swapLabel:    Swap Label operation
      *@param: newLabel - The new Label to use
@@ -129,12 +50,10 @@ class MPLSPacket: public cPacket
     inline void swapLabel(int newLabel){label.pop();label.push(newLabel);}
 
     /*pushLabel:    Push new label
-     *@param: newLabel - The added label
      **/
     inline void pushLabel(int newLabel){label.push(newLabel);}
 
     /*popLabel:    Pop out top label
-     *@param: none
      **/
     inline void popLabel(){label.pop();}
 
@@ -146,38 +65,6 @@ class MPLSPacket: public cPacket
      *@param: none
      **/
     inline int getLabel(){return label.front();}
-
-    /*getCOS:    Get the COS
-    *@param: none
-    **/
-    inline int getCOS(){return cos;}
-
-    /*SetCOS:    Set the COS
-     *@param: newCos - The new cos
-     **/
-    inline void setCOS(int newCOS){cos = newCOS;} //not implemented
-
-    /*getTTL: Get the Time to live
-     *@param: none
-     **/
-    inline int getTTL() {return ttl;}
-
-    /*setTTL:    Set Time to Live
-     *@param: none
-     **/
-    inline void setTTL(int newTTL) {ttl = newTTL;}
-
-    /*getSbit:    Get the S bit
-     *@param: none
-     **/
-    inline int getSbit() {return s;}
-
-    /*setSbit:    Set the S bit
-     *@param newS - Th new S bit
-     **/
-    inline void setSbit(bool newS) {s = newS;}
-
-
 };
 
 #endif
