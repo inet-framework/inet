c4b52a105d6a36b90a1ca5cec89eca30afbf19bb
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2004-09-16 19:25:19 +0000

    *** empty log message ***
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@477 539eec35-cc16-0410-b457-356aab9780e1

--- Examples/MPLS/Tester/NewBWRequestTester.cc
+++ Examples/MPLS/Tester/NewBWRequestTester.cc
@@ -1,14 +1,14 @@
 /*******************************************************************
 *
-*	This library is free software, you can redistribute it 
-*	and/or modify 
-*	it under  the terms of the GNU Lesser General Public License 
-*	as published by the Free Software Foundation; 
-*	either version 2 of the License, or any later version.
-*	The library is distributed in the hope that it will be useful, 
-*	but WITHOUT ANY WARRANTY; without even the implied warranty of
-*	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
-*	See the GNU Lesser General Public License for more details.
+*       This library is free software, you can redistribute it
+*       and/or modify
+*       it under  the terms of the GNU Lesser General Public License
+*       as published by the Free Software Foundation;
+*       either version 2 of the License, or any later version.
+*       The library is distributed in the hope that it will be useful,
+*       but WITHOUT ANY WARRANTY; without even the implied warranty of
+*       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*       See the GNU Lesser General Public License for more details.
 *
 *
 *********************************************************************/
@@ -21,80 +21,60 @@
 
 class NewBWRequestTester : public cSimpleModule
 {
-private:
+  private:
+    int timeOut;
 
-   int timeOut;
-
-public:
-	Module_Class_Members(NewBWRequestTester, cSimpleModule, 16384);
-	virtual void initialize();
-	virtual void activity();
-  	cObject* findObject(char* modName);
-   
-
+  public:
+    Module_Class_Members(NewBWRequestTester, cSimpleModule, 16384);
+    virtual void initialize();
+    virtual void activity();
+    cObject* findObject(char* modName);
 };
+
 Define_Module_Like(NewBWRequestTester, NetworkManager);
-
-
 
 
 void NewBWRequestTester::initialize()
 {
-
-timeOut = par("timeStart").longValue();
-  
-
+    timeOut = par("timeStart").longValue();
 }
-
-
 
 void NewBWRequestTester::activity()
 {
-cMessage *timeout_msg = new cMessage;
-scheduleAt( simTime()+timeOut, timeout_msg );
+    cMessage *timeout_msg = new cMessage;
+    scheduleAt( simTime()+timeOut, timeout_msg );
 
-cMessage* msg = receive();
+    cMessage* msg = receive();
 
-   //Send command to IR to signal the path recalculation
-cModule* irNode = (cModule*)findObject("LSR1");
-cModule* signalMod = (cModule*)(irNode->findObject("signal_module", false));
-if(signalMod ==NULL)
-{
-	ev << "Error ! cannot locate signal module in IR node\n";
-  return;
-}
-cMessage* cmdMsg = new cMessage();
-cmdMsg->addPar("test_command")=NEW_BW_REQUEST;
-cmdMsg->addPar("src") = "10.0.0.1";
-cmdMsg->addPar("dest") = "10.0.1.2";
-cmdMsg->addPar("bandwidth") = 130;
-sendDirect(cmdMsg, 0.0, signalMod, "from_tester");
+    //Send command to IR to signal the path recalculation
+    cModule* irNode = (cModule*)findObject("LSR1");
+    cModule* signalMod = (cModule*)(irNode->findObject("signal_module", false));
+    if (!signalMod)
+        error("Cannot locate signal module in IR node");
 
+    cMessage* cmdMsg = new cMessage();
+    cmdMsg->addPar("test_command")=NEW_BW_REQUEST;
+    cmdMsg->addPar("src") = "10.0.0.1";
+    cmdMsg->addPar("dest") = "10.0.1.2";
+    cmdMsg->addPar("bandwidth") = 130;
+    sendDirect(cmdMsg, 0.0, signalMod, "from_tester");
 
-delete msg;
-
-
+    delete msg;
 }
 
 cObject* NewBWRequestTester::findObject(char* modName)
 {
+    cObject *foundmod =NULL;
+    cModule *curmod = this;
 
-	cObject *foundmod =NULL;
-	cModule *curmod = this;
-
-	for (curmod = parentModule(); curmod != NULL;
-			curmod = curmod->parentModule())
-	{
-		if ((foundmod = curmod->findObject(modName, false)) != NULL)
-		{
-
-			ev << "TESTER: Module" << modName << " found\n";
-			return foundmod;
-		}
-	}
-
-	ev << "TESTER: Fail to find " << modName << "\n";
-
-
+    for (curmod = parentModule(); curmod; curmod = curmod->parentModule())
+    {
+        if ((foundmod = curmod->findObject(modName, false)) != NULL)
+        {
+            ev << "TESTER: Module" << modName << " found\n";
+            return foundmod;
+        }
+    }
+    ev << "TESTER: Failed to find " << modName << "\n";
 }
 
