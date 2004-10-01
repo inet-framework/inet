dce314e8a54aa32b60841683e53f1a40ca1f40d3
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2004-10-01 10:07:11 +0000

    *** empty log message ***
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@655 539eec35-cc16-0410-b457-356aab9780e1

---
+++ Applications/Generic/IPTrafGen.cc
@@ -0,0 +1,159 @@
+//
+// Copyright (C) 2000 Institut fuer Telematik, Universitaet Karlsruhe
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+
+#include <omnetpp.h>
+#include "IPTrafGen.h"
+#include "IPControlInfo_m.h"
+#include "StringTokenizer.h"
+#include "IPAddressResolver.h"
+
+
+Define_Module(IPTrafSink);
+
+
+void IPTrafSink::initialize()
+{
+    numReceived = 0;
+    WATCH(numReceived);
+}
+
+void IPTrafSink::handleMessage(cMessage *msg)
+{
+    processPacket(msg);
+
+    if (ev.isGUI())
+    {
+        char buf[32];
+        sprintf(buf, "rcvd: %d pks", numReceived);
+        displayString().setTagArg("t",0,buf);
+    }
+
+}
+
+void IPTrafSink::printPacket(cMessage *msg)
+{
+    IPControlInfo *controlInfo = check_and_cast<IPControlInfo *>(msg->controlInfo());
+
+    IPAddress src = controlInfo->srcAddr();
+    IPAddress dest = controlInfo->destAddr();
+    int protocol = controlInfo->protocol();
+
+    ev  << msg << endl;
+    ev  << "Payload length: " << (msg->length()/8) << " bytes" << endl;
+    ev  << "src: " << src << "  dest: " << dest << "  protocol=" << protocol << "\n";
+}
+
+void IPTrafSink::processPacket(cMessage *msg)
+{
+    ev << "Received packet: ";
+    printPacket(msg);
+    delete msg;
+
+    numReceived++;
+}
+
+
+
+//===============================================
+
+
+Define_Module(IPTrafGen);
+
+int IPTrafGen::counter;
+
+void IPTrafGen::initialize(int stage)
+{
+    // because of IPAddressResolver, we need to wait until interfaces are registered,
+    // address auto-assignment takes place etc.
+    if (stage!=3)
+        return;
+
+    IPTrafSink::initialize();
+
+    protocol = par("protocol");
+    msgLength = par("message_length");
+
+    const char *destAddrs = par("dest_addresses");
+    StringTokenizer tokenizer(destAddrs);
+    const char *token;
+    while ((token = tokenizer.nextToken())!=NULL)
+        destAddresses.push_back(IPAddressResolver().resolve(token));
+
+    counter = 0;
+
+    numSent = 0;
+    WATCH(numSent);
+
+    if (destAddresses.empty())
+        return;
+
+    cMessage *timer = new cMessage("sendTimer");
+    scheduleAt((double)par("message_freq"), timer);
+}
+
+IPAddress IPTrafGen::chooseDestAddr()
+{
+    int k = intrand(destAddresses.size());
+    return destAddresses[k];
+}
+
+void IPTrafGen::sendPacket()
+{
+    char msgName[32];
+    sprintf(msgName,"appData-%d", counter++);
+
+    cMessage *payload = new cMessage(msgName);
+    payload->setLength(msgLength);
+
+    IPControlInfo *controlInfo = new IPControlInfo();
+    IPAddress destAddr = chooseDestAddr();
+    controlInfo->setDestAddr(destAddr);
+    controlInfo->setProtocol(protocol);
+    payload->setControlInfo(controlInfo);
+
+    ev << "Sending packet: ";
+    printPacket(payload);
+
+    send(payload, "to_ip");
+    numSent++;
+}
+
+void IPTrafGen::handleMessage(cMessage *msg)
+{
+    if (msg->isSelfMessage())
+    {
+        // send, then reschedule next sending
+        sendPacket();
+        scheduleAt(simTime()+(double)par("message_freq"), msg);
+    }
+    else
+    {
+        // process incoming packet
+        processPacket(msg);
+    }
+
+    if (ev.isGUI())
+    {
+        char buf[40];
+        sprintf(buf, "rcvd: %d pks\nsent: %d pks", numReceived, numSent);
+        displayString().setTagArg("t",0,buf);
+    }
+}
+
+
---
+++ Applications/Generic/IPTrafGen.h
@@ -0,0 +1,77 @@
+//
+// Copyright (C) 2004 Andras Varga
+// Copyright (C) 2000 Institut fuer Telematik, Universitaet Karlsruhe
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+
+#ifndef __CBRAPP_H__
+#define __CBRAPP_H__
+
+#include <vector>
+#include <omnetpp.h>
+
+#include "basic_consts.h"
+#include "IPControlInfo_m.h"
+
+
+/**
+ * Consumes and prints packets received from the UDP module. See NED for more info.
+ */
+class IPTrafSink : public cSimpleModule
+{
+  protected:
+    int numReceived;
+
+    virtual void printPacket(cMessage *msg);
+    virtual void processPacket(cMessage *msg);
+
+  public:
+    Module_Class_Members(IPTrafSink, cSimpleModule, 0);
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+
+/**
+ * UDP application. See NED for more info.
+ */
+class IPTrafGen : public IPTrafSink
+{
+  protected:
+    std::string nodeName;
+    int protocol;
+    int msgLength;
+    std::vector<IPAddress> destAddresses;
+
+    static int counter; // counter for generating a global number for each packet
+
+    int numSent;
+
+    // chooses random destination address
+    virtual IPAddress chooseDestAddr();
+    virtual void sendPacket();
+
+  public:
+    Module_Class_Members(IPTrafGen, IPTrafSink, 0);
+    virtual int numInitStages() const {return 4;}
+    virtual void initialize(int stage);
+    virtual void handleMessage(cMessage *msg);
+};
+
+#endif
+
+
