4a63cb260bed236b0e2042b2f327feaed0f80093
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2005-10-03 13:14:59 +0000

    *** empty log message ***
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@845 539eec35-cc16-0410-b457-356aab9780e1

---
+++ World/NAMTrace.h
@@ -0,0 +1,39 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
+#ifndef NAMTRACEWRITER_H
+#define NAMTRACEWRITER_H
+
+#include <omnetpp.h>
+#include "INETDefs.h"
+
+/**
+ * Writes a nam trace file. See NED file for more info.
+ *
+ * @author Andras Varga
+ */
+class INET_API NAMTraceWriter : public cSimpleModule
+{
+  public:
+    Module_Class_Members(NAMTraceWriter, cSimpleModule, 0);
+    virtual ~NAMTraceWriter() {}
+    virtual void initialize();
+    virtual void handleMessage(cMessage *msg);
+};
+
+#endif
