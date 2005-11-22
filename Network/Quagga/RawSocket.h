181dddcf55a0893fa2b6ef273b5a3a05e7691a3d
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2005-11-22 21:48:18 +0000

    *** empty log message ***
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@926 539eec35-cc16-0410-b457-356aab9780e1

--- Network/Quagga/RawSocket.h
+++ Network/Quagga/RawSocket.h
@@ -8,41 +8,42 @@
 
 class RawSocket
 {
-	public:
-		RawSocket(int userId, int protocol);
-		
-		void setOutputGate(cGate *toIP);
-		
-		// pure unix interface
-		int send(const struct msghdr *message, int flags);
-		
-		int getProtocol();
-		
-		void setHdrincl(bool b);
-		bool getHdrincl();
-		
-		void setPktinfo(bool b);
-		void setMulticastLoop(bool b);
-		void setMulticastTtl(bool b);
-		
-		void setMulticastIf(IPAddress addr);
-		IPAddress getMulticastIf();
-		
-	private:
-	
-		void sendToIP(cMessage *msg);
-	
-		int userId;
-		int protocol;
-			
-		bool hdrincl;
-		bool pktinfo;
-		bool multicastLoop;
-		bool multicastTtl;
-		IPAddress multicastIf;
-		
-		cGate *gateToIP;
-		
+    public:
+        RawSocket(int userId, int protocol);
+
+        void setOutputGate(cGate *toIP);
+
+        // pure unix interface
+        int send(const struct msghdr *message, int flags);
+
+        int getProtocol();
+
+        void setHdrincl(bool b);
+        bool getHdrincl();
+
+        void setPktinfo(bool b);
+        void setMulticastLoop(bool b);
+        void setMulticastTtl(bool b);
+
+        void setMulticastIf(IPAddress addr);
+        IPAddress getMulticastIf();
+
+    private:
+
+        void sendToIP(cMessage *msg);
+
+        int userId;
+        int protocol;
+
+        bool hdrincl;
+        bool pktinfo;
+        bool multicastLoop;
+        bool multicastTtl;
+        IPAddress multicastIf;
+        int multicastOutputPort; // determined from multicastIf address
+
+        cGate *gateToIP;
+
 };
 
 #endif
