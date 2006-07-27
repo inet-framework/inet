506d2f0b7b37383968a5223f2b960ecbb1d72373
tuexen <tuexen@539eec35-cc16-0410-b457-356aab9780e1>
2006-07-27 09:31:14 +0000

    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@1287 539eec35-cc16-0410-b457-356aab9780e1

---
+++ svntag.bat
@@ -0,0 +1,8 @@
+@echo off
+if "%1"=="" echo usage: %~n0 tagname && exit
+echo.
+echo Tagging current directory with "%1":
+echo ---^> svn copy . https://dev.omnetpp.org/svn/inet-framework/tags/%1
+echo.
+pause
+svn copy . https://dev.omnetpp.org/svn/inet-framework/tags/%1
