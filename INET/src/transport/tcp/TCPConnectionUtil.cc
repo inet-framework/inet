1f78f839a927b3d2c9bfa44f1db2279ac2aeae67
rhornig <rhornig@539eec35-cc16-0410-b457-356aab9780e1>
2008-02-04 18:05:50 +0000

    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@1750 539eec35-cc16-0410-b457-356aab9780e1

--- INET/src/transport/tcp/TCPConnectionUtil.cc
+++ INET/src/transport/tcp/TCPConnectionUtil.cc
@@ -307,7 +307,7 @@ void TCPConnection::configureStateVariables()
 void TCPConnection::selectInitialSeqNum()
 {
     // set the initial send sequence number
-    state->iss = (unsigned long)SIMTIME_DBL(fmod(tcpMain->simTime()*250000.0, 1.0+(double)(unsigned)0xffffffffUL)) & 0xffffffffUL;
+    state->iss = (unsigned long)fmod(SIMTIME_DBL(tcpMain->simTime())*250000.0, 1.0+(double)(unsigned)0xffffffffUL) & 0xffffffffUL;
 
     state->snd_una = state->snd_nxt = state->snd_max = state->iss;
 
