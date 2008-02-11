1a0da5e77c73af1aa8ea50e0fb5b56043f1714aa
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2008-02-11 10:43:32 +0000

    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@1758 539eec35-cc16-0410-b457-356aab9780e1

--- INET/makemake.cmd
+++ INET/makemake.cmd
@@ -4,17 +4,8 @@ if exist C:\home\omnetpp40\omnetpp\setenv-vc71.bat call C:\home\omnetpp40\omnetp
 nmake /? >nul 2>nul || echo *** ERROR: nmake.exe is not in the PATH *** && goto end
 call opp_test >nul 2>nul || echo *** ERROR: OMNeT++/bin is not in the PATH *** && goto end
 
-if not exist inetconfig.vc perl Etc\create-inetconfigvc.pl || echo *** ERROR CREATING DEFAULT inetconfig.vc FROM inetconfig.vc-SAMPLE *** && goto end
-
-echo Generating makefiles...
-nmake /nologo ROOT=%~dp0 MAKEMAKE=opp_nmakemake EXT=.vc -f makemakefiles || echo *** ERROR GENERATING MAKEFILES *** && goto end
-
-echo Adding dependencies to makefiles...
-nmake /nologo -f Makefile.vc depend || echo *** ERROR ADDING DEPENDENCIES TO MAKEFILES *** && goto end
-
-echo Generating list of framework NED files (nedfiles.lst)...
-dir /s/b *.ned > nedfiles.lst
-perl Etc\processNEDFileList.pl %~dp0\nedfiles.lst %~dp0 || echo *** ERROR CREATING NEDFILES.LST *** && goto end
+echo Generating makefile...
+cd src && opp_nmakemake -f --deep || echo *** ERROR GENERATING MAKEFILES *** && goto end
 
 echo.
 echo ================================================================
