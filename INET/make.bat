8183f95773a4d70b14cee918d48dfb1ecc613de7
rhornig <rhornig@539eec35-cc16-0410-b457-356aab9780e1>
2008-02-20 14:16:44 +0000

    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@1768 539eec35-cc16-0410-b457-356aab9780e1

--- INET/make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem ***** ADJUST THE DIRECTORIES BELOW ACCORDING TO YOUR INSTALLATION ******
-call "C:\Program Files\Microsoft Visual Studio 8\VC\vcvarsall.bat"
-call "E:\Microsoft Platform SDK\SetEnv.Cmd"
-
-cd %~dp0
-if not exist Makefile.vc call makemake.cmd
-pause
-nmake -f Makefile.vc %*
