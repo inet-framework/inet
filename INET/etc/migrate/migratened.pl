888da97a1dd67c7a920d5abb7a99d97208aa8468
andras <andras@539eec35-cc16-0410-b457-356aab9780e1>
2008-05-07 10:05:45 +0000

    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@1807 539eec35-cc16-0410-b457-356aab9780e1

--- INET/etc/migrate/migratened.pl
+++ INET/etc/migrate/migratened.pl
@@ -18,15 +18,55 @@ while (<INFILE>) {
 }
 #print join(' ', @fnames);
 
+%replacements = (
+    # gates
+    "from_ip"   => "ipIn",
+    "from_ipv6" => "ipv6In",
+    "from_udp"  => "udpIn",
+    "from_app"  => "appIn",
+    "from_mpls_switch" => "mplsSwitchIn",
+    "to_ip"     => "ipOut",
+    "to_ipv6"   => "ipv6Out",
+    "to_udp"    => "udpOut",
+    "to_app"    => "appOut",
+    "to_appl"   => "appOut",
+
+    # from RTP -- TBD only when RTP code has been patched!
+#    "fromApp" =>            "appIn",
+#    "fromProfile" =>        "profileIn",
+#    "fromRTP" =>            "rtpIn",
+#    "fromRTCP" =>           "rtcpIn",
+#    "fromSocketLayer" =>    "socketLayerIn",
+#    "fromSocketLayerRTP" => "socketLayerRTPIn",
+#    "fromSocketLayerRTCP" =>"socketLayerRTCPIn",
+#    "toApp" =>              "appOut",
+#    "toProfile" =>          "profileOut",
+#    "toRTCP" =>             "rtcpOut",
+#    "toRTP" =>              "rtpOut",
+#    "toSocketLayer" =>      "socketLayerOut",
+#    "toSocketLayerRTP" =>   "socketLayerRTPOut",
+#    "toSocketLayerRTCP" =>  "socketLayerRTCPOut",
+
+
+    # parameters
+    "local_port" => "localPort",
+    "dest_port" => "destPort",
+    "message_length" => "messageLength",
+    "message_freq" => "messageFreq",
+    "dest_addresses" => "destAddresses",
+
+);
+
 foreach $fname (@fnames)
 {
     print "reading $fname...\n" if ($verbose);
     $txt = readfile($fname);
 
     # process $txt:
-    .......
-    .......
-    .......
+    foreach my $from (keys(%replacements)) {
+        my $to = $replacements{$from};
+        $txt =~ s/\b$from\b/$to/sg;
+    }
 
     writefile($fname, $txt);
     #writefile("$fname.new", $txt);
