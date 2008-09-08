1cb2e84ae45efccea6619176b2c701ebca16dcfd
tomi <tomi@539eec35-cc16-0410-b457-356aab9780e1>
2008-09-08 15:55:14 +0000

    fix simtime overflow in EtherMAC::finish()
    git-svn-id: file:///home/rhornig/tmp/svn/inet-framework/trunk@2058 539eec35-cc16-0410-b457-356aab9780e1

--- INET/src/linklayer/ethernet/EtherMAC.cc
+++ INET/src/linklayer/ethernet/EtherMAC.cc
@@ -628,9 +628,9 @@ void EtherMAC::finish()
 
     simtime_t t = simTime();
     simtime_t totalChannelIdleTime = t - totalSuccessfulRxTxTime - totalCollisionTime;
-    recordScalar("rx channel idle (%)", 100*totalChannelIdleTime/t);
-    recordScalar("rx channel utilization (%)", 100*totalSuccessfulRxTxTime/t);
-    recordScalar("rx channel collision (%)", 100*totalCollisionTime);
+    recordScalar("rx channel idle (%)", 100*(totalChannelIdleTime/t));
+    recordScalar("rx channel utilization (%)", 100*(totalSuccessfulRxTxTime/t));
+    recordScalar("rx channel collision (%)", 100*(totalCollisionTime/t));
     recordScalar("collisions",     numCollisions);
     recordScalar("backoffs",       numBackoffs);
 }
