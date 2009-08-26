a9f770f9f8f27ac34a535a044be33fcf6745cf04
Andras Varga <andras@omnetpp.org>
2009-08-26 23:08:43 +0200

    mingw compile fix: redeclaration of ssize_t in cSocketRTScheduler

--- src/linklayer/ext/cSocketRTScheduler.cc
+++ src/linklayer/ext/cSocketRTScheduler.cc
@@ -31,7 +31,6 @@
 
 #if defined(_WIN32) || defined(__WIN32__) || defined(WIN32) || defined(__CYGWIN__) || defined(_WIN64)
 #include <ws2tcpip.h>
-typedef int ssize_t;
 #endif
 
 #define PCAP_SNAPLEN 65536 /* capture all data packets with up to pcap_snaplen bytes */
@@ -326,9 +325,9 @@ void cSocketRTScheduler::sendBytes(uint8 *buf, size_t numBytes, struct sockaddr 
 	if (fd == INVALID_SOCKET)
 		throw cRuntimeError("cSocketRTScheduler::sendBytes(): no raw socket.");
 
-	ssize_t sent = sendto(fd, (char *)buf, numBytes, 0, to, addrlen);
+    int sent = sendto(fd, (char *)buf, numBytes, 0, to, addrlen);  // note: no ssize_t on Windows
 
-	if (sent == (ssize_t)numBytes)
+	if (sent == (int)numBytes)
 		EV << "Sent an IP packet with length of " << sent << " bytes.\n";
 	else
 		EV << "Sending of an IP packet FAILED! (sendto returned " << sent << " (" << strerror(errno) << ") instead of " << numBytes << ").\n";
