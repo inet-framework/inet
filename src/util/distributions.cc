2ba51d4558cbd3bde5d6be27bb93b9927f4e6be0
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2009-10-20 17:48:05 +0100

    Added to implement additional probability distributions.

---
+++ src/util/distributions.cc
@@ -0,0 +1,27 @@
+//-------------------------------------------------------------------------------
+//	distributions.cc --
+//
+//	This file implements classes for additional probability distributions
+//	which are not implemented in OMNeT++.
+//
+//	Copyright (C) 2009 Kyeong Soo (Joseph) Kim
+//-------------------------------------------------------------------------------
+
+
+#include "distributions.h"
+
+
+//------------------------------------------------------------------------------
+//	Distribution functions
+//------------------------------------------------------------------------------
+
+double trunc_lognormal(double m, double w, char t, double min, double max, int rng=0)
+{
+
+}
+
+double trunc_pareto_shifted(double a, double b, double c, char t, double min, double max, int rng=0)
+{
+
+}
+
