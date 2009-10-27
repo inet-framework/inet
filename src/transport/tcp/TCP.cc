6f648c871eb4b5f7e91555499f5127c4553e36c1
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2009-10-27 16:52:21 +0000

    Made changes to solve the issues with port number assignment in TCP clients. -	modified:   examples/ngoa/ecr/ECRTest.ned -	modified:   examples/ngoa/ecr/http.ini -	modified:   src/applications/tcpapp/HttpClientApp.ned -	new file:   src/applications/tcpapp/TCPApp2.ned -	new file:   src/nodes/inet/StandardHost2.ned -	modified:   src/transport/tcp/TCP.cc

--- src/transport/tcp/TCP.cc
+++ src/transport/tcp/TCP.cc
@@ -34,10 +34,12 @@ bool TCP::logverbose;
 // Ephemeral port range settings
 // - BSD: 1024...4999
 // - IANA: 49152...65535
-//#define EPHEMERAL_PORTRANGE_START 1024
+//		--> may cause issues with the current INET implementation
+//		    due to its use of *** signed short *** for port numbers
+#define EPHEMERAL_PORTRANGE_START 1024
+//#define EPHEMERAL_PORTRANGE_START 49152
 //#define EPHEMERAL_PORTRANGE_END   5000
-#define EPHEMERAL_PORTRANGE_START 49152
-#define EPHEMERAL_PORTRANGE_END   65535
+#define EPHEMERAL_PORTRANGE_END   32767
 
 static std::ostream& operator<<(std::ostream& os, const TCP::SockPair& sp)
 {
