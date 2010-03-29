eb5c560d5568cc5caafeb9a224af9663ff25dd15
Kyeong Soo (Joseph) Kim <kyeongsoo.kim@gmail.com>
2010-03-29 11:46:26 +0100

    Changed back to intuniform() for random selection of start frame assuming sizeof(int) = sizeof(long) in both 32bit and 64bit platforms.

--- src/applications/udpapp/UDPVideoStreamSvrWithTrace.cc
+++ src/applications/udpapp/UDPVideoStreamSvrWithTrace.cc
@@ -151,7 +151,8 @@ void UDPVideoStreamSvrWithTrace::processStreamRequest(cMessage *msg)
 	d->numPktSent = 0;
 	d->numFrames = numFrames;
 	d->framePeriod = framePeriod;
-	d->currentFrame = long(uniform(1.0, numFrames+1.0)); ///< start frame is randomly selected
+//	d->currentFrame = long(uniform(1.0, numFrames+1.0)); ///< start frame is randomly selected
+	d->currentFrame = intuniform(1, numFrames); ///< start frame is randomly selected; here we assume sizeof(int) = sizeof(long)
 	streamVector.push_back(d);
 
 	// initialize self messages
