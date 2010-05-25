747271111909831cb1639c12ec34aaff65603fc4
Zoltan Bojthe <zbojthe@urdeso.hu>
2010-05-25 19:02:08 +0200

    Correcting interpretation of ThruputMeter parameters

--- src/linklayer/ppp/ThruputMeter.h
+++ src/linklayer/ppp/ThruputMeter.h
@@ -33,8 +33,8 @@ class INET_API ThruputMeter : public cSimpleModule
   protected:
     // config
     simtime_t startTime; // start time
-    int batchSize;   // number of packets in a batch
-    int maxInterval; // max length of measurement interval (measurement ends
+    unsigned int batchSize;   // number of packets in a batch
+    simtime_t maxInterval; // max length of measurement interval (measurement ends
                      // if either batchSize or maxInterval is reached, whichever
                      // is reached first)
 
