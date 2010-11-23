cc5aa48d3eecc4cd7896c87f9bb6cc7ad3b15570
Rudolf Hornig <rudi@omnetpp.org>
2010-11-23 16:07:40 +0100

    Bugfix for change channel(state must be IDLE if channel change occurred).

--- src/base/Sink.cc
+++ src/base/Sink.cc
@@ -2,7 +2,7 @@
 // Copyright (C) 1992-2004 Andras Varga
 //
 // This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License
+// modify it under the terms of the GNU Lesser General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
 //
