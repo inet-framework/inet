d4dc769557697f61d6b1921d85b1bd745d4e07ad
Rudolf Hornig <rudi@omnetpp.org>
2010-12-06 15:56:48 +0100

    Reverting previous commit.

--- src/transport/sctp/SCTPAssociation.h
+++ src/transport/sctp/SCTPAssociation.h
@@ -263,18 +263,10 @@ class INET_API SCTPPathVariables : public cPolymorphic
         unsigned int        numberOfDuplicates;
         unsigned int        numberOfFastRetransmissions;
         unsigned int        numberOfTimerBasedRetransmissions;
-        unsigned int        numberOfHeartbeatsSent;
-        unsigned int        numberOfHeartbeatAcksSent;
-        unsigned int        numberOfHeartbeatsRcvd;
-        unsigned int        numberOfHeartbeatAcksRcvd;
 
         // ====== Output Vectors ==============================================
         cOutVector*         pathTSN;
         cOutVector*         pathRcvdTSN;
-        cOutVector*         pathHb;
-        cOutVector*         pathRcvdHb;
-        cOutVector*         pathHbAck;
-        cOutVector*         pathRcvdHbAck;
         cOutVector*         statisticsPathRTO;
         cOutVector*         statisticsPathRTT;
         cOutVector*         statisticsPathSSthresh;
@@ -726,7 +718,7 @@ class INET_API SCTPAssociation : public cObject
         inline void sendToIP(SCTPMessage* sctpmsg, const bool qs = false) {
             sendToIP(sctpmsg, remoteAddr, qs);
         }
-        void recordInPathVectors(SCTPMessage* pMsg, const IPvXAddress& rDest);
+        void recordInPathTsnVector(SCTPMessage* p_msg, const IPvXAddress& dest);
         void scheduleSack();
         /** Utility: signal to user that connection timed out */
         void signalConnectionTimeout();
