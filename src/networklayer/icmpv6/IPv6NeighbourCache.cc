da72f5a3fa266fe940f005c10e22d18fcd9d34c7
Zoltan Bojthe <zbojthe@urdeso.hu>
2011-01-19 16:02:12 +0100

    IPv6NeighbourCache: add isHomeAgent flag (merged from xMIPv6)

--- src/networklayer/icmpv6/IPv6NeighbourCache.cc
+++ src/networklayer/icmpv6/IPv6NeighbourCache.cc
@@ -29,7 +29,8 @@ std::ostream& operator<<(std::ostream& os, const IPv6NeighbourCache::Neighbour& 
 {
     os << e.macAddress;
     if (e.isRouter) os << " ROUTER";
-    if (e.isDefaultRouter) os << " defaultRtr";
+    if (e.isDefaultRouter) os << "DefaultRtr";
+    if(e.isHomeAgent) os <<" Home Agent";
     os << " " << IPv6NeighbourCache::stateName(e.reachabilityState);
     os << " reachabilityExp:"  << e.reachabilityExpires;
     if (e.numProbesSent) os << " probesSent:" << e.numProbesSent;
@@ -65,6 +66,7 @@ IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addNeighbour(const IPv6Addres
     nbor.nceKey = lookupKeyAddr(key);//a ptr that links to the key.-WEI for convenience.
     nbor.isRouter = false;
     nbor.isDefaultRouter = false;
+    nbor.isHomeAgent = false;         //Zarrar 09.03.07
     nbor.reachabilityState = INCOMPLETE;
     nbor.reachabilityExpires = 0;
     nbor.numProbesSent = 0;
@@ -84,6 +86,7 @@ IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addNeighbour(const IPv6Addres
     nbor.macAddress = macAddress;
     nbor.isRouter = false;
     nbor.isDefaultRouter = false;
+    nbor.isHomeAgent = false;         //Zarrar 09.03.07
     nbor.reachabilityState = STALE;
     nbor.reachabilityExpires = 0;
     nbor.numProbesSent = 0;
@@ -92,8 +95,12 @@ IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addNeighbour(const IPv6Addres
     return &nbor;
 }
 
-/** Creates and initializes a router entry (isRouter=isDefaultRouter=true), state=INCOMPLETE. */
-IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& addr, int interfaceID, simtime_t expiryTime)
+/**
+ * Creates and initializes a router entry (isRouter=isDefaultRouter=true), state=INCOMPLETE.
+ *
+ * Update by CB: Added an optional parameter which is false by default. Specifies whether a router is also a home agent.
+ */
+IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& addr, int interfaceID, simtime_t expiryTime, bool isHomeAgent)
 {
     Key key(addr, interfaceID);
     ASSERT(neighbourMap.find(key)==neighbourMap.end()); // entry must not exist yet
@@ -102,6 +109,7 @@ IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& 
     nbor.nceKey = lookupKeyAddr(key);//a ptr that links to the key.-WEI for convenience.
     nbor.isRouter = true;
     nbor.isDefaultRouter = true;//FIXME: a router may advertise itself it self as a router but not as a default one.-WEI
+    nbor.isHomeAgent = isHomeAgent; //Zarrar 09.03.07 --- FIXME: NOT EVERY ROUTER IS A HOME AGENT // update 3.9.07 - CB
     nbor.reachabilityState = INCOMPLETE;
     nbor.reachabilityExpires = 0;
     nbor.numProbesSent = 0;
@@ -110,8 +118,12 @@ IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& 
     return &nbor;
 }
 
-/** Creates and initializes a router entry (isRouter=isDefaultRouter=true), MAC address and state=STALE. */
-IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& addr, int interfaceID, MACAddress macAddress, simtime_t expiryTime)
+/**
+ * Creates and initializes a router entry (isRouter=isDefaultRouter=true), MAC address and state=STALE.
+ *
+ * Update by CB: Added an optional parameter which is false by default. Specifies whether a router is also a home agent.
+ */
+IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& addr, int interfaceID, MACAddress macAddress, simtime_t expiryTime, bool isHomeAgent)
 {
     Key key(addr, interfaceID);
     ASSERT(neighbourMap.find(key)==neighbourMap.end()); // entry must not exist yet
@@ -121,6 +133,7 @@ IPv6NeighbourCache::Neighbour *IPv6NeighbourCache::addRouter(const IPv6Address& 
     nbor.macAddress = macAddress;
     nbor.isRouter = true;
     nbor.isDefaultRouter = true;
+    nbor.isHomeAgent = isHomeAgent; //Zarrar 09.03.07 --- FIXME: NOT EVERY ROUTER IS A HOME AGENT // update 3.9.07 - CB
     nbor.reachabilityState = STALE;
     nbor.reachabilityExpires = 0;
     nbor.numProbesSent = 0;
@@ -146,6 +159,38 @@ void IPv6NeighbourCache::remove(NeighbourMap::iterator it)
     neighbourDiscovery.cancelAndDelete(it->second.nudTimeoutEvent); // 20.9.07 - CB
     it->second.nudTimeoutEvent = NULL;
     neighbourMap.erase(it);
+}
+
+// Added by CB
+void IPv6NeighbourCache::invalidateEntriesForInterfaceID(int interfaceID)
+{
+    for (NeighbourMap::iterator it=neighbourMap.begin(); it!=neighbourMap.end(); it++)
+    {
+        if ( it->first.interfaceID == interfaceID )
+        {
+            it->second.reachabilityState = PROBE; // we make sure this neighbour is not used anymore in the future, unless reachability can be confirmed
+            neighbourDiscovery.cancelAndDelete(it->second.nudTimeoutEvent); // 20.9.07 - CB
+            it->second.nudTimeoutEvent = NULL;
+        }
+    }
+}
+
+// Added by CB
+void IPv6NeighbourCache::invalidateAllEntries()
+{
+    while (! neighbourMap.empty() )
+    {
+        NeighbourMap::iterator it = neighbourMap.begin();
+        remove(it);
+    }
+    /*
+    int size = neighbourMap.size();
+    EV << "size: " << size << endl;
+    for (NeighbourMap::iterator it=neighbourMap.begin(); it!=neighbourMap.end(); it++)
+    {
+        it->second.reachabilityState = PROBE; // we make sure this neighbour is not used anymore in the future, unless reachability can be confirmed
+    }
+    */
 }
 
 const char *IPv6NeighbourCache::stateName(ReachabilityState state)
