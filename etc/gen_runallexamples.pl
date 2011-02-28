b1d13cd451d4f789bb25058ead017cad05d676c4
Zoltan Bojthe <zbojthe@urdeso.hu>
2011-02-28 18:01:01 +0100

    gen_runallexamples: checking '__interactive__' text in comments in 'Config' lines in ini files

--- etc/gen_runallexamples.pl
+++ etc/gen_runallexamples.pl
@@ -2,26 +2,21 @@
 
 
 @the_skiplist = <<END_OF_SKIPLIST =~ m/(\S.*\S)/g;
-examples/adhoc/ieee80211/omnetpp.ini  Ping2                             # <!> Error in module (cCompoundModule) Net80211 (id=1) during network setup: The simulation wanted to ask a question, set cmdenv-interactive=true to allow it: "Enter parameter `Net80211.numHosts' (unassigned):".
-examples/adhoc/mf80211/omnetpp.ini  Ping2                               # <!> Error in module (cCompoundModule) Net80211 (id=1) during network setup: The simulation wanted to ask a question, set cmdenv-interactive=true to allow it: "Enter parameter `Net80211.numHosts' (unassigned):".
 examples/emulation/extclient/omnetpp.ini  General                       # <!> Error in module (ExtInterface) ExtClient.peer.ext[0] (id=14) during network initialization: cSocketRTScheduler::setInterfaceModule(): pcap devices not supported.
 examples/emulation/extserver/omnetpp.ini  Uplink_Traffic                # <!> Error in module (ExtInterface) extserver.router.ext[0] (id=12) during network initialization: cSocketRTScheduler::setInterfaceModule(): pcap devices not supported.
 examples/emulation/extserver/omnetpp.ini  Downlink_Traffic              # <!> Error in module (ExtInterface) extserver.router.ext[0] (id=12) during network initialization: cSocketRTScheduler::setInterfaceModule(): pcap devices not supported.
 examples/emulation/extserver/omnetpp.ini  Uplink_and_Downlink_Traffic   # <!> Error in module (ExtInterface) extserver.router.ext[0] (id=12) during network initialization: cSocketRTScheduler::setInterfaceModule(): pcap devices not supported.
 examples/emulation/traceroute/omnetpp.ini  General                      # <!> Error in module (ExtInterface) Traceroute.extRouter.ext[0] (id=310) during network initialization: cSocketRTScheduler::setInterfaceModule(): pcap devices not supported.
-examples/ethernet/lans/defaults.ini  General                            # <!> Error: Network `' or `inet.examples.ethernet.lans.' not found, check .ini and .ned files.
-examples/inet/tcpclientserver/omnetpp.ini  NSCfreebsd__NSCfreebsd       # return value: CRASH (139)
-examples/inet/tcpclientserver/omnetpp.ini  NSClwip__inet                # return value: CRASH (134)
-examples/mpls/ldp/omnetpp.ini  General :                                # <!> Error in module (LDP) LDPTEST.LSR5.ldp (id=133) at event #893, t=0.023541800236: Model error: ASSERT: condition uit == fecUp.end() false in function processLABEL_REQUEST, networklayer/ldp/LDP.cc line 952.
-examples/mpls/testte_failure2/omnetpp.ini  General                      # <!> Error in module (RSVP) RSVPTE4.LSR1.rsvp (id=22) at event #2, t=0: Model error: not a local peer: 10.1.1.1.
-examples/wireless/lan80211/omnetpp-ftp.ini  NHosts                      # <!> Error in module (cCompoundModule) Lan80211 (id=1) during network setup: The simulation wanted to ask a question, set cmdenv-interactive=true to allow it: "Enter parameter `Lan80211.numHosts' (unassigned):".
-examples/wireless/lan80211/omnetpp-streaming.ini  Streaming2            # <!> Error in module (cCompoundModule) Lan80211 (id=1) during network setup: The simulation wanted to ask a question, set cmdenv-interactive=true to allow it: "Enter parameter `Lan80211.numHosts' (unassigned):".
-examples/wireless/lan80211/omnetpp.ini  Ping2                           # <!> Error in module (cCompoundModule) Lan80211 (id=1) during network setup: The simulation wanted to ask a question, set cmdenv-interactive=true to allow it: "Enter parameter `Lan80211.numHosts' (unassigned):".
+examples/ethernet/lans/defaults.ini  General                            # <!> Error: Network `' or `inet.examples.ethernet.lans.' not found, check .ini and .ned files. # The defaults.ini file included from other ini files
+#examples/inet/tcpclientserver/omnetpp.ini  NSCfreebsd__NSCfreebsd       # return value: CRASH (139)
+#examples/inet/tcpclientserver/omnetpp.ini  NSClwip__inet                # return value: CRASH (134)
+#examples/mpls/ldp/omnetpp.ini  General :                                # <!> Error in module (LDP) LDPTEST.LSR5.ldp (id=133) at event #893, t=0.023541800236: Model error: ASSERT: condition uit == fecUp.end() false in function processLABEL_REQUEST, networklayer/ldp/LDP.cc line 952.
+#examples/mpls/testte_failure2/omnetpp.ini  General                      # <!> Error in module (RSVP) RSVPTE4.LSR1.rsvp (id=22) at event #2, t=0: Model error: not a local peer: 10.1.1.1.
 END_OF_SKIPLIST
 
 
 # next line DISABLE previous skiplist:
-@the_skiplist = ();
+#@the_skiplist = ();
 
 
 foreach $i ( @the_skiplist )
@@ -99,22 +94,30 @@ foreach $fname (@inifiles)
 
     next if ($txt =~ /\bfingerprint\s*=/); #skip fingerprint tests
 
-    @configs = ($txt =~ /\[Config (\w+)\]/g);
+    @configs = ($txt =~ /^\s*(\[Config \w+\].*)$/mg);
 
 #    print "-1- ",$#configs," ---------------->file=$fname, configs={",join(',',@configs),"}\n";
 
-    @configs = ( "General" ) if ($#configs lt 0);
+    @configs = ( "[Config General]" ) if ($#configs lt 0);
 
 #    print "-2- ",$#configs," --------------->file=$fname, configs={",join(',',@configs),"}\n";
 
     foreach $conf (@configs)
     {
-        $run = "$fname  $conf";
+        ($cfg,$comm) = ($conf =~ /^\[Config (\w+)\]\s*(\#.*)?$/g);
+        $run = "$fname  $cfg";
         $x = "run $run";
+
+        if ($comm =~ /\b__interactive__\b/i)
+        {
+            $skiplist{$run} = '# '.$conf;
+        }
+
         if (length($skiplist{$run}))
         {
             $x = "# run $run   ".$skiplist{$run};
         }
+
         print "$x\n";
     }
 }
