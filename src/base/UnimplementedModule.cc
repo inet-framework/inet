00b422dfce5dda6eb5f972603703f12af6a05da0
Andras Varga <andras@omnetpp.org>
2011-05-10 14:33:31 +0200

    UnimplementedModule: comment, copyright header

--- src/base/UnimplementedModule.cc
+++ src/base/UnimplementedModule.cc
@@ -1,15 +1,24 @@
 //
-// This program is property of its copyright holder. All rights reserved.
-// 
+// Copyright (C) 2011 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
 
 #include "UnimplementedModule.h"
 
 Define_Module(UnimplementedModule);
 
-void UnimplementedModule::initialize()
-{
-    // TODO - Generated method body
-}
 
 void UnimplementedModule::handleMessage(cMessage *msg)
 {
--- src/base/UnimplementedModule.h
+++ src/base/UnimplementedModule.h
@@ -1,6 +1,19 @@
 //
-// This program is property of its copyright holder. All rights reserved.
-// 
+// Copyright (C) 2011 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
 
 #ifndef __INET_UNIMPLEMENTEDMODULE_H_
 #define __INET_UNIMPLEMENTEDMODULE_H_
@@ -8,12 +21,11 @@
 #include <omnetpp.h>
 
 /**
- * TODO - Generated class
+ * A module class whose handleMessage() throws a "not implmemented" exception.
  */
 class UnimplementedModule : public cSimpleModule
 {
   protected:
-    virtual void initialize();
     virtual void handleMessage(cMessage *msg);
 };
 
