81bc757aaea343a639f94b0da484a9a65164e73c
Rudolf Hornig <rudi@omnetpp.org>
2012-02-23 13:58:24 +0100

    ieee80211: default AC was not set correctly for the 802.11e classifier.
    (was always 0, independent of the value set in the NED folder).

--- src/linklayer/ieee80211/mgmt/Ieee80211eClassifier.h
+++ src/linklayer/ieee80211/mgmt/Ieee80211eClassifier.h
@@ -36,8 +36,8 @@ class INET_API Ieee80211eClassifier : public IQoSClassifier
      */
     Ieee80211eClassifier();
     virtual int getNumQueues();
-    virtual void setDefaultClass(int i = 0){defaultAC = 0;}
-    virtual int getDefaultClass(){return defaultAC;}
+    virtual void setDefaultClass(int i) { defaultAC = i; }
+    virtual int getDefaultClass() { return defaultAC; }
 
     /**
      * The method should return the priority (the index of subqueue)
