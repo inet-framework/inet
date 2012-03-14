3acca26f98ccbc66d8547350883a0f71239dc17b
Tamas Borbely <tomi.borbely@gmail.com>
2012-03-14 16:36:10 +0100

    IGMP: add copyright comments + use #ifndef sentinels in headers

--- src/networklayer/ipv4/IGMP.h
+++ src/networklayer/ipv4/IGMP.h
@@ -1,4 +1,23 @@
-#pragma once
+//
+// Copyright (C) 2011 CoCo Communications
+// Copyright (C) 2012 Opensim Ltd
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IGMP_H
+#define __INET_IGMP_H
 
 #include "INETDefs.h"
 #include "INotifiable.h"
@@ -172,3 +191,5 @@ private:
 	void processV2Report(InterfaceEntry *ie, IGMPMessage *msg);
 	void processLeave(InterfaceEntry *ie, IGMPMessage *msg);
 };
+
+#endif
--- src/networklayer/ipv4/IPv4MulticastData.h
+++ src/networklayer/ipv4/IPv4MulticastData.h
@@ -1,4 +1,22 @@
-#pragma once
+//
+// Copyright (C) 2012 Opensim Ltd
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IPv4MULTICASTDATA_H
+#define __INET_IPv4MULTICASTDATA_H
 
 #include "INETDefs.h"
 
@@ -54,3 +72,5 @@ public:
 	void setIGMPQueryTimer(IPv4InterfaceTimer* t) {igmpQueryTimer = t; changed1();}
 	//@}
 };
+
+#endif
