745aefd218db4a1f8f0c8361b6291b94cc6c78f8
Rudolf Hornig <rudi@omnetpp.org>
2012-06-08 14:25:33 +0200

    mobility: added new model that arranges the modules in a row but the direction can be specified

---
+++ src/mobility/models/LinealNodeDistributionMobility.h
@@ -0,0 +1,58 @@
+/*
+ *  Copyright (C) 2012 Alfonso Ariza, Universidad de Malaga
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ */
+
+
+#ifndef STATIC_GRID_MOBILITY_H
+#define STATIC_GRID_MOBILITY_H
+
+#include "INETDefs.h"
+
+#include "StationaryMobility.h"
+
+
+/**
+ * @brief Mobility model which places all hosts at constant distances
+ * in a line with an orientation
+ *
+ * @ingroup mobility
+ * @author Alfonso Ariza
+ */
+class INET_API LinealNodeDistributionMobility : public StationaryMobility
+{
+  protected:
+
+    double separation;
+    double initialX;
+    double initialY;
+    double orientation;
+
+  protected:
+    /** @brief Initializes mobility model parameters.*/
+    virtual void initialize(int stage);
+
+    /** @brief Initializes the position according to the mobility model. */
+    virtual void initializePosition();
+
+    /** @brief Save the host position. */
+    virtual void finish();
+
+  public:
+    LinealNodeDistributionMobility();
+};
+
+#endif
