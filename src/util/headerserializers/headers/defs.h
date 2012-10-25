efaab016a741260fec1b917c0a05887a1974b7b0
Tamas Borbely <tamas.borbely@omnest.com>
2012-10-25 16:34:30 +0200

    headerserializers: compile fix for Visual C++ 10.0 and later
    they finally have <stdint.h>

--- src/util/headerserializers/headers/defs.h
+++ src/util/headerserializers/headers/defs.h
@@ -21,7 +21,7 @@
 //
 // Get integer types defined
 //
-#if defined(_MSC_VER)
+#if defined(_MSC_VER) && (_MSC_VER < 1600)
 typedef __int8            int8_t;
 typedef __int16           int16_t;
 typedef __int32           int32_t;
@@ -31,7 +31,7 @@ typedef unsigned __int16  uint16_t;
 typedef unsigned __int32  uint32_t;
 typedef unsigned __int64  uint64_t;
 #else
-#include <stdint.h>  /* ISO C compliant platforms: Linux, Mac OS/X */
+#include <stdint.h>  /* ISO C compliant platforms: Linux, Mac OS/X, recent MSVC versions */
 #endif
 
 /* if nothing else helps:
