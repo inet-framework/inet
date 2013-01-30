cb4255fff084ef2226e9778aa46ab3ebebeb83b2
Zoltan Bojthe <zbojthe@omnetpp.org>
2013-01-30 11:33:07 +0100

    EtherMACFullDuplex: wait IFG only between frames (by Till Steinbach)
    the IEEE 802.3 spec explicitly says that IFG should be *after* frames, not before.
    
    http://standards.ieee.org/getieee802/download/802.3-2008_section1.pdf
    
    Section 4.2.3.2.1, full duplex operation:
            After the last bit of a transmitted frame, (that is, when transmitting
            changes from true to false), the MAC continues to defer for a proper interPacket-Gap (see 4.2.3.2.2)
    And further on (4.2.3.2.2):
            This is intended to provide interpacket recovery time for other CSMA/CD
            sublayers and for the physical medium."

--- src/linklayer/ethernet/EtherMACFullDuplex.cc
+++ src/linklayer/ethernet/EtherMACFullDuplex.cc
@@ -91,6 +91,7 @@ void EtherMACFullDuplex::handleSelfMessage(cMessage *msg)
 
 void EtherMACFullDuplex::startFrameTransmission()
 {
+    ASSERT(curTxFrame);
     EV << "Transmitting a copy of frame " << curTxFrame << endl;
 
     EtherFrame *frame = curTxFrame->dup();  // note: we need to duplicate the frame because we emit a signal with it in endTxPeriod()
@@ -179,7 +180,7 @@ void EtherMACFullDuplex::processFrameFromUpperLayer(EtherFrame *frame)
     }
 
     if (transmitState == TX_IDLE_STATE)
-        scheduleEndIFGPeriod();
+        startFrameTransmission();
 }
 
 void EtherMACFullDuplex::processMsgFromNetwork(EtherTraffic *msg)
@@ -240,13 +241,10 @@ void EtherMACFullDuplex::handleEndIFGPeriod()
     if (transmitState != WAIT_IFG_STATE)
         error("Not in WAIT_IFG_STATE at the end of IFG period");
 
-    if (NULL == curTxFrame)
-        error("End of IFG and no frame to transmit");
-
     // End of IFG period, okay to transmit
-    EV << "IFG elapsed, now begin transmission of frame " << curTxFrame << endl;
+    EV << "IFG elapsed" << endl;
 
-    startFrameTransmission();
+    beginSendFrames();
 }
 
 void EtherMACFullDuplex::handleEndTxPeriod()
@@ -277,6 +275,7 @@ void EtherMACFullDuplex::handleEndTxPeriod()
     delete curTxFrame;
     curTxFrame = NULL;
     lastTxFinishTime = simTime();
+    getNextFrameFromQueue();
 
     if (pauseUnitsRequested > 0)
     {
@@ -288,8 +287,8 @@ void EtherMACFullDuplex::handleEndTxPeriod()
     }
     else
     {
-        getNextFrameFromQueue();
-        beginSendFrames();
+        EV << "Start IFG period\n";
+        scheduleEndIFGPeriod();
     }
 }
 
@@ -359,8 +358,6 @@ void EtherMACFullDuplex::processPauseCommand(int pauseUnits)
 
 void EtherMACFullDuplex::scheduleEndIFGPeriod()
 {
-    ASSERT(curTxFrame);
-
     EtherIFG gap;
     transmitState = WAIT_IFG_STATE;
     scheduleAt(simTime() + transmissionChannel->calculateDuration(&gap), endIFGMsg);
@@ -380,15 +377,19 @@ void EtherMACFullDuplex::beginSendFrames()
 {
     if (curTxFrame)
     {
-        // Other frames are queued, therefore wait IFG period and transmit next frame
-        EV << "Transmit next frame in output queue, after IFG period\n";
-        scheduleEndIFGPeriod();
+        // Other frames are queued, transmit next frame
+        EV << "Transmit next frame in output queue\n";
+        startFrameTransmission();
     }
     else
     {
-        transmitState = TX_IDLE_STATE;
         // No more frames set transmitter to idle
-        EV << "No more frames to send, transmitter set to idle\n";
+        transmitState = TX_IDLE_STATE;
+        if (!txQueue.extQueue){
+            // Output only for internal queue (we cannot be shure that there
+            //are no other frames in external queue)
+            EV << "No more frames to send, transmitter set to idle\n";
+        }
     }
 }
 
