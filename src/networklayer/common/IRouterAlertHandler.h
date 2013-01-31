5cc8b207eaf020a1264fde1710afb71d565d3090
Markus Brueckner <markus.brueckner@tu-ilmenau.de>
2013-01-31 11:06:18 +0100

    - fixed: added documentation and license information

--- src/networklayer/common/IRouterAlertHandler.h
+++ src/networklayer/common/IRouterAlertHandler.h
@@ -1,13 +1,27 @@
-/*
- * IRouterAlertHandler.h
- *
- *  Created on: Jan 25, 2013
- *      Author: Markus Brueckner
- */
+//
+// Copyright (C) 2013 Markus Brueckner
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 
 #ifndef IROUTERALERTHANDLER_H_
 #define IROUTERALERTHANDLER_H_
 
+/**
+ * Base class for all router alert handlers. See the NED file for more
+ * information.
+ */
 class IRouterAlertHandler : public cSimpleModule
 {
 };
