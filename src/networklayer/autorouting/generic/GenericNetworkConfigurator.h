b273c8c90ee86bc9e0a4d5f6a9f0b7d34fbb587b
Levente Mészáros <levy@omnetpp.org>
2013-04-11 17:54:40 +0200

    logging: Removed duplicate EV_INFO and EV_DEBUG.

--- src/networklayer/autorouting/generic/GenericNetworkConfigurator.h
+++ src/networklayer/autorouting/generic/GenericNetworkConfigurator.h
@@ -30,14 +30,6 @@
 
 namespace inet { class PatternMatcher; }
 
-// compile time macros to disable or enable logging
-#define EV_ENABLED EV
-#define EV_DISABLED true?EV:EV
-
-// compile time log levels
-#define EV_DEBUG EV_DISABLED
-#define EV_INFO EV_ENABLED
-
 /**
  * This module configures generic routing tables for a network.
  *
