f7ffe207449474500af6e5811d103ad4c489bf84
Tamás Borbély <tomi.borbely@gmail.com>
2013-07-26 15:07:13 +0200

    RoutingTable6: join to ALL_NODES_2 and ALL_ROUTERS_2 (in routers) multicast group at initialization

--- src/networklayer/ipv6/RoutingTable6.cc
+++ src/networklayer/ipv6/RoutingTable6.cc
@@ -292,6 +292,14 @@ void RoutingTable6::configureInterfaceForIPv6(InterfaceEntry *ie)
     // add link-local prefix to each interface according to RFC 4861 5.1
     if (!ie->isLoopback())
         addStaticRoute(IPv6Address::LINKLOCAL_PREFIX, 10, ie->getInterfaceId(), IPv6Address::UNSPECIFIED_ADDRESS);
+
+    if (ie->isMulticast())
+    {
+        // XXX join other ALL_NODES_x and ALL_ROUTERS_x addresses too?
+        ipv6IfData->joinMulticastGroup(IPv6Address::ALL_NODES_2);
+        if (isrouter)
+            ipv6IfData->joinMulticastGroup(IPv6Address::ALL_ROUTERS_2);
+    }
 }
 
 void RoutingTable6::assignRequiredNodeAddresses(InterfaceEntry *ie)
