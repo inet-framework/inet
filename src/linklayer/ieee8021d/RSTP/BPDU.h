fc4625935f59d21f3f855cd4d0098541a74c148e
ProZsolt <prozsolt@gmail.com>
2013-09-18 14:14:07 +0200

    802.1d: Fixed copyright headers

--- src/linklayer/ieee8021d/RSTP/BPDU.h
+++ src/linklayer/ieee8021d/RSTP/BPDU.h
@@ -1,12 +1,20 @@
- /**
-******************************************************
-* @file BPDU.h
-* @brief RSTP BPDU modification
-*
-* @author Juan Luis Garrote Molinero
-* @version 1.0
-* @date Feb 2011
-******************************************************/
+//
+// Copyright (C) 2011 Juan Luis Garrote Molinero
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
 #ifndef __BPDU_H
 #define __BPDU_H
 
