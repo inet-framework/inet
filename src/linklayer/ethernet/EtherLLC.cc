671418292871c9daee5fd0ceb42eda485439733c
Andras Varga <andras@omnetpp.org>
2013-11-04 11:55:02 +0100

    backport: EtherLLC: do not refer to msg after having sent it

--- src/linklayer/ethernet/EtherLLC.cc
+++ src/linklayer/ethernet/EtherLLC.cc
@@ -194,9 +194,9 @@ void EtherLLC::processFrameFromMAC(EtherFrameWithLLC *frame)
     emit(decapPkSignal, higherlayermsg);
 
     // pass up to higher layer
-    send(higherlayermsg, "upperLayerOut", port);
     totalPassedUp++;
     emit(passedUpPkSignal, higherlayermsg);
+    send(higherlayermsg, "upperLayerOut", port);
     delete frame;
 }
 
