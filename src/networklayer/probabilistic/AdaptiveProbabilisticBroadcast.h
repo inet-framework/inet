76eb721a3f0a0672a9ea47c11aa8de85028d7538
Levente Mészáros <levy@omnetpp.org>
2013-11-19 14:10:47 +0100

    Ported probabilistic network layers from MiXiM to INET.

---
+++ src/networklayer/probabilistic/AdaptiveProbabilisticBroadcast.h
@@ -0,0 +1,56 @@
+/*
+ * ProbabilisticBroadcast.h
+ *
+ *  Created on: Nov 26, 2008
+ *      Author: Damien Piguet, Dimitris Kotsakos, Jérôme Rousselot
+ */
+
+#ifndef __INET_ADAPTIVEPROBABILISTICBROADCAST_H_
+#define __INET_ADAPTIVEPROBABILISTICBROADCAST_H_
+
+#include <map>
+
+#include "ProbabilisticBroadcast.h"
+#include "Address.h"
+
+/**
+ * @brief This class extends ProbabilisticBroadcast by adding
+ *        an algorithm which adapts broadcasting parameters
+ *        according to network conditions.
+ *
+ * @ingroup netwLayer
+ * @author Dimitris Kotsakos, George Alyfantis, Damien Piguet
+ **/
+class INET_API AdaptiveProbabilisticBroadcast : public ProbabilisticBroadcast
+{
+public:
+    AdaptiveProbabilisticBroadcast()
+        : ProbabilisticBroadcast()
+        , timeInNeighboursTable()
+        , bvec()
+        , neighMap()
+    {}
+
+    /** @brief Initialization of the module and some variables*/
+    virtual void initialize(int);
+
+protected:
+    typedef std::map<Address, cMessage*> NeighborMap;
+
+    /** @brief Handle messages from lower layer */
+    virtual void handleLowerMsg(cMessage* msg);
+
+    /** @brief Handle self messages */
+    virtual void handleSelfMsg(cMessage* msg);
+
+    void updateNeighMap(ProbabilisticBroadcastPkt* m);
+
+    void updateBeta();
+
+    //read from omnetpp.ini
+    simtime_t   timeInNeighboursTable; ///< @brief Default ttl for NeighborTable entries in seconds
+    cOutVector  bvec;
+    NeighborMap neighMap;
+};
+
+#endif
