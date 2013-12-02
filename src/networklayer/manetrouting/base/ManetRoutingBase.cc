256806d3e8de4de410c029cdb03ee60134805a54
Zoltan Bojthe <zbojthe@omnetpp.org>
2013-12-02 12:51:49 +0100

    fixes for C++ 11

--- src/networklayer/manetrouting/base/ManetRoutingBase.cc
+++ src/networklayer/manetrouting/base/ManetRoutingBase.cc
@@ -44,7 +44,6 @@ simsignal_t ManetRoutingBase::mobilityStateChangedSignal = SIMSIGNAL_NULL;
 ManetRoutingBase::GlobalRouteMap *ManetRoutingBase::globalRouteMap = NULL;
 bool ManetRoutingBase::createInternalStore = false;
 
-
 ManetRoutingBase::ManetRoutingBase()
 {
 #ifdef WITH_80211MESH
@@ -280,7 +279,7 @@ void ManetRoutingBase::registerRoutingModule()
             data.isProactive = isProactive();
             data.routesVector = routesVector;
             vect.push_back(data);
-            globalRouteMap->insert(std::make_pair<ManetAddress,ProtocolsRoutes>(getAddress(),vect));
+            globalRouteMap->insert(std::pair<ManetAddress,ProtocolsRoutes>(getAddress(),vect));
         }
         else
         {
@@ -1636,7 +1635,7 @@ void ManetRoutingBase::setRouteInternalStorege(const ManetAddress &dest, const M
                  it->second = next;
          }
          else
-             routesVector->insert(std::make_pair<ManetAddress,ManetAddress>(dest, next));
+             routesVector->insert(std::pair<ManetAddress,ManetAddress>(dest, next));
      }
 }
 
