2b3b84c725f1e5b43c0d12ac50da6bc961931078
Levente Mészáros <levy@omnetpp.org>
2014-01-16 11:57:59 +0100

    Added shell scripts to run wireless tests.

---
+++ tests/fingerprint/runWirelessTests.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+./fingerprints omnet4/*.csv -m nic -m wlan -m wireless -m adhoc
---
+++ tests/module/runWirelessTests.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+./runtest IdealRadio_*
+./runtest Ieee80211_*
+./runtest ReceptionState_*
+./runtest Interference_*
