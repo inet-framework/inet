5524c7891e236959d408015d649b1d30b56660fb
andry-tino <andry.tino@gmail.com>
2014-02-06 23:22:24 +0100

    Modified files

---
+++ src/linklayer/ieee80211ac/Ieee80211acTypes.h
@@ -0,0 +1,39 @@
+//
+// Copyright (C) 2014 Andrea Tino
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+//
+// Ieee80211acTypes.h
+//
+
+#ifndef IEEE80211ACTYPES_H_
+#define IEEE80211ACTYPES_H_
+
+/**
+ *
+ */
+enum BitType {
+  BIT_0 = 0x0,
+  BIT_1 = 0xf
+};
+
+/**
+ *
+ */
+typedef char* DataType;
+
+#endif
+
