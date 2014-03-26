3e23d99e016a16a1578178e2ffcb6074a2f5fe65
Zoltan Bojthe <zbojthe@omnetpp.org>
2014-03-26 11:13:04 +0100

    EV fix after merge

--- src/transport/tcp/TCPConnectionEventProc.cc
+++ src/transport/tcp/TCPConnectionEventProc.cc
@@ -297,7 +297,7 @@ void TCPConnection::process_QUEUE_BYTES_LIMIT(TCPEventCode& event, TCPCommand *t
         opp_error("Called process_QUEUE_BYTES_LIMIT on uninitialized TCPConnection!");
 
     state->sendQueueLimit = tcpCommand->getUserId(); // Set queue size limit
-    tcpEV << "state->sendQueueLimit set to " << state->sendQueueLimit << "\n";
+    EV << "state->sendQueueLimit set to " << state->sendQueueLimit << "\n";
     delete msg;
     delete tcpCommand;
 }
