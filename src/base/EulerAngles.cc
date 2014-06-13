045eb6defa4359e11812e225c528afbe35945038
Rudolf Hornig <rudi@omnetpp.org>
2014-06-13 14:13:58 +0200

    Rearranged directory structure.

--- src/base/EulerAngles.cc
+++
@@ -1,7 +0,0 @@
-//
-// This program is property of its copyright holder. All rights reserved.
-// 
-
-#include <EulerAngles.h>
-
-const EulerAngles EulerAngles::IDENTITY = EulerAngles(0.0, 0.0, 0.0);
---
+++ src/common/geometry/Quaternion.cc
@@ -0,0 +1,7 @@
+//
+// This program is property of its copyright holder. All rights reserved.
+//
+
+#include <Quaternion.h>
+
+const Quaternion Quaternion::IDENTITY = Quaternion();
