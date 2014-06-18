e77be07dde6ebc1c4aa9530ad17f10debc6bd9bd
Zsolt Prontvai <zsolt@omnetpp.org>
2014-06-18 17:23:22 +0200

    XXXXXXXXX

--- src/util/headerserializers/ipv6/IPv6Serializer.cc
+++ src/util/headerserializers/ipv6/IPv6Serializer.cc
@@ -89,10 +89,12 @@ int IPv6Serializer::serialize(const IPv6Datagram *dgram, unsigned char *buf, uns
 
     switch (dgram->getTransportProtocol())
     {
-/*      case IP_PROT_IPv6_ICMP:
-        packetLength += ICMPv6Serializer().serialize(check_and_cast<ICMPv6Message *>(encapPacket),
+      case IP_PROT_IPv6_ICMP:
+        EV_DEBUG<<"ICMP Frame TODO TODO"<<endl; //TODO
+        /*packetLength += ICMPv6Serializer().serialize(check_and_cast<ICMPv6Message *>(encapPacket),
                                                    buf+IPv6_HEADER_BYTES, bufsize-IPv6_HEADER_BYTES);
-        break;*/
+        */
+        break;
 #ifdef WITH_UDP
       case IP_PROT_UDP:
         packetLength = UDPSerializer().serialize(check_and_cast<UDPPacket *>(encapPacket),
@@ -116,11 +118,94 @@ int IPv6Serializer::serialize(const IPv6Datagram *dgram, unsigned char *buf, uns
 #endif
 
       default:
-          printf("IPv6Serializer: cannot serialize protocol %d\n", dgram->getTransportProtocol());
-          return -1;
+          throw cRuntimeError(dgram, "IPv6Serializer: cannot serialize protocol %d", dgram->getTransportProtocol());
     }
 
     ip6h->ip6_plen = htons(packetLength);
 
     return (packetLength + IPv6_HEADER_BYTES);
+}
+
+
+void IPv6Serializer::parse(const unsigned char *buf, unsigned int bufsize, IPv6Datagram *dest)
+{
+    const struct ip6_hdr *ip6h = (struct ip6_hdr *) buf;
+
+    flowinfo = 0x06;
+    flowinfo <<= 8;
+    flowinfo |= dgram->getTrafficClass();
+    flowinfo <<= 20;
+    flowinfo |= dgram->getFlowLabel();
+    ip6h->ip6_flow = htonl(flowinfo);
+    ip6h->ip6_hlim = htons(dgram->getHopLimit());
+
+    ip6h->ip6_nxt = dgram->getTransportProtocol();
+
+    for (i = 0; i < 4; i++) {
+        ip6h->ip6_src.__u6_addr.__u6_addr32[i] = htonl(dgram->getSrcAddress().words()[i]);
+    }
+    for (i = 0; i < 4; i++) {
+        ip6h->ip6_dst.__u6_addr.__u6_addr32[i] = htonl(dgram->getDestAddress().words()[i]);
+    }
+
+    /*dest->setVersion(ip->ip_v);
+    dest->setHeaderLength(IP_HEADER_BYTES);
+    dest->setSrcAddress(IPv4Address(ntohl(ip->ip_src.s_addr)));
+    dest->setDestAddress(IPv4Address(ntohl(ip->ip_dst.s_addr)));
+    dest->setTransportProtocol(ip->ip_p);
+    dest->setTimeToLive(ip->ip_ttl);
+    dest->setIdentification(ntohs(ip->ip_id));
+    uint16_t ip_off = ntohs(ip->ip_off);
+    dest->setMoreFragments((ip_off & IP_MF) != 0);
+    dest->setDontFragment((ip_off & IP_DF) != 0);
+    dest->setFragmentOffset((ntohs(ip->ip_off) & IP_OFFMASK)*8);
+    dest->setTypeOfService(ip->ip_tos);
+    totalLength = ntohs(ip->ip_len);
+    headerLength = ip->ip_hl << 2;*/
+
+    if (headerLength > (unsigned int)IP_HEADER_BYTES)
+        EV << "Handling an captured IPv4 packet with options. Dropping the options.\n";
+
+    if (totalLength > bufsize)
+        EV << "Can not handle IPv4 packet of total length " << totalLength << "(captured only " << bufsize << " bytes).\n";
+
+    dest->setByteLength(IP_HEADER_BYTES);
+
+    cPacket *encapPacket = NULL;
+    unsigned int encapLength = std::min(totalLength, bufsize) - headerLength;
+
+    switch (dest->getTransportProtocol())
+    {
+      case IIP_PROT_IPv6_ICMP:
+        encapPacket = new ICMPv6Message("icmp-from-wire");
+        break;
+
+#ifdef WITH_UDP
+      case IP_PROT_UDP:
+        encapPacket = new UDPPacket("udp-from-wire");
+        UDPSerializer().parse(buf + headerLength, encapLength, (UDPPacket *)encapPacket);
+        break;
+#endif
+
+#ifdef WITH_SCTP
+      case IP_PROT_SCTP:
+        encapPacket = new SCTPMessage("sctp-from-wire");
+        SCTPSerializer().parse(buf + headerLength, encapLength, (SCTPMessage *)encapPacket);
+        break;
+#endif
+
+#ifdef WITH_TCP_COMMON
+      case IP_PROT_TCP:
+        encapPacket = new TCPSegment("tcp-from-wire");
+        TCPSerializer().parse(buf + headerLength, encapLength, (TCPSegment *)encapPacket, true);
+        break;
+#endif
+
+      default:
+        throw cRuntimeError("IPv6Serializer: cannot parse protocol %d", dest->getTransportProtocol());
+    }
+
+    ASSERT(encapPacket);
+    dest->encapsulate(encapPacket);
+    dest->setName(encapPacket->getName());
 }
