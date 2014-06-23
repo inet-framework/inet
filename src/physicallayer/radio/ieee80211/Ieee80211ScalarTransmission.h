ae5758bea489b7354f6935f3719be03fe3217521
Levente Mészáros <levy@omnetpp.org>
2014-06-23 10:38:20 +0200

    Radio: Added Ieee80211 specific transmission class.

---
+++ src/physicallayer/radio/ieee80211/Ieee80211ScalarTransmission.h
@@ -0,0 +1,40 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IEEE80211SCALARTRANSMISSION_H_
+#define __INET_IEEE80211SCALARTRANSMISSION_H_
+
+#include "ScalarTransmission.h"
+
+namespace radio
+{
+
+class INET_API Ieee80211ScalarTransmission : public ScalarTransmission
+{
+    public:
+        Ieee80211ScalarTransmission(const IRadio *transmitter, const cPacket *macFrame, const simtime_t startTime, const simtime_t endTime, const Coord startPosition, const Coord endPosition, const EulerAngles startOrientation, const EulerAngles endOrientation, const IModulation *modulation, int headerBitLength, int payloadBitLength, Hz carrierFrequency, Hz bandwidth, bps bitrate, W power) :
+            ScalarTransmission(transmitter, macFrame, startTime, endTime, startPosition, endPosition, startOrientation, endOrientation, modulation, headerBitLength, payloadBitLength, carrierFrequency, bandwidth, bitrate, power)
+        {}
+
+        virtual void printToStream(std::ostream &stream) const;
+
+        virtual W getPower() const { return power; }
+};
+
+}
+
+#endif
