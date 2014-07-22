22a01e494c868292d283fbc5090492a374c174a7
Levente Mészáros <levy@omnetpp.org>
2014-07-22 11:05:15 +0200

    Test: Changed to run all tests at once.

--- tests/module/runWirelessTests.sh
+++ tests/module/runWirelessTests.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-#./runtest IdealRadio_*.test
-./runtest Ieee80211_*.test
-./runtest ReceptionState_*.test
-./runtest Interference_*.test
+./runtest ReceptionState_*.test Interference_*.test Ieee80211_*.test
