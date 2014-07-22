138c0f06a5d0579c41d70bbdc8d7062932bedf5b
Levente Mészáros <levy@omnetpp.org>
2014-07-22 16:36:01 +0200

    Radio: Ported breakpoint path loss model from mixim.

---
+++ src/physicallayer/pathloss/BreakpointPathLoss.cc
@@ -0,0 +1,54 @@
+#include "BreakpointPathLoss.h"
+
+namespace inet {
+
+namespace physicallayer {
+
+Define_Module(BreakpointPathLoss);
+
+BreakpointPathLoss::BreakpointPathLoss() :
+    l01(sNaN),
+    l02(sNaN),
+    alpha1(sNaN),
+    alpha2(sNaN),
+    breakpointDistance(m(sNaN))
+{
+}
+
+void BreakpointPathLoss::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL) {
+        l01 = FWMath::dB2fraction(par("L01"));
+        l02 = FWMath::dB2fraction(par("L02"));
+        alpha1 = par("alpha1");
+        alpha2 = par("alpha2");
+        breakpointDistance = m(par("breakpointDistance"));
+    }
+}
+
+void BreakpointPathLoss::printToStream(std::ostream& stream) const
+{
+    stream << "breakpoint path loss,"
+           << " L01 = " << l01
+           << " L02 = " << l02
+           << " alpha1 = " << alpha1
+           << " alpha2 = " << alpha2
+           << " breakpointDistance = " << breakpointDistance;
+}
+
+double BreakpointPathLoss::computePathLoss(mps propagationSpeed, Hz frequency, m distance) const
+{
+    // PL(d) = PL0 + 10 alpha log10 (d/d0)
+    // 10 ^ { PL(d)/10 } = 10 ^{PL0 + 10 alpha log10 (d/d0)}/10
+    // 10 ^ { PL(d)/10 } = 10 ^ PL0/10 * 10 ^ { 10 log10 (d/d0)^alpha }/10
+    // 10 ^ { PL(d)/10 } = 10 ^ PL0/10 * 10 ^ { log10 (d/d0)^alpha }
+    // 10 ^ { PL(d)/10 } = 10 ^ PL0/10 * (d/d0)^alpha
+    if (distance < breakpointDistance)
+        return 1 / (l01 * pow(distance.get(), alpha1));
+    else
+        return 1 / (l02 * pow(unit(distance / breakpointDistance).get(), alpha2));
+}
+
+} // namespace physicallayer
+
+} // namespace inet
---
+++ src/physicallayer/pathloss/BreakpointPathLoss.h
@@ -0,0 +1,37 @@
+#ifndef __INET_BREAKPOINTPATHLOSS_H
+#define __INET_BREAKPOINTPATHLOSS_H
+
+#include "IPathLoss.h"
+
+namespace inet {
+
+namespace physicallayer {
+
+/**
+ * Implementation of a breakpoint path loss model.
+ */
+class INET_API BreakpointPathLoss : public cModule, public IPathLoss
+{
+  protected:
+    /** @brief initial path loss */
+    double l01, l02;
+    /** @brief pathloss exponents */
+    double alpha1, alpha2;
+    /** @brief Breakpoint distance squared. */
+    m breakpointDistance;
+
+  protected:
+    virtual void initialize(int stage);
+
+  public:
+	BreakpointPathLoss();
+	virtual void printToStream(std::ostream& stream) const;
+    virtual double computePathLoss(mps propagationSpeed, Hz frequency, m distance) const;
+    virtual m computeRange(mps propagationSpeed, Hz frequency, double loss) const { return m(NaN); }
+};
+
+} // namespace physicallayer
+
+} // namespace inet
+
+#endif // ifndef __INET_BREAKPOINTPATHLOSS_H
