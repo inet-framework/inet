17f3cb2929401c4524070a4f42351970e9d2da93
Zsolt Prontvai <zsolt@omnetpp.org>
2014-08-25 16:46:43 +0200

    Serializer: Added utility for set/geting bytes

---
+++ src/util/headerserializers/SerializerUtil.cc
@@ -0,0 +1,96 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#include <SerializerUtil.h>
+
+void setOneByte(uint8_t value, unsigned char *buf, unsigned int offset)
+{
+    uint8_t *p = (uint8_t *) (buf + offset);
+    *p = value;
+}
+
+void setTwoByte(uint16_t value, unsigned char *buf, unsigned int offset)
+{
+    uint16_t *p = (uint16_t *) (buf + offset);
+    *p = htons(value);
+}
+
+void setFourByte(uint32_t value, unsigned char *buf, unsigned int offset)
+{
+    uint32_t *p = (uint32_t *) (buf + offset);
+    *p = htonl(value);
+}
+
+void setNByte(uint8_t *value, unsigned int n, unsigned char *buf, unsigned int offset)
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
+        uint8_t *p = (uint8_t *) (buf + offset + i);
+#  if BYTE_ORDER == LITTLE_ENDIAN
+        *p = value[i];
+#  elif BYTE_ORDER == BIG_ENDIAN
+        *p = value[n-i-1];
+#else
+# error "Please check BYTE_ORDER declaration"
+#  endif
+    }
+}
+
+void setBit(bool value, unsigned int bitOffset, unsigned char *buf, unsigned int offset)
+{
+    uint8_t *p = (uint8_t *) (buf + offset);
+    uint8_t byteValue = 1U << bitOffset;
+    *p = value ? *p | byteValue : *p & ~byteValue;
+}
+
+uint8_t getOneByte(unsigned char *buf, unsigned int offset)
+{
+    uint8_t *p = (uint8_t *) (buf + offset);
+    return *p;
+}
+
+uint16_t getTwoByte(unsigned char *buf, unsigned int offset)
+{
+    uint16_t *p = (uint16_t *) (buf + offset);
+    return ntohs(*p);
+}
+
+uint32_t getFourByte(unsigned char *buf, unsigned int offset)
+{
+    uint32_t *p = (uint32_t *) (buf + offset);
+    return ntohl(*p);
+}
+
+void getNByte(uint8_t *value, unsigned int n, unsigned char *buf, unsigned int offset)
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
+        uint8_t *p = (uint8_t *) (buf + offset + i);
+#  if BYTE_ORDER == LITTLE_ENDIAN
+        value[i] = *p;
+#  elif BYTE_ORDER == BIG_ENDIAN
+        value[n-i-1] = *p;
+#else
+# error "Please check BYTE_ORDER declaration"
+#  endif
+    }
+}
+
+bool getBit(unsigned int bitOffset, unsigned char *buf, unsigned int offset)
+{
+    uint8_t *p = (uint8_t *) (buf + offset);
+    uint8_t byteValue = 1U << bitOffset;
+    return *p & byteValue;
+}
---
+++ src/util/headerserializers/SerializerUtil.h
@@ -0,0 +1,33 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef SERIALIZERUTIL_H_
+#define SERIALIZERUTIL_H_
+
+#include "INETDefs.h"
+
+void setOneByte(uint8_t value, unsigned char *buf, unsigned int offset = 0);
+void setTwoByte(uint16_t value, unsigned char *buf, unsigned int offset = 0);
+void setFourByte(uint32_t value, unsigned char *buf, unsigned int offset = 0);
+void setNByte(uint8_t *value, unsigned int n, unsigned char *buf, unsigned int offset = 0);
+void setBit(bool value, unsigned int bitOffset, unsigned char *buf, unsigned int offset = 0);
+
+uint8_t getOneByte(unsigned char *buf, unsigned int offset = 0);
+uint16_t getTwoByte(unsigned char *buf, unsigned int offset = 0);
+uint32_t getFourByte(unsigned char *buf, unsigned int offset = 0);
+void getNByte(uint8_t *value, unsigned int n, unsigned char *buf, unsigned int offset = 0);
+bool getBit(unsigned int bitOffset, unsigned char *buf, unsigned int offset = 0);
+
+#endif /* SERIALIZERUTIL_H_ */
