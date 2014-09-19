fe0154d9faa84a7277428eee9e76725df501f0c4
Benjamin Seregi <benjamin@omnetpp.org>
2014-09-19 16:44:40 +0200

    Ieee80211: layered related modules and src files moved to the layered directory.

---
+++ src/physicallayer/ieee80211/layered/Ieee80211Interleaver.h
@@ -0,0 +1,67 @@
+//
+// Copyright (C) 2014 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IEEE80211INTERLEAVER_H_
+#define __INET_IEEE80211INTERLEAVER_H_
+
+#include "INETDefs.h"
+#include "BitVector.h"
+#include "IInterleaver.h"
+
+namespace inet {
+namespace physicallayer {
+
+/*
+ * It is a IEEE 802.11 block interleaver/deinterleaver implementation.
+ * The permutation equations and all the details can be found in:
+ * Part 11: Wireless LAN Medium Access Control (MAC) and Physical Layer (PHY) Specifications,
+ * 18.3.5.7 Data interleaving
+ */
+class Ieee80211Interleaver : public cSimpleModule, public IInterleaver
+{
+  public:
+    class Ieee80211InterleaverInfo : public IInterleaverInfo
+    {
+        const Ieee80211Interleaver *interleaver;
+        public:
+            Ieee80211InterleaverInfo(const Ieee80211Interleaver *interleaver) : interleaver(interleaver) {}
+            int getNumberOfCodedBitsPerSymbol() const { return interleaver->numberOfCodedBitsPerSymbol; }
+            int getNumberOfCodedBitsPerSubcarrier() const { return interleaver->numberOfCodedBitsPerSubcarrier; }
+            void printToStream(std::ostream& stream) const;
+    };
+
+  protected:
+    int numberOfCodedBitsPerSymbol;
+    int numberOfCodedBitsPerSubcarrier;
+    int s;
+    const Ieee80211InterleaverInfo *info;
+
+  protected:
+    virtual void initialize(int stage);
+    virtual void handleMessage(cMessage *msg) { cRuntimeError("This module doesn't handle self messages."); }
+
+  public:
+    BitVector interleaving(const BitVector& bits) const;
+    BitVector deinterleaving(const BitVector& bits) const;
+    const Ieee80211InterleaverInfo *getInfo() const { return info; }
+    ~Ieee80211Interleaver();
+};
+
+} /* namespace physicallayer */
+} /* namespace inet */
+
+#endif /* __INET_IEEE80211INTERLEAVER_H_ */
---
+++ src/physicallayer/ieee80211/layered/Ieee80211Scrambler.cc
@@ -0,0 +1,83 @@
+//
+// Copyright (C) 2014 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "Ieee80211Scrambler.h"
+#include "ShortBitVector.h"
+
+namespace inet {
+namespace physicallayer {
+
+Define_Module(Ieee80211Scrambler);
+
+void Ieee80211Scrambler::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+    {
+        info = new Ieee80211ScramblerInfo(this);
+        ShortBitVector seed(par("seed").stringValue());
+        ShortBitVector generatorPolynomial(par("generatorPolynomial").stringValue());
+        scramblingSequence = generateScramblingSequence(generatorPolynomial, seed);
+    }
+}
+
+BitVector Ieee80211Scrambler::scramble(const BitVector& bits) const
+{
+    EV_DETAIL << "Scrambling the following bits: " << bits << endl;
+    BitVector scrambledBits;
+    int sequenceLength = scramblingSequence.getSize();
+    for (unsigned int i = 0; i < bits.getSize(); i++)
+    {
+        int scramblingIndex = i % sequenceLength;
+        bool scrambledBit = eXOR(bits.getBit(i), scramblingSequence.getBit(scramblingIndex));
+        scrambledBits.appendBit(scrambledBit);
+    }
+    EV_DETAIL << "The scrambled bits are: " << scrambledBits << endl;
+    return scrambledBits;
+}
+
+BitVector Ieee80211Scrambler::generateScramblingSequence(const ShortBitVector& generatorPolynomial, const ShortBitVector& seed) const
+{
+    BitVector scramblingSequence;
+    int sequenceLength = (int)pow(2, seed.getSize()) - 1;
+    ShortBitVector shiftRegisters = seed;
+    for (int i = 0; i < sequenceLength; i++)
+    {
+        bool registerSum = false;
+        for (unsigned int j = 0; j < generatorPolynomial.getSize(); j++)
+        {
+            if (generatorPolynomial.getBit(j))
+                registerSum = eXOR(shiftRegisters.getBit(j), registerSum);
+        }
+        shiftRegisters.leftShift(1);
+        shiftRegisters.setBit(0, registerSum);
+        scramblingSequence.appendBit(registerSum);
+    }
+    return scramblingSequence;
+}
+
+void Ieee80211Scrambler::Ieee80211ScramblerInfo::printToStream(std::ostream& stream) const
+{
+    stream << "Ieee80211Scrambler with scrambling sequence: " << scrambler->scramblingSequence;
+}
+
+Ieee80211Scrambler::~Ieee80211Scrambler()
+{
+    delete info;
+}
+
+} /* namespace physicallayer */
+} /* namespace inet */
