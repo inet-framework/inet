d98e7dd60dcf69372dad42ded4ad3d91dc55aef7
Levente Mészáros <levy@omnetpp.org>
2014-11-04 10:30:56 +0100

    Radio: Renamed directory to energyconsumer.

--- src/inet/physicallayer/energyconsumer/StateBasedEnergyConsumer.cc
+++ src/inet/physicallayer/energyconsumer/StateBasedEnergyConsumer.cc
@@ -15,7 +15,7 @@
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
 
-#include "inet/physicallayer/power/StateBasedEnergyConsumer.h"
+#include "inet/physicallayer/energyconsumer/StateBasedEnergyConsumer.h"
 #include "inet/common/ModuleAccess.h"
 
 namespace inet {
