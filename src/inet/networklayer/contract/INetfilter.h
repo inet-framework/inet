06fbba4882e2369c127e8d18580dd39824fde753
Levente Mészáros <levy@omnetpp.org>
2014-12-12 10:56:47 +0100

    NetworkLayer: Added documentation comments.

--- src/inet/networklayer/contract/INetfilter.h
+++ src/inet/networklayer/contract/INetfilter.h
@@ -25,13 +25,15 @@
 namespace inet {
 
 /**
- * TODO
+ * This interface is implemented by network protocols which want provide netfilter
+ * hooks to customize their behavior. For example, implementing a reactive routing
+ * protocol can be done using this interface.
  */
 class INET_API INetfilter
 {
   public:
     /**
-     * TODO
+     * This interface is used by the network protocol during processing datagrams.
      */
     class INET_API IHook
     {
@@ -45,38 +47,45 @@ class INET_API INetfilter
         };
 
         enum Result {
-            ACCEPT,    /**< allow datagram to pass to next hook */
-            DROP,    /**< do not allow datagram to pass to next hook, delete it */
-            QUEUE,    /**< queue datagram for later re-injection */
-            STOLEN    /**< do not allow datagram to pass to next hook, but do not delete it */
+            ACCEPT, ///< allows the datagram to pass to the next hook
+            DROP,   ///< doesn't allow the datagram to pass to the next hook, will be deleted
+            QUEUE,  ///< queues the datagram for later re-injection (e.g. when route discovery completes)
+            STOLEN  ///< doesn't allow datagram to pass to next hook, but won't be deleted
         };
 
         virtual ~IHook() {};
 
         /**
-         * TODO
-         * nextHopAddress ignored when outputInterfaceEntry is nullptr
+         * This is the first hook called by the network protocol before it routes
+         * a datagram that was received from the lower layer. The nextHopAddress
+         * is ignored when the outputInterfaceEntry is nullptr.
          */
         virtual Result datagramPreRoutingHook(INetworkDatagram *datagram, const InterfaceEntry *inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, L3Address& nextHopAddress) = 0;
 
         /**
-         * TODO
+         * This is the second hook called by the network protocol before it sends
+         * a datagram to the lower layer. This is done after the datagramPreRoutingHook
+         * or the datagramLocalInHook is called and the datagram is routed.
          */
         virtual Result datagramForwardHook(INetworkDatagram *datagram, const InterfaceEntry *inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, L3Address& nextHopAddress) = 0;
 
         /**
-         * TODO
+         * This is the last hook called by the network protocol before it sends
+         * a datagram to the lower layer.
          */
         virtual Result datagramPostRoutingHook(INetworkDatagram *datagram, const InterfaceEntry *inputInterfaceEntry, const InterfaceEntry *& outputInterfaceEntry, L3Address& nextHopAddress) = 0;
 
         /**
-         * TODO
+         * This is the last hook called by the network protocol before it sends
+         * a datagram to the upper layer. This is done after the datagramPreRoutingHook
+         * is called and the datagram is routed.
          */
         virtual Result datagramLocalInHook(INetworkDatagram *datagram, const InterfaceEntry *inputInterfaceEntry) = 0;
 
         /**
-         * TODO
-         * nextHopAddress ignored when outputInterfaceEntry is nullptr
+         * This is the first hook called by the network protocol before it routes
+         * a datagram that was received from the upper layer. The nextHopAddress
+         * is ignored when the outputInterfaceEntry is a nullptr. After this is done
          */
         virtual Result datagramLocalOutHook(INetworkDatagram *datagram, const InterfaceEntry *& outputInterfaceEntry, L3Address& nextHopAddress) = 0;
     };
@@ -84,22 +93,27 @@ class INET_API INetfilter
     virtual ~INetfilter() {}
 
     /**
-     * TODO
+     * Adds the provided hook to the list of registered hooks that will be called
+     * by the network layer when it processes datagrams.
      */
     virtual void registerHook(int priority, IHook *hook) = 0;
 
     /**
-     * TODO
+     * Removes the provided hook from the list of registered hooks.
      */
     virtual void unregisterHook(int priority, IHook *hook) = 0;
 
     /**
-     * TODO
+     * Requests the network layer to drop the datagram, because it's no longer
+     * needed. This function may be used by a reactive routing protocol when it
+     * cancels the route discovery process.
      */
     virtual void dropQueuedDatagram(const INetworkDatagram *daragram) = 0;
 
     /**
-     * TODO
+     * Requests the network layer to restart the processing of the datagram. This
+     * function may be used by a reactive routing protocol when it completes the
+     * route discovery process.
      */
     virtual void reinjectQueuedDatagram(const INetworkDatagram *datagram) = 0;
 };
