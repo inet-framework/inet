e29d927cdf0b0c3184a7ad958648dd2b20a5d162
Zoltan Bojthe <zbojthe@omnetpp.org>
2015-01-14 10:00:45 +0100

    coverity: manet dymo: Unchecked dynamic_cast

--- src/inet/routing/extras/dymo/dymo_packet_queue_omnet.cc
+++ src/inet/routing/extras/dymo/dymo_packet_queue_omnet.cc
@@ -183,7 +183,7 @@ int NS_CLASS packet_queue_set_verdict(struct in_addr dest_addr, int verdict)
                         * a burst of packets. So to keep ARP happy, buffered
                         * packets are sent with ARP_DELAY seconds between
                         * sends. */
-                    qp->p = pkt_encapsulate(dynamic_cast <IPv4Datagram *> (qp->p), *gateWayAddress);
+                    qp->p = pkt_encapsulate(static_cast <IPv4Datagram *> (qp->p), *gateWayAddress);
                     // now Ip layer decremented again
                     // sendDelayed(qp->p, delay, "to_ip_from_network");
                     sendDelayed(qp->p, delay, "to_ip");
@@ -415,7 +415,7 @@ int NS_CLASS packet_queue_set_verdict(struct in_addr dest_addr, int verdict)
                              * a burst of packets. So to keep ARP happy, buffered
                              * packets are sent with ARP_DELAY seconds between
                              * sends. */
-                            qp->p = pkt_encapsulate(dynamic_cast <IPv4Datagram *> (qp->p), *gateWayAddress);
+                            qp->p = pkt_encapsulate(static_cast <IPv4Datagram *> (qp->p), *gateWayAddress);
                             // now Ip layer decremented again
                             // sendDelayed(qp->p, delay, "to_ip_from_network");
                             sendDelayed(qp->p, delay, "to_ip");
