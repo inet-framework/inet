d5d390a6d935a38ead8c86a00170d2921d1559e7
Levente Mészáros <levy@omnetpp.org>
2015-02-17 09:56:23 +0100

    Removed superfluous, weird and precision loosing time conversion.
    Updated fingerprints.

--- src/inet/physicallayer/ieee80211/Ieee80211ScalarTransmitter.cc
+++ src/inet/physicallayer/ieee80211/Ieee80211ScalarTransmitter.cc
@@ -72,7 +72,7 @@ const ITransmission *Ieee80211ScalarTransmitter::createTransmission(const IRadio
     W transmissionPower = controlInfo && !isNaN(controlInfo->getPower().get()) ? controlInfo->getPower() : power;
     bps transmissionBitrate = controlInfo && !isNaN(controlInfo->getBitrate().get()) ? controlInfo->getBitrate() : bitrate;
     ModulationType modulationType = Ieee80211Descriptor::getModulationType(opMode, transmissionBitrate.get());
-    const simtime_t duration = SIMTIME_DBL(Ieee80211Modulation::calculateTxDuration(macFrame->getBitLength(), modulationType, preambleMode));
+    const simtime_t duration = Ieee80211Modulation::calculateTxDuration(macFrame->getBitLength(), modulationType, preambleMode);
     const simtime_t endTime = startTime + duration;
     IMobility *mobility = transmitter->getAntenna()->getMobility();
     const Coord startPosition = mobility->getCurrentPosition();
