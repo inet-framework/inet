0342258c05bf4c8b6f8ee9971f2ba29ae328eb31
Tey <teyut@free.fr>
2015-03-17 02:18:06 +0100

    Allow the user to disable RTP logging to file.

--- src/transport/rtp/RTPPayloadReceiver.cc
+++ src/transport/rtp/RTPPayloadReceiver.cc
@@ -38,10 +38,13 @@ void RTPPayloadReceiver::initialize()
 {
     const char *fileName = par("outputFileName");
     const char *logFileName = par("outputLogFileName");
-    openOutputFile(fileName);
-    char logName[200];
-    sprintf(logName, logFileName, getId());
-    _outputLogLoss.open(logName);
+    if (strcmp(fileName, "")) openOutputFile(fileName);
+    if (strcmp(logFileName, ""))
+    {
+        char logName[200];
+        sprintf(logName, logFileName, getId());
+        _outputLogLoss.open(logName);
+    }
 }
 
 void RTPPayloadReceiver::handleMessage(cMessage *msg)
