3c302702745379c4d4f9132904254f7cdaa465f0
Levente Mészáros <levy@omnetpp.org>
2015-03-18 12:20:19 +0100

    Test: Changed to avoid running manetrouting/aodv examples twice.

--- tests/fingerprint/runWirelessTests.sh
+++ tests/fingerprint/runWirelessTests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./fingerprints *.csv -m wireless -m adhoc -m manet -m mobileipv6 -m WirelessDHCP -m aodv -m neighborcache
+./fingerprints *.csv -m wireless -m adhoc -m manet -m mobileipv6 -m WirelessDHCP -m examples/aodv -m neighborcache
