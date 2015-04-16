0fb4700d9bd0986b74d18a81c50190f4b5008502
Zoltan Bojthe <zbojthe@omnetpp.org>
2015-04-16 12:12:57 +0200

    serializer: ICMPv6Serializer fix a copy-paste bug

--- src/inet/common/serializer/ipv6/ICMPv6Serializer.cc
+++ src/inet/common/serializer/ipv6/ICMPv6Serializer.cc
@@ -90,7 +90,7 @@ void ICMPv6Serializer::serialize(const cPacket *_pkt, Buffer &b, Context& c)
             b.writeByte(frame->getCode());
             b.writeUint16(0);   // crc
             b.writeUint32(0);   // unused
-            SerializerBase::lookupAndSerialize(pkt->getEncapsulatedPacket(), b, c, ETHERTYPE, ETHERTYPE_IPv4, 0);
+            SerializerBase::lookupAndSerialize(pkt->getEncapsulatedPacket(), b, c, ETHERTYPE, ETHERTYPE_IPv6, 0);
             break;
         }
 
