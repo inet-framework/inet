a7f543d681f24eb473f64ce9f69a9e1f2727dcf8
Zoltan Bojthe <zbojthe@omnetpp.org>
2015-05-04 11:35:07 +0200

    coverity: fix CID 93081

--- src/inet/applications/httptools/server/HttpServerBase.cc
+++ src/inet/applications/httptools/server/HttpServerBase.cc
@@ -348,7 +348,7 @@ HttpReplyMessage *HttpServerBase::generateResourceMessage(HttpRequestMessage *re
     else if (category==CT_IMAGE)
         size = (int) rdImageResourceSize->draw();
     else
-        error("Invalid resource category");
+        throw cRuntimeError("Invalid resource category");
 
     char szReply[512];
     sprintf(szReply, "HTTP/1.1 200 OK (%s)", resource.c_str());
