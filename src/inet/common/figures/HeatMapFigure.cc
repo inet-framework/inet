17eaa724855e881967c0d2997b355455f8d68349
Rudolf Hornig <rudi@omnetpp.org>
2015-05-08 13:28:43 +0200

    common: moved all figures to the common/figures folder.

--- src/inet/common/figures/HeatMapFigure.cc
+++ src/inet/common/figures/HeatMapFigure.cc
@@ -15,7 +15,7 @@
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
 
-#include "inet/common/HeatMapFigure.h"
+#include "HeatMapFigure.h"
 
 namespace inet {
 
--- src/inet/common/figures/TrailFigure.cc
+++ src/inet/common/figures/TrailFigure.cc
@@ -15,7 +15,7 @@
 // along with this program; if not, see <http://www.gnu.org/licenses/>.
 //
 
-#include "inet/common/TrailFigure.h"
+#include "TrailFigure.h"
 
 namespace inet {
 
