367463d650e5477c326931061a975718eace0739
Benjamin Seregi <benjamin@omnetpp.org>
2015-06-24 17:39:30 +0200

    Ieee80211MacDistributeMmpdus

---
+++ src/inet/linklayer/ieee80211/thenewmac/mlme/services/Ieee80211MacDistributeMmpdusService.cc
@@ -0,0 +1,134 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "Ieee80211MacDistributeMmpdusService.h"
+
+namespace inet {
+namespace ieee80211 {
+
+void Ieee80211MacDistributeMmpdusService::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+    {
+
+    }
+}
+
+
+void Ieee80211MacDistributeMmpdusService::handleMessage(cMessage* msg)
+{
+    if (msg->isSelfMessage())
+    {
+
+    }
+    else
+    {
+        if (dynamic_cast<Ieee80211MacSignalXport *>(msg))
+            handleXport();
+        else if (dynamic_cast<Ieee80211MacSignalSst *>(msg))
+            handleSst(dynamic_cast<Ieee80211MacSignalSst *>(msg));
+        else if (dynamic_cast<Ieee80211MacSignalMmConfirm *>(msg))
+            handleMmConfirm(dynamic_cast<Ieee80211MacSignalMmConfirm *>(msg));
+        else if (dynamic_cast<Ieee80211MacSignalMmIndicate *>(msg))
+            handleMmIndicate(dynamic_cast<Ieee80211MacSignalMmIndicate *>(msg));
+    }
+}
+
+void Ieee80211MacDistributeMmpdusService::handleXport()
+{
+
+}
+
+void Ieee80211MacDistributeMmpdusService::handleSst(Ieee80211MacSignalSst* sst)
+{
+//    mAdr = sst->getAddr();
+//    mSst = sst->getStationState();
+//    emitStaState();
+}
+
+void Ieee80211MacDistributeMmpdusService::handleSend(Ieee80211MacSignalSend* send)
+{
+    mSpdu = send->getFrame();
+    mIm = send->getPriority();
+//    The selection criteria for Mmpdu Tx data rate are
+//    not specified. Frames to group addresses must
+//    use one of the basic rates. Requests should use one of
+//    the basic rates unless the operational rates of the
+//    recipient station are known. Responses must use a basic
+//    rate or the rate at which the request was received.
+    //emitMmRequest();
+}
+
+void Ieee80211MacDistributeMmpdusService::handleMmConfirm(Ieee80211MacSignalMmConfirm* mmConfirm)
+{
+    mSpdu = mmConfirm->getFrame();
+//    if (ftype(mSpdu) == beacon || ftype(mSpdu) == probe_rsp)
+//    {
+//        emitSent();
+//    }
+}
+
+void Ieee80211MacDistributeMmpdusService::handleMmIndicate(Ieee80211MacSignalMmIndicate* mmIndicate)
+{
+}
+
+void Ieee80211MacDistributeMmpdusService::reExport()
+{
+    //TODO: export intra-mac remote variables
+}
+
+void Ieee80211MacDistributeMmpdusService::emitStaState(MACAddress addr, StationState stationState)
+{
+}
+
+void Ieee80211MacDistributeMmpdusService::emitMmRequest(cPacket* mspdu, CfPriority mim, bps mRate)
+{
+}
+
+void Ieee80211MacDistributeMmpdusService::emitSent(cPacket* mspdu, TxStatus txStatus)
+{
+}
+
+void Ieee80211MacDistributeMmpdusService::emitCls2Err(MACAddress addr)
+{
+}
+
+void Ieee80211MacDistributeMmpdusService::emitCls3Err(MACAddress addr)
+{
+}
+
+void Ieee80211MacDistributeMmpdusService::chkSigtype()
+{
+    if (mSerr == StateErr::StateErr_class2)
+    {
+//        emitCls2Err();
+    }
+    else if (mSerr == StateErr::StateErr_class3)
+    {
+//        emitCls3Err();
+    }
+    else
+    {
+//        if (ftype(mRpdu) == cfend, cfendack)
+//        {
+//        }
+    }
+}
+
+} /* namespace inet */
+} /* namespace ieee80211 */
+
---
+++ src/inet/linklayer/ieee80211/thenewmac/mlme/services/Ieee80211MacDistributeMmpdusService.h
@@ -0,0 +1,95 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IEEE80211MACDISTRIBUTEMMPDUSSERVICE_H
+#define __INET_IEEE80211MACDISTRIBUTEMMPDUSSERVICE_H
+
+#include "inet/common/INETDefs.h"
+#include "inet/linklayer/ieee80211/thenewmac/signals/Ieee80211MacSignals_m.h"
+#include "inet/linklayer/ieee80211/thenewmac/macsorts/Ieee80211MacMacsorts.h"
+
+namespace inet {
+namespace ieee80211 {
+
+class INET_API IIeee80211MacDistributeMmpdusService
+{
+    protected:
+        virtual void handleXport() = 0;
+        virtual void handleSst(Ieee80211MacSignalSst *sst) = 0;
+        virtual void handleSend(Ieee80211MacSignalSend *send) = 0;
+        virtual void handleMmConfirm(Ieee80211MacSignalMmConfirm *mmConfirm) = 0;
+        virtual void handleMmIndicate(Ieee80211MacSignalMmIndicate *mmIndicate) = 0;
+
+        virtual void emitStaState(MACAddress addr, StationState stationState) = 0;
+        virtual void emitMmRequest(cPacket *mspdu, CfPriority mim, bps mRate) = 0;
+        virtual void emitSent(cPacket *mspdu, TxStatus txStatus) = 0;
+        virtual void emitCls2Err(MACAddress addr) = 0;
+        virtual void emitCls3Err(MACAddress addr) = 0;
+        virtual void emitCfend() = 0;
+        virtual void emitAsocReq(cPacket mrpdu) = 0;
+        virtual void emitAsocRsp(cPacket mrpdu) = 0;
+        virtual void emitReasocReq(cPacket mrpdu) = 0;
+        virtual void emitReasocRsp(cPacket mrpdu) = 0;
+};
+
+class INET_API Ieee80211MacDistributeMmpdusService : public IIeee80211MacDistributeMmpdusService, public cSimpleModule
+{
+    protected:
+        enum DistributeMmpdusState
+        {
+            DISTRIBUTE_MMPDUS_STATE_MMPDU_IDLE,
+        };
+
+    protected:
+        DistributeMmpdusState state = DISTRIBUTE_MMPDUS_STATE_MMPDU_IDLE;
+
+        MACAddress mAdr;
+        Imed mIm;
+        CfPriority pri;
+        bps mRate;
+        cPacket mRpdu;
+        cPacket mSpdu;
+        StateErr mSerr;
+        StationState mSst;
+        simtime_t mtE, mtT;
+        TxStatus mTxstat;
+
+    protected:
+        void handleMessage(cMessage *msg) override;
+        void initialize(int stage) override;
+
+    protected:
+        virtual void handleXport() override;
+        virtual void handleSst(Ieee80211MacSignalSst *sst) override;
+        virtual void handleSend(Ieee80211MacSignalSend *send) override;
+        virtual void handleMmConfirm(Ieee80211MacSignalMmConfirm *mmConfirm) override;
+        virtual void handleMmIndicate(Ieee80211MacSignalMmIndicate *mmIndicate) override;
+
+        virtual void emitStaState(MACAddress addr, StationState stationState);
+        virtual void emitMmRequest(cPacket *mspdu, CfPriority mim, bps mRate);
+        virtual void emitSent(cPacket *mspdu, TxStatus txStatus);
+        virtual void emitCls2Err(MACAddress addr);
+        virtual void emitCls3Err(MACAddress addr);
+
+        void reExport();
+        void chkSigtype();
+};
+
+} /* namespace inet */
+} /* namespace ieee80211 */
+
+#endif // ifndef __INET_IEEE80211MACDISTRIBUTEMMPDUSSERVICE_H
