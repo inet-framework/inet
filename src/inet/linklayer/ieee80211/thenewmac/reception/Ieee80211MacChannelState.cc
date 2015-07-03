8f2477bb197f11f0cb23d27a96eb9fa578c9b338
Benjamin Seregi <benjamin@omnetpp.org>
2015-07-03 12:34:51 +0200

    ChannelState

--- src/inet/linklayer/ieee80211/thenewmac/reception/Ieee80211MacChannelState.cc
+++ src/inet/linklayer/ieee80211/thenewmac/reception/Ieee80211MacChannelState.cc
@@ -28,22 +28,258 @@ void Ieee80211MacChannelState::initialize(int stage)
 {
     if (stage == INITSTAGE_LOCAL)
     {
+        macsorts = getModuleFromPar<Ieee80211MacMacsorts>(par("macsortsPackage"), this);
+        macmib = getModuleFromPar<Ieee80211MacMacmibPackage>(par("macmibPackage"), this);
+        dSifs = usecToSimtime(macmib->getPhyOperationTable()->getSifsTime());
+        dSlot = usecToSimtime(macmib->getPhyOperationTable()->getSlotTime());
+//        Eifs based on the lowest basic rate.
+//        dEifs =  TODO
     }
     else if (stage == INITSTAGE_LAST)
     {
-        emitChannelStateSignalToDataPump(CHANNEL_STATE_BUSY);
+
     }
 }
 
 void Ieee80211MacChannelState::handleMessage(cMessage* msg)
 {
+    if (msg->isSelfMessage())
+    {
+        if (msg == tNav)
+        {
+
+        }
+        else if (msg == tIfs)
+            handleTifs();
+        else if (msg == tSlot)
+        {
+
+        }
+        else
+            throw cRuntimeError("Unknown self message");
+    }
+    else
+    {
+        if (dynamic_cast<Ieee80211MacSignalPhyCcaIndication*>(msg->getControlInfo()))
+            handlePhyCcaIndication(dynamic_cast<Ieee80211MacSignalPhyCcaIndication*>(msg->getControlInfo()));
+    }
 }
 
-void Ieee80211MacChannelState::emitChannelStateSignalToDataPump(ChannelState state) const
+void Ieee80211MacChannelState::handleResetMac()
 {
-//    const cGate *gate = this->gate("toDataPump")->getPathEndGate();
-//    IIeee80211MacDataPump *dataPump = dynamic_cast<IIeee80211MacDataPump *>(gate->getOwnerModule());
-//    dataPump->handleFromCsSignal(CHANNEL_STATE_BUSY);
+    dIfs = dEifs;
+//    Assume channel busy until Phy
+//    indicates idle. tNavEnd is =0
+//    until first rx that sets Nav.
+    cs = CcaStatus_busy;
+    tNavEnd = SIMTIME_ZERO;
+    cancelEvent(tNav);
+    emitPhyCcarstRequest();
+    macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+    curSrc = NavSrc_nosrc;
+    emitBusy();
+    state = CHANNEL_STATE_STATE_CS_NO_NAV;
+}
+
+void Ieee80211MacChannelState::emitIdle()
+{
+}
+
+void Ieee80211MacChannelState::emitSlot()
+{
+}
+
+void Ieee80211MacChannelState::emitBusy()
+{
+}
+
+void Ieee80211MacChannelState::handleTifs()
+{
+    if (state == CHANNEL_STATE_STATE_WAIT_IFS)
+    {
+        emitIdle();
+        scheduleAt(simTime() + dSlot, tSlot);
+        state = CHANNEL_STATE_STATE_NO_CS_NO_NAV;
+    }
+}
+
+void Ieee80211MacChannelState::handlePhyCcaIndication(Ieee80211MacSignalPhyCcaIndication* phyCcaIndication)
+{
+    if (state == CHANNEL_STATE_STATE_WAIT_IFS)
+    {
+        cs = phyCcaIndication->getStatus();
+        if (cs == CcaStatus_busy)
+            state = CHANNEL_STATE_STATE_CS_NO_NAV;
+    }
+    else if (state == CHANNEL_STATE_STATE_CS_NO_NAV)
+    {
+        cs = phyCcaIndication->getStatus();
+        if (cs == CcaStatus_idle)
+        {
+            scheduleAt(simTime() + dIfs, tIfs);
+            state = CHANNEL_STATE_STATE_WAIT_IFS;
+        }
+    }
+    else if (state == CHANNEL_STATE_STATE_NO_CS_NO_NAV)
+    {
+        cs = phyCcaIndication->getStatus();
+        if (cs == CcaStatus_busy)
+        {
+            emitBusy();
+            state = CHANNEL_STATE_STATE_CS_NO_NAV;
+        }
+    }
+    else if (state == CHANNEL_STATE_STATE_NO_CS_NAV)
+    {
+        cs = phyCcaIndication->getStatus();
+        if (cs == CcaStatus_busy)
+            state = CHANNEL_STATE_STATE_CS_NAV;
+    }
+    else if (state == CHANNEL_STATE_STATE_CS_NAV)
+    {
+        if (cs == CcaStatus_idle)
+            state = CHANNEL_STATE_STATE_NO_CS_NAV;
+    }
+}
+
+void Ieee80211MacChannelState::handleSetNav(Ieee80211MacSignalSetNav *setNav)
+{
+    if (state == CHANNEL_STATE_STATE_CS_NO_NAV || state == CHANNEL_STATE_STATE_WAIT_IFS)
+    {
+        this->tRef = setNav->getTime();
+        this->dNav = setNav->getDuration();
+        this->curSrc = setNav->getNavSrc();
+        tNavEnd = tRef + dNav;
+        scheduleAt(tNavEnd, tNav);
+        macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+        state = CHANNEL_STATE_STATE_CS_NAV;
+    }
+    else if (state == CHANNEL_STATE_STATE_NO_CS_NO_NAV)
+    {
+        this->tRef = setNav->getTime();
+        this->dNav = setNav->getDuration();
+        this->curSrc = setNav->getNavSrc();
+        tNavEnd = tRef + dNav;
+        scheduleAt(tNavEnd, tNav);
+        emitBusy();
+        macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+        state = CHANNEL_STATE_STATE_NO_CS_NAV;
+    }
+    else if (state == CHANNEL_STATE_STATE_CS_NAV || state == CHANNEL_STATE_STATE_NO_CS_NAV)
+    {
+        this->tRef = setNav->getTime();
+        this->dNav = setNav->getDuration();
+        this->curSrc = setNav->getNavSrc();
+        tNew = tRef + dNav;
+        if (tNew > tNavEnd)
+        {
+            tNavEnd = tNew;
+            curSrc = newSrc;
+        }
+        scheduleAt(tNavEnd, tNav);
+        macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+    }
+}
+
+void Ieee80211MacChannelState::handleTslot()
+{
+    if (state == CHANNEL_STATE_STATE_NO_CS_NO_NAV)
+    {
+        emitSlot();
+        scheduleAt(simTime() + dSlot, tSlot);
+    }
+}
+
+void Ieee80211MacChannelState::handleTnav()
+{
+    if (state == CHANNEL_STATE_STATE_NO_CS_NAV)
+    {
+        emitPhyCcarstRequest();
+        curSrc = NavSrc_nosrc;
+        scheduleAt(simTime() + dIfs, tIfs);
+        state = CHANNEL_STATE_STATE_WAIT_IFS;
+    }
+    else if (state == CHANNEL_STATE_STATE_CS_NAV)
+    {
+        emitPhyCcarstRequest();
+        curSrc = NavSrc_nosrc;
+        scheduleAt(simTime() + dIfs, tIfs);
+        state = CHANNEL_STATE_STATE_CS_NO_NAV;
+    }
+}
+
+void Ieee80211MacChannelState::handleUseEifs(Ieee80211MacSignalUseEifs *useEifs)
+{
+    tRxEnd = useEifs->getTime();
+    dIfs = dEifs - dRxTx;
+    scheduleAt(tRxEnd + dIfs, tIfs);
+}
+
+void Ieee80211MacChannelState::handleUseDifs(Ieee80211MacSignalUseDifs *useDifs)
+{
+    tRxEnd = useDifs->getTime();
+    dIfs = dDifs - dRxTx;
+    scheduleAt(tRxEnd + dIfs, tIfs);
+}
+
+void Ieee80211MacChannelState::handleChangeNav(Ieee80211MacSignalChangeNav* changeNav)
+{
+    this->tRef = changeNav->getTime();
+    this->dNav = changeNav->getDuration();
+    this->curSrc = changeNav->getNavSrc();
+    if (newSrc == NavSrc_cswitch)
+    {
+        dIfs = dEifs - dRxTx;
+        scheduleAt(simTime(), tNav);
+        tNavEnd = SIMTIME_ZERO;
+        curSrc = NavSrc_nosrc;
+        macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+    }
+    else
+    {
+        tNew = tRef + dNav;
+//        Nav is cleared by setting Tnav
+//        to now. This causes immediate
+//        Tnav signal to enable exit from
+//        noCs_Nav or Cs_Nav state.
+        if (tNew > tNavEnd)
+        {
+            tNavEnd = tNew;
+            curSrc = newSrc;
+        }
+        scheduleAt(tNavEnd, tNav);
+        macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+    }
+}
+
+void Ieee80211MacChannelState::handleRtsTimeout()
+{
+    if (state == CHANNEL_STATE_STATE_CS_NAV || state == CHANNEL_STATE_STATE_NO_CS_NAV)
+    {
+        if (curSrc == NavSrc_rts)
+        {
+            tNavEnd = simTime();
+            curSrc = NavSrc_nosrc;
+            scheduleAt(tNavEnd, tNav);
+            macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+        }
+    }
+}
+
+void Ieee80211MacChannelState::handleClearNav(Ieee80211MacSignalClearNav *clearNav)
+{
+    if (state == CHANNEL_STATE_STATE_CS_NAV || state == CHANNEL_STATE_STATE_NO_CS_NAV)
+    {
+        newSrc = clearNav->getNavSrc();
+        tNavEnd = simTime();
+        curSrc = NavSrc_nosrc;
+        scheduleAt(tNavEnd, tNav);
+        macsorts->getIntraMacRemoteVariables()->setNavEnd(tNavEnd);
+    }
+}
+
+void Ieee80211MacChannelState::emitPhyCcarstRequest()
+{
 }
 
 } /* namespace ieee80211 */
--- src/inet/linklayer/ieee80211/thenewmac/reception/Ieee80211MacChannelState.h
+++ src/inet/linklayer/ieee80211/thenewmac/reception/Ieee80211MacChannelState.h
@@ -19,31 +19,104 @@
 #define __INET_IEEE80211MACCHANNELSTATE_H
 
 #include "inet/common/INETDefs.h"
+#include "inet/linklayer/ieee80211/thenewmac/signals/Ieee80211MacSignals_m.h"
 #include "inet/linklayer/ieee80211/thenewmac/reception/Ieee80211MacReception.h"
+#include "inet/linklayer/ieee80211/thenewmac/base/Ieee80211MacMacProcessBase.h"
+#include "inet/linklayer/ieee80211/thenewmac/macsorts/Ieee80211MacMacsorts.h"
+#include "inet/linklayer/ieee80211/thenewmac/macmib/Ieee80211MacMacmib.h"
+#include "inet/linklayer/ieee80211/thenewmac/macsorts/Ieee80211MacEnumeratedPhyTypes.h"
 
 namespace inet {
 namespace ieee80211 {
 
-class INET_API IIeee80211MacChannelState : public cSimpleModule
+class INET_API IIeee80211MacChannelState
 {
-    public:
-        enum ChannelState
-        {
-            CHANNEL_STATE_BUSY,
-            CHANNEL_STATE_IDLE,
-            CHANNEL_STATE_SLOT
-        };
+    protected:
+        virtual void handleResetMac() = 0;
+        virtual void handleTifs() = 0;
+        virtual void handleTslot() = 0;
+        virtual void handleTnav() = 0;
+        virtual void handleRtsTimeout() = 0;
+        virtual void handleClearNav(Ieee80211MacSignalClearNav *clearNav) = 0;
+        virtual void handleChangeNav(Ieee80211MacSignalChangeNav *changeNav) = 0;
+        virtual void handleUseEifs(Ieee80211MacSignalUseEifs *useEifs) = 0;
+        virtual void handleUseDifs(Ieee80211MacSignalUseDifs *useDifs) = 0;
+        virtual void handlePhyCcaIndication(Ieee80211MacSignalPhyCcaIndication *phyCcaIndication) = 0;
+        virtual void handleSetNav(Ieee80211MacSignalSetNav *setNav) = 0;
+
+        virtual void emitIdle() = 0;
+        virtual void emitSlot() = 0;
+        virtual void emitBusy() = 0;
+        virtual void emitPhyCcarstRequest() = 0;
 };
 
-class INET_API Ieee80211MacChannelState : public IIeee80211MacChannelState
+class INET_API Ieee80211MacChannelState : public IIeee80211MacChannelState, public Ieee80211MacMacProcessBase
 {
+    protected:
+        enum ChannelStateState
+        {
+            CHANNEL_STATE_STATE_START,
+            CHANNEL_STATE_STATE_BUSY,
+            CHANNEL_STATE_STATE_IDLE,
+            CHANNEL_STATE_STATE_SLOT,
+            CHANNEL_STATE_STATE_CS_NO_NAV,
+            CHANNEL_STATE_STATE_WAIT_IFS,
+            CHANNEL_STATE_STATE_CS_NAV,
+            CHANNEL_STATE_STATE_NO_CS_NO_NAV,
+            CHANNEL_STATE_STATE_NO_CS_NAV
+        };
+
+    protected:
+        ChannelStateState state = CHANNEL_STATE_STATE_START;
+        simtime_t tNavEnd = SIMTIME_ZERO;
+        cMessage *tIfs = nullptr;
+        cMessage *tNav = nullptr;
+        cMessage *tSlot = nullptr;
+
+        Ieee80211MacMacmibPackage *macmib = nullptr;
+        Ieee80211MacMacsorts *macsorts = nullptr;
+
+        CcaStatus cs;
+        int rxtx;
+        int slot;
+        int sifs;
+
+        simtime_t dDifs = SIMTIME_ZERO;
+        simtime_t dEifs = SIMTIME_ZERO;
+        simtime_t dIfs = SIMTIME_ZERO;
+        simtime_t dNav = SIMTIME_ZERO;
+        simtime_t dRxTx = SIMTIME_ZERO;
+        simtime_t dSifs = SIMTIME_ZERO;
+        simtime_t dSlot = SIMTIME_ZERO;
+
+        simtime_t tNew = SIMTIME_ZERO;
+        simtime_t tRef = SIMTIME_ZERO;
+        simtime_t tRxEnd = SIMTIME_ZERO;
+
+        NavSrc newSrc;
+        NavSrc curSrc;
+
     protected:
         void handleMessage(cMessage *msg) override;
         void initialize(int stage) override;
         virtual int numInitStages() const override { return NUM_INIT_STAGES; }
 
-        // Signal emitters
-        void emitChannelStateSignalToDataPump(ChannelState state) const;
+        virtual void handleTifs();
+        virtual void handlePhyCcaIndication(Ieee80211MacSignalPhyCcaIndication *phyCcaIndication);
+        virtual void handleSetNav(Ieee80211MacSignalSetNav *setNav);
+        virtual void handleTslot();
+        virtual void handleTnav();
+        virtual void handleUseEifs(Ieee80211MacSignalUseEifs *useEifs);
+        virtual void handleUseDifs(Ieee80211MacSignalUseDifs *useDifs);
+        virtual void handleChangeNav(Ieee80211MacSignalChangeNav *changeNav);
+        virtual void handleRtsTimeout();
+        virtual void handleClearNav(Ieee80211MacSignalClearNav *clearNav);
+
+        virtual void handleResetMac();
+        virtual void emitIdle();
+        virtual void emitSlot();
+        virtual void emitBusy();
+        virtual void emitPhyCcarstRequest();
 };
 
 } /* namespace ieee80211 */
