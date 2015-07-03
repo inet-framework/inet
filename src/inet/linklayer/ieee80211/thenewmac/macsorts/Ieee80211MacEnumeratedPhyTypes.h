42e1a30309094657acda17ff12427d15f1ca426c
Benjamin Seregi <benjamin@omnetpp.org>
2015-07-03 13:43:55 +0200

    EnumeratedPhyTypes

---
+++ src/inet/linklayer/ieee80211/thenewmac/macsorts/Ieee80211MacEnumeratedPhyTypes.h
@@ -0,0 +1,48 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IEEE80211MACENUMERATEDPHYTYPES_H
+#define __INET_IEEE80211MACENUMERATEDPHYTYPES_H
+
+#include "inet/common/INETDefs.h"
+
+namespace inet {
+namespace ieee80211 {
+
+/*
+ * Enumerated types used in PHY service primitives
+ */
+
+/* <state> parameter of PhyCca.indication */
+enum CcaStatus {
+    CcaStatus_busy,
+    CcaStatus_idle
+};
+
+/* <rxerror> parameter of PhyRxEnd.indication */
+enum PhyRxStat
+{
+   PhyRxStat_no_error,
+   PhyRxStat_fmt_violation,
+   PhyRxStat_carrier_lost,
+   PhyRxStat_unsupt_rate
+};
+
+} /* namespace inet */
+} /* namespace ieee80211 */
+
+#endif // ifndef __INET_IEEE80211MACENUMERATEDMACSTATYPES_H
