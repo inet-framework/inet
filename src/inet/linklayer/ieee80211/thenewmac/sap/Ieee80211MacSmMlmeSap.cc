67c4c03a03dcd7f3bab240116285910da9f06848
Benjamin Seregi <benjamin@omnetpp.org>
2015-07-15 11:55:23 +0200

    MlmeSap

---
+++ src/inet/linklayer/ieee80211/thenewmac/sap/Ieee80211MacSmMlmeSap.cc
@@ -0,0 +1,35 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "Ieee80211MacSmMlmeSap.h"
+
+namespace inet {
+namespace ieee80211 {
+
+Define_Module(Ieee80211MacSmMlmeSap);
+
+void Ieee80211MacSmMlmeSap::handleMessage(cMessage* msg)
+{
+}
+
+void Ieee80211MacSmMlmeSap::initialize(int stage)
+{
+}
+
+} /* namespace inet */
+} /* namespace ieee80211 */
+
