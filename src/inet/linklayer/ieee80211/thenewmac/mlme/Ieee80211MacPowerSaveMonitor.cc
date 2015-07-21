791352f0b7d905f0b9e103ee24f671cd7dc6bdc4
Benjamin Seregi <benjamin@omnetpp.org>
2015-07-21 12:15:43 +0200

    PowerSaveMonitor

--- src/inet/linklayer/ieee80211/thenewmac/mlme/Ieee80211MacPowerSaveMonitor.cc
+++ src/inet/linklayer/ieee80211/thenewmac/mlme/Ieee80211MacPowerSaveMonitor.cc
@@ -16,11 +16,194 @@
 //
 
 #include "Ieee80211MacPowerSaveMonitor.h"
+#include "inet/common/ModuleAccess.h"
 
 namespace inet {
 namespace ieee80211 {
 
 Define_Module(Ieee80211MacPowerSaveMonitor);
+
+void Ieee80211MacPowerSaveMonitor::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+    {
+        handleResetMac();
+        macsorts = getModuleFromPar<Ieee80211MacMacsorts>(par("macsortsPackage"), this);
+        macmib = getModuleFromPar<Ieee80211MacMacmibPackage>(par("macmibPackage"), this);
+    }
+}
+
+void Ieee80211MacPowerSaveMonitor::processSignal(cMessage *msg)
+{
+    if (dynamic_cast<Ieee80211MacSignalPsIndicate *>(msg->getControlInfo()))
+        handlePsIndicate(dynamic_cast<Ieee80211MacSignalPsIndicate *>(msg->getControlInfo()));
+    else if (dynamic_cast<Ieee80211MacSignalStaState *>(msg->getControlInfo()))
+        handleStaState(dynamic_cast<Ieee80211MacSignalStaState *>(msg->getControlInfo()));
+    else if (dynamic_cast<Ieee80211MacSignalPsInquiry *>(msg->getControlInfo()))
+        handlePsInquiry(dynamic_cast<Ieee80211MacSignalPsInquiry *>(msg->getControlInfo()));
+    else if (dynamic_cast<Ieee80211MacSignalSsInquiry *>(msg->getControlInfo()))
+        handleSsInquiry(dynamic_cast<Ieee80211MacSignalSsInquiry *>(msg->getControlInfo()));
+}
+
+void Ieee80211MacPowerSaveMonitor::handleResetMac()
+{
+    asoc.clear();
+    awake.clear();
+    asleep.clear();
+    state = POWER_SAVE_MONITOR_STATE_MONITOR_IDLE;
+}
+
+void Ieee80211MacPowerSaveMonitor::handlePsIndicate(Ieee80211MacSignalPsIndicate* psIndicate)
+{
+    if (state == POWER_SAVE_MONITOR_STATE_MONITOR_IDLE)
+    {
+        sta = psIndicate->getSta();
+        psm = psIndicate->getPsm();
+        if (psm == PsMode_sta_active)
+        {
+            awake.insert(sta);
+            // Send PsChange when sleeping
+            // station reports active mode.
+            if (asleep.count(sta) == 1)
+                emitPsChange(sta, PsMode_sta_active);
+            asleep.erase(sta);
+        }
+        else if (psm == PsMode_power_save)
+        {
+            awake.erase(sta);
+            asleep.insert(sta);
+        }
+    }
+}
+
+void Ieee80211MacPowerSaveMonitor::handleStaState(Ieee80211MacSignalStaState* staState)
+{
+    if (state == POWER_SAVE_MONITOR_STATE_MONITOR_IDLE)
+    {
+        sta = staState->getSta();
+        sst = staState->getSst();
+        if (sst == StationState_asoc)
+            asoc.insert(sta);
+        else if (sst == StationState_auth_open)
+        {
+            authOs.insert(sta);
+            authKey.erase(sta);
+        }
+        else if (sst == StationState_auth_key)
+        {
+            authKey.insert(sta);
+            authOs.erase(sta);
+        }
+        else if (sst == StationState_not_auth) // Note: de_auth is undefined in the standard
+        {
+            authOs.erase(sta);
+            authKey.erase(sta);
+            // Deauthenticate of associated
+            // station causes disassociate
+            // at same time.
+            if (asoc.count(sta) == 1)
+                asoc.erase(sta);
+        }
+        else if (sst == StationState_dis_asoc)
+            asoc.erase(sta);
+        else ;
+    }
+}
+
+void Ieee80211MacPowerSaveMonitor::handlePsInquiry(Ieee80211MacSignalPsInquiry* psInquiry)
+{
+    if (state == POWER_SAVE_MONITOR_STATE_MONITOR_IDLE)
+    {
+        sta = psInquiry->getMacAddress();
+        if (!sta.isMulticast())
+        {
+            if (awake.count(sta) == 1)
+            {
+                psm = PsMode_sta_active; // TODO: awake?
+            }
+            else if (asleep.count(sta) == 1)
+                psm = PsMode_power_save; // TODO: asleep??
+            else
+                psm = PsMode_unknown;
+        }
+        else
+            psm = PsMode_unknown;
+        emitPsResponse(sta, psm);
+    }
+}
+
+void Ieee80211MacPowerSaveMonitor::handleSsInquiry(Ieee80211MacSignalSsInquiry* ssInquiry)
+{
+    if (state == POWER_SAVE_MONITOR_STATE_MONITOR_IDLE)
+    {
+        sta = ssInquiry->getSta();
+        if (sta.isMulticast())
+            grpAddr();
+        else if (authOs.count(sta) == 1)
+        {
+            asst = StationState_auth_open;
+            function1();
+        }
+        else if (authKey.count(sta) == 1)
+        {
+            asst = StationState_auth_key;
+            function1();
+        }
+        else
+        {
+            asst = StationState_not_auth;
+            sst = asst;
+        }
+        emitSsResponse(sta, sst, asst);
+    }
+}
+
+void Ieee80211MacPowerSaveMonitor::grpAddr()
+{
+    sst = StationState_not_auth;
+    if (macsorts->getIntraMacRemoteVariables()->isAssoc())
+        sst = StationState_asoc;
+    else
+        sst = StationState_dis_asoc;
+    emitSsResponse(sta, sst, asst);
+}
+
+
+void Ieee80211MacPowerSaveMonitor::function1()
+{
+    if (asoc.count(sta) == 1)
+        sst = StationState_asoc;
+    else
+        sst = asst;
+}
+
+void Ieee80211MacPowerSaveMonitor::emitSsResponse(MACAddress sta, StationState sst, StationState asst)
+{
+    Ieee80211MacSignalSsResponse *signal = new Ieee80211MacSignalSsResponse();
+    signal->setSta(sta);
+    signal->setSst(sst);
+    signal->setAsst(asst);
+    cMessage *ssResponse = createSignal("ssResponse", signal);
+    send(ssResponse, "sst$o");
+}
+
+void Ieee80211MacPowerSaveMonitor::emitPsChange(MACAddress sta, PsMode psMode)
+{
+    Ieee80211MacSignalPsChange *signal = new Ieee80211MacSignalPsChange();
+    signal->setAddr(sta);
+    signal->setPsMode(psMode);
+    cMessage *psChange = createSignal("psChange", signal);
+    send(psChange, "psm$o");
+}
+
+void Ieee80211MacPowerSaveMonitor::emitPsResponse(MACAddress sta, PsMode psMode)
+{
+    Ieee80211MacSignalPsResponse *signal = new Ieee80211MacSignalPsResponse();
+    signal->setAdr(sta);
+    signal->setPsMode(psMode);
+    cMessage *psResponse = createSignal("psResponse", signal);
+    send(psResponse, "psm$o");
+}
 
 } /* namespace inet */
 } /* namespace ieee80211 */
--- src/inet/linklayer/ieee80211/thenewmac/mlme/Ieee80211MacPowerSaveMonitor.h
+++ src/inet/linklayer/ieee80211/thenewmac/mlme/Ieee80211MacPowerSaveMonitor.h
@@ -19,17 +19,76 @@
 #define __INET_IEEE80211MACPOWERSAVEMONITOR_H
 
 #include "inet/common/INETDefs.h"
+#include "inet/linklayer/ieee80211/thenewmac/signals/Ieee80211MacSignals_m.h"
+#include "inet/linklayer/ieee80211/thenewmac/macsorts/Ieee80211MacEnumeratedMacStaTypes.h"
+#include "inet/linklayer/ieee80211/thenewmac/macmib/Ieee80211MacMacmib.h"
+#include "inet/linklayer/ieee80211/thenewmac/base/Ieee80211MacMacProcessBase.h"
+#include <set>
 
 namespace inet {
 namespace ieee80211 {
 
-class INET_API IIeee80211MacPowerSaveMonitor: public cSimpleModule
+class INET_API IIeee80211MacPowerSaveMonitor
 {
+    protected:
+        virtual void handlePsIndicate(Ieee80211MacSignalPsIndicate *psIndicate) = 0;
+        virtual void handleStaState(Ieee80211MacSignalStaState *staState) = 0;
+        virtual void handlePsInquiry(Ieee80211MacSignalPsInquiry *psInquiry) = 0;
+        virtual void handleSsInquiry(Ieee80211MacSignalSsInquiry *ssInquiry) = 0;
+        virtual void handleResetMac() = 0;
+
+        virtual void emitPsChange(MACAddress sta, PsMode psMode) = 0;
+        virtual void emitPsResponse(MACAddress sta, PsMode psMode) = 0;
+        virtual void emitSsResponse(MACAddress sta, StationState sst, StationState asst) = 0;
 };
 
 
-class INET_API Ieee80211MacPowerSaveMonitor : public IIeee80211MacPowerSaveMonitor
+class INET_API Ieee80211MacPowerSaveMonitor : public IIeee80211MacPowerSaveMonitor, public Ieee80211MacMacProcessBase
 {
+    protected:
+        enum PowerSaveMonitorState
+        {
+            POWER_SAVE_MONITOR_STATE_START,
+            POWER_SAVE_MONITOR_STATE_MONITOR_IDLE
+        };
+    /* Each of these sets holds MAC addresses of
+    stations with a particular operational state.
+    Stations are added to and removed from sets
+    due to MLME requests, received management
+    frames, and bits in received MAC headers.
+    Sets are not aged, as there is no requirement
+    for periodic activity, but aging to expunge
+    addresses of inactive stations is permitted.
+    */
+    protected:
+        PowerSaveMonitorState state = POWER_SAVE_MONITOR_STATE_START;
+
+        std::set<MACAddress> awake, asleep, authOs, authKey, asoc;
+        PsMode psm;
+        bool psquery;
+        StationState sst, asst;
+        MACAddress sta;
+        Ieee80211MacMacsorts *macsorts = nullptr;
+        Ieee80211MacMacmibPackage *macmib = nullptr;
+
+    protected:
+        void handleMessage(cMessage *msg) override;
+        void initialize(int stage) override;
+        void processSignal(cMessage *msg);
+
+    protected:
+        void grpAddr();
+        void function1(); // TODO: rename
+
+        void handleResetMac() override;
+        virtual void handlePsIndicate(Ieee80211MacSignalPsIndicate *psIndicate) override;
+        virtual void handleStaState(Ieee80211MacSignalStaState *staState) override;
+        virtual void handlePsInquiry(Ieee80211MacSignalPsInquiry *psInquiry) override;
+        virtual void handleSsInquiry(Ieee80211MacSignalSsInquiry *ssInquiry) override;
+
+        virtual void emitPsChange(MACAddress sta, PsMode psMode) override;
+        virtual void emitPsResponse(MACAddress sta, PsMode psMode) override;
+        virtual void emitSsResponse(MACAddress sta, StationState sst, StationState asst) override;
 };
 
 } /* namespace inet */
