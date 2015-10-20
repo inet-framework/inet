afd8539cab5f4f3975bdec4c7ca8a5e7ec7b2baf
Andras Varga <andras@omnetpp.org>
2015-10-20 16:25:04 +0200

    Ieee80211NewMac: MacParameters: added getPhyRxStartDelay()

--- src/inet/linklayer/ieee80211/newmac/EdcaUpperMac.cc
+++ src/inet/linklayer/ieee80211/newmac/EdcaUpperMac.cc
@@ -103,6 +103,7 @@ void EdcaUpperMac::readParameters()
     params->setDefaultDataFrameMode(dataFrameMode);
     params->setShortRetryLimit(fallback(par("shortRetryLimit"), 7));
     params->setRtsThreshold(par("rtsThreshold"));
+    params->setPhyRxStartDelay(basicFrameMode->getPhyRxStartDelay());
 
     params->setEdcaEnabled(true);
     params->setSlotTime(fallback(par("slotTime"), dataFrameMode->getSlotTime()));
