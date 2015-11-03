70facff1485a57e3393864bfe1591899e3a302a4
Andras Varga <andras@omnetpp.org>
2015-11-03 12:51:45 +0100

    Ieee80211NewMac: fix: frames we send should set the NAV too (to prevent one AC from transmitting while another AC owns the channel)

--- src/inet/linklayer/ieee80211/newmac/BasicRx.h
+++ src/inet/linklayer/ieee80211/newmac/BasicRx.h
@@ -63,7 +63,7 @@ class INET_API BasicRx : public cSimpleModule, public IRx
         virtual void transmissionStateChanged(IRadio::TransmissionState transmissionState) override;
         virtual void receivedSignalPartChanged(IRadioSignal::SignalPart part) override;
         virtual void lowerFrameReceived(Ieee80211Frame *frame) override;
-
+        virtual void frameTransmitted(simtime_t durationField) override;
 };
 
 } // namespace ieee80211
