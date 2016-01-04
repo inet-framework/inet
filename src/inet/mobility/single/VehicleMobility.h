ae899f31ef85166b0e7d97137223cd8937f93f45
Levente Mészáros <levy@omnetpp.org>
2016-01-04 17:20:11 +0100

    Mobility: Added VehicleMobility that simulates turning at waypoints.

---
+++ src/inet/mobility/single/VehicleMobility.h
@@ -0,0 +1,61 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_VEHICLEMOBILITY_H
+#define __INET_VEHICLEMOBILITY_H
+
+#include "inet/mobility/base/MovingMobilityBase.h"
+
+namespace inet {
+
+class INET_API VehicleMobility : public MovingMobilityBase
+{
+  protected:
+    struct Waypoint
+    {
+        double x;
+        double y;
+        double timestamp;
+
+        Waypoint(double x, double y, double timestamp) : x(x), y(y), timestamp(timestamp) { }
+    };
+
+  protected:
+    // configuration
+    std::vector<Waypoint> waypoints;
+
+    double speed;
+    double heading;
+    double waypointProximity;
+    double angularSpeed;
+    int targetPointIndex;
+
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void setInitialPosition() override;
+    virtual void move() override;
+
+    virtual void readWaypointsFromFile(const char *fileName);
+
+  public:
+    virtual double getMaxSpeed() const override { return speed; }
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_VEHICLEMOBILITY_H
+
