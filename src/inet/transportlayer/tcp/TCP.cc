3281dd1f518396d8fe6c240c61399854485b3d40
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-02-23 19:32:36 +0100

    tcp: omnetpp 5.x compatibility fixes

--- src/inet/transportlayer/tcp/TCP.cc
+++ src/inet/transportlayer/tcp/TCP.cc
@@ -211,6 +211,17 @@ void TCP::segmentArrivalWhileClosed(TCPSegment *tcpseg, L3Address srcAddr, L3Add
 
 void TCP::updateDisplayString()
 {
+#if OMNETPP_VERSION < 0x0500
+    if (getEnvir()->isDisabled()) {
+#else
+    if (getEnvir()->isExpressMode()) {
+#endif
+        // in express mode, we don't bother to update the display
+        // (std::map's iteration is not very fast if map is large)
+        getDisplayString().setTagArg("t", 0, "");
+        return;
+    }
+
     //char buf[40];
     //sprintf(buf,"%d conns", tcpAppConnMap.size());
     //getDisplayString().setTagArg("t",0,buf);
