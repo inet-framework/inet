633393b1fc69898c58a3f9c725650269f5011805
fastriker <github@kai.kientopf.eu>
2016-02-27 00:53:16 +0100

    Complement the documentation of StaticEllipseMobility.

--- src/inet/mobility/static/StaticEllipseMobility.h
+++ src/inet/mobility/static/StaticEllipseMobility.h
@@ -25,6 +25,9 @@ namespace inet {
 /**
  * @brief Places all hosts on a ellipse based on an given area over constraintAreaMin and constraintAreaMax.
  *
+ * The construction is based on Steiner's generation of a conic.
+ *
+ * @attention The number of hosts must be a multiple of 4.
  * @attention constraintAreaMax.x and constraintAreaMax.y should't be infinity.
  * @note If constraintAreaMin.x or constraintAreaMin.y is infinity 0 is assumed.
  */
@@ -54,10 +57,10 @@ private:
     // Calculate the intersection point of two lines.
     virtual Point2D getIntersection(struct Line2D p, struct Line2D q);
 
-    //Calculate a ellipse which fits in the given area.
-    //Construction based on Steiner's generation of a conic.
+    // Calculate a ellipse which fits in the given area.
+    // The construction is based on Steiner's generation of a conic.
     // An area should be defined over the Parameters constraintAreaMax.x and constraintAreaMax.y unequal infinity.
-    //The number of hosts must be a multiple of 4.
+    // The number of hosts must be a multiple of 4.
     virtual void setAreaPosition(int numHosts);
 
 protected:
