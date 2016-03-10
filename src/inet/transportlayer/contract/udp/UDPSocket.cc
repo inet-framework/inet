14fbff0c5a8eff4f11a90fb1fd9b5189ccd1e088
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-03-10 12:45:40 +0100

    UDPSocket: EV_TRACE send

--- src/inet/transportlayer/contract/udp/UDPSocket.cc
+++ src/inet/transportlayer/contract/udp/UDPSocket.cc
@@ -41,6 +41,12 @@ void UDPSocket::sendToUDP(cMessage *msg)
     if (!gateToUdp)
         throw cRuntimeError("UDPSocket: setOutputGate() must be invoked before socket can be used");
 
+    cObject *ctrl = msg->getControlInfo();
+    EV_TRACE << "UDPSocket: Send (" << msg->getClassName() << ")" << msg->getFullName();
+    if (ctrl)
+        EV_TRACE << "  control info: (" << ctrl->getClassName() << ")" << ctrl->getFullName();
+    EV_TRACE << endl;
+
     check_and_cast<cSimpleModule *>(gateToUdp->getOwnerModule())->send(msg, gateToUdp);
 }
 
