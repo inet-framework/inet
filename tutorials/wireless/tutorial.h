780b0458c7049a259b61f6b027aac4c44cde1b3e
Gyorgy Szaszko <adam.szaszko@gmail.com>
2016-04-07 11:45:11 +0200

    Initial commit

---
+++ tutorials/wireless/tutorial.h
@@ -0,0 +1,30 @@
+/**
+@mainpage Wireless Tutorial for the INET framework
+
+In this tutorial we show you how build wireless simulations in the INET framework. This is an advanced tutorial, and it 
+assumes that you are familiar with creating and running simulations in @opp and INET. If you are yet not, check out 
+these tutorials to get started:
+
+- 	<a href="https://omnetpp.org/doc/omnetpp/tictoc-tutorial/" target="_blank">TicToc Tutorial for OMNeT++</a> - basic 
+	tutorial on how @opp works
+- 	<a href="../../../doc/walkthrough/tutorial.html" target="_blank">ARPTest guide</a> - the same for INET
+
+If you need more information at any time, feel free to refer to the @opp and INET documentation:
+
+- 	<a href="https://omnetpp.org/doc/omnetpp/manual/usman.html" target="_blank">@opp User Manual</a>
+- 	<a href="https://omnetpp.org/doc/inet/api-current/inet-manual-draft.pdf" target="_blank">INET Manual draft</a>
+- 	<a href="https://omnetpp.org/doc/inet/api-current/neddoc/index.html" target="_blank">INET Reference</a>
+
+
+@section contents Contents
+
+-	@ref step1
+
+NEXT: @ref step1
+*/
+------------------------------------------------------------------------------------------------------------------------
+/*
+
+@page step1 Step 1.
+asdasd
+**/
