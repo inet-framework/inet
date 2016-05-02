248869cafc043686a0513d698508a5431065b35c
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-05-02 12:42:52 +0200

    Ethernet: customized EtherPhyFrame: getDisplayString redirected to encapsulated packet

--- src/inet/linklayer/ethernet/EtherFrame.h
+++ src/inet/linklayer/ethernet/EtherFrame.h
@@ -5,6 +5,20 @@
 
 namespace inet {
 
+class EtherPhyFrame : public EtherPhyFrame_Base
+{
+  private:
+    void copy(const EtherPhyFrame& other) {}
+
+  public:
+    EtherPhyFrame(const char *name=nullptr, int kind=0) : EtherPhyFrame_Base(name,kind) {}
+    EtherPhyFrame(const EtherPhyFrame& other) : EtherPhyFrame_Base(other) {copy(other);}
+    EtherPhyFrame& operator=(const EtherPhyFrame& other) {if (this==&other) return *this; EtherPhyFrame_Base::operator=(other); copy(other); return *this;}
+    virtual EtherPhyFrame *dup() const override {return new EtherPhyFrame(*this);}
+    // ADD CODE HERE to redefine and implement pure virtual functions from EtherPhyFrame_Base
+    virtual const char *getDisplayString() const override { return hasEncapsulatedPacket() ? const_cast<EtherPhyFrame *>(this)->_getEncapMsg()->getDisplayString() : EtherPhyFrame_Base::getDisplayString(); }
+};
+
 } // namespace inet
 
 #endif // ifndef __INET_ETHERFRAME_H
