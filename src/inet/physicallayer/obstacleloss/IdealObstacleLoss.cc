e640ecd05ed8bd328000401e8b4f66fa8b460384
Levente Mészáros <levy@omnetpp.org>
2016-05-04 10:56:18 +0200

    Radio: Added a very simple IdealObstacleLoss, power loss is either 0 or 1.

---
+++ src/inet/physicallayer/obstacleloss/IdealObstacleLoss.cc
@@ -0,0 +1,81 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/common/geometry/base/ShapeBase.h"
+#include "inet/common/geometry/common/Rotation.h"
+#include "inet/common/geometry/object/LineSegment.h"
+#include "inet/physicallayer/obstacleloss/IdealObstacleLoss.h"
+
+namespace inet {
+
+namespace physicallayer {
+
+Define_Module(IdealObstacleLoss);
+
+IdealObstacleLoss::IdealObstacleLoss()
+{
+}
+
+void IdealObstacleLoss::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL) {
+        medium = check_and_cast<IRadioMedium *>(getParentModule());
+        environment = check_and_cast<IPhysicalEnvironment *>(getModuleByPath(par("environmentModule")));
+    }
+}
+
+std::ostream& IdealObstacleLoss::printToStream(std::ostream& stream, int level) const
+{
+    return stream << "IdealObstacleLoss";
+}
+
+bool IdealObstacleLoss::isObstacle(const IPhysicalObject *object, const Coord& transmissionPosition, const Coord& receptionPosition) const
+{
+    const ShapeBase *shape = object->getShape();
+    const Coord& position = object->getPosition();
+    const EulerAngles& orientation = object->getOrientation();
+    Rotation rotation(orientation);
+    const LineSegment lineSegment(rotation.rotateVectorCounterClockwise(transmissionPosition - position), rotation.rotateVectorCounterClockwise(receptionPosition - position));
+    Coord intersection1, intersection2, normal1, normal2;
+    bool hasIntersections = shape->computeIntersection(lineSegment, intersection1, intersection2, normal1, normal2);
+    return hasIntersections && intersection1 != intersection2;
+}
+
+double IdealObstacleLoss::computeObstacleLoss(Hz frequency, const Coord& transmissionPosition, const Coord& receptionPosition) const
+{
+    TotalObstacleLossComputation obstacleLossVisitor(this, transmissionPosition, receptionPosition);
+    environment->visitObjects(&obstacleLossVisitor, LineSegment(transmissionPosition, receptionPosition));
+    return obstacleLossVisitor.isObstacleFound() ? 0 : 1;
+}
+
+IdealObstacleLoss::TotalObstacleLossComputation::TotalObstacleLossComputation(const IdealObstacleLoss *obstacleLoss, const Coord& transmissionPosition, const Coord& receptionPosition) :
+    obstacleLoss(obstacleLoss),
+    transmissionPosition(transmissionPosition),
+    receptionPosition(receptionPosition)
+{
+}
+
+void IdealObstacleLoss::TotalObstacleLossComputation::visit(const cObject *object) const
+{
+    if (!isObstacleFound_)
+        isObstacleFound_ = obstacleLoss->isObstacle(check_and_cast<const IPhysicalObject *>(object), transmissionPosition, receptionPosition);
+}
+
+} // namespace physicallayer
+
+} // namespace inet
+
---
+++ src/inet/physicallayer/obstacleloss/IdealObstacleLoss.h
@@ -0,0 +1,76 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IDEALOBSTACLELOSS_H
+#define __INET_IDEALOBSTACLELOSS_H
+
+#include "inet/common/IVisitor.h"
+#include "inet/common/figures/TrailFigure.h"
+#include "inet/environment/contract/IPhysicalEnvironment.h"
+#include "inet/physicallayer/contract/packetlevel/IRadioMedium.h"
+#include "inet/physicallayer/contract/packetlevel/IObstacleLoss.h"
+
+namespace inet {
+
+namespace physicallayer {
+
+class INET_API IdealObstacleLoss : public cModule, public IObstacleLoss
+{
+  protected:
+    class TotalObstacleLossComputation : public IVisitor
+    {
+      protected:
+        const IdealObstacleLoss *obstacleLoss = nullptr;
+        const Coord transmissionPosition;
+        const Coord receptionPosition;
+        mutable bool isObstacleFound_ = false;
+
+      public:
+        TotalObstacleLossComputation(const IdealObstacleLoss *obstacleLoss, const Coord& transmissionPosition, const Coord& receptionPosition);
+        void visit(const cObject *object) const override;
+        bool isObstacleFound() const { return isObstacleFound_; }
+    };
+
+    /** @name Parameters */
+    //@{
+    /**
+     * The radio medium where the radio signal propagation takes place.
+     */
+    IRadioMedium *medium = nullptr;
+    /**
+     * The physical environment that provides to obstacles.
+     */
+    IPhysicalEnvironment *environment = nullptr;
+    //@}
+
+  protected:
+    virtual void initialize(int stage) override;
+
+    virtual bool isObstacle(const IPhysicalObject *object, const Coord& transmissionPosition, const Coord& receptionPosition) const;
+
+  public:
+    IdealObstacleLoss();
+    virtual std::ostream& printToStream(std::ostream& stream, int level) const override;
+    virtual double computeObstacleLoss(Hz frequency, const Coord& transmissionPosition, const Coord& receptionPosition) const override;
+};
+
+} // namespace physicallayer
+
+} // namespace inet
+
+#endif // ifndef __INET_IDEALOBSTACLELOSS_H
+
