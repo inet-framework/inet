50492ff30412656945303888253893c12b05620f
Andras Varga <andras@omnetpp.org>
2016-05-05 20:32:24 +0200

    step 11

--- tutorials/wireless/tutorial.h
+++ tutorials/wireless/tutorial.h
@@ -846,44 +846,60 @@ Sources: omnetpp.ini, WirelessC.ned, walls.xml
 @section s11goals Goals
 
 After so many steps, we let go of the ideal radio model, and introduce a
-more realistic one. Our new radio will use APSK modulation, but still
-without other techniques like forward error correction, interleaving or
-spreading. We also want our model of the radio channel to model attenuation
-and obstacle loss.
+more realistic one. Our new radio will use an APSK modulation scheme, but
+still without other techniques like forward error correction, interleaving
+or spreading. We also want our model of the radio channel to simulate
+attenuation and obstacle loss.
 
 @section s11model The model
 
-We will have to replace `IdealRadio` with APSKScalarRadio.
+<b>Switching to APSK radio</b>
 
-So let's switch `IdealRadioMedium` with `APSKScalarRadioMedium`:
+In this step, we replace `IdealRadio` with `APSKScalarRadio`.
+`APSKScalarRadio` models a radio with an APSK (amplitude and phase-shift
+keying) modulation scheme. By default it uses BPSK, but QPSK, QAM-16,
+QAM-64, QAM-256 and several other modulations can also be configured.
+(Modulation is a parameter of the radio's transmitter component.)
 
-@dontinclude omnetpp.ini
-@skipline *.mediumType = "APSKScalarRadioMedium"
+Since we are moving away from the "unit disc radio" type of abstraction, we
+need to specify the carrier frequency, signal bandwidth and transmission
+power of the radios. Together with other parameters, they will allow the
+radio channel and the receiver models to compute path loss, SNIR, bit error
+rate and other values, and ultimately determine the success of reception.
 
-Set up some background noise:
+`APSKScalarRadio` also adds realism in that it simulates that the data are
+preceded by a preamble and a physical layer header. Their lengths are also
+parameters (and may be set to zero when not needed.)
 
-@dontinclude omnetpp.ini
-@skipline *.radioMedium.backgroundNoise.power = -110dBm
+Physical parameters of the receiver are important, too. We configure the
+following receiver parameters:
+- sensitivity [dBm]: if the signal power is below this threshold, reception
+  is not possible  (i.e. the receiver cannot go from the *channel busy*
+  state to *receiving*)
+- energy detection threshold [dBm]: if reception power is below this
+  threshold, no signal is detected and the channel is sensed to be empty
+  (this is significant for the "carrier sense" part of CSMA)
+- SNIR threshold [dB]: reception is not successful if the SNIR is below
+  this threshold
 
-`APSKRadioMedium` uses `IsotropicScalarBackgroundNoise` by
-default. This is basically white noise that is constant in space, time and
-frequency.<!white noise already means every frequency>
+The concrete values in the inifile were chosen to approximately
+reproduce the communication and interference ranges used in the
+previous steps.
 
-<!frequency 2 ghz>
+<b>Setting up the wireless channel</b>
 
-Set up `APSKScalarRadio`'s in the nodes and configure each radio:
+Since we switched the radio to `APSKScalarRadio`, we also need to change
+the medium to `APSKScalarRadioMedium`. In general, one always needs to use
+a medium that is compatible with the given radio. (With `IdealRadio`, we
+also used `IdealRadioMedium`.)
 
-@dontinclude omnetpp.ini
-@skip *.host*.wlan[*].radioType = "APSKScalarRadio"
-@until *.host*.wlan[*].radio.receiver.snirThreshold = 4dB
+`APSKScalarRadioMedium` has "slots" to plug in various propagation models,
+path loss models, analog models and background noise models. Here we make
+use of the fact that the default background noise model is homogeous
+isotropic white noise, and set up the noise level to a nonzero value
+(-110dBm).
 
-@note Each `radioMedium` model has to be used with the corresponding
-radio model -- this case `APSKScalarRadioMedium` with
-`APSKScalarRadio`. The same was true with `IdealRadio`.
-<!do we need this - is it correct - do we need this here and not at idealradio -
-last 3 lines - preambleduration?>
-
-<!img>
+Configuration:
 
 @dontinclude omnetpp.ini
 @skipline [Config Wireless11]
