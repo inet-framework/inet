de2b95b8ccc69a2625e06db9e552efb0242ae7ce
Levente Mészáros <levy@omnetpp.org>
2016-05-09 14:47:54 +0200

    Environment: Added IGround interface, and a very simple FlatGround.

--- src/inet/environment/common/PhysicalEnvironment.h
+++ src/inet/environment/common/PhysicalEnvironment.h
@@ -49,6 +49,12 @@ class INET_API PhysicalEnvironment : public cModule, public IPhysicalEnvironment
     Coord spaceMax;
     //@}
 
+    /** @name Submodules */
+    //@{
+    IObjectCache *objectCache = nullptr;
+    IGround *ground = nullptr;
+    //@}
+
     /** @name Internal state */
     //@{
     std::vector<const ShapeBase *> shapes;
@@ -58,7 +64,6 @@ class INET_API PhysicalEnvironment : public cModule, public IPhysicalEnvironment
 
     /** @name Cache */
     //@{
-    IObjectCache *objectCache;
     std::map<int, const ShapeBase *> idToShapeMap;  // shared shapes
     std::map<int, const Material *> idToMaterialMap;
     std::map<int, const PhysicalObject *> idToObjectMap;
@@ -76,6 +81,9 @@ class INET_API PhysicalEnvironment : public cModule, public IPhysicalEnvironment
   public:
     PhysicalEnvironment();
     virtual ~PhysicalEnvironment();
+
+    virtual IObjectCache *getObjectCache() const override { return objectCache; }
+    virtual IGround *getGround() const override { return ground; }
 
     virtual K getTemperature() const { return temperature; }
     virtual const Coord& getSpaceMin() const override { return spaceMin; }
---
+++ src/inet/environment/ground/FlatGround.cc
@@ -0,0 +1,40 @@
+//
+// Copyright (C) 2014 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/environment/ground/FlatGround.h"
+
+namespace inet {
+
+namespace physicalenvironment {
+
+Define_Module(FlatGround);
+
+void FlatGround::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+        elevation = par("elevation");
+}
+
+double FlatGround::getElevation(const Coord &position) const
+{
+    return elevation;
+}
+
+} // namespace physicalenvironment
+
+} // namespace inet
+
