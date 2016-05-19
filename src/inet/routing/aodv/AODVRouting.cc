59e281584b5b9f97349f0e1bd50846588c8b3dd4
Levente Mészáros <levy@omnetpp.org>
2016-05-19 12:15:42 +0200

    AODV: Removed AODV-RERR messages for inactive routes.

--- src/inet/routing/aodv/AODVRouting.cc
+++ src/inet/routing/aodv/AODVRouting.cc
@@ -1075,10 +1075,13 @@ void AODVRouting::handleLinkBreakSendRERR(const L3Address& unreachableAddr)
 
     std::vector<UnreachableNode> unreachableNodes;
     AODVRouteData *unreachableRouteData = check_and_cast<AODVRouteData *>(unreachableRoute->getProtocolData());
-    UnreachableNode node;
-    node.addr = unreachableAddr;
-    node.seqNum = unreachableRouteData->getDestSeqNum();
-    unreachableNodes.push_back(node);
+
+    if (unreachableRouteData->isActive()) {
+        UnreachableNode node;
+        node.addr = unreachableAddr;
+        node.seqNum = unreachableRouteData->getDestSeqNum();
+        unreachableNodes.push_back(node);
+    }
 
     // For case (i), the node first makes a list of unreachable destinations
     // consisting of the unreachable neighbor and any additional destinations
@@ -1088,9 +1091,8 @@ void AODVRouting::handleLinkBreakSendRERR(const L3Address& unreachableAddr)
     for (int i = 0; i < routingTable->getNumRoutes(); i++) {
         IRoute *route = routingTable->getRoute(i);
 
-        if (route->getNextHopAsGeneric() == unreachableAddr) {
-            AODVRouteData *routeData = check_and_cast<AODVRouteData *>(route->getProtocolData());
-
+        AODVRouteData *routeData = dynamic_cast<AODVRouteData *>(route->getProtocolData());
+        if (routeData && routeData->isActive() && route->getNextHopAsGeneric() == unreachableAddr) {
             if (routeData->hasValidDestNum())
                 routeData->setDestSeqNum(routeData->getDestSeqNum() + 1);
 
@@ -1120,6 +1122,9 @@ void AODVRouting::handleLinkBreakSendRERR(const L3Address& unreachableAddr)
         EV_WARN << "A node should not generate more than RERR_RATELIMIT RERR messages per second. Canceling sending RERR" << endl;
         return;
     }
+
+    if (unreachableNodes.empty())
+        return;
 
     AODVRERR *rerr = createRERR(unreachableNodes);
     rerrCount++;
@@ -1170,7 +1175,7 @@ void AODVRouting::handleRERR(AODVRERR *rerr, const L3Address& sourceAddr)
         // for which there exists a corresponding entry in the local routing
         // table that has the transmitter of the received RERR as the next hop.
 
-        if (route->getNextHopAsGeneric() == sourceAddr) {
+        if (routeData->isActive() && route->getNextHopAsGeneric() == sourceAddr) {
             for (unsigned int j = 0; j < unreachableArraySize; j++) {
                 if (route->getDestinationAsGeneric() == rerr->getUnreachableNodes(j).addr) {
                     // 1. The destination sequence number of this routing entry, if it
