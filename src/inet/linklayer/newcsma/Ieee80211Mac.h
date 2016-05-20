d2aec932c2bda08f200d30c00f2855044513ee1e
Levente Mészáros <levy@omnetpp.org>
2016-05-20 14:11:13 +0200

    CSMA: Dropped separate consts file.

--- src/inet/linklayer/newcsma/Ieee80211Mac.h
+++ src/inet/linklayer/newcsma/Ieee80211Mac.h
@@ -25,12 +25,39 @@
 #include "WirelessMacBase.h"
 #include "inet/common/queue/IPassiveQueue.h"
 #include "Ieee80211Frame_m.h"
-#include "Ieee80211Consts.h"
 #include "inet/common/FSMA.h"
 
 namespace inet {
 
 namespace newcsma {
+
+// XXX these are taken over from Consts80211.h
+// the phy has recognized a bit error in the packet
+#define BITERROR -1
+// packet lost due to collision
+#define COLLISION 9
+
+// frame lengths in bits
+// XXX this is duplicate, it's already in Ieee80211Frame.msg
+const unsigned int LENGTH_ACK = 112;
+
+// time slot ST, short interframe space SIFS, distributed interframe
+// space DIFS, and extended interframe space EIFS
+
+const_simtime_t ST = 20E-6;
+const_simtime_t SIFS = 10E-6;
+const_simtime_t DIFS = 2 * ST + SIFS;
+const_simtime_t MAX_PROPAGATION_DELAY = 2E-6;  // 300 meters at the speed of light
+
+/** Minimum size (initial size) of contention window */
+const int CW_MIN = 31;
+
+/** Maximum size of contention window */
+const int CW_MAX = 1023;
+
+const int PHY_HEADER_LENGTH = 192;
+const double BITRATE_HEADER = 1E+6;
+const double BANDWIDTH = 2E+6;
 
 /**
  * IEEE 802.11b Media Access Control Layer.
