c8917a99eb5bf7abc6439ec5806a68f2c68981a9
Levente Mészáros <levy@omnetpp.org>
2016-06-02 12:11:51 +0200

    Network: Added a new raw socket called L3Socket.

---
+++ src/inet/networklayer/contract/L3Socket.cc
@@ -0,0 +1,75 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/networklayer/contract/L3Socket.h"
+#include "inet/networklayer/contract/L3SocketCommand_m.h"
+
+namespace inet {
+
+L3Socket::L3Socket(int controlInfoProtocolId, cGate *outputGate) :
+    controlInfoProtocolId(controlInfoProtocolId),
+    socketId(getEnvir()->getUniqueNumber()),
+    outputGate(outputGate)
+{
+}
+
+void L3Socket::setControlInfoProtocolId(int _controlInfoProtocolId)
+{
+    ASSERT(!bound);
+    controlInfoProtocolId = _controlInfoProtocolId;
+}
+
+void L3Socket::sendToOutput(cMessage *message)
+{
+    if (!outputGate)
+        throw cRuntimeError("L3Socket: setOutputGate() must be invoked before the socket can be used");
+    check_and_cast<cSimpleModule *>(outputGate->getOwnerModule())->send(message, outputGate);
+}
+
+void L3Socket::bind(int protocolId)
+{
+    ASSERT(!bound);
+    ASSERT(controlInfoProtocolId != -1);
+    L3SocketBindCommand *command = new L3SocketBindCommand();
+    command->setControlInfoProtocolId(controlInfoProtocolId);
+    command->setSocketId(socketId);
+    command->setProtoclId(protocolId);
+    cMessage *bind = new cMessage("bind");
+    bind->setControlInfo(command);
+    sendToOutput(bind);
+    bound = true;
+}
+
+void L3Socket::send(cPacket *msg)
+{
+    sendToOutput(msg);
+}
+
+void L3Socket::close()
+{
+    ASSERT(bound);
+    ASSERT(controlInfoProtocolId != -1);
+    L3SocketCloseCommand *command = new L3SocketCloseCommand();
+    command->setControlInfoProtocolId(controlInfoProtocolId);
+    command->setSocketId(socketId);
+    cMessage *close = new cMessage("close");
+    close->setControlInfo(command);
+    sendToOutput(close);
+}
+
+} // namespace inet
+
---
+++ src/inet/networklayer/contract/L3Socket.h
@@ -0,0 +1,81 @@
+//
+// Copyright (C) 2015 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_L3SOCKET_H
+#define __INET_L3SOCKET_H
+
+#include "inet/common/INETDefs.h"
+
+namespace inet {
+
+/**
+ * Thie class implements a raw L3 socket.
+ */
+class INET_API L3Socket
+{
+  protected:
+    bool bound = false;
+    int controlInfoProtocolId = -1;
+    int socketId = -1;
+    cGate *outputGate = nullptr;
+
+  protected:
+    void sendToOutput(cMessage *message);
+
+  public:
+    L3Socket(int controlInfoProtocolId = -1, cGate *outputGate = nullptr);
+    virtual ~L3Socket() {}
+
+    /**
+     * Sets controlInfoProtocolId, for example setControlInfoProtocolId(Protocol::ipv4.getId())
+     */
+    void setControlInfoProtocolId(int controlInfoProtocolId);
+
+    int getControlInfoProtocolId() const { return controlInfoProtocolId;}
+
+    /**
+     * Returns the internal socket Id.
+     */
+    int getSocketId() const { return socketId; }
+
+    /**
+     * Sets the gate on which to send raw packets. Must be invoked before socket
+     * can be used. Example: <tt>socket.setOutputGate(gate("ipOut"));</tt>
+     */
+    void setOutputGate(cGate *outputGate) { this->outputGate = outputGate; }
+
+    /**
+     * Bind the socket to a protocol.
+     */
+    void bind(int protocolId);
+
+    /**
+     * Sends a data packet.
+     */
+    void send(cPacket *msg);
+
+    /**
+     * Unbinds the socket. Once closed, a closed socket may be bound to another
+     * (or the same) protocol, and reused.
+     */
+    void close();
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_L3SOCKET_H
+
