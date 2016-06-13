d6cd063e1839f23cf918a21eb54acab4c2a61481
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-06-13 11:46:52 +0200

    pingapp kludge

--- src/inet/applications/pingapp/PingApp.cc
+++ src/inet/applications/pingapp/PingApp.cc
@@ -61,7 +61,8 @@ simsignal_t PingApp::pingRxSeqSignal = registerSignal("pingRxSeq");
 enum PingSelfKinds {
     PING_FIRST_ADDR = 1001,
     PING_CHANGE_ADDR,
-    PING_SEND
+    PING_SEND,
+    PING_DO_SEND = 8888,
 };
 
 PingApp::PingApp()
@@ -151,6 +152,11 @@ void PingApp::handleMessage(cMessage *msg)
         }
     }
     if (msg->isSelfMessage()) {
+        if (msg->getKind() == PING_DO_SEND) {   //KLUDGE
+            l3Socket->send(dynamic_cast<cPacket*>(msg->removeControlInfo()));
+            delete msg;
+            return;
+        }
         if (msg->getKind() == PING_FIRST_ADDR) {
             srcAddr = L3AddressResolver().resolve(par("srcAddr"));
             parseDestAddressesPar();
@@ -409,7 +415,11 @@ void PingApp::sendPingRequest()
     }
 
     EV_INFO << "Sending ping request #" << msg->getSeqNo() << " to lower layer.\n";
-    l3Socket->send(outPacket);
+    //KLUDGE:
+    cMessage *kludgeMsg = new cMessage("do send", PING_DO_SEND);
+    kludgeMsg->setControlInfo(outPacket);
+    scheduleAt(simTime(), kludgeMsg);
+    // l3Socket->send(outPacket);
 }
 
 void PingApp::processPingResponse(PingPayload *msg)
