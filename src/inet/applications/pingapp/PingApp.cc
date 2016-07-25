43f55e0143e9c5a0e49d7d3678c936ba3b66e28d
Andras Varga <andras@omnetpp.org>
2016-07-25 11:37:28 +0200

    PingApp: fix inconsistency between @signal and @statistic

--- src/inet/applications/pingapp/PingApp.cc
+++ src/inet/applications/pingapp/PingApp.cc
@@ -54,7 +54,7 @@ Define_Module(PingApp);
 
 simsignal_t PingApp::rttSignal = registerSignal("rtt");
 simsignal_t PingApp::numLostSignal = registerSignal("numLost");
-simsignal_t PingApp::outOfOrderArrivalsSignal = registerSignal("outOfOrderArrivals");
+simsignal_t PingApp::numOutOfOrderArrivalsSignal = registerSignal("numOutOfOrderArrivals");
 simsignal_t PingApp::pingTxSeqSignal = registerSignal("pingTxSeq");
 simsignal_t PingApp::pingRxSeqSignal = registerSignal("pingRxSeq");
 
@@ -480,7 +480,7 @@ void PingApp::countPingResponse(int bytes, long seqNo, simtime_t rtt)
         EV_DETAIL << "Arrived out of order (too late)\n";
         outOfOrderArrivalCount++;
         lossCount--;
-        emit(outOfOrderArrivalsSignal, outOfOrderArrivalCount);
+        emit(numOutOfOrderArrivalsSignal, outOfOrderArrivalCount);
         emit(numLostSignal, lossCount);
     }
 }
