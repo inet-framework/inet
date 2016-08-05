431dd9d3ede763746451a0f62984000f7143a9a0
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-08-05 15:30:12 +0200

    tags: added IPv6ExtHeaderTag

---
+++ src/inet/networklayer/contract/ipv6/IPv6ExtHeaderTagBase.cc
@@ -0,0 +1,113 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/networklayer/contract/ipv6/IPv6ExtHeaderTagBase.h"
+
+#ifdef WITH_IPv6
+#include "inet/networklayer/ipv6/IPv6Datagram.h"
+#endif // ifdef WITH_IPv6
+
+namespace inet {
+
+void IPv6ExtHeaderTagBase::copy(const IPv6ExtHeaderTagBase& other)
+{
+#ifdef WITH_IPv6
+    for (const auto & elem : other.extensionHeaders)
+        extensionHeaders.push_back((elem)->dup());
+#endif // ifdef WITH_IPv6
+}
+
+IPv6ExtHeaderTagBase& IPv6ExtHeaderTagBase::operator=(const IPv6ExtHeaderTagBase& other)
+{
+    if (this == &other)
+        return *this;
+    clean();
+    IPv6ExtHeaderTagBase_Base::operator=(other);
+    copy(other);
+    return *this;
+}
+
+void IPv6ExtHeaderTagBase::clean()
+{
+#ifdef WITH_IPv6
+    while (!extensionHeaders.empty()) {
+        IPv6ExtensionHeader *eh = extensionHeaders.back();
+        extensionHeaders.pop_back();
+        delete eh;
+    }
+#endif // ifdef WITH_IPv6
+}
+
+IPv6ExtHeaderTagBase::~IPv6ExtHeaderTagBase()
+{
+    clean();
+}
+
+unsigned int IPv6ExtHeaderTagBase::getExtensionHeaderArraySize() const
+{
+    return extensionHeaders.size();
+}
+
+void IPv6ExtHeaderTagBase::setExtensionHeaderArraySize(unsigned int size)
+{
+    throw cRuntimeError(this, "setExtensionHeaderArraySize() not supported, use addExtensionHeader()");
+}
+
+IPv6ExtensionHeaderPtr& IPv6ExtHeaderTagBase::getExtensionHeader(unsigned int k)
+{
+    ASSERT(k < extensionHeaders.size());
+    return extensionHeaders[k];
+}
+
+void IPv6ExtHeaderTagBase::setExtensionHeader(unsigned int k, const IPv6ExtensionHeaderPtr& extensionHeader_var)
+{
+    throw cRuntimeError(this, "setExtensionHeader() not supported, use addExtensionHeader()");
+}
+
+void IPv6ExtHeaderTagBase::addExtensionHeader(IPv6ExtensionHeader *eh, int atPos)
+{
+#ifdef WITH_IPv6
+    ASSERT(eh);
+    if (atPos < 0 || (ExtensionHeaders::size_type)atPos >= extensionHeaders.size()) {
+        extensionHeaders.push_back(eh);
+        return;
+    }
+
+    // insert at position atPos, shift up the rest of the array
+    extensionHeaders.insert(extensionHeaders.begin() + atPos, eh);
+#else // ifdef WITH_IPv6
+    throw cRuntimeError(this, "INET was compiled without IPv6 support");
+#endif // ifdef WITH_IPv6
+}
+
+IPv6ExtensionHeader *IPv6ExtHeaderTagBase::removeFirstExtensionHeader()
+{
+    if (extensionHeaders.empty())
+        return nullptr;
+
+#ifdef WITH_IPv6
+    auto first = extensionHeaders.begin();
+    IPv6ExtensionHeader *ret = *first;
+    extensionHeaders.erase(first);
+    return ret;
+#else // ifdef WITH_IPv6
+    throw cRuntimeError(this, "INET was compiled without IPv6 support");
+#endif // ifdef WITH_IPv6
+}
+
+} // namespace inet
+
---
+++ src/inet/networklayer/contract/ipv6/IPv6ExtHeaderTagBase.h
@@ -0,0 +1,82 @@
+//
+// Copyright (C) 2005 Andras Varga
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_IPV6EXTHEADERTAGBASE_H
+#define __INET_IPV6EXTHEADERTAGBASE_H
+
+#include "inet/networklayer/contract/ipv6/IPv6ExtHeaderTagBase_m.h"
+#include "inet/linklayer/common/Ieee802Ctrl.h"
+
+namespace inet {
+
+class IPv6Datagram;
+class IPv6ExtensionHeader;
+
+/**
+ *
+ * See the IPv6ExtHeaderTagBase.msg file for more info.
+ */
+class INET_API IPv6ExtHeaderTagBase : public IPv6ExtHeaderTagBase_Base
+{
+  protected:
+    typedef std::vector<IPv6ExtensionHeader *> ExtensionHeaders;
+    ExtensionHeaders extensionHeaders;
+
+  private:
+    void copy(const IPv6ExtHeaderTagBase& other);
+    void clean();
+
+  public:
+    IPv6ExtHeaderTagBase() : IPv6ExtHeaderTagBase_Base() { }
+    virtual ~IPv6ExtHeaderTagBase();
+    IPv6ExtHeaderTagBase(const IPv6ExtHeaderTagBase& other) : IPv6ExtHeaderTagBase_Base(other) { copy(other); }
+    IPv6ExtHeaderTagBase& operator=(const IPv6ExtHeaderTagBase& other);
+    virtual IPv6ExtHeaderTagBase *dup() const override { return new IPv6ExtHeaderTagBase(*this); }
+
+    /**
+     * Returns the number of extension headers in this datagram
+     */
+    virtual unsigned int getExtensionHeaderArraySize() const override;
+
+    /** Generated but unused method, should not be called. */
+    virtual void setExtensionHeaderArraySize(unsigned int size) override;
+
+    /**
+     * Returns the kth extension header in this datagram
+     */
+    virtual IPv6ExtensionHeaderPtr& getExtensionHeader(unsigned int k) override;
+
+    /** Generated but unused method, should not be called. */
+    virtual void setExtensionHeader(unsigned int k, const IPv6ExtensionHeaderPtr& extensionHeader_var) override;
+
+    /**
+     * Adds an extension header to the datagram, at the given position.
+     * The default (atPos==-1) is to add the header at the end.
+     */
+    virtual void addExtensionHeader(IPv6ExtensionHeader *eh, int atPos = -1);
+
+    /**
+     * Remove the first extension header and return it.
+     */
+    IPv6ExtensionHeader *removeFirstExtensionHeader();
+
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_IPV6EXTHEADERTAGBASE_H
+
