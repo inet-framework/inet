2f84075502d4d5d4c97d88d34d51d2b6ddc8848e
Zoltan Bojthe <zbojthe@omnetpp.org>
2016-09-22 18:03:11 +0200

    ExtInterface: fixes for refreshDisplay()

--- src/inet/linklayer/ext/ExtInterface.cc
+++ src/inet/linklayer/ext/ExtInterface.cc
@@ -67,13 +67,6 @@ void ExtInterface::initialize(int stage)
     else if (stage == INITSTAGE_LINK_LAYER) {
         registerInterface();
     }
-    else if (stage == INITSTAGE_LAST) {
-        // if not connected, make it gray
-        if (hasGUI() && !connected) {
-            getDisplayString().setTagArg("i", 1, "#707070");
-            getDisplayString().setTagArg("i", 2, "100");
-        }
-    }
 }
 
 InterfaceEntry *ExtInterface::createInterfaceEntry()
@@ -179,15 +172,18 @@ void ExtInterface::refreshDisplay() const
         return;
 
     const char *str;
-    char buf[80];
 
     if (connected) {
+        char buf[80];
         sprintf(buf, "pcap device: %s\nrcv:%d snt:%d", device, numRcvd, numSent);
         str = buf;
+        getDisplayString().setTagArg("t", 0, buf);
     }
-    else
-        str = "not connected";
-    getDisplayString().setTagArg("t", 0, str);
+    else {
+        getDisplayString().setTagArg("i", 1, "#707070");
+        getDisplayString().setTagArg("i", 2, "100");
+        getDisplayString().setTagArg("t", 0, "not connected");
+    }
 }
 
 void ExtInterface::finish()
