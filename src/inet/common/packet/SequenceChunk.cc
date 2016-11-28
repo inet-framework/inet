874296a3f204fe1f0c6acf6318d926ad10e5dc3b
Levente Mészáros <levy@omnetpp.org>
2016-11-28 11:28:15 +0100

    Common: Cleaned up SequenceChunk.

--- src/inet/common/packet/SequenceChunk.cc
+++ src/inet/common/packet/SequenceChunk.cc
@@ -131,16 +131,8 @@ bool SequenceChunk::mergeToBeginning(const std::shared_ptr<Chunk>& chunk)
         auto& firstChunk = chunks.front();
         if (firstChunk->isImmutable() && chunk->isImmutable()) {
             auto mergedChunk = firstChunk->dupShared();
-            if (mergedChunk->insertToBeginning(chunk)) // {
+            if (mergedChunk->insertToBeginning(chunk))
                 chunks.front() = mergedChunk->peek(0);
-//                if (mergedChunk->getChunkType() == TYPE_SLICE) {
-//                    auto sliceChunk = std::static_pointer_cast<SliceChunk>(mergedChunk);
-//                    if (sliceChunk->getOffset() == 0 && sliceChunk->getChunkLength() == sliceChunk->getChunk()->getChunkLength()) {
-//                        chunks.front() = sliceChunk->getChunk();
-//                        return true;
-//                    }
-//                }
-//            }
         }
     }
     return false;
@@ -152,16 +144,8 @@ bool SequenceChunk::mergeToEnd(const std::shared_ptr<Chunk>& chunk)
         auto& lastChunk = chunks.back();
         if (lastChunk->isImmutable() && chunk->isImmutable()) {
             auto mergedChunk = lastChunk->dupShared();
-            if (mergedChunk->insertToEnd(chunk)) // {
+            if (mergedChunk->insertToEnd(chunk))
                 chunks.back() = mergedChunk->peek(0);
-//                if (mergedChunk->getChunkType() == TYPE_SLICE) {
-//                    auto sliceChunk = std::static_pointer_cast<SliceChunk>(mergedChunk);
-//                    if (sliceChunk->getOffset() == 0 && sliceChunk->getChunkLength() == sliceChunk->getChunk()->getChunkLength()) {
-//                        chunks.back() = sliceChunk->getChunk();
-//                        return true;
-//                    }
-//                }
-//            }
         }
     }
     return false;
@@ -169,8 +153,7 @@ bool SequenceChunk::mergeToEnd(const std::shared_ptr<Chunk>& chunk)
 
 void SequenceChunk::doInsertToBeginning(const std::shared_ptr<Chunk>& chunk)
 {
-    if (chunk->getChunkLength() <= 0)
-        throw cRuntimeError("Invalid chunk length: %li", chunk->getChunkLength());
+    assert(chunk->getChunkLength() > 0);
     if (!mergeToBeginning(chunk))
         chunks.push_front(chunk);
 }
@@ -220,8 +203,7 @@ bool SequenceChunk::insertToBeginning(const std::shared_ptr<Chunk>& chunk)
 
 void SequenceChunk::doInsertToEnd(const std::shared_ptr<Chunk>& chunk)
 {
-    if (chunk->getChunkLength() <= 0)
-        throw cRuntimeError("Invalid chunk length: %li", chunk->getChunkLength());
+    assert(chunk->getChunkLength() > 0);
     if (!mergeToEnd(chunk))
         chunks.push_back(chunk);
 }
