02c21441a20c2a8c3b87e154229f36da91873856
Pal Sipos <sipos.pal92@gmail.com>
2016-12-06 11:45:39 +0100

    step 10 text

--- tutorials/visualization/doxy/tutorial.h
+++ tutorials/visualization/doxy/tutorial.h
@@ -458,15 +458,41 @@ Sources: @ref omnetpp.ini, @ref VisualizationNetworks.ned
 
 <!------------------------------------------------------------------------>
 
-@page step10 Step 10 -
+@page step10 Step 10 - Showing active physical links
 
 @nav{step9,step11}
 
 @section s10goals Goals
 
+In this step we want to show the active physical links of the VoIP communication.
+Physical links appear, when a node receives VoIP message. With this visualizer
+we can monitor which network node transmits VoIP message and which node receives that.
+
 @section s10model The model
 
+The communication is given, we need to handle the visualizer only.
+We can turn off some visualizer, that can confuse us.
+Now we have to use the physicalLinkVisualizer. We need to filter
+which packets are considered to determine active links, in this case we add
+VoIP packets. This visualizer display an arrow from the sender to the receiver.
+We can change the color, the width and the style of the arrow, and we can adjust how quickly
+inactive links fade away. With the <i>fadeOutMode</i> we can set the base
+of the elapsed time.
+
+Here is the configuration:
+
+@dontinclude omnetpp.ini
+@skipline [Config Visualization10]
+@until ####
+
 @section s10results Results
+
+The VoIP application starts at 1s. Then the pedestrian0 sends the first VoIP message. Because only
+the accessPoint0 is in its communication range, only between them appears an arrow. But When the sender
+is the accessPoint0, and the destination is the pedestrian1, an array turns up towards
+the pedestrian0 too. This happens, because the pedestrian0 is in the accessPoint0's communication
+range too, so its wlan NIC also can receive the VoIP packet.The array always points
+to the receiver.
 
 Sources: @ref omnetpp.ini, @ref VisualizationNetworks.ned
 
