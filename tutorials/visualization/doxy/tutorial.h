6baf2ac49d4f260c81b0cef4a6acae6c61367cbd
Pal Sipos <sipos.pal92@gmail.com>
2016-12-06 10:36:20 +0100

    step 9 text

--- tutorials/visualization/doxy/tutorial.h
+++ tutorials/visualization/doxy/tutorial.h
@@ -421,11 +421,14 @@ In this step we want to display the signal propagation.
 
 @section s9model The model
 
-We don't have to configure anything on the communication, we only have to show that. To this we need to turn on the mediumVisualizer's
-displaySignals parameter, and set a propagation interval. It's possible to set the signal shape. We have three options: ring, sphere and both.
-If we set the shape ring the signals propagate on the ground in 2 dimension. The Module view mode can displays only that.
-The sphere is a 3D displaying mode, the signals propagate as a sphere under and over the ground. If we set both, we can see a ring on the ground,
-and a sphere in the air. This is the default option.
+in the previous step we configured the communication already, so now we only have to show that.
+To this we need to turn on the mediumVisualizer's displaySignals parameter,
+and set a propagation interval. It's possible to set the signal shape.
+We have three options: ring, sphere and both.
+If we set the shape ring the signals propagate on the ground in 2 dimension.
+The Module view mode can display signals only this way.
+The sphere is a 3D displaying mode, the signals propagate as a sphere under and over the ground.
+If we set both, we can see a ring on the ground, and a sphere in the air. This is the default option.
 
 @dontinclude omnetpp.ini
 @skipline [Config Visualization09]
@@ -442,10 +445,10 @@ There's an animation of a VoIP message, that goes from the pedestrian0 to the pe
 And there's a similar message in 3D view mode, with <i>both</i> option:
 <img src="step9_result2.gif">
 
-Ring signal propagation:
+<i>Ring</i> signal propagation:
 <img src="step9_result3.gif">
 
-Sphere signal propagation:
+<i>Sphere</i> signal propagation:
 <img src="step9_result4.gif">
 
 Sources: @ref omnetpp.ini, @ref VisualizationNetworks.ned
