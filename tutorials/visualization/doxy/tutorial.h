48b4607a9f565710d970dae4df5eca0fa5d48282
Pal Sipos <sipos.pal92@gmail.com>
2016-12-08 14:28:17 +0100

    step 14 text

--- tutorials/visualization/doxy/tutorial.h
+++ tutorials/visualization/doxy/tutorial.h
@@ -655,15 +655,47 @@ Sources: @ref omnetpp.ini, @ref VisualizationNetworks.ned
 
 <!------------------------------------------------------------------------>
 
-@page step14 Step 14 -
+@page step14 Step 14 - Displaying 802.11 channel access state
 
 @nav{step13,step15}
 
 @section s14goals Goals
 
+CSMA/CA in computer networking, is a network multiple access method
+in which carrier sensing is used, but nodes attempt to avoid collisions
+by transmitting only when the channel is sensed to be <i>"idle"</i>. That operates in
+data link layer (Layer 2).
+
+Network nodes can be in different channel access states like <i>idle</i>,
+<i>owning</i>, <i>ifs+back off</i>, that show, who transmit on the channel
+and who listen. We want to display that in this step.
+
 @section s14model The model
 
+Firstly we hide some visualizers, because they are distracting.
+The communication is the same as in the previous step, we have to configure only the visualizer.
+To display the channel access states, we use infoVisualizer. <br>
+Here is the configuration:
+@dontinclude omnetpp.ini
+@skipline [Config Visualization14]
+@until ####
+
+The module parameter specifies the submodules of network nodes, and the content
+determines what is displayed on network nodes. In addition we can adjust the
+background color, the font color, and the opacity. These are optional settings.
+
 @section s14results Results
+
+Here's what happens, when the simulation is running:
+[gif simulation is running]
+
+We see, the nodes wait until the channel is sensed to be idle. If the medium is clear,
+instead of immediately transmitting network nodes are waiting a predefined amount of time.
+This waiting period is called the interframe spacing (IFS).
+It depends on the priority of the packet.
+In addition to having a different IFS, a station will add a "random backoff"
+to its waiting period, to reduce the collision probability.
+After that the the network node starts transmitting the data, and it's owning the channel.
 
 Sources: @ref omnetpp.ini, @ref VisualizationNetworks.ned
 
