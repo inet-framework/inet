749397fc9a9dd05360af8075125d9057c9782abe
Levente Mészáros <levy@omnetpp.org>
2017-01-18 10:09:04 +0100

    Radio: Adapted error model base class to new ISignalPacketModel.

--- src/inet/physicallayer/base/bitlevel/LayeredErrorModelBase.cc
+++ src/inet/physicallayer/base/bitlevel/LayeredErrorModelBase.cc
@@ -32,12 +32,12 @@ const IReceptionPacketModel *LayeredErrorModelBase::computePacketModel(const Lay
     const TransmissionPacketModel *transmissionPacketModel = check_and_cast<const TransmissionPacketModel *>(transmission->getPacketModel());
     const Packet* packet = transmissionPacketModel->getPacket();
     if (packetErrorRate == 0)
-        return new ReceptionPacketModel(packet, new BitVector(*transmissionPacketModel->getSerializedPacket()), transmissionPacketModel->getBitrate(), packetErrorRate, true);
+        return new ReceptionPacketModel(packet, transmissionPacketModel->getBitrate(), packetErrorRate, true);
     else {
         if (uniform(0, 1) < packetErrorRate)
-            return new const ReceptionPacketModel(packet, new BitVector(), transmissionPacketModel->getBitrate(), packetErrorRate, false);
+            return new const ReceptionPacketModel(packet, transmissionPacketModel->getBitrate(), packetErrorRate, false);
         else
-            return new const ReceptionPacketModel(packet, new BitVector(*transmissionPacketModel->getSerializedPacket()), transmissionPacketModel->getBitrate(), packetErrorRate, true);
+            return new const ReceptionPacketModel(packet, transmissionPacketModel->getBitrate(), packetErrorRate, true);
     }
 }
 
