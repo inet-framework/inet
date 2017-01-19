6106ea72ab8e5930d6c00c7f69a35cd87ec8e454
Levente Mészáros <levy@omnetpp.org>
2017-01-19 12:23:52 +0100

    PPP: Removed markContentsImmutable call.

--- src/inet/linklayer/ppp/PPP.cc
+++ src/inet/linklayer/ppp/PPP.cc
@@ -219,7 +219,6 @@ void PPP::startTransmitting(cPacket *msg)
     emit(txStateSignal, 1L);
     emit(packetSentToLowerSignal, pppFrame);
     pppFrame->clearTags();
-    pppFrame->markContentsImmutable();
     send(pppFrame, physOutGate);
 
     ASSERT(datarateChannel == physOutGate->getTransmissionChannel());    //FIXME reread datarateChannel when changed
