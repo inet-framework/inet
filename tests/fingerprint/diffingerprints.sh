2fffc853ff3759399d3e7a93ce60b2b96a13e152
Benjamin Martin Seregi <benjamin@omnetpp.org>
2017-01-31 23:23:33 +0100

    Old diffingerprint: fix

--- tests/fingerprint/diffingerprints.sh
+++ tests/fingerprint/diffingerprints.sh
@@ -3,12 +3,12 @@
 # add these lines in omnetpp.ini
 #
 # record-eventlog = true
-# cmdenv-log-format = "f %g [%l] "
+# cmdenv-log-format = "%f %g [%l] "
 # fingerprint = 0000-0000
 
-grep -e "^-" -e "^E #" $1 | awk '{print $2 " " $3}' | awk '!x[$0]++' > $1.tmp
-grep -e "^-" -e "^E #" $2 | awk '{print $2 " " $3}' | awk '!x[$0]++' > $2.tmp
+grep -e "^E #" $1 | awk '{print $3 " " $13}' | awk '!x[$1]++' > $1.tmp
+grep -e "^E #" $2 | awk '{print $3 " " $13}' | awk '!x[$1]++' > $2.tmp
 diff -U 1 $1.tmp $2.tmp > diffingerprints.diff
 head -n 4 diffingerprints.diff | tail -n 1
-rm $1.tmp
-rm $2.tmp
+#rm $1.tmp
+#rm $2.tmp
