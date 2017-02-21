707ec56ac7f80634dc8a1fc0b322318a193d7e4d
Levente Mészáros <levy@omnetpp.org>
2017-02-21 11:52:50 +0100

    Serializer: Added PppHeaderSerializer.

---
+++ src/inet/common/serializer/headerserializers/ppp/PPPHeaderSerializer.cc
@@ -0,0 +1,64 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "inet/common/packet/SerializerRegistry.h"
+#include "inet/common/serializer/headerserializers/ppp/PPPHeaderSerializer.h"
+#include "inet/linklayer/ppp/PPPFrame_m.h"
+
+namespace inet {
+
+namespace serializer {
+
+Register_Serializer(PppHeader, PppHeaderSerializer);
+Register_Serializer(PppTrailer, PppTrailerSerializer);
+
+void PppHeaderSerializer::serialize(ByteOutputStream& stream, const std::shared_ptr<Chunk>& chunk) const
+{
+    const auto& pppHeader = std::static_pointer_cast<const PppHeader>(chunk);
+    stream.writeUint8(pppHeader->getFlag());
+    stream.writeUint8(pppHeader->getAddress());
+    stream.writeUint8(pppHeader->getControl());
+    stream.writeUint16(pppHeader->getProtocol());
+}
+
+std::shared_ptr<Chunk> PppHeaderSerializer::deserialize(ByteInputStream& stream) const
+{
+    auto pppHeader = std::make_shared<PppHeader>();
+    pppHeader->setFlag(stream.readUint8());
+    pppHeader->setAddress(stream.readUint8());
+    pppHeader->setControl(stream.readUint8());
+    pppHeader->setProtocol(stream.readUint16());
+    return pppHeader;
+}
+
+void PppTrailerSerializer::serialize(ByteOutputStream& stream, const std::shared_ptr<Chunk>& chunk) const
+{
+    const auto& pppTrailer = std::static_pointer_cast<const PppTrailer>(chunk);
+    stream.writeUint16(pppTrailer->getFcs());
+    stream.writeUint8(pppTrailer->getFlag());
+}
+
+std::shared_ptr<Chunk> PppTrailerSerializer::deserialize(ByteInputStream& stream) const
+{
+    auto pppTrailer = std::make_shared<PppTrailer>();
+    pppTrailer->setFcs(stream.readUint16());
+    pppTrailer->setFlag(stream.readUint8());
+    return pppTrailer;
+}
+
+} // namespace serializer
+
+} // namespace inet
+
