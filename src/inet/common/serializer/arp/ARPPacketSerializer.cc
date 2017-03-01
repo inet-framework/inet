2f7f0976d55dc529a2ac80d7995b37dc8c855711
Levente Mészáros <levy@omnetpp.org>
2017-03-01 16:49:21 +0100

    Serializer: Removed unused code.

--- src/inet/common/serializer/arp/ARPPacketSerializer.cc
+++ src/inet/common/serializer/arp/ARPPacketSerializer.cc
@@ -26,20 +26,6 @@ namespace serializer {
 
 Register_Serializer(ARPPacket, ARPPacketSerializer);
 
-/*
-struct arphdr {
-        uint16_t ar_hrd;                 // Hardware type (16 bits)
-        uint16_t ar_pro;                 // Protocol type (16 bits)
-        uint8_t ar_hln;                  // Byte length of each hardware address (n) (8 bits)
-        uint8_t ar_pln;                  // Byte length of each protocol address (m) (8 bits)
-        uint16_t ar_op;                  // Operation code (16 bits)
-        uint8_t ar_sha[ETHER_ADDR_LEN];  // source hardware address (n bytes)
-        uint32_t ar_spa;                 // source protocol address (m bytes)
-        uint8_t ar_tha[ETHER_ADDR_LEN];  // target hardware address (n bytes)
-        uint32_t ar_tpa;                 // target protocol address (m bytes)
-} __PACKED__;
-*/
-
 MACAddress ARPPacketSerializer::readMACAddress(ByteInputStream& stream, unsigned int size) const
 {
     unsigned int curpos = stream.getPosition();
