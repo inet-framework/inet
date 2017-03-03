557f34753a7f3c1c0d9cc5bfc16ac04d834bb59d
Levente Mészáros <levy@omnetpp.org>
2017-03-03 10:45:52 +0100

    Test: Removed 802.11 filtered fingerprint tests.

--- tests/fingerprint/runMacFilteredWirelessTests.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-./fingerprints mac-filtered-examples.csv -m wireless -m adhoc -m mobileipv6 -m WirelessDHCP -m examples/aodv -m neighborcache -a fingerprint-events=NOT\\\(**.mac.**\\\) cmdenv-log-prefix=%f\\ %g\\ [%l] record-eventlog=false cmdenv-express-mode=true $*
