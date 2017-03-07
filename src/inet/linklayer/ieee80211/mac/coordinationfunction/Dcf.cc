4f6dff318b0ae1966d6b2abf235c6cb88c3bce8a
Levente Mészáros <levy@omnetpp.org>
2017-03-07 15:09:32 +0100

    802.11: Removed @nic from Ieee80211Mac, the MAC module is not the NIC module.

--- src/inet/linklayer/ieee80211/mac/coordinationfunction/Dcf.cc
+++ src/inet/linklayer/ieee80211/mac/coordinationfunction/Dcf.cc
@@ -32,7 +32,7 @@ void Dcf::initialize(int stage)
     ModeSetListener::initialize(stage);
     if (stage == INITSTAGE_LINK_LAYER_2) {
         startRxTimer = new cMessage("startRxTimeout");
-        mac = check_and_cast<Ieee80211Mac *>(getContainingNicModule(this));
+        mac = check_and_cast<Ieee80211Mac *>(getContainingNicModule(this)->getSubmodule("mac"));
         dataAndMgmtRateControl = dynamic_cast<IRateControl *>(getSubmodule(("rateControl")));
         tx = check_and_cast<ITx *>(getModuleByPath(par("txModule")));
         rx = check_and_cast<IRx *>(getModuleByPath(par("rxModule")));
