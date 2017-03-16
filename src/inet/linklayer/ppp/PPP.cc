9034fb34c957be9145445cdefb51dd480462c8a3
Levente Mészáros <levy@omnetpp.org>
2017-03-16 10:25:18 +0100

    PPP: Changed append/prepend to the more intentional push variants.

--- src/inet/linklayer/ppp/PPP.cc
+++ src/inet/linklayer/ppp/PPP.cc
@@ -370,10 +370,10 @@ Packet *PPP::encapsulate(cPacket *msg)
     auto pppHeader = std::make_shared<PppHeader>();
     pppHeader->setProtocol(ProtocolGroup::pppprotocol.getProtocolNumber(msg->getMandatoryTag<PacketProtocolTag>()->getProtocol()));
     pppHeader->markImmutable();
-    packet->prepend(pppHeader);
+    packet->pushHeader(pppHeader);
     auto pppTrailer = std::make_shared<PppTrailer>();
     pppTrailer->markImmutable();
-    packet->append(pppTrailer);
+    packet->pushTrailer(pppTrailer);
     return packet;
 }
 
