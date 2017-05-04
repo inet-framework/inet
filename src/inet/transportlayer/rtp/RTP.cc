24bb0b475c7601a059dcef31172dd10a6a070afe
Zoltan Bojthe <zbojthe@omnetpp.org>
2017-05-04 17:04:38 +0200

    RTP: bugfix: _commonName was pointed to a deallocated memory

--- src/inet/transportlayer/rtp/RTP.cc
+++ src/inet/transportlayer/rtp/RTP.cc
@@ -49,6 +49,7 @@ void RTP::initialize(int stage)
     cSimpleModule::initialize(stage);
 
     if (stage == INITSTAGE_LOCAL) {
+        _commonName = "";
         _leaveSession = false;
         appInGate = findGate("appIn");
         profileInGate = findGate("profileIn");
@@ -416,7 +417,7 @@ void RTP::initializeRTCP()
 {
     RTPInnerPacket *rinp = new RTPInnerPacket("initializeRTCP()");
     int rtcpPort = _port + 1;
-    rinp->setInitializeRTCPPkt(_commonName, _mtu, _bandwidth, _rtcpPercentage, _destinationAddress, rtcpPort);
+    rinp->setInitializeRTCPPkt(_commonName.c_str(), _mtu, _bandwidth, _rtcpPercentage, _destinationAddress, rtcpPort);
     send(rinp, "rtcpOut");
 }
 
