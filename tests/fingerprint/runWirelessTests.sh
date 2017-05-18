ad36cda98a368d833997a9f3b4aee269eac82cb1
Levente Mészáros <levy@omnetpp.org>
2017-05-18 15:14:11 +0200

    Test: Added missing wireless test cases.

--- tests/fingerprint/runWirelessTests.sh
+++ tests/fingerprint/runWirelessTests.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./fingerprints *.csv -m wireless -m Wireless -m adhoc -m mobileipv6 -m dymo -m examples/aodv -m neighborcache -m objectcache $*
+./fingerprints *.csv -m wireless -m Wireless -m adhoc -m mobileipv6 -m dymo -m examples/aodv -m neighborcache -m objectcache -m geometry -m complex.ini $*
