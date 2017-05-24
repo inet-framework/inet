16ded078e127de4a647c7b8c12928ff7d9ae4894
Levente Mészáros <levy@omnetpp.org>
2017-05-24 09:56:26 +0200

    802.11: Added ERP only OFDM modes.

--- src/inet/physicallayer/ieee80211/mode/Ieee80211ERPOFDMMode.h
+++ src/inet/physicallayer/ieee80211/mode/Ieee80211ERPOFDMMode.h
@@ -26,14 +26,17 @@ namespace physicallayer {
 
 class INET_API Ieee80211ErpOfdmMode : public Ieee80211OFDMMode
 {
+    protected:
+        bool isErpOnly;
+
     public:
-        Ieee80211ErpOfdmMode(const char *name, const Ieee80211OFDMPreambleMode *preambleMode, const Ieee80211OFDMSignalMode *signalMode, const Ieee80211OFDMDataMode *dataMode);
+        Ieee80211ErpOfdmMode(const char *name, bool isErpOnly, const Ieee80211OFDMPreambleMode *preambleMode, const Ieee80211OFDMSignalMode *signalMode, const Ieee80211OFDMDataMode *dataMode);
 
         virtual std::ostream& printToStream(std::ostream& stream, int level) const override { return stream << "Ieee80211ErpOfdmMode"; }
 
         // The slot time is 20 μs in accordance with 17.3.3, except that an optional 9 μs
-        // slot time may be used when the BSS consists of only ERP STAs. TODO: isOnlyERPSTAs
-        virtual inline const simtime_t getSlotTime() const override { return 20E-6; }
+        // slot time may be used when the BSS consists of only ERP STAs.
+        virtual inline const simtime_t getSlotTime() const override { return isErpOnly ? 9E-6 : 20E-6; }
         const simtime_t getRifsTime() const override;
         // SIFS time is 10 μs in accordance with 17.3.3. See 19.3.2.4 for more detail.
         virtual inline const simtime_t getSifsTime() const override { return 10E-6; }
@@ -53,6 +56,15 @@ class INET_API Ieee80211ErpOfdmCompliantModes
         static const Ieee80211ErpOfdmMode erpOfdmMode36Mbps;
         static const Ieee80211ErpOfdmMode erpOfdmMode48Mbps;
         static const Ieee80211ErpOfdmMode erpOfdmMode54Mbps;
+
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode6Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode9Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode12Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode18Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode24Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode36Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode48Mbps;
+        static const Ieee80211ErpOfdmMode erpOnlyOfdmMode54Mbps;
 };
 
 } /* namespace physicallayer */
