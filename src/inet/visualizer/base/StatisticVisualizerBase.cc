b98620a02543bbffb452ba967b984b9bbb30fef6
Levente Mészáros <levy@omnetpp.org>
2017-06-06 18:07:03 +0200

    Visualizer: Fixed bug related to using %u.

--- src/inet/visualizer/base/StatisticVisualizerBase.cc
+++ src/inet/visualizer/base/StatisticVisualizerBase.cc
@@ -218,20 +218,16 @@ void StatisticVisualizerBase::processSignal(cComponent *source, simsignal_t sign
 void StatisticVisualizerBase::refreshStatisticVisualization(const StatisticVisualization *statisticVisualization)
 {
     double value = statisticVisualization->recorder->getLastValue();
-    if (std::isnan(value))
+    if (std::isnan(value) || units.empty()) {
         statisticVisualization->printValue = value;
+        statisticVisualization->printUnit = statisticVisualization->unit == nullptr ? "" : statisticVisualization->unit;
+    }
     else {
-        if (!units.empty()) {
-            for (auto& unit : units) {
-                statisticVisualization->printUnit = unit.c_str();
-                statisticVisualization->printValue = cNEDValue::convertUnit(value, statisticVisualization->unit, statisticVisualization->printUnit);
-                if (statisticVisualization->printValue > 1)
-                    break;
-            }
-        }
-        else {
-            statisticVisualization->printValue = value;
-            statisticVisualization->printUnit = statisticVisualization->unit == nullptr ? "" : statisticVisualization->unit;
+        for (auto& unit : units) {
+            statisticVisualization->printUnit = unit.c_str();
+            statisticVisualization->printValue = cNEDValue::convertUnit(value, statisticVisualization->unit, statisticVisualization->printUnit);
+            if (statisticVisualization->printValue > 1)
+                break;
         }
     }
 }
