7b966065632519bfdaf22b9d94e8a91bdaaa3a36
Rudolf Hornig <rudi@omnetpp.org>
2017-07-06 15:47:41 +0200

    doc: whatsnew update + version bump and changelog tags

--- src/inet/common/INETDefs.h
+++ src/inet/common/INETDefs.h
@@ -44,7 +44,7 @@ using namespace omnetpp;
 #endif // if OMNETPP_VERSION < 0x0501
 
 #define INET_VERSION  0x0306
-#define INET_PATCH_LEVEL 0x00
+#define INET_PATCH_LEVEL 0x01
 
 #if defined(INET_EXPORT)
 #  define INET_API    OPP_DLLEXPORT
