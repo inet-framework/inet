25d2ce8fd22f1e03437b7a5bac7f6a2e07010ab7
Levente Mészáros <levy@omnetpp.org>
2017-07-25 16:03:05 +0200

    Common: Added documentation to Memory*Streams.

--- src/inet/common/MemoryInputStream.h
+++ src/inet/common/MemoryInputStream.h
@@ -26,17 +26,35 @@ namespace inet {
 using namespace units::values;
 
 /**
- * This class provides an efficient in memory byte input stream.
- * Most functions are implemented in the header to allow inlining.
+ * This class provides an efficient in memory bit input stream. The stream
+ * provides a set of read functions that read data at the current position of
+ * the stream. Most functions are implemented in the header to allow inlining.
  */
 // TODO: allow arbitrary mixed bit/byte reads
 // TODO: add parameter checks
 // TODO: review efficiency
 class INET_API MemoryInputStream {
   protected:
+    /**
+     * This vector contains the bits that are read from this stream. The first
+     * bit of the bit stream is stored in the most significant bit of the first
+     * byte. For the longest possible bit stream given the same number of bytes,
+     * the last bit of the bit stream is stored in the least significant bit of
+     * the last byte. In other cases some of the lower bits of the last byte are
+     * not used.
+     */
     std::vector<uint8_t> data;
+    /**
+     * The length of the bit stream measured in bits.
+     */
     bit length;
+    /**
+     * The position of the next bit that will be read measured in bits.
+     */
     bit position;
+    /**
+     * This flag indicates if the stream has been read beyond the end of data.
+     */
     bool isReadBeyondEnd_ = false;
 
   protected:
@@ -65,12 +83,24 @@ class INET_API MemoryInputStream {
 
     /** @name Stream querying functions */
     //@{
+    /**
+     * Returns true if a read operation ever read beyond the end of the stream.
+     */
     bool isReadBeyondEnd() const { return isReadBeyondEnd_; }
 
+    /**
+     * Returns the total length of the stream measured in bits.
+     */
     bit getLength() const { return length; }
 
+    /**
+     * Returns the remaining unread length of the stream measured in bits.
+     */
     bit getRemainingLength() const { return length - position; }
 
+    /**
+     * Returns the current read position of the stream measured in bits.
+     */
     bit getPosition() const { return position; }
 
     const std::vector<uint8_t>& getData() const { return data; }
@@ -95,6 +125,9 @@ class INET_API MemoryInputStream {
 
     /** @name Stream updating functions */
     //@{
+    /**
+     * Updates the read position of the stream.
+     */
     void seek(bit position) {
         assert(bit(0) <= position && position <= length);
         this->position = position;
@@ -103,6 +136,9 @@ class INET_API MemoryInputStream {
 
     /** @name Bit streaming functions */
     //@{
+    /**
+     * Reads a bit at the current position of the stream.
+     */
     bool readBit() {
         if (position == length) {
             isReadBeyondEnd_ = true;
@@ -119,6 +155,9 @@ class INET_API MemoryInputStream {
         }
     }
 
+    /**
+     * Reads the same bit repeatedly at the current position of the stream.
+     */
     bool readBitRepeatedly(bool value, size_t count) {
         bool success = true;
         for (size_t i = 0; i < count; i++)
@@ -126,6 +165,10 @@ class INET_API MemoryInputStream {
         return success;
     }
 
+    /**
+     * Reads a sequence of bits at the current position of the stream keeping
+     * the original bit order.
+     */
     bit readBits(std::vector<bool>& bits, bit length) {
         size_t i;
         for (i = 0; i < bit(length).get(); i++) {
@@ -139,6 +182,9 @@ class INET_API MemoryInputStream {
 
     /** @name Byte streaming functions */
     //@{
+    /**
+     * Reads a byte at the current position of the stream in MSB to LSB bit order.
+     */
     uint8_t readByte() {
         assert(isByteAligned());
         if (position + byte(1) > length) {
@@ -153,6 +199,10 @@ class INET_API MemoryInputStream {
         }
     }
 
+    /**
+     * Reads the same byte repeatedly at the current position of the stream in
+     * MSB to LSB bit order.
+     */
     bool readByteRepeatedly(uint8_t value, size_t count) {
         bool success = true;
         for (size_t i = 0; i < count; i++)
@@ -160,6 +210,10 @@ class INET_API MemoryInputStream {
         return success;
     }
 
+    /**
+     * Reads a sequence of bytes at the current position of the stream keeping
+     * the original byte order in MSB to LSB bit order.
+     */
     byte readBytes(std::vector<uint8_t>& bytes, byte length) {
         assert(isByteAligned());
         if (position + length > this->length) {
@@ -171,6 +225,10 @@ class INET_API MemoryInputStream {
         return length;
     }
 
+    /**
+     * Reads a sequence of bytes at the current position of the stream keeping
+     * the original byte order in MSB to LSB bit order.
+     */
     byte readBytes(uint8_t *buffer, byte length) {
         assert(isByteAligned());
         if (position + length > this->length) {
@@ -185,10 +243,18 @@ class INET_API MemoryInputStream {
 
     /** @name Basic type streaming functions */
     //@{
+    /**
+     * Reads an 8 bit unsigned integer at the current position of the stream in
+     * MSB to LSB bit order.
+     */
     uint8_t readUint8() {
         return readByte();
     }
 
+    /**
+     * Reads a 16 bit unsigned integer at the current position of the stream in
+     * big endian byte order and MSB to LSB bit order.
+     */
     uint16_t readUint16Be() {
         uint16_t value = 0;
         value |= ((uint16_t)(readByte()) << 8);
@@ -196,6 +262,10 @@ class INET_API MemoryInputStream {
         return value;
     }
 
+    /**
+     * Reads a 16 bit unsigned integer at the current position of the stream in
+     * little endian byte order and MSB to LSB bit order.
+     */
     uint16_t readUint16Le() {
         uint16_t value = 0;
         value |= ((uint16_t)(readByte()) << 0);
@@ -203,6 +273,10 @@ class INET_API MemoryInputStream {
         return value;
     }
 
+    /**
+     * Reads a 32 bit unsigned integer at the current position of the stream in
+     * big endian byte order and MSB to LSB bit order.
+     */
     uint32_t readUint32Be() {
         uint32_t value = 0;
         value |= ((uint32_t)(readByte()) << 24);
@@ -212,6 +286,10 @@ class INET_API MemoryInputStream {
         return value;
     }
 
+    /**
+     * Reads a 32 bit unsigned integer at the current position of the stream in
+     * little endian byte order and MSB to LSB bit order.
+     */
     uint32_t readUint32Le() {
         uint32_t value = 0;
         value |= ((uint32_t)(readByte()) << 0);
@@ -221,6 +299,10 @@ class INET_API MemoryInputStream {
         return value;
     }
 
+    /**
+     * Reads a 64 bit unsigned integer at the current position of the stream in
+     * big endian byte order and MSB to LSB bit order.
+     */
     uint64_t readUint64Be() {
         uint64_t value = 0;
         value |= ((uint64_t)(readByte()) << 56);
@@ -234,6 +316,10 @@ class INET_API MemoryInputStream {
         return value;
     }
 
+    /**
+     * Reads a 64 bit unsigned integer at the current position of the stream in
+     * little endian byte order and MSB to LSB bit order.
+     */
     uint64_t readUint64Le() {
         uint64_t value = 0;
         value |= ((uint64_t)(readByte()) << 0);
@@ -250,6 +336,10 @@ class INET_API MemoryInputStream {
 
     /** @name INET specific type streaming functions */
     //@{
+    /**
+     * Reads a MAC address at the current position of the stream in big endian
+     * byte order and MSB to LSB bit order.
+     */
     MACAddress readMACAddress() {
         MACAddress address;
         for (int i = 0; i < MAC_ADDRESS_SIZE; i++)
@@ -257,10 +347,18 @@ class INET_API MemoryInputStream {
         return address;
     }
 
+    /**
+     * Reads an IPv4 address at the current position of the stream in big endian
+     * byte order and MSB to LSB bit order.
+     */
     IPv4Address readIPv4Address() {
         return IPv4Address(readUint32Be());
     }
 
+    /**
+     * Reads an IPv6 address at the current position of the stream in big endian
+     * byte order and MSB to LSB bit order.
+     */
     IPv6Address readIPv6Address() {
         uint32_t d[4];
         for (auto & element : d)
--- src/inet/common/MemoryOutputStream.h
+++ src/inet/common/MemoryOutputStream.h
@@ -26,7 +26,8 @@ namespace inet {
 using namespace units::values;
 
 /**
- * This class provides an efficient in memory output stream.
+ * This class provides an efficient in memory bit output stream. The stream
+ * provides a set of write functions that write data to the end of the stream.
  * Most functions are implemented in the header to allow inlining.
  */
 // TODO: allow arbitrary mixed bit/byte writes
@@ -34,7 +35,18 @@ using namespace units::values;
 // TODO: review efficiency
 class INET_API MemoryOutputStream {
   protected:
+    /**
+     * This vector contains the bits that were written to this stream so far.
+     * The first bit of the bit stream is stored in the most significant bit
+     * of the first byte. For the longest possible bit stream given the same
+     * number of bytes, the last bit of the bit stream is stored in the least
+     * significant bit of the last byte. In other cases some of the lower bits
+     * of the last byte are not used.
+     */
     std::vector<uint8_t> data;
+    /**
+     * The length of the bit stream measured in bits.
+     */
     bit length;
 
   protected:
@@ -51,6 +63,9 @@ class INET_API MemoryOutputStream {
 
     /** @name Stream querying functions */
     //@{
+    /**
+     * Returns the length of the bit stream measured in bits.
+     */
     bit getLength() const { return length; }
 
     const std::vector<uint8_t>& getData() const { return data; }
@@ -75,6 +90,9 @@ class INET_API MemoryOutputStream {
 
     /** @name Bit streaming functions */
     //@{
+    /**
+     * Writes a bit to the end of the stream.
+     */
     void writeBit(bool value) {
         size_t i = bit(length).get();
         size_t byteIndex = i / 8;
@@ -86,11 +104,18 @@ class INET_API MemoryOutputStream {
         length += bit(1);
     }
 
+    /**
+     * Writes the same bit repeatedly to the end of the stream.
+     */
     void writeBitRepeatedly(bool value, size_t count) {
         for (size_t i = 0; i < count; i++)
             writeBit(value);
     }
 
+    /**
+     * Writes a sequence of bits to the end of the stream keeping the original
+     * bit order.
+     */
     void writeBits(const std::vector<bool>& bits, bit offset = bit(0), bit length = bit(-1)) {
         auto end = length == bit(-1) ? bits.size() : bit(offset + length).get();
         for (size_t i = bit(offset).get(); i < end; i++)
@@ -100,17 +125,28 @@ class INET_API MemoryOutputStream {
 
     /** @name Byte streaming functions */
     //@{
+    /**
+     * Writes a byte to the end of the stream in MSB to LSB bit order.
+     */
     void writeByte(uint8_t value) {
         assert(isByteAligned());
         data.push_back(value);
         length += byte(1);
     }
 
+    /**
+     * Writes the same byte repeatedly to the end of the stream in MSB to LSB
+     * bit order.
+     */
     void writeByteRepeatedly(uint8_t value, size_t count) {
         for (size_t i = 0; i < count; i++)
             writeByte(value);
     }
 
+    /**
+     * Writes a sequence of bytes to the end of the stream keeping the original
+     * byte order and in MSB to LSB bit order.
+     */
     void writeBytes(const std::vector<uint8_t>& bytes, byte offset = byte(0), byte length = byte(-1)) {
         assert(isByteAligned());
         auto end = length == byte(-1) ? byte(bytes.size()) : offset + length;
@@ -118,6 +154,10 @@ class INET_API MemoryOutputStream {
         this->length += end - offset;
     }
 
+    /**
+     * Writes a sequence of bytes to the end of the stream keeping the original
+     * byte order and in MSB to LSB bit order.
+     */
     void writeBytes(uint8_t *buffer, byte length) {
         assert(isByteAligned());
         data.insert(data.end(), buffer, buffer + byte(length).get());
@@ -127,20 +167,36 @@ class INET_API MemoryOutputStream {
 
     /** @name Basic type streaming functions */
     //@{
+    /**
+     * Writes an 8 bit unsigned integer to the end of the stream in MSB to LSB
+     * bit order.
+     */
     void writeUint8(uint8_t value) {
         writeByte(value);
     }
 
+    /**
+     * Writes a 16 bit unsigned integer to the end of the stream in big endian
+     * byte order and MSB to LSB bit order.
+     */
     void writeUint16Be(uint16_t value) {
         writeByte((uint8_t)(value >> 8));
         writeByte((uint8_t)(value >> 0));
     }
 
+    /**
+     * Writes a 16 bit unsigned integer to the end of the stream in little endian
+     * byte order and MSB to LSB bit order.
+     */
     void writeUint16Le(uint16_t value) {
         writeByte((uint8_t)(value >> 0));
         writeByte((uint8_t)(value >> 8));
     }
 
+    /**
+     * Writes a 32 bit unsigned integer to the end of the stream in big endian
+     * byte order and MSB to LSB bit order.
+     */
     void writeUint32Be(uint32_t value) {
         writeByte((uint8_t)(value >> 24));
         writeByte((uint8_t)(value >> 16));
@@ -148,6 +204,10 @@ class INET_API MemoryOutputStream {
         writeByte((uint8_t)(value >> 0));
     }
 
+    /**
+     * Writes a 32 bit unsigned integer to the end of the stream in little endian
+     * byte order and MSB to LSB bit order.
+     */
     void writeUint32Le(uint32_t value) {
         writeByte((uint8_t)(value >> 0));
         writeByte((uint8_t)(value >> 8));
@@ -155,6 +215,10 @@ class INET_API MemoryOutputStream {
         writeByte((uint8_t)(value >> 24));
     }
 
+    /**
+     * Writes a 64 bit unsigned integer to the end of the stream in big endian
+     * byte order and MSB to LSB bit order.
+     */
     void writeUint64Be(uint64_t value) {
         writeByte((uint8_t)(value >> 56));
         writeByte((uint8_t)(value >> 48));
@@ -166,6 +230,10 @@ class INET_API MemoryOutputStream {
         writeByte((uint8_t)(value >> 0));
     }
 
+    /**
+     * Writes a 64 bit unsigned integer to the end of the stream in little endian
+     * byte order and MSB to LSB bit order.
+     */
     void writeUint64Le(uint64_t value) {
         writeByte((uint8_t)(value >> 0));
         writeByte((uint8_t)(value >> 8));
@@ -180,15 +248,27 @@ class INET_API MemoryOutputStream {
 
     /** @name INET specific type streaming functions */
     //@{
+    /**
+     * Writes a MAC address to the end of the stream in big endian byte order
+     * and MSB to LSB bit order.
+     */
     void writeMACAddress(MACAddress address) {
         for (int i = 0; i < MAC_ADDRESS_SIZE; i++)
             writeByte(address.getAddressByte(i));
     }
 
+    /**
+     * Writes an IPv4 address to the end of the stream in big endian byte order
+     * and MSB to LSB bit order.
+     */
     void writeIPv4Address(IPv4Address address) {
         writeUint32Be(address.getInt());
     }
 
+    /**
+     * Writes an IPv6 address to the end of the stream in big endian byte order
+     * and MSB to LSB bit order.
+     */
     void writeIPv6Address(IPv6Address address) {
         for (int i = 0; i < 4; i++)
             writeUint32Be(address.words()[i]);
