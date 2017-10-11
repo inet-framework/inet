2749370f6a50c3dc0551349756d9a232060cad2a
Zoltan Bojthe <zbojthe@omnetpp.org>
2017-10-11 15:56:53 +0200

    updated inet version

--- src/inet/common/INETDefs.h
+++ src/inet/common/INETDefs.h
@@ -45,7 +45,7 @@ using namespace omnetpp;
 #  error At least OMNeT++/OMNEST version 5.1 required
 #endif // if OMNETPP_VERSION < 0x0501
 
-#define INET_VERSION  0x0363
+#define INET_VERSION  0x0399
 #define INET_PATCH_LEVEL 0x00
 
 #if defined(INET_EXPORT)
