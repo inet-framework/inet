e0ee924935841ed8ac1ce8382b0cdc3bb0738574
TÖRÖK Attila <torokati44@gmail.com>
2017-11-14 12:48:24 +0100

    Travis updates. Details:
    - split the Docker image:
        the first contains only the 3 OMNeT++ builds (linux, macosx, windows)
        the second one adds the inet-specific things (ccache, ffmpeg)
    - the job script is no longer in the image, now launched from the checked-out repo
    - enable VoIP feature (only if not cross-compiling, we only have native ffmpeg)
    - enable ccache in jobs that run fingerprints to avoid job timeouts
        note that the use of the precompiled header has to be disabled for ccache to work

--- _scripts/travis/travis-runner.sh
+++ _scripts/travis/travis-runner.sh
@@ -6,27 +6,41 @@
 #    TARGET_PLATFORM       - must be one of "linux", "windows", "macosx"
 #    MODE                  - must be "debug" or "release"
 #    RUN_FINGERPRINT_TESTS - must be "yes" or "no"
+
 #    TRAVIS_REPO_SLUG      - this is provided by Travis, most likely contains "inet-framework/inet"
 
 
 set -e # make the script exit with error if any executed command exits with error
 
+ccache -s
 
 export PATH="/root/omnetpp-5.2-$TARGET_PLATFORM/bin:$PATH"
 
 # this is where the cloned INET repo is mounted into the container (as prescribed in /.travis.yml)
 cd /$TRAVIS_REPO_SLUG
 
+# only enabling VoIP on native compilation, because we don't [want to?] have cross-compiled ffmpeg
+if [ "$TARGET_PLATFORM" = "linux" ]; then
+    opp_featuretool enable VoIPStream
+    opp_featuretool enable VoIPStream_examples
+fi
+
 make makefiles
 
-make MODE=$MODE -j $(nproc)
-
-POSTFIX=""
-if [ "$MODE" = "debug" ]; then
-    POSTFIX="_dbg"
-fi
 
 if [ "$RUN_FINGERPRINT_TESTS" = "yes" ]; then
+    export PATH=/usr/lib/ccache:$PATH
+    make MODE=$MODE USE_PRECOMPILED_HEADER=no -j $(nproc)
+
+    POSTFIX=""
+    if [ "$MODE" = "debug" ]; then
+        POSTFIX="_dbg"
+    fi
+
     cd tests/fingerprint
-    ./runDefaultTests.sh -e opp_run$POSTFIX
+    ./fingerprinttest -e opp_run$POSTFIX
+else
+    make MODE=$MODE -j $(nproc)
 fi
+
+ccache -s
