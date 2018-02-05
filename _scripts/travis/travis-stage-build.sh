d2fd581d0636bd8c6bc4cc996a035ffc5fb48e85
TÖRÖK Attila <torokati44@gmail.com>
2018-02-05 13:56:41 +0100

    travis: Fix ccache misses by adding the -fcolorize-diagnostics flag.
    By adding it to CFLAGS in makefrag in both stages.
    This should solve the timeouts during silent building
    in the fingerprints stage.
    See https://github.com/ccache/ccache/issues/222 for why.

--- _scripts/travis/travis-stage-build.sh
+++ _scripts/travis/travis-stage-build.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-# This script runs the jobs on Travis.
+# This script runs the build stage of the Travis builds.
 #
 # The following environment variables must be set when invoked:
 #    TARGET_PLATFORM        - must be one of "linux", "windows", "macosx"
@@ -19,9 +19,10 @@ if [ "$TARGET_PLATFORM" = "windows" ]; then
     ccache -s
     echo -e "\n----\n"
 else
-    # on linux and macosx, we don't use the old cache contents for builds,
-    # only populate it for the fingerprint stage (on linux that is)
-    ccache -cC
+    # On linux and macosx, we don't use the old cache contents for builds.
+    # On linux we populate the cache for the fingerprints stage of the same build only.
+    # I think ccache doesn't even recoginze the Mac cross-compiler, but just in case...
+    ccache -cCz
 fi
 
 export PATH="/root/omnetpp-5.3p2-$TARGET_PLATFORM/bin:/usr/lib/ccache:$PATH"
@@ -31,9 +32,16 @@ cd /$TRAVIS_REPO_SLUG
 
 cp -r /root/nsc-0.5.3 3rdparty
 
-# only enabling some features only with native compilation, because we don't [want to?] have cross-compiled ffmpeg and NSC
+# enabling some features only with native compilation, because we don't [want to?] have cross-compiled ffmpeg and NSC
 if [ "$TARGET_PLATFORM" = "linux" ]; then
     opp_featuretool enable VoIPStream VoIPStream_examples TCP_NSC TCP_lwIP
+
+    # In the fingerprints stage we have to force enable diagnostics coloring
+    # to make ccache work well, see https://github.com/ccache/ccache/issues/222
+    # We do it here as well to make the compiler arguments match.
+    # Only when compiling to linux though, as for cross-compiling we use GCC,
+    # and it has a different flag for this. And we only need this for linux anyway.
+    echo -e "CFLAGS += -fcolor-diagnostics\n\n$(cat src/makefrag)" > src/makefrag
 fi
 
 make makefiles
