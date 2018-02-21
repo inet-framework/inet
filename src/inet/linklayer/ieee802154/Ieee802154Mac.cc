caf400c2d81ff4fe0f2393db6d86aaa138c93491
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-02-21 10:52:21 +0100

    ieee802154: set protocol tag to ieee802154 at encapsulation

--- src/inet/linklayer/ieee802154/Ieee802154Mac.cc
+++ src/inet/linklayer/ieee802154/Ieee802154Mac.cc
@@ -28,16 +28,16 @@
 
 #include <cassert>
 
-#include "inet/common/INETUtils.h"
-#include "inet/common/INETMath.h"
-#include "inet/common/ModuleAccess.h"
-#include "inet/common/ProtocolTag_m.h"
-#include "inet/common/ProtocolGroup.h"
-#include "inet/networklayer/common/InterfaceEntry.h"
 #include "inet/common/FindModule.h"
-#include "inet/linklayer/ieee802154/Ieee802154MacHeader_m.h"
+#include "inet/common/INETMath.h"
+#include "inet/common/INETUtils.h"
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/ProtocolGroup.h"
+#include "inet/common/ProtocolTag_m.h"
 #include "inet/linklayer/common/InterfaceTag_m.h"
 #include "inet/linklayer/common/MacAddressTag_m.h"
+#include "inet/linklayer/ieee802154/Ieee802154MacHeader_m.h"
+#include "inet/networklayer/common/InterfaceEntry.h"
 
 namespace inet {
 
@@ -238,6 +238,7 @@ void Ieee802154Mac::handleUpperPacket(Packet *packet)
     //macPkt->setControlInfo(pco);
     packet->insertHeader(macPkt);
     EV_DETAIL << "pkt encapsulated, length: " << macPkt->getChunkLength() * 8 << "\n";
+    packet->getTag<PacketProtocolTag>()->setProtocol(&Protocol::ieee802154);
     executeMac(EV_SEND_REQUEST, packet);
 }
 
