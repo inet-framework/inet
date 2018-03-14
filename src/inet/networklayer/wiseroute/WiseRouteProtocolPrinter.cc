aba145fb604784868650216479bc4d60da517d02
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-03-14 16:20:36 +0100

    networklayer: added some ProtocolPrinters

---
+++ src/inet/networklayer/wiseroute/WiseRouteProtocolPrinter.cc
@@ -0,0 +1,49 @@
+//
+// Copyright (C) 2018 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+// @author: Zoltan Bojthe
+//
+
+#include "inet/networklayer/wiseroute/WiseRouteProtocolPrinter.h"
+
+#include "inet/common/packet/printer/PacketPrinter.h"
+#include "inet/common/packet/printer/ProtocolPrinterRegistry.h"
+#include "inet/networklayer/wiseroute/WiseRouteHeader_m.h"
+
+namespace inet {
+
+Register_Protocol_Printer(&Protocol::wiseroute, WiseRouteProtocolPrinter);
+
+void WiseRouteProtocolPrinter::print(const Ptr<const Chunk>& chunk, const Protocol *protocol, const cMessagePrinter::Options *options, Context& context) const
+{
+    if (auto header = dynamicPtrCast<const WiseRouteHeader>(chunk)) {
+        context.sourceColumn << header->getSourceAddress();
+        context.destinationColumn << header->getDestinationAddress();
+        B payloadLength = header->getPayloadLengthField();
+        context.infoColumn << "WiseRoute"
+                << " ttl:" << header->getNbHops();
+        auto payloadProtocol = header->getProtocol();
+        if (payloadProtocol)
+            context.infoColumn << " payload:" << payloadProtocol->getName() << " " << payloadLength;
+        else
+            context.infoColumn << " payload: protocol(" << header->getProtocolId() << ") " << payloadLength;
+    }
+    else
+        context.infoColumn << "(WiseRoute) " << chunk;
+}
+
+} // namespace inet
+
