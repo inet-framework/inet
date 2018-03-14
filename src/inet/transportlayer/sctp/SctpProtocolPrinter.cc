e49934813ceec271275042e39c413afa81af0b76
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-03-14 10:57:36 +0100

    sctp: added ProtocolPrinter

---
+++ src/inet/transportlayer/sctp/SctpProtocolPrinter.cc
@@ -0,0 +1,45 @@
+//
+// Copyright (C) 2018 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+// @author: Zoltan Bojthe
+//
+
+#include "inet/transportlayer/sctp/SctpProtocolPrinter.h"
+
+#include "inet/common/packet/printer/PacketPrinter.h"
+#include "inet/common/packet/printer/ProtocolPrinterRegistry.h"
+#include "inet/transportlayer/sctp/SctpHeader.h"
+
+namespace inet {
+namespace sctp {
+
+Register_Protocol_Printer(&Protocol::sctp, SctpProtocolPrinter);
+
+void SctpProtocolPrinter::print(const Ptr<const Chunk>& chunk, const Protocol *protocol, const cMessagePrinter::Options *options, Context& context) const
+{
+    if (auto header = dynamicPtrCast<const SctpHeader>(chunk)) {
+        context.sourceColumn << header->getSrcPort();
+        context.destinationColumn << header->getDestPort();
+        context.infoColumn << header->getSrcPort() << "->" << header->getDestPort();
+        // << ", payload:" << (B(header->getTotalLengthField()) - header->getChunkLength());    //TODO
+    }
+    else
+        context.infoColumn << "(UDP) " << chunk;
+}
+
+} // namespace sctp
+} // namespace inet
+
