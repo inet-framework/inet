c7052e98387ab824d4cfe365229ab9aaebaf1032
Levente Mészáros <levy@omnetpp.org>
2018-03-19 11:43:41 +0100

    Common: Added documentation to EulerAngles.

--- src/inet/common/geometry/common/EulerAngles.h
+++ src/inet/common/geometry/common/EulerAngles.h
@@ -5,6 +5,16 @@
 
 namespace inet {
 
+/**
+ * Orientations are represented by 3D double precision Tait-Bryan (Euler) tuples
+ * called EulerAngles. The angles are in Z, Y', X" order that is often called
+ * intrinsic rotations, they are measured in radians. The default (unrotated)
+ * orientation is along the X axis. Conceptually, the Z axis rotation is heading,
+ * the Y' axis rotation is descending, the X" axis rotation is bank. For example,
+ * positive rotation along the Z axis rotates X into Y (turns left), positive
+ * rotation along the Y axis rotates Z into X (leans forward), positive rotation
+ * along the X axis rotates Y into Z (leans right).
+ */
 class INET_API EulerAngles
 {
   public:
