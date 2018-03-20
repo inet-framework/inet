945e4442d5228f95b4fdea177839f3436eac9780
Levente Mészáros <levy@omnetpp.org>
2018-03-20 09:50:14 +0100

    Mobility: Moved static LinearNodeDistributionMobility into static folder.

--- src/inet/mobility/static/LinearNodeDistributionMobility.cc
+++ src/inet/mobility/static/LinearNodeDistributionMobility.cc
@@ -16,7 +16,7 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
 
-#include "inet/mobility/single/LinearNodeDistributionMobility.h"
+#include "inet/mobility/static/LinearNodeDistributionMobility.h"
 
 namespace inet {
 
