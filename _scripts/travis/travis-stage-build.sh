9fab9db3bc21dd7cf9f55b177991adeda0d7c88e
TÖRÖK Attila <torokati44@gmail.com>
2018-03-31 19:07:36 +0200

    travis: Changes aiming to solve the intermittent timeouts and/or deadlocks.
    It's not pretty, but hopefully it will work better.

--- _scripts/travis/travis-stage-build.sh
+++ _scripts/travis/travis-stage-build.sh
@@ -12,18 +12,7 @@
 
 set -e # make the script exit with error if any executed command exits with error
 
-if [ "$TARGET_PLATFORM" = "windows" ]; then
-    # we have to use ccache across builds when compiling to windows, because it is
-    # just so darn slow that otherwise we would get intermittent job timeouts...
-    echo -e "\nccache summary:\n"
-    ccache -s
-    echo -e "\n----\n"
-else
-    # On linux and macosx, we don't use the old cache contents for builds.
-    # On linux we populate the cache for the fingerprints stage of the same build only.
-    # I think ccache doesn't even recoginze the Mac cross-compiler, but just in case...
-    ccache -cCz
-fi
+ccache -cCz # we don't use ccache across builds, only across stages within each build
 
 export PATH="/root/omnetpp-5.3p4-$TARGET_PLATFORM/bin:/usr/lib/ccache:$PATH"
 
@@ -42,10 +31,29 @@ if [ "$TARGET_PLATFORM" = "linux" ]; then
     # Only when compiling to linux though, as for cross-compiling we use GCC,
     # and it has a different flag for this. And we only need this for linux anyway.
     echo -e "CFLAGS += -fcolor-diagnostics\n\n$(cat src/makefrag)" > src/makefrag
+
+    # On linux we can't use precompiled headers, because ccache can't work with them,
+    # and we need ccache, but we are fine without precompiled headers (on linux that is).
+    PCH=no
+else
+    # When compiling to windows, we have to enable precompiled headers, otherwise the
+    # debug builds take too long, sometimes exceeding 50 minutes.
+    # And we don't need ccache here anyway (only the linux build is used in the second stage).
+    # When compiling to macos, it doesn't really matter, but let's enable them just in case.
+    PCH=yes
+fi
+
+if [ "$TARGET_PLATFORM" = "windows" ]; then
+    # The --no-keep-memory flag decreased linking time by ~10 percent in debug builds
+    # (a bit more in release), which is needed because otherwise it sometimes took over 10 minutes,
+    # triggering the other kind of Travis timeout. The --stats option is just to check, and it
+    # produces some output before the command terminates (before the .dll file is written to disk),
+    # which also helps. We only need these with mingw, and they don't work with clang (for the macos builds).
+    echo -e "LDFLAGS += -Wl,--stats -Wl,--no-keep-memory\n\n$(cat src/makefrag)" > src/makefrag
 fi
 
 make makefiles
-make MODE=$MODE USE_PRECOMPILED_HEADER=no -j $(nproc)
+make MODE=$MODE USE_PRECOMPILED_HEADER=$PCH -j $(nproc)
 
 echo -e "\nccache summary:\n"
 ccache -s
