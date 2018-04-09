bfab35499dab2cbe49ed9b9a8e0aca8d2c8387ed
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-04-09 17:52:09 +0200

    module tests: remove colorizing from outputs

--- tests/module/runFailedTests.sh
+++ tests/module/runFailedTests.sh
@@ -1,6 +1,2 @@
 ./runtest \
-    AODVSimpleTest.test IPv4NetworkConfigurator_2.test IPv4NetworkConfigurator_3.test \
-    Ieee80211Retransmission1.test Ieee80211Retransmission2.test Ieee80211Retransmission3.test \
-    Ieee80211Retransmission4.test Ieee80211_1.test Ieee80211_2.test Ieee80211_3.test Ieee80211_4.test \
-    Power_4.test Power_5.test lifecycle_AccessPoint_1.test lifecycle_AccessPoint_2.test \
-    lifecycle_AccessPoint_3.test lifecycle_WirelessHost_4.test lifecycle_WirelessHost_5.test
+     sctp_streamReset.test
