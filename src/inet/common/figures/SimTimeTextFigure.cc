7915b0852d77c7571374a1b5660bd31566f6c96b
Gyorgy Szaszko <adam.szaszko@gmail.com>
2018-06-12 15:46:54 +0200

    figures: Added SimTimeTextFigure
    Useful for displaying the simTime on the canvas.

---
+++ src/inet/common/figures/SimTimeTextFigure.cc
@@ -0,0 +1,52 @@
+//
+// Copyright (C) 2018 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "SimTimeTextFigure.h"
+
+namespace inet {
+
+Register_Figure("simTimeText", SimTimeTextFigure);
+
+static const char *PKEY_PREFIX = "prefix";
+
+void SimTimeTextFigure::refreshDisplay() {
+    setText((prefix + simTime().format(SimTime::getScaleExp(), ".", "'", true, "", " ")).c_str());
+}
+
+void SimTimeTextFigure::parse(cProperty *property)
+{
+    cTextFigure::parse(property);
+
+    const char *s;
+
+    if ((s = property->getValue(PKEY_PREFIX)) != nullptr)
+        prefix = s;
+}
+
+const char **SimTimeTextFigure::getAllowedPropertyKeys() const
+{
+    static const char *keys[32];
+    if (!keys[0]) {
+        const char *localKeys[] = {
+            PKEY_PREFIX, nullptr
+        };
+        concatArrays(keys, cTextFigure::getAllowedPropertyKeys(), localKeys);
+    }
+    return keys;
+}
+
+}
---
+++ src/inet/common/figures/SimTimeTextFigure.h
@@ -0,0 +1,38 @@
+//
+// Copyright (C) 2018 OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_SIMTIMETEXTFIGURE_H
+#define __INET_SIMTIMETEXTFIGURE_H
+
+#include "inet/common/INETDefs.h"
+
+namespace inet {
+
+class SimTimeTextFigure: public cTextFigure {
+protected:
+    std::string prefix;
+public:
+    using cTextFigure::cTextFigure;
+    void refreshDisplay() override;
+
+    void parse(cProperty *property) override;
+    const char **getAllowedPropertyKeys() const override;
+};
+
+}
+
+#endif /* INET_SIMTIMETEXTFIGURE_H */
