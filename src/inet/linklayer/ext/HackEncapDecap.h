5038bf4a07907582ad562aa19d13bc8aa78fa112
Levente Mészáros <levy@omnetpp.org>
2018-06-29 16:22:56 +0200

    Ext: Added HackEncapDecap module, temporary name.

---
+++ src/inet/linklayer/ext/HackEncapDecap.h
@@ -0,0 +1,44 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef __INET_HACKENCAPDECAP_H
+#define __INET_HACKENCAPDECAP_H
+
+#include "inet/common/INETDefs.h"
+
+namespace inet {
+
+/**
+ * TODO
+ *
+ * See NED file for more details.
+ */
+class INET_API HackEncapDecap : public cSimpleModule
+{
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage *msg) override;
+
+  public:
+    virtual ~HackEncapDecap();
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_HACKENCAPDECAP_H
+
