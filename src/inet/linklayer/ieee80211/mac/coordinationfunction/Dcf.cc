79bd7913d3c27b035f439781d5abcb484368c639
Raphael Riebl <raphael.riebl@thi.de>
2018-07-11 11:39:06 +0200

    ieee80211: remove superfluous 802.2 LLC header inclusion from Dcf and Hcf

--- src/inet/linklayer/ieee80211/mac/coordinationfunction/Dcf.cc
+++ src/inet/linklayer/ieee80211/mac/coordinationfunction/Dcf.cc
@@ -16,7 +16,6 @@
 //
 
 #include "inet/common/ModuleAccess.h"
-#include "inet/linklayer/ieee8022/Ieee8022LlcHeader_m.h"
 #include "inet/linklayer/ieee80211/mac/coordinationfunction/Dcf.h"
 #include "inet/linklayer/ieee80211/mac/framesequence/DcfFs.h"
 #include "inet/linklayer/ieee80211/mac/Ieee80211Mac.h"
