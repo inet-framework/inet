612b47a33a2cf345ad1cc766c220bff14c38c6ad
Levente Mészáros <levy@omnetpp.org>
2018-07-20 16:58:09 +0200

    Ext: Added ExtInterface module class to register network interface.

---
+++ src/inet/emulation/common/ExtInterface.cc
@@ -0,0 +1,46 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/emulation/common/ExtInterface.h"
+#include "inet/networklayer/contract/IInterfaceTable.h"
+
+namespace inet {
+
+Define_Module(ExtInterface);
+
+void ExtInterface::initialize(int stage)
+{
+    if (stage == INITSTAGE_LINK_LAYER)
+        registerInterface();
+}
+
+void ExtInterface::registerInterface()
+{
+    setMtu(par("mtu"));      //TODO get mtu from real interface / or set mtu in real interface
+    setBroadcast(true);      //TODO
+    setMulticast(true);      //TODO
+    setPointToPoint(true);   //TODO
+
+    IInterfaceTable *ift = findModuleFromPar<IInterfaceTable>(par("interfaceTableModule"), this);
+    if (ift)
+        ift->addInterface(this);
+    inet::registerInterface(*this, gate("upperLayerIn"), gate("upperLayerOut"));
+}
+
+} // namespace inet
+
