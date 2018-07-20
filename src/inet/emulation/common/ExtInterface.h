a2dc8563ba18d7863e6a0215c8f5464897be3a98
Levente Mészáros <levy@omnetpp.org>
2018-07-20 16:58:09 +0200

    Ext: Added ExtInterface module class to register network interface.

---
+++ src/inet/emulation/common/ExtInterface.h
@@ -0,0 +1,39 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef __INET_EXTINTERFACE_H
+#define __INET_EXTINTERFACE_H
+
+#include "inet/common/IInterfaceRegistrationListener.h"
+#include "inet/networklayer/common/InterfaceEntry.h"
+
+namespace inet {
+
+class INET_API ExtInterface : public InterfaceEntry
+{
+  protected:
+    virtual void registerInterface();
+
+  public:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_EXTINTERFACE_H
+
