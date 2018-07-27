030158f1b7d1f03ddd234c0b289b2486338a9202
Levente Mészáros <levy@omnetpp.org>
2018-07-27 14:33:14 +0200

    Ext: Added copyConfiguration parameter to ExtInterface.

--- src/inet/emulation/common/ExtInterface.cc
+++ src/inet/emulation/common/ExtInterface.cc
@@ -40,66 +40,81 @@ void ExtInterface::initialize(int stage)
     InterfaceEntry::initialize(stage);
     if (stage == INITSTAGE_LINK_LAYER) {
         registerInterface();
-        std::string device = par("device").stdstringValue();
-        int fd;
-        struct ifreq ifr;
-
-        fd = socket(AF_INET, SOCK_DGRAM, 0);
-
-        //Type of address to retrieve - IPv4 IP address
-        ifr.ifr_addr.sa_family = AF_INET;
-
-        //Copy the interface name in the ifreq structure
-        strncpy(ifr.ifr_name , device.c_str() , IFNAMSIZ-1);
-
-        //get the IPv4 address
-        ioctl(fd, SIOCGIFADDR, &ifr);
-        Ipv4Address ipv4Address = Ipv4Address(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-
-        //get the IPv4 netmask
-        ioctl(fd, SIOCGIFNETMASK, &ifr);
-        Ipv4Address ipv4Netmask = Ipv4Address(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
-
-        ioctl(fd, SIOCGIFHWADDR, &ifr);
-        MacAddress macAddress;
-        macAddress.setAddressBytes((unsigned char *)ifr.ifr_hwaddr.sa_data);
-
-        ioctl(fd, SIOCGIFMTU, &ifr);
-        int mtu = ifr.ifr_mtu;
-
-        //TODO get IPv4 multicast addresses
-
-        //TODO get IPv6 addresses
-
-        close(fd);
-
-        Ipv4InterfaceData *interfaceData = ipv4Data();
-        if (interfaceData == nullptr)
-            setIpv4Data(interfaceData = new Ipv4InterfaceData());
-        setMacAddress(macAddress);
-        setMtu(mtu);
-        //interfaceData->setMetric(metric);
-        interfaceData->setIPAddress(Ipv4Address(ipv4Address));
-        interfaceData->setNetmask(Ipv4Address(ipv4Netmask));
+        if (!strcmp("copyFromExt", par("copyConfiguration")))
+            copyInterfaceConfigurationFromExt();
+        else
+            configureInterface();
+    }
+    else if (stage == INITSTAGE_LINK_LAYER_2) {
+        if (!strcmp("copyToExt", par("copyConfiguration")))
+            copyInterfaceConfigurationToExt();
     }
 }
 
-void ExtInterface::registerInterface()
+void ExtInterface::configureInterface()
 {
     const char *addressString = par("address");
     MacAddress address = strcmp(addressString, "auto") ? MacAddress(addressString) : MacAddress::generateAutoAddress();
     setMacAddress(address);
     setInterfaceToken(address.formInterfaceIdentifier());
-
     setMtu(par("mtu"));
     setBroadcast(true);      //TODO
     setMulticast(true);      //TODO
     setPointToPoint(true);   //TODO
+}
 
+void ExtInterface::registerInterface()
+{
     IInterfaceTable *interfaceTable = findModuleFromPar<IInterfaceTable>(par("interfaceTableModule"), this);
     if (interfaceTable)
         interfaceTable->addInterface(this);
     inet::registerInterface(*this, gate("upperLayerIn"), gate("upperLayerOut"));
+}
+
+void ExtInterface::copyInterfaceConfigurationFromExt()
+{
+    std::string device = par("device").stdstringValue();
+    int fd = socket(AF_INET, SOCK_DGRAM, 0);
+
+    struct ifreq ifr;
+    ifr.ifr_addr.sa_family = AF_INET;
+    strncpy(ifr.ifr_name , device.c_str() , IFNAMSIZ-1);
+
+    //get the IPv4 address
+    ioctl(fd, SIOCGIFADDR, &ifr);
+    // TODO: use uint32_t
+    Ipv4Address ipv4Address = Ipv4Address(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+
+    //get the IPv4 netmask
+    ioctl(fd, SIOCGIFNETMASK, &ifr);
+    // TODO: use uint32_t
+    Ipv4Address ipv4Netmask = Ipv4Address(inet_ntoa(((struct sockaddr_in *)&ifr.ifr_addr)->sin_addr));
+
+    ioctl(fd, SIOCGIFHWADDR, &ifr);
+    MacAddress macAddress;
+    macAddress.setAddressBytes((unsigned char *)ifr.ifr_hwaddr.sa_data);
+
+    ioctl(fd, SIOCGIFMTU, &ifr);
+    int mtu = ifr.ifr_mtu;
+
+    //TODO get IPv4 multicast addresses
+
+    //TODO get IPv6 addresses
+
+    close(fd);
+
+    Ipv4InterfaceData *interfaceData = ipv4Data();
+    if (interfaceData == nullptr)
+        setIpv4Data(interfaceData = new Ipv4InterfaceData());
+    setMacAddress(macAddress);
+    setMtu(mtu);
+    interfaceData->setIPAddress(Ipv4Address(ipv4Address));
+    interfaceData->setNetmask(Ipv4Address(ipv4Netmask));
+}
+
+void ExtInterface::copyInterfaceConfigurationToExt()
+{
+    // TODO:
 }
 
 } // namespace inet
