52735b2226378f9d2259eb5bd17f40951fe280de
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-08-28 17:51:04 +0200

    IPv4Route: added dBABEL, dLISP, getSourceTypeAbbreviation()

--- src/inet/networklayer/ipv4/IPv4Route.cc
+++ src/inet/networklayer/ipv4/IPv4Route.cc
@@ -39,6 +39,33 @@ IPv4Route::~IPv4Route()
     delete protocolData;
 }
 
+const char* inet::IPv4Route::getSourceTypeAbbreviation() const {
+    switch (sourceType) {
+        case IFACENETMASK:
+            return "C";
+        case MANUAL:
+            return (getDestination().isUnspecified() ? "S*": "S");
+        case ROUTER_ADVERTISEMENT:
+            return "ra";
+        case RIP:
+            return "R";
+        case OSPF:
+            return "O";
+        case BGP:
+            return "B";
+        case EIGRP:
+            return getAdminDist() < IPv4Route::dEIGRPExternal ? "D" : "D EX";
+        case LISP:
+            return "l";
+        case BABEL:
+            return "ba";
+        case ODR:
+            return "o";
+        default:
+            return "???";
+    }
+}
+
 std::string IPv4Route::info() const
 {
     std::stringstream out;
