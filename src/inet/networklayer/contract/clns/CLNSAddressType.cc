ad1da55ed2477cc44519acc70c1d5794977a5418
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-08-28 17:31:46 +0200

    networklayer: added CLNSAddress, CLNSAddressType, CLNSControlInfo (needed for CLNSAddressType)

---
+++ src/inet/networklayer/contract/clns/CLNSAddressType.cc
@@ -0,0 +1,32 @@
+// Copyright (C) 2012 - 2016 Brno University of Technology (http://nes.fit.vutbr.cz/ansa)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+/**
+ * @file CLNSAddressType.cc
+ * @author Marcel Marek (mailto:imarek@fit.vutbr.cz), Vladimir Vesely (mailto:ivesely@fit.vutbr.cz)
+ * @date 10.8.2016
+ * @brief Class representing a CLNS Address type
+ */
+
+#include "inet/networklayer/contract/clns/CLNSAddressType.h"
+
+namespace inet {
+
+CLNSAddressType CLNSAddressType::INSTANCE;
+
+
+
+} /* namespace inet */
