e51d013dc3fe7cf2c045aa6e5eb02c8db3af048c
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-08-29 18:08:01 +0200

    Arp: convert to inet4

--- src/inet/networklayer/arp/ipv4/Arp.h
+++ src/inet/networklayer/arp/ipv4/Arp.h
@@ -120,7 +120,7 @@ class INET_API Arp : public cSimpleModule, public IArp, public ILifecycle
     virtual void processARPPacket(Packet *packet);
     virtual void updateARPCache(ArpCacheEntry *entry, const MacAddress& macAddress);
 
-    virtual MACAddress getMacAddressForArpReply(InterfaceEntry *ie, ARPPacket *arp);
+    virtual MacAddress getMacAddressForArpReply(const InterfaceEntry *ie, const ArpPacket *arp);
 
     virtual void dumpARPPacket(const ArpPacket *arp);
     virtual void refreshDisplay() const override;
