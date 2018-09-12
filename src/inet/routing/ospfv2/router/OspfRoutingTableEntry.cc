ad3b2c1d97e2a569de30d586dd481995dd10700a
screw <screw@users.noreply.github.com>
2018-09-12 14:38:34 +0200

    Adding ift initialisation to the constructor.

--- src/inet/routing/ospfv2/router/OspfRoutingTableEntry.cc
+++ src/inet/routing/ospfv2/router/OspfRoutingTableEntry.cc
@@ -32,6 +32,7 @@ RoutingTableEntry::RoutingTableEntry(IInterfaceTable *_ift) :
 }
 
 RoutingTableEntry::RoutingTableEntry(const RoutingTableEntry& entry) :
+    ift(entry.ift),
     destinationType(entry.destinationType),
     optionalCapabilities(entry.optionalCapabilities),
     area(entry.area),
