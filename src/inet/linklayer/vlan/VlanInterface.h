96998c5100ae4be40cc64cd802be10faa2eb3426
Levente Mészáros <levy@omnetpp.org>
2018-10-10 14:50:41 +0200

    Vlan: Added VlanInterface module.

---
+++ src/inet/linklayer/vlan/VlanInterface.h
@@ -0,0 +1,47 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef __INET_VLANINTERFACE_H
+#define __INET_VLANINTERFACE_H
+
+#include "inet/networklayer/common/InterfaceEntry.h"
+
+namespace inet {
+
+class INET_API VlanInterface : public InterfaceEntry
+{
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+};
+
+class Vlan : public cSimpleModule
+{
+  protected:
+    int vlanId = -1;
+    InterfaceEntry *interfaceEntry = nullptr;
+
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage *message) override;
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_VLANINTERFACE_H
+
