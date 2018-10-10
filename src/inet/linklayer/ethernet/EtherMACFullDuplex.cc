7f7419476d3c8c4b36b70ea1f8089966c72877d8
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-10-10 17:53:14 +0200

    eth: fixed 'Cannot abort PAUSE (LFC) before timeout (#390)' (author: Danushka Menikkumbura)

--- src/inet/linklayer/ethernet/EtherMACFullDuplex.cc
+++ src/inet/linklayer/ethernet/EtherMACFullDuplex.cc
@@ -343,8 +343,8 @@ void EtherMACFullDuplex::processPauseCommand(int pauseUnits)
                   << " more time units from now\n";
         cancelEvent(endPauseMsg);
 
-        if (pauseUnits > 0)
-            scheduleEndPausePeriod(pauseUnits);
+        // Terminate PAUSE if pauseUnits == 0; Extend PAUSE if pauseUnits > 0
+        scheduleEndPausePeriod(pauseUnits);
     }
     else {
         // transmitter busy -- wait until it finishes with current frame (endTx)
