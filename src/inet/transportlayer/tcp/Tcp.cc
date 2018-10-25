7f0327bf8711e0b92c1dd36ba29a6595ea721452
Zoltan Bojthe <zbojthe@omnetpp.org>
2018-10-26 00:06:58 +0200

    fix for "tcp: lifecycle fixup"

--- src/inet/transportlayer/tcp/Tcp.cc
+++ src/inet/transportlayer/tcp/Tcp.cc
@@ -257,7 +257,7 @@ void Tcp::refreshDisplay() const
     //char buf[40];
     //sprintf(buf,"%d conns", tcpAppConnMap.size());
     //getDisplayString().setTagArg("t",0,buf);
-    if (operational != NOT_OPERATING)
+    if (isOperational)
         getDisplayString().removeTag("i2");
     else
         getDisplayString().setTagArg("i2", 0, "status/cross");
