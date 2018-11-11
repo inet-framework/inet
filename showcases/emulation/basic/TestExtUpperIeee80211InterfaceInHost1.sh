d9a21d65ada66128ef8f23f2729f1e985d90ad64
Levente Mészáros <levy@omnetpp.org>
2018-11-11 16:57:03 +0100

    basic emulation

--- showcases/emulation/basic/TestExtUpperIeee80211InterfaceInHost1.sh
+++ showcases/emulation/basic/TestExtUpperIeee80211InterfaceInHost1.sh
@@ -10,7 +10,7 @@ sudo ip addr add 192.168.2.1/24 dev tap0
 sudo ip link set dev tap0 up
 
 # run simulation
-inet -u Cmdenv -c ExtUpperEthernetInterfaceInHost1 --sim-time-limit=2s &> inet.out &
+inet -u Cmdenv -c ExtUpperIeee80211InterfaceInHost1 --sim-time-limit=2s &> inet.out &
 
 # ping into simulation
 ping -c 2 -W 2 192.168.2.2 > ping.out
