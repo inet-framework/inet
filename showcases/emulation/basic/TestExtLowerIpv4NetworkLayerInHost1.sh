1ad70bd670d82ec00dc3e76d1640fba6007b47e8
Levente Mészáros <levy@omnetpp.org>
2018-11-11 16:57:03 +0100

    basic emulation

--- showcases/emulation/basic/TestExtLowerIpv4NetworkLayerInHost1.sh
+++ showcases/emulation/basic/TestExtLowerIpv4NetworkLayerInHost1.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# create virtual ethernet link: vetha <--> vethb
-sudo ip link add vetha type veth peer name vethb
+# create virtual ethernet link: veth0 <--> veth1
+sudo ip link add veth0 type veth peer name veth1
 
-# vetha <--> vethb link uses 192.168.2.x addresses
-sudo ip addr add 192.168.2.2 dev vethb
+# veth0 <--> veth1 link uses 192.168.2.x addresses
+sudo ip addr add 192.168.2.2 dev veth1
 
 # bring up both interfaces
-sudo ip link set vetha up
-sudo ip link set vethb up
+sudo ip link set veth0 up
+sudo ip link set veth1 up
 
 # add routes for new link
-sudo route add -net 192.168.2.0 netmask 255.255.255.0 dev vetha
+sudo route add -net 192.168.2.0 netmask 255.255.255.0 dev veth0
 
 # run simulation
 inet -u Cmdenv -c ExtLowerIpv4NetworkLayerInHost1 --sim-time-limit=2s &> inet.out
 
 # check output
 if grep -q "from 192.168.2.2" "inet.out"; then echo $0 ": PASS"; else echo $0 ": FAIL"; fi
-#rm *.out
+rm *.out
 
 # destroy virtual ethernet link
-sudo ip link del vetha
+sleep 1
+sudo ip link del veth0
 
