95367ed9a08be7f201d48cadcd305624b1e84b04
Gyorgy Szaszko <adam.szaszko@gmail.com>
2019-01-21 17:38:22 +0100

    osgdemo

--- showcases/visualizer/osgdemo/tiledownloader.sh
+++ showcases/visualizer/osgdemo/tiledownloader.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-osgearth_package --tms mymap_source.earth  --bounds 16.4472 47.3925 16.4794 47.4080 --max-level 19 --out mymap_v0301 --out-earth mymap_v0301.earth
+osgearth_package --tms arcgisonline.earth  --bounds 16.4472 47.3925 16.4794 47.4080 --max-level 19 --out mymap_arcgislocal --out-earth mymap_arcgislocal.earth
