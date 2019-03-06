1c86cdb71291b300f757c100c4dbb361ce361ca8
Rudolf Hornig <rudi@omnetpp.org>
2019-03-06 11:45:29 +0100

    doc: reduced font size for PDF docs so code examples fit horizontally

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -178,7 +178,7 @@ latex_elements = {
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    'pointsize': '12pt',
+    'pointsize': '10pt',
 
     # Additional stuff for the LaTeX preamble.
     #
