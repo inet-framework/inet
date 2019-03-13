4308a5e78ca9e5a1b9a71fd995fb3d00bfe22f70
Zoltan Bojthe <zbojthe@omnetpp.org>
2019-03-13 18:19:16 +0100

    Protocol: added TSN protocol with ethertype 0x22F0

--- src/inet/common/ProtocolGroup.cc
+++ src/inet/common/ProtocolGroup.cc
@@ -72,6 +72,7 @@ ProtocolGroup ProtocolGroup::ethertype("ethertype", {
     { 0x0800, &Protocol::ipv4 },
     { 0x0806, &Protocol::arp },
     { 0x2000, &Protocol::cdp },              // TODO remove it, it's a CISCO code for LLC, ANSAINET project use it currently
+    { 0x22F0, &Protocol::tsn},
     { 0x22F3, &Protocol::trill},
     { 0x22F4, &Protocol::l2isis},
     { 0x86DD, &Protocol::ipv6 },
