86876d0868d44b468b59b8f4e864df87950e7a4d
marcell <juhaszmarcell46@gmail.com>
2019-06-20 14:58:00 +0200

    ieee80211MgmtFrame: serializers separated (no wireshark test)

--- src/inet/linklayer/ieee80211/mgmt/Ieee80211MgmtFrameSerializer.h
+++ src/inet/linklayer/ieee80211/mgmt/Ieee80211MgmtFrameSerializer.h
@@ -26,16 +26,133 @@ namespace inet {
 namespace ieee80211 {
 
 /**
- * Converts between Ieee80211MgmtFrame and binary network byte order IEEE 802.11 mgmt frame.
+ * Converts between Ieee80211AuthenticationFrame and binary network byte order IEEE 802.11 Authentication frame.
  */
-class INET_API Ieee80211MgmtFrameSerializer : public FieldsChunkSerializer
+class INET_API Ieee80211AuthenticationFrameSerializer : public FieldsChunkSerializer
 {
   protected:
     virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
     virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
 
   public:
-    Ieee80211MgmtFrameSerializer() : FieldsChunkSerializer() {}
+    Ieee80211AuthenticationFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211DeauthenticationFrame and binary network byte order IEEE 802.11 Deauthentication frame.
+ */
+class INET_API Ieee80211DeauthenticationFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211DeauthenticationFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211DisassociationFrame and binary network byte order IEEE 802.11 Disassociation frame.
+ */
+class INET_API Ieee80211DisassociationFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211DisassociationFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211ProbeRequestFrame and binary network byte order IEEE 802.11 Probe Request frame.
+ */
+class INET_API Ieee80211ProbeRequestFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211ProbeRequestFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211AssociationRequestFrame and binary network byte order IEEE 802.11 Association Request frame.
+ */
+class INET_API Ieee80211AssociationRequestFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211AssociationRequestFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211ReassociationRequestFrame and binary network byte order IEEE 802.11 Reassotiation Request frame.
+ */
+class INET_API Ieee80211ReassociationRequestFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211ReassociationRequestFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211AssociationResponseFrame and binary network byte order IEEE 802.11 Association Response frame.
+ */
+class INET_API Ieee80211AssociationResponseFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211AssociationResponseFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211ReassociationResponseFrame and binary network byte order IEEE 802.11 Reassociation Response frame.
+ */
+class INET_API Ieee80211ReassociationResponseFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211ReassociationResponseFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211BeaconFrame and binary network byte order IEEE 802.11 Beacon frame.
+ */
+class INET_API Ieee80211BeaconFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211BeaconFrameSerializer() : FieldsChunkSerializer() {}
+};
+
+/**
+ * Converts between Ieee80211ProbeResponseFrame and binary network byte order IEEE 802.11 Probe Response frame.
+ */
+class INET_API Ieee80211ProbeResponseFrameSerializer : public FieldsChunkSerializer
+{
+  protected:
+    virtual void serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const override;
+    virtual const Ptr<Chunk> deserialize(MemoryInputStream& stream) const override;
+
+  public:
+    Ieee80211ProbeResponseFrameSerializer() : FieldsChunkSerializer() {}
 };
 
 } // namespace ieee80211
