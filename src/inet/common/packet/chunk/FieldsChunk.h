68fb06bab95d49548654afdbe6496677afb98768
marcell <juhaszmarcell46@gmail.com>
2019-08-02 15:54:04 +0200

    FieldsChunk: temporary fix of length check to avoid meaningless 'cannot convert between integers' error message

--- src/inet/common/packet/chunk/FieldsChunk.h
+++ src/inet/common/packet/chunk/FieldsChunk.h
@@ -44,7 +44,13 @@ class INET_API FieldsChunk : public Chunk
     /** @name Field accessor functions */
     //@{
     const std::vector<uint8_t> *getSerializedBytes() const { return serializedBytes; }
-    void setSerializedBytes(const std::vector<uint8_t> *bytes) const { CHUNK_CHECK_IMPLEMENTATION(B(bytes->size()) == chunkLength); this->serializedBytes = bytes; }
+    void setSerializedBytes(const std::vector<uint8_t> *bytes) const {
+        if (b(bytes->size() * 8).get() != b(chunkLength).get()) {
+            std::cout << typeid(*this).name() << ": " << "b(bytes->size()): " << b(bytes->size() * 8).get() << " bits       " << "chunkLength: " << b(chunkLength).get() << " bits" << endl;
+            throw cRuntimeError("%s ; \"b(bytes->size() * 8) == b(chunkLength)\" does not hold in function 'setSerializedBytes' at ./inet/common/packet/chunk/FieldsChunk.h:51. chunkLength = %d bits ; bytes->size() = %d bits", typeid(*this).name(), b(chunkLength).get(), b(bytes->size() * 8).get());
+        }
+        //CHUNK_CHECK_IMPLEMENTATION(b(bytes->size() * 8) == b(chunkLength));
+        this->serializedBytes = bytes; }
     //@}
 
     virtual const Ptr<Chunk> peekUnchecked(PeekPredicate predicate, PeekConverter converter, const Iterator& iterator, b length, int flags) const override;
