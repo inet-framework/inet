a8a3537b42f96794466e3516cc59108fe73ea2a6
marcell <juhaszmarcell46@gmail.com>
2019-08-02 13:50:52 +0200

    serializer: rtpMpeg: adjust to extra fields, not working

--- src/inet/transportlayer/rtp/profiles/avprofile/RtpMpegPacketSerializer.cc
+++ src/inet/transportlayer/rtp/profiles/avprofile/RtpMpegPacketSerializer.cc
@@ -24,6 +24,19 @@ Register_Serializer(RtpMpegHeader, RtpMpegPacketSerializer);
 void RtpMpegPacketSerializer::serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const
 {
     const auto& rtpMpegHeader = staticPtrCast<const RtpMpegHeader>(chunk);
+    /*stream.writeNBitsOfUint64Be(rtpMpegHeader->getMbz(), 5);
+    stream.writeBit(rtpMpegHeader->getTwo());
+    stream.writeNBitsOfUint64Be(rtpMpegHeader->getTemporalReference(), 10);
+    stream.writeBit(rtpMpegHeader->getActiveN());
+    stream.writeBit(rtpMpegHeader->getNewPictureHeader());
+    stream.writeBit(rtpMpegHeader->getSequenceHeaderPresent());
+    stream.writeBit(rtpMpegHeader->getBeginningOfSlice());
+    stream.writeBit(rtpMpegHeader->getEndOfSlice());
+    stream.writeNBitsOfUint64Be(rtpMpegHeader->getPictureType(), 3);
+    stream.writeBit(rtpMpegHeader->getFbv());
+    stream.writeNBitsOfUint64Be(rtpMpegHeader->getBfc(), 3);
+    stream.writeBit(rtpMpegHeader->getFfv());
+    stream.writeNBitsOfUint64Be(rtpMpegHeader->getFfc(), 3);*/
     stream.writeUint16Be(rtpMpegHeader->getPayloadLength());
     stream.writeUint16Be(rtpMpegHeader->getPictureType());
 }
@@ -31,6 +44,19 @@ void RtpMpegPacketSerializer::serialize(MemoryOutputStream& stream, const Ptr<co
 const Ptr<Chunk> RtpMpegPacketSerializer::deserialize(MemoryInputStream& stream) const
 {
     auto rtpMpegHeader = makeShared<RtpMpegHeader>();
+    /*rtpMpegHeader->setMbz(stream.readNBitsToUint64Be(5));
+    rtpMpegHeader->setTwo(stream.readBit());
+    rtpMpegHeader->setTemporalReference(stream.readNBitsToUint64Be(10));
+    rtpMpegHeader->setActiveN(stream.readBit());
+    rtpMpegHeader->setNewPictureHeader(stream.readBit());
+    rtpMpegHeader->setSequenceHeaderPresent(stream.readBit());
+    rtpMpegHeader->setBeginningOfSlice(stream.readBit());
+    rtpMpegHeader->setEndOfSlice(stream.readBit());
+    rtpMpegHeader->setPictureType(stream.readNBitsToUint64Be(3));
+    rtpMpegHeader->setFbv(stream.readBit());
+    rtpMpegHeader->setBfc(stream.readNBitsToUint64Be(3));
+    rtpMpegHeader->setFfv(stream.readBit());
+    rtpMpegHeader->setFfc(stream.readNBitsToUint64Be(3));*/
     rtpMpegHeader->setPayloadLength(stream.readUint16Be());
     rtpMpegHeader->setPictureType(stream.readUint16Be());
     return rtpMpegHeader;
