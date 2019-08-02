4995266e0d57375230b7dd2c4bb066f599b09baa
Zoltan Bojthe <zbojthe@omnetpp.org>
2019-08-02 14:20:13 +0200

    phy: Radio: added FIXME

--- src/inet/physicallayer/common/packetlevel/Radio.cc
+++ src/inet/physicallayer/common/packetlevel/Radio.cc
@@ -474,6 +474,12 @@ void Radio::endReception(cMessage *timer)
         auto isReceptionSuccessful = medium->getReceptionDecision(this, signal->getListening(), transmission, part)->isReceptionSuccessful();
         EV_INFO << "Reception ended: " << (isReceptionSuccessful ? "\x1b[1msuccessfully\x1b[0m" : "\x1b[1munsuccessfully\x1b[0m") << " for " << (ISignal *)signal << " " << IRadioSignal::getSignalPartName(part) << " as " << reception << endl;
         auto macFrame = medium->receivePacket(this, signal);
+
+        // FIXME:
+        // Where we can check the phy header is correct or not? decapsulate() drops the phy header.
+        // When phy header has checksum field and checksum is incorrect, we should drop the frame here instead of sends to upper layer.
+        // Is missing the isReceptionSuccessful flag, too?
+
         decapsulate(macFrame);
         sendUp(macFrame);
         receptionTimer = nullptr;
