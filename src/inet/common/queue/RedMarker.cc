15e50e2536b3060aedda95a96c031d65f1da1fab
Marcel Marek <marcelma@ifi.uio.no>
2019-09-11 13:49:40 +0200

    RedMarker: Adding author

--- src/inet/common/queue/RedMarker.cc
+++ src/inet/common/queue/RedMarker.cc
@@ -1,4 +1,6 @@
 //
+// Copyright (C) 2019 Marcel Marek
+//
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -139,7 +141,7 @@ void RedMarker::handleMessage(cMessage *msg)
     }
   }
 
-
+  // For packets that does not support marking - shouldDrop is called instead
   if (shouldDrop(packet)){
     EV_DETAIL<< "RedMarker: Dropping packet\n";
     dropPacket(packet);
