5590ee8132e7bd281a949ad6ffd6e8ac9e3fa632
Levente Mészáros <levy@omnetpp.org>
2019-09-16 15:57:59 +0200

    Radio: Added a new NoiseSource network node type.
    This node is capable of producing noise signals with arbitrary spectrum and random time parameters.

---
+++ src/inet/physicallayer/common/packetlevel/NoiseSource.cc
@@ -0,0 +1,98 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/physicallayer/common/packetlevel/NoiseSource.h"
+#include "inet/physicallayer/common/packetlevel/RadioMedium.h"
+
+namespace inet {
+namespace physicallayer {
+
+Define_Module(NoiseSource);
+
+NoiseSource::~NoiseSource()
+{
+    // NOTE: can't use the medium module here, because it may have been already deleted
+    cModule *medium = getSimulation()->getModule(mediumModuleId);
+    if (medium != nullptr)
+        check_and_cast<IRadioMedium *>(medium)->removeRadio(this);
+    cancelAndDelete(sleepTimer);
+    cancelAndDelete(transmissionTimer);
+}
+
+void NoiseSource::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL) {
+        sleepTimer = new cMessage("sleepTimer");
+        transmissionTimer = new cMessage("transmissionTimer");
+        antenna = check_and_cast<IAntenna *>(getSubmodule("antenna"));
+        transmitter = check_and_cast<ITransmitter *>(getSubmodule("transmitter"));
+        medium = getModuleFromPar<IRadioMedium>(par("radioMediumModule"), this);
+        mediumModuleId = check_and_cast<cModule *>(medium)->getId();
+        radioIn = gate("radioIn");
+        radioIn->setDeliverOnReceptionStart(true);
+    }
+    else if (stage == INITSTAGE_PHYSICAL_LAYER) {
+        medium->addRadio(this);
+        scheduleSleepTimer();
+    }
+}
+
+void NoiseSource::handleMessage(cMessage *message)
+{
+    if (message == sleepTimer)
+        startTransmission();
+    else if (message == transmissionTimer)
+        endTransmission();
+    else
+        delete message;
+}
+
+void NoiseSource::startTransmission()
+{
+    Signal *signal = check_and_cast<Signal *>(medium->transmitPacket(this, nullptr));
+    transmissionTimer->setContextPointer(const_cast<Signal *>(signal));
+    scheduleTransmissionTimer(signal->getTransmission());
+}
+
+void NoiseSource::endTransmission()
+{
+    transmissionTimer->setContextPointer(nullptr);
+    scheduleSleepTimer();
+}
+
+void NoiseSource::scheduleSleepTimer()
+{
+    scheduleAt(simTime() + par("sleepInterval"), sleepTimer);
+}
+
+void NoiseSource::scheduleTransmissionTimer(const ITransmission *transmission)
+{
+    scheduleAt(simTime() + transmission->getDuration(), transmissionTimer);
+}
+
+const ITransmission *NoiseSource::getTransmissionInProgress() const
+{
+    if (!transmissionTimer->isScheduled())
+        return nullptr;
+    else
+        return static_cast<Signal *>(transmissionTimer->getContextPointer())->getTransmission();
+}
+
+} // namespace physicallayer
+} // namespace inet
+
