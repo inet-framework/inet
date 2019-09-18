f1533552812c5b9152f489c87c420f3324385560
Levente Mészáros <levy@omnetpp.org>
2019-09-18 15:29:14 +0200

    Test: Added new packet tests for lazy serialization of SequenceChunk.

--- tests/packet/UnitTest.cc
+++ tests/packet/UnitTest.cc
@@ -1208,6 +1208,45 @@ static void testPeeking()
     ASSERT(dynamicPtrCast<const BytesChunk>(chunk4) != nullptr);
 }
 
+static void testSequenceSerialization()
+{
+    { // 1. test sequence doesn't serializes front if not necessary
+    Packet packet;
+    auto header = makeShared<HeaderWithoutSerializer>();
+    packet.insertAtFront(header);
+    packet.insertAtBack(makeImmutableBytesChunk(makeVector(10)));
+    packet.insertAtBack(makeImmutableByteCountChunk(B(10)));
+    packet.popAtFront<HeaderWithoutSerializer>();
+    const auto& bytesChunk = packet.peekDataAsBytes();
+    ASSERT(bytesChunk != nullptr);
+    ASSERT(bytesChunk->getChunkLength() == B(20));
+    }
+
+    { // 2. test sequence doesn't serializes back if not necessary
+    Packet packet;
+    auto header = makeShared<HeaderWithoutSerializer>();
+    packet.insertAtBack(makeImmutableBytesChunk(makeVector(10)));
+    packet.insertAtBack(makeImmutableByteCountChunk(B(10)));
+    packet.insertAtBack(header);
+    packet.popAtBack<HeaderWithoutSerializer>(B(8));
+    const auto& bytesChunk = packet.peekDataAsBytes();
+    ASSERT(bytesChunk != nullptr);
+    ASSERT(bytesChunk->getChunkLength() == B(20));
+    }
+
+    { // 3. test sequence serialization fails if no serializer is present
+    Packet packet;
+    auto header = makeShared<HeaderWithoutSerializer>();
+    packet.insertAtBack(makeImmutableBytesChunk(makeVector(10)));
+    packet.insertAtBack(header);
+    packet.insertAtBack(makeImmutableByteCountChunk(B(10)));
+    const auto& dataChunk = packet.peekData();
+    ASSERT(dataChunk != nullptr);
+    ASSERT(dataChunk->getChunkLength() == B(28));
+    ASSERT_ERROR(packet.peekDataAsBytes(), "Cannot find serializer");
+    }
+}
+
 static void testSequence()
 {
     // 1. sequence merges immutable slices
@@ -1893,6 +1932,7 @@ void UnitTest::initialize()
     testNesting();
     testPeeking();
     testSequence();
+    testSequenceSerialization();
     testChunkQueue();
     testChunkBuffer(getRNG(0));
     testReassemblyBuffer();
