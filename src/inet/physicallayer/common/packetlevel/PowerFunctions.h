2a2de4c32ae3503b44d830ecc4e69d52252ea9df
Levente Mészáros <levy@omnetpp.org>
2019-10-04 09:32:51 +0200

    Radio: Renamed PowerFunction.h to PowerFunctions.h.

--- src/inet/physicallayer/common/packetlevel/PowerFunctions.h
+++ src/inet/physicallayer/common/packetlevel/PowerFunctions.h
@@ -13,8 +13,8 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
-#ifndef __INET_POWERFUNCTION_H_
-#define __INET_POWERFUNCTION_H_
+#ifndef __INET_POWERFUNCTIONS_H_
+#define __INET_POWERFUNCTIONS_H_
 
 #include "inet/common/geometry/common/Coord.h"
 #include "inet/common/math/Functions.h"
@@ -468,5 +468,5 @@ class INET_API AntennaGainFunction : public IFunction<double, Domain<Quaternion>
 
 } // namespace inet
 
-#endif // #ifndef __INET_POWERFUNCTION_H_
+#endif // #ifndef __INET_POWERFUNCTIONS_H_
 
