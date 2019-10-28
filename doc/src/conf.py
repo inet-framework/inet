2aabd26f845c30785822b3247c7ce31338d3ad42
Rudolf Hornig <rudi@omnetpp.org>
2019-10-28 12:12:45 +0100

    doc: extended doc generator to be able to deploy to github
    - Use '[docker-]make clean generate' to build the HTML and PDF version.
    - Use 'make deploy' to create a new commit in the _deploy dir.
    - From the _deploy directory use 'git push' to publish on GitHub pages.

--- doc/src/conf.py
+++ doc/src/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('_themes'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'INET Documentation'
+project = 'INET'
 copyright = 'INET community'
 author = 'INET community'
 
