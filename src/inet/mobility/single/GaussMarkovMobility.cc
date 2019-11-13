46ef7ef0907cb83bf9a7710434e106fec3d0b425
Zoltan Bojthe <zbojthe@omnetpp.org>
2019-11-13 11:41:19 +0100

    mobility: GaussMarkovMobility: fixed missing variance parameter, #101
    GaussMarkovMobility:
    parameter 'variance' replaced to 'speedStdDev' and 'angleStdDev' parameters

--- src/inet/mobility/single/GaussMarkovMobility.cc
+++ src/inet/mobility/single/GaussMarkovMobility.cc
@@ -25,13 +25,6 @@ Define_Module(GaussMarkovMobility);
 
 GaussMarkovMobility::GaussMarkovMobility()
 {
-    speed = 0;
-    angle = deg(0);
-    alpha = 0;
-    margin = 0;
-    speedMean = 0;
-    angleMean = deg(0);
-    variance = 0;
 }
 
 void GaussMarkovMobility::initialize(int stage)
@@ -41,15 +34,14 @@ void GaussMarkovMobility::initialize(int stage)
     EV_TRACE << "initializing GaussMarkovMobility stage " << stage << endl;
     if (stage == INITSTAGE_LOCAL) {
         speedMean = par("speed");
-        angleMean = deg(par("angle"));
+        speedStdDev = par("speedStdDev");
+        angleMean = deg(fmod(deg(par("angle")).get(), 360.0));
+        angleStdDev = deg(par("angleStdDev"));
         alpha = par("alpha");
-        margin = par("margin");
-        variance = par("variance");
-        angle = deg(fmod(deg(angle).get(), 360));
+        if (alpha < 0.0 || alpha > 1.0)
+            throw cRuntimeError("The parameter 'alpha' is out of [0;1] interval");
         //constrain alpha to [0.0;1.0]
-        alpha = fmax(0.0, alpha);
-        alpha = fmin(1.0, alpha);
-
+        margin = par("margin");
         speed = speedMean;
         angle = angleMean;
         stationary = (speed == 0);
@@ -87,17 +79,15 @@ void GaussMarkovMobility::setTargetPosition()
     // calculate new speed and direction based on the model
     speed = alpha * speed
         + (1.0 - alpha) * speedMean
-        + sqrt(1.0 - alpha * alpha)
-        * normal(0.0, 1.0)
-        * variance;
+        + sqrt(1.0 - alpha * alpha) * normal(0.0, 1.0) * speedStdDev;
 
     angle = alpha * angle
         + (1.0 - alpha) * angleMean
-        + rad(sqrt(1.0 - alpha * alpha) * normal(0.0, 1.0) * variance);
+        + rad(sqrt(1.0 - alpha * alpha) * normal(0.0, 1.0) * angleStdDev);
 
     Coord direction(cos(rad(angle).get()), sin(rad(angle).get()));
     nextChange = simTime() + updateInterval;
-    targetPosition = lastPosition + direction * speed * updateInterval.dbl();
+    targetPosition = lastPosition + direction * (speed * updateInterval.dbl());
 
     EV_DEBUG << " speed = " << speed << " angle = " << angle << endl;
     EV_DEBUG << " mspeed = " << speedMean << " mangle = " << angleMean << endl;
