cccdd32b3e7cd1be8b4c9dd45dc81e6782243f9a
Zoltan Bojthe <zbojthe@omnetpp.org>
2019-11-18 15:56:37 +0100

    gueueing: added TOS support

--- src/inet/queueing/base/PacketTaggerBase.h
+++ src/inet/queueing/base/PacketTaggerBase.h
@@ -27,6 +27,7 @@ class INET_API PacketTaggerBase : public PacketMarkerBase
 {
   protected:
     int dscp = -1;
+    int tos = -1;
     int userPriority = -1;
     int interfaceId = -1;
     int hopLimit = -1;
