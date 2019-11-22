3b290c1847b42238172430b68c9106741b18b714
Zoltan Bojthe <zbojthe@omnetpp.org>
2019-11-22 16:39:44 +0100

    bmac: fixes for serializer
    - converting startPos to B maybe generates conversion error
    - headerlength specified in bit unit, not in Byte
    - fixed reading/writng of remainder part to bit precision
    - unknown BMac header type:
      - filled the type and length fields,
      - fixed reading of remainder part

--- src/inet/linklayer/bmac/BMacHeaderSerializer.cc
+++ src/inet/linklayer/bmac/BMacHeaderSerializer.cc
@@ -25,10 +25,10 @@ Register_Serializer(BMacDataFrameHeader, BMacHeaderSerializer);
 
 void BMacHeaderSerializer::serialize(MemoryOutputStream& stream, const Ptr<const Chunk>& chunk) const
 {
-    B startPos = B(stream.getLength());
+    b startPos = stream.getLength();
     const auto& header = staticPtrCast<const BMacHeaderBase>(chunk);
     stream.writeByte(header->getType());
-    stream.writeByte(B(header->getChunkLength()).get());
+    stream.writeUint16Be(b(header->getChunkLength()).get());
     stream.writeMacAddress(header->getSrcAddr());
     stream.writeMacAddress(header->getDestAddr());
     switch (header->getType()) {
@@ -45,17 +45,20 @@ void BMacHeaderSerializer::serialize(MemoryOutputStream& stream, const Ptr<const
         default:
             throw cRuntimeError("Unknown header type: %d", header->getType());
     }
-    if ((B(stream.getLength()) - startPos) > B(header->getChunkLength()))
-        throw cRuntimeError("BMacHeader length = %d smaller than required %d bytes, try to increase the 'headerLength' parameter", B(header->getChunkLength()).get(), B(stream.getLength() - startPos).get());
-    while ((B(stream.getLength()) - startPos) < B(header->getChunkLength()))
-        stream.writeByte('?');
+    auto remainderBits = b(header->getChunkLength() - (stream.getLength() - startPos)).get();
+    if (remainderBits < 0)
+        throw cRuntimeError("BMacHeader length = %s smaller than required %s, try to increase the 'headerLength' parameter", header->getChunkLength().str().c_str(), (stream.getLength() - startPos).str().c_str());
+    if (remainderBits >= 8)
+        stream.writeByteRepeatedly('?', remainderBits >> 3);
+    if (remainderBits & 7)
+        stream.writeBitRepeatedly(0, remainderBits & 7);
 }
 
 const Ptr<Chunk> BMacHeaderSerializer::deserialize(MemoryInputStream& stream) const
 {
-    B startPos = stream.getPosition();
+    b startPos = stream.getPosition();
     BMacType type = static_cast<BMacType>(stream.readByte());
-    uint8_t length = stream.readByte();
+    b length = b(stream.readUint16Be());
     MacAddress srcAddr = stream.readMacAddress();
     MacAddress destAddr = stream.readMacAddress();
     switch (type) {
@@ -63,31 +66,42 @@ const Ptr<Chunk> BMacHeaderSerializer::deserialize(MemoryInputStream& stream) co
         case BMAC_ACK: {
             auto ctrlFrame = makeShared<BMacControlFrame>();
             ctrlFrame->setType(type);
-            ctrlFrame->setChunkLength(B(length));
+            ctrlFrame->setChunkLength(length);
             ctrlFrame->setSrcAddr(srcAddr);
             ctrlFrame->setDestAddr(destAddr);
-            while (B(length) - (stream.getPosition() - startPos) > B(0))
-                stream.readByte();
+            auto remainderBits = b(length - (stream.getPosition() - startPos)).get();
+            if (remainderBits >= 8)
+                stream.readByteRepeatedly('?', remainderBits >> 3);
+            if (remainderBits & 7)
+                stream.readBitRepeatedly(0, remainderBits & 7);
             return ctrlFrame;
         }
         case BMAC_DATA: {
             auto dataFrame = makeShared<BMacDataFrameHeader>();
             dataFrame->setType(type);
-            dataFrame->setChunkLength(B(length));
+            dataFrame->setChunkLength(length);
             dataFrame->setSrcAddr(srcAddr);
             dataFrame->setDestAddr(destAddr);
             dataFrame->setSequenceId(stream.readUint64Be());
             dataFrame->setNetworkProtocol(stream.readUint16Be());
-            while (B(length) - (stream.getPosition() - startPos) > B(0))
-                stream.readByte();
+            auto remainderBits = b(length - (stream.getPosition() - startPos)).get();
+            if (remainderBits >= 8)
+                stream.readByteRepeatedly('?', remainderBits >> 3);
+            if (remainderBits & 7)
+                stream.readBitRepeatedly(0, remainderBits & 7);
             return dataFrame;
         }
         default: {
-            auto BMacHeader = makeShared<BMacHeaderBase>();
-            while (B(length) - (stream.getPosition() - startPos) > B(0))
-                stream.readByte();
-            BMacHeader->markIncorrect();
-            return BMacHeader;
+            auto unknownFrame = makeShared<BMacHeaderBase>();
+            unknownFrame->setType(type);
+            unknownFrame->setChunkLength(length);
+            auto remainderBits = b(length - (stream.getPosition() - startPos)).get();
+            if (remainderBits >= 8)
+                stream.readByteRepeatedly('?', remainderBits >> 3);
+            if (remainderBits & 7)
+                stream.readBitRepeatedly(0, remainderBits & 7);
+            unknownFrame->markIncorrect();
+            return unknownFrame;
         }
     }
 }
