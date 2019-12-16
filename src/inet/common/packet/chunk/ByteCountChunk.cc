22e74707e29ef4d4b213275f71153ab00273002c
Levente Mészáros <levy@omnetpp.org>
2019-12-16 13:02:45 +0100

    Common: Fixed ByteCountChunk and BitCountChunk str() data field

--- src/inet/common/packet/chunk/ByteCountChunk.cc
+++ src/inet/common/packet/chunk/ByteCountChunk.cc
@@ -124,7 +124,7 @@ void ByteCountChunk::doRemoveAtBack(b length)
 std::string ByteCountChunk::str() const
 {
     std::ostringstream os;
-    os << "ByteCountChunk, length = " << B(getChunkLength()) << ", data = " << data;
+    os << "ByteCountChunk, length = " << B(getChunkLength()) << ", data = " << (int)data;
     return os.str();
 }
 
