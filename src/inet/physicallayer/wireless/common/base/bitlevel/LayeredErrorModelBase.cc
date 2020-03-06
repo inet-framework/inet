0f42fe377b79ea5f3c36ce8a0b29010cea73d102
Levente Mészáros <levy@omnetpp.org>
2020-03-06 14:56:14 +0100

    Radio: Refactored stochastic symbol level corruption in LayeredErrorModelBase.
    Added symbolCorruptionMode parameter.

--- src/inet/physicallayer/wireless/common/base/bitlevel/LayeredErrorModelBase.cc
+++ src/inet/physicallayer/wireless/common/base/bitlevel/LayeredErrorModelBase.cc
@@ -4,7 +4,6 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 //
 
-
 #include "inet/physicallayer/wireless/common/base/bitlevel/LayeredErrorModelBase.h"
 
 #include "inet/physicallayer/wireless/apsk/bitlevel/ApskSymbol.h"
@@ -14,9 +13,16 @@
 #include "inet/physicallayer/wireless/common/radio/bitlevel/SignalPacketModel.h"
 #include "inet/physicallayer/wireless/common/radio/bitlevel/SignalSampleModel.h"
 #include "inet/physicallayer/wireless/common/radio/bitlevel/SignalSymbolModel.h"
+// TODO AFTER REBASE: add missing headers here
 
 namespace inet {
 namespace physicallayer {
+
+void LayeredErrorModelBase::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+        symbolCorruptionMode = par("symbolCorruptionMode");
+}
 
 const IReceptionPacketModel *LayeredErrorModelBase::computePacketModel(const LayeredTransmission *transmission, double packetErrorRate) const
 {
@@ -26,50 +32,62 @@ const IReceptionPacketModel *LayeredErrorModelBase::computePacketModel(const Lay
     if (packetErrorRate != 0 && uniform(0, 1) < packetErrorRate)
         receivedPacket->setBitError(true);
     receivedPacket->addTagIfAbsent<ErrorRateInd>()->setPacketErrorRate(packetErrorRate);
-    auto bitrate = transmissionPacketModel->getDataNetBitrate();
-    return new ReceptionPacketModel(receivedPacket, bitrate, bitrate);
+    return new ReceptionPacketModel(receivedPacket, transmissionPacketModel->getHeaderNetBitrate(), transmissionPacketModel->getDataNetBitrate(), packetErrorRate);
 }
 
 const IReceptionBitModel *LayeredErrorModelBase::computeBitModel(const LayeredTransmission *transmission, double bitErrorRate) const
 {
     const TransmissionBitModel *transmissionBitModel = check_and_cast<const TransmissionBitModel *>(transmission->getBitModel());
     if (bitErrorRate == 0)
-        return new ReceptionBitModel(transmissionBitModel->getHeaderLength(), transmissionBitModel->getHeaderGrossBitrate(), transmissionBitModel->getDataLength(), transmissionBitModel->getDataGrossBitrate(), new BitVector(*transmissionBitModel->getAllBits()), NaN);
+        return new ReceptionBitModel(transmissionBitModel->getHeaderLength(), transmissionBitModel->getHeaderGrossBitrate(), transmissionBitModel->getDataLength(), transmissionBitModel->getDataGrossBitrate(), new BitVector(*transmissionBitModel->getAllBits()), bitErrorRate);
     else {
         BitVector *receivedBits = new BitVector(*transmissionBitModel->getAllBits());
-        for (unsigned int i = 0; i < receivedBits->getSize(); i++) {
+        for (unsigned int i = 0; i < receivedBits->getSize(); i++)
             if (uniform(0, 1) < bitErrorRate)
                 receivedBits->toggleBit(i);
-        }
         return new ReceptionBitModel(transmissionBitModel->getHeaderLength(), transmissionBitModel->getHeaderGrossBitrate(), transmissionBitModel->getDataLength(), transmissionBitModel->getDataGrossBitrate(), receivedBits, bitErrorRate);
     }
 }
 
 const IReceptionSymbolModel *LayeredErrorModelBase::computeSymbolModel(const LayeredTransmission *transmission, double symbolErrorRate) const
 {
-    if (symbolErrorRate == 0) {
-        const TransmissionSymbolModel *transmissionSymbolModel = check_and_cast<const TransmissionSymbolModel *>(transmission->getSymbolModel());
-        return new ReceptionSymbolModel(transmissionSymbolModel->getHeaderSymbolLength(), transmissionSymbolModel->getHeaderSymbolRate(), transmissionSymbolModel->getDataSymbolLength(), transmissionSymbolModel->getDataSymbolRate(), new std::vector<const ISymbol *>(*transmissionSymbolModel->getAllSymbols()), symbolErrorRate);
-    }
+    auto transmissionSymbolModel = check_and_cast<const TransmissionSymbolModel *>(transmission->getSymbolModel());
+    if (symbolErrorRate == 0)
+        return new ReceptionSymbolModel(transmissionSymbolModel->getHeaderSymbolLength(), transmissionSymbolModel->getHeaderSymbolRate(), transmissionSymbolModel->getDataSymbolLength(), transmissionSymbolModel->getDataSymbolRate(), new std::vector<const ISymbol*>(*transmissionSymbolModel->getAllSymbols()));
     else {
-        const TransmissionSymbolModel *transmissionSymbolModel = check_and_cast<const TransmissionSymbolModel *>(transmission->getSymbolModel());
-        const ApskModulationBase *modulation = check_and_cast<const ApskModulationBase *>(transmissionSymbolModel->getDataModulation());
-        const std::vector<const ISymbol *> *transmittedSymbols = transmissionSymbolModel->getAllSymbols();
-        std::vector<const ISymbol *> *receivedSymbols = new std::vector<const ISymbol *>();
-        for (auto& transmittedSymbols_i : *transmittedSymbols) {
-            if (uniform(0, 1) < symbolErrorRate) {
-                const ApskSymbol *transmittedSymbol = check_and_cast<const ApskSymbol *>(transmittedSymbols_i);
-                ShortBitVector bits = modulation->demapToBitRepresentation(transmittedSymbol);
-                int errorIndex = intuniform(0, bits.getSize() - 1);
-                bits.setBit(errorIndex, !bits.getBit(errorIndex));
-                const ApskSymbol *receivedSymbol = modulation->mapToConstellationDiagram(bits);
-                receivedSymbols->push_back(receivedSymbol);
-            }
-            else
-                receivedSymbols->push_back(transmittedSymbols_i);
+        auto modulation = check_and_cast<const ApskModulationBase *>(transmissionSymbolModel->getDataModulation());
+        auto transmittedSymbols = transmissionSymbolModel->getAllSymbols();
+        int symbolCount = transmittedSymbols->size();
+        auto receivedSymbols = new std::vector<const ISymbol *>(symbolCount);
+        for (int i = 0; i < symbolCount; i++) {
+            auto transmittedSymbol = check_and_cast<const ApskSymbol *>(transmittedSymbols->at(i));
+            bool isCorruptSymbol = symbolErrorRate == 1 || (symbolErrorRate != 0 && uniform(0, 1) < symbolErrorRate);
+            auto receivedSymbol = isCorruptSymbol ? computeCorruptSymbol(modulation, transmittedSymbol) : transmittedSymbol;
+            receivedSymbols->at(i) = receivedSymbol;
         }
-        return new ReceptionSymbolModel(transmissionSymbolModel->getHeaderSymbolLength(), transmissionSymbolModel->getHeaderSymbolRate(), transmissionSymbolModel->getDataSymbolLength(), transmissionSymbolModel->getDataSymbolRate(), receivedSymbols, symbolErrorRate);
+        return new ReceptionSymbolModel(transmissionSymbolModel->getHeaderSymbolLength(), transmissionSymbolModel->getHeaderSymbolRate(), transmissionSymbolModel->getDataSymbolLength(), transmissionSymbolModel->getDataSymbolRate(), receivedSymbols);
     }
+}
+
+const ISymbol *LayeredErrorModelBase::computeCorruptSymbol(const ApskModulationBase *modulation, const ApskSymbol *transmittedSymbol) const
+{
+    auto constellation = modulation->getConstellation();
+    if (!strcmp("uniform", symbolCorruptionMode)) {
+        auto index = intuniform(0, constellation->size() - 2);
+        auto receivedSymbol = &constellation->at(index);
+        if (receivedSymbol != transmittedSymbol)
+            return receivedSymbol;
+        else
+            return &constellation->at(constellation->size() - 1);
+    }
+    else if (!strcmp("1bit", symbolCorruptionMode)) {
+        auto bits = modulation->demapToBitRepresentation(transmittedSymbol);
+        int errorIndex = intuniform(0, bits.getSize() - 1);
+        bits.setBit(errorIndex, !bits.getBit(errorIndex));
+        return modulation->mapToConstellationDiagram(bits);
+    }
+    else
+        throw cRuntimeError("Unknown symbolCorruptionMode parameter value");
 }
 
 } // namespace physicallayer
--- src/inet/physicallayer/wireless/common/base/bitlevel/LayeredErrorModelBase.h
+++ src/inet/physicallayer/wireless/common/base/bitlevel/LayeredErrorModelBase.h
@@ -9,6 +9,7 @@
 #define __INET_LAYEREDERRORMODELBASE_H
 
 #include "inet/physicallayer/wireless/common/contract/bitlevel/ILayeredErrorModel.h"
+#include "inet/physicallayer/base/packetlevel/ApskModulationBase.h"
 
 namespace inet {
 
@@ -17,9 +18,17 @@ namespace physicallayer {
 class INET_API LayeredErrorModelBase : public cModule, public ILayeredErrorModel
 {
   protected:
+    const char *symbolCorruptionMode = nullptr;
+
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+
     virtual const IReceptionPacketModel *computePacketModel(const LayeredTransmission *transmission, double packetErrorRate) const;
     virtual const IReceptionBitModel *computeBitModel(const LayeredTransmission *transmission, double bitErrorRate) const;
     virtual const IReceptionSymbolModel *computeSymbolModel(const LayeredTransmission *transmission, double symbolErrorRate) const;
+
+    virtual const ISymbol *computeCorruptSymbol(const ApskModulationBase *modulation, const ApskSymbol *transmittedSymbol) const;
 };
 
 } // namespace physicallayer
