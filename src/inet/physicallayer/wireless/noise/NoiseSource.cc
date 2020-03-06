74c440ca5fcea2ad731b12cdb7f19fbfc282f9b9
Levente Mészáros <levy@omnetpp.org>
2020-03-06 14:50:11 +0100

    Radio: Added startTime parameter to NoiseSource and fixed sleepInterval parameter.
    The new version interprets sleepInterval parameter to specify the duration between
    the transmission start of subsequent noise signals.

--- src/inet/physicallayer/wireless/noise/NoiseSource.cc
+++ src/inet/physicallayer/wireless/noise/NoiseSource.cc
@@ -39,7 +39,7 @@ void NoiseSource::initialize(int stage)
     }
     else if (stage == INITSTAGE_PHYSICAL_LAYER) {
         medium->addRadio(this);
-        scheduleSleepTimer();
+        scheduleAt(par("startTime"), sleepTimer);
     }
 }
 
@@ -62,13 +62,14 @@ void NoiseSource::startTransmission()
 
 void NoiseSource::endTransmission()
 {
+    Signal *signal = static_cast<Signal *>(transmissionTimer->getContextPointer());
     transmissionTimer->setContextPointer(nullptr);
-    scheduleSleepTimer();
+    scheduleSleepTimer(signal->getDuration());
 }
 
-void NoiseSource::scheduleSleepTimer()
+void NoiseSource::scheduleSleepTimer(simtime_t transmissionDuration)
 {
-    scheduleAfter(par("sleepInterval"), sleepTimer);
+    scheduleAfter(par("sleepInterval") - transmissionDuration, sleepTimer);
 }
 
 void NoiseSource::scheduleTransmissionTimer(const ITransmission *transmission)
