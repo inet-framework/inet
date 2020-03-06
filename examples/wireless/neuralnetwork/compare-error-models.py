82fdd5ddf31983a62f4c2b1ed9f28355e36f2b28
Levente Mészáros <levy@omnetpp.org>
2020-03-06 16:44:20 +0100

    Examples: Added command line arguments to control what should be run.

--- examples/wireless/neuralnetwork/compare-error-models.py
+++ examples/wireless/neuralnetwork/compare-error-models.py
@@ -5,11 +5,26 @@ import sys
 import argparse
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--scalarPacketlevel", default=False)
+parser.add_argument("-sn", "--scalarPacketlevelNeuralNetwork", default=False)
+parser.add_argument("-d", "--dimensionalPacketlevel", default=True)
+parser.add_argument("-dn", "--dimensionalPacketlevelNeuralNetwork", default=True)
+parser.add_argument("-b", "--dimensionalBitlevel", default=True)
+parser.add_argument("-o", "--output", default="/dev/null")
 parser.add_argument("inifile")
 args = parser.parse_args()
 
-os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevel")
-os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevelNeuralNetwork")
-os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevel")
-os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevelNeuralNetwork")
-os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalBitlevel")
+if args.scalarPacketlevel :
+    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevel --cmdenv-output-file={args.output}")
+
+if args.scalarPacketlevelNeuralNetwork :
+    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevelNeuralNetwork --cmdenv-output-file={args.output}")
+
+if args.dimensionalPacketlevel :
+    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevel --cmdenv-output-file={args.output}")
+
+if args.dimensionalPacketlevelNeuralNetwork :
+    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevelNeuralNetwork --cmdenv-output-file={args.output}")
+
+if args.dimensionalBitlevel :
+    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalBitlevel --cmdenv-output-file={args.output}")
