c6dc5a7e7dc01749328640fa95be862071a516eb
Levente Mészáros <levy@omnetpp.org>
2020-03-06 16:29:32 +0100

    Radio: Added checks for transmission type when deciding if reception is possible to layered radios.

--- src/inet/physicallayer/ieee80211/bitlevel/Ieee80211LayeredOfdmReceiver.h
+++ src/inet/physicallayer/ieee80211/bitlevel/Ieee80211LayeredOfdmReceiver.h
@@ -96,6 +96,7 @@ class INET_API Ieee80211LayeredOfdmReceiver : public SnirReceiverBase
   public:
     virtual ~Ieee80211LayeredOfdmReceiver();
 
+    virtual bool computeIsReceptionPossible(const IListening *listening, const ITransmission *transmission) const override;
     virtual bool computeIsReceptionPossible(const IListening *listening, const IReception *reception, IRadioSignal::SignalPart part) const override;
     virtual const IListeningDecision *computeListeningDecision(const IListening *listening, const IInterference *interference) const override;
     virtual const IListening *createListening(const IRadio *radio, const simtime_t startTime, const simtime_t endTime, const Coord startPosition, const Coord endPosition) const override;
