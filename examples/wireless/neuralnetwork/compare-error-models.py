ef84cb616c7bfc1ba7318dc2648d02876a6d4e4d
Levente Mészáros <levy@omnetpp.org>
2020-03-17 14:47:33 +0100

    Examples: Added new chart to neural network based error model example.

--- examples/wireless/neuralnetwork/compare-error-models.py
+++ examples/wireless/neuralnetwork/compare-error-models.py
@@ -9,22 +9,21 @@ parser.add_argument("-s", "--scalarPacketlevel", default=False)
 parser.add_argument("-sn", "--scalarPacketlevelNeuralNetwork", default=False)
 parser.add_argument("-d", "--dimensionalPacketlevel", default=True)
 parser.add_argument("-dn", "--dimensionalPacketlevelNeuralNetwork", default=True)
-parser.add_argument("-b", "--dimensionalBitlevel", default=True)
-parser.add_argument("-o", "--output", default="/dev/null")
+parser.add_argument("-b", "--dimensionalSymbollevel", default=True)
 parser.add_argument("inifile")
 args = parser.parse_args()
 
 if args.scalarPacketlevel :
-    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevel --cmdenv-output-file={args.output}")
+    os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevel")
 
 if args.scalarPacketlevelNeuralNetwork :
-    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevelNeuralNetwork --cmdenv-output-file={args.output}")
+    os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevelNeuralNetwork")
 
 if args.dimensionalPacketlevel :
-    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevel --cmdenv-output-file={args.output}")
+    os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevel")
 
 if args.dimensionalPacketlevelNeuralNetwork :
-    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevelNeuralNetwork --cmdenv-output-file={args.output}")
+    os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevelNeuralNetwork")
 
-if args.dimensionalBitlevel :
-    os.system(f"opp_runall inet -f {args.inifile} -u Cmdenv -c DimensionalBitlevel --cmdenv-output-file={args.output}")
+if args.dimensionalSymbollevel :
+    os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c DimensionalSymbollevel")
