abec5fef5c36c076930611468d97f4c4ee9e3eac
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-04-07 19:59:35 +0200

    code style

--- src/inet/common/FingerprintCalculator.h
+++ src/inet/common/FingerprintCalculator.h
@@ -30,7 +30,7 @@ class INET_API FingerprintCalculator : public cSingleFingerprintCalculator
         NETWORK_NODE_PATH            = 'N',
         NETWORK_INTERFACE_PATH       = 'I',
         PACKET_DATA                  = 'D',
-        PROGRESS_CONVERTER_FILTER              = 'P',     // decode cProgress::PACKET_END to packet, filtering out other cProgress messages
+        PROGRESS_CONVERTER_FILTER    = 'P',     // decode cProgress::PACKET_END to packet, filtering out other cProgress messages
     };
 
   protected:
