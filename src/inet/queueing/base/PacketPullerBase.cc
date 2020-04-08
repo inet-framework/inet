96a5a504bbc1134f3454af2246653d72414674da
Levente Mészáros <levy@omnetpp.org>
2020-04-08 16:57:53 +0200

    Queueing: Added new interfaces IPacketPuller, IPacketPusher, IPacketFlow and base classes PacketPullerBase, PacketPusherBase, PacketFlowBase.

---
+++ src/inet/queueing/base/PacketPullerBase.cc
@@ -0,0 +1,59 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see http://www.gnu.org/licenses/.
+//
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/Simsignals.h"
+#include "inet/queueing/base/PacketPullerBase.h"
+
+namespace inet {
+namespace queueing {
+
+void PacketPullerBase::initialize(int stage)
+{
+    PacketProcessorBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        inputGate = gate("in");
+        outputGate = gate("out");
+        collector = findConnectedModule<IActivePacketSink>(outputGate);
+        provider = findConnectedModule<IPassivePacketSource>(inputGate);
+    }
+    else if (stage == INITSTAGE_QUEUEING) {
+        checkPacketOperationSupport(inputGate);
+        checkPacketOperationSupport(outputGate);
+    }
+}
+
+bool PacketPullerBase::canPullSomePacket(cGate *gate) const
+{
+    return provider->canPullSomePacket(inputGate->getPathStartGate());
+}
+
+Packet *PacketPullerBase::canPullPacket(cGate *gate) const
+{
+    return provider->canPullPacket(inputGate->getPathStartGate());
+}
+
+void PacketPullerBase::handleCanPullPacket(cGate *gate)
+{
+    Enter_Method("handleCanPullPacket");
+    if (collector != nullptr)
+        collector->handleCanPullPacket(outputGate);
+}
+
+} // namespace queueing
+} // namespace inet
+
---
+++ src/inet/queueing/base/PacketPullerBase.h
@@ -0,0 +1,57 @@
+//
+// Copyright (C) OpenSim Ltd.
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program; if not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef __INET_PACKETPULLERBASE_H
+#define __INET_PACKETPULLERBASE_H
+
+#include "inet/queueing/base/PacketProcessorBase.h"
+#include "inet/queueing/contract/IActivePacketSink.h"
+#include "inet/queueing/contract/IPacketPuller.h"
+#include "inet/queueing/contract/IPassivePacketSource.h"
+
+namespace inet {
+namespace queueing {
+
+class INET_API PacketPullerBase : public PacketProcessorBase, public virtual IPacketPuller
+{
+  protected:
+    cGate *inputGate = nullptr;
+    IPassivePacketSource *provider = nullptr;
+
+    cGate *outputGate = nullptr;
+    IActivePacketSink *collector = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+
+  public:
+    virtual IPassivePacketSource *getProvider(cGate *gate) override { return this; }
+
+    virtual bool supportsPacketPushing(cGate *gate) const override { return false; }
+    virtual bool supportsPacketPulling(cGate *gate) const override { return true; }
+
+    virtual bool canPullSomePacket(cGate *gate) const override;
+    virtual Packet *canPullPacket(cGate *gate) const override;
+
+    virtual void handleCanPullPacket(cGate *gate) override;
+};
+
+} // namespace queueing
+} // namespace inet
+
+#endif // ifndef __INET_PACKETPULLERBASE_H
+
