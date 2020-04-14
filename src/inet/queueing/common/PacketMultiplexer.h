22c6e19562306e19d672c81e37bef65b433130cb
Levente Mészáros <levy@omnetpp.org>
2020-04-14 13:28:35 +0200

    Queueing: Implemented packet streaming support in PacketMultiplexer.

--- src/inet/queueing/common/PacketMultiplexer.h
+++ src/inet/queueing/common/PacketMultiplexer.h
@@ -33,21 +33,39 @@ class INET_API PacketMultiplexer : public PassivePacketSinkBase, public IActiveP
     cGate *outputGate = nullptr;
     IPassivePacketSink *consumer = nullptr;
 
+    int inProgressStreamId = -1;
+
   protected:
     virtual void initialize(int stage) override;
+
+    virtual bool isStreamingPacket() const { return inProgressStreamId != -1; }
+    virtual void startPacketStreaming(Packet *packet);
+    virtual void endPacketStreaming(Packet *packet);
+    virtual void checkPacketStreaming(Packet *packet);
 
   public:
     virtual IPassivePacketSink *getConsumer(cGate *gate) override { return consumer; }
 
     virtual bool supportsPacketPulling(cGate *gate) const override { return false; }
     virtual bool supportsPacketPushing(cGate *gate) const override { return true; }
+    virtual bool supportsPacketStreaming(cGate *gate) const override { return true; }
 
     virtual bool canPushSomePacket(cGate *gate) const override { return consumer->canPushSomePacket(outputGate); }
     virtual bool canPushPacket(Packet *packet, cGate *gate) const override { return consumer->canPushPacket(packet, gate); }
+
     virtual void pushPacket(Packet *packet, cGate *gate) override;
+
+    virtual void pushPacketStart(Packet *packet, cGate *gate) override;
+    virtual void pushPacketProgress(Packet *packet, cGate *gate, b position, b extraProcessableLength = b(0)) override;
+    virtual void pushPacketEnd(Packet *packet, cGate *gate) override;
+
+    virtual b getPushPacketProcessedLength(Packet *packet, cGate *gate) override;
 
     virtual void handleCanPushPacket(cGate *gate) override;
     virtual void handlePushPacketProcessed(Packet *packet, cGate *gate, bool successful) override;
+
+    virtual void handleRegisterService(const Protocol& protocol, cGate *out, ServicePrimitive servicePrimitive) override;
+    virtual void handleRegisterProtocol(const Protocol& protocol, cGate *in, ServicePrimitive servicePrimitive) override;
 };
 
 } // namespace queueing
