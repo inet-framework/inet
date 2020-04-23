769ab9f9b3d33754b56d78fa199627a9448d3a68
Levente Mészáros <levy@omnetpp.org>
2020-04-23 11:18:04 +0200

    Queueing: Refactored PacketGate in preparation for a separate PeriodicGate.

--- src/inet/queueing/gate/PacketGate.cc
+++ src/inet/queueing/gate/PacketGate.cc
@@ -15,6 +15,7 @@
 // along with this program; if not, see http://www.gnu.org/licenses/.
 //
 
+#include <algorithm>
 #include "inet/common/ModuleAccess.h"
 #include "inet/queueing/gate/PacketGate.h"
 
@@ -22,52 +23,38 @@ namespace inet {
 namespace queueing {
 
 Define_Module(PacketGate);
-
 void PacketGate::initialize(int stage)
 {
-    PacketFilterBase::initialize(stage);
+    PacketGateBase::initialize(stage);
     if (stage == INITSTAGE_LOCAL) {
         isOpen_ = par("initiallyOpen");
+        openTime = par("openTime");
+        closeTime = par("closeTime");
         changeTimer = new cMessage("ChangeTimer");
-        double openTime = par("openTime");
-        if (!std::isnan(openTime))
-            changeTimes.push_back(openTime);
-        double closeTime = par("closeTime");
-        if (!std::isnan(closeTime))
-            changeTimes.push_back(closeTime);
-        const char *changeTimesAsString = par("changeTimes");
-        cStringTokenizer tokenizer(changeTimesAsString);
-        while (tokenizer.hasMoreTokens())
-            changeTimes.push_back(SimTime::parse(tokenizer.nextToken()));
-
     }
-    else if (stage == INITSTAGE_QUEUEING) {
-        if (changeIndex < (int)changeTimes.size())
-            scheduleChangeTimer();
-    }
+    else if (stage == INITSTAGE_QUEUEING)
+        scheduleChangeTimer();
 }
 
 void PacketGate::handleMessage(cMessage *message)
 {
     if (message == changeTimer) {
         processChangeTimer();
-        if (changeIndex < (int)changeTimes.size())
-            scheduleChangeTimer();
+        scheduleChangeTimer();
     }
     else
         throw cRuntimeError("Unknown message");
 }
 
-bool PacketGate::matchesPacket(const Packet *packet) const
-{
-    return isOpen_;
-}
-
 void PacketGate::scheduleChangeTimer()
 {
-    ASSERT(0 <= changeIndex && changeIndex < (int)changeTimes.size());
-    scheduleAt(changeTimes[changeIndex], changeTimer);
-    changeIndex++;
+    simtime_t changeTime;
+    if (isOpen_)
+        changeTime = closeTime;
+    else
+        changeTime = openTime;
+    if (changeTime > simTime())
+        scheduleAt(changeTime, changeTimer);
 }
 
 void PacketGate::processChangeTimer()
@@ -76,58 +63,6 @@ void PacketGate::processChangeTimer()
         close();
     else
         open();
-}
-
-void PacketGate::open()
-{
-    ASSERT(!isOpen_);
-    EV_DEBUG << "Opening gate.\n";
-    isOpen_ = true;
-    if (producer != nullptr)
-        producer->handleCanPushPacket(inputGate->getPathStartGate());
-    if (collector != nullptr)
-        collector->handleCanPullPacket(outputGate->getPathEndGate());
-}
-
-void PacketGate::close()
-{
-    ASSERT(isOpen_);
-    EV_DEBUG << "Closing gate.\n";
-    isOpen_ = false;
-}
-
-bool PacketGate::canPushSomePacket(cGate *gate) const
-{
-    return isOpen_ && consumer->canPushSomePacket(outputGate->getPathStartGate());
-}
-
-bool PacketGate::canPushPacket(Packet *packet, cGate *gate) const
-{
-    return isOpen_ && consumer->canPushPacket(packet, outputGate->getPathStartGate());
-}
-
-bool PacketGate::canPullSomePacket(cGate *gate) const
-{
-    return isOpen_ && provider->canPullSomePacket(inputGate->getPathStartGate());
-}
-
-Packet *PacketGate::canPullPacket(cGate *gate) const
-{
-    return isOpen_ ? provider->canPullPacket(inputGate->getPathStartGate()) : nullptr;
-}
-
-void PacketGate::handleCanPushPacket(cGate *gate)
-{
-    Enter_Method("handleCanPushPacket");
-    if (isOpen_)
-        PacketFilterBase::handleCanPushPacket(gate);
-}
-
-void PacketGate::handleCanPullPacket(cGate *gate)
-{
-    Enter_Method("handleCanPullPacket");
-    if (isOpen_)
-        PacketFilterBase::handleCanPullPacket(gate);
 }
 
 } // namespace queueing
