9f14c69c91bcb01cb5fc3fbd4e00456b0d88007d
Levente Mészáros <levy@omnetpp.org>
2020-05-14 16:26:29 +0200

    Queueing: Fixed EmptyPacketSource.

--- src/inet/queueing/source/EmptyPacketSource.h
+++ src/inet/queueing/source/EmptyPacketSource.h
@@ -18,14 +18,27 @@
 #ifndef __INET_EMPTYPACKETSOURCE_H
 #define __INET_EMPTYPACKETSOURCE_H
 
-#include "inet/queueing/base/ActivePacketSourceBase.h"
+#include "inet/queueing/base/PacketProcessorBase.h"
+#include "inet/queueing/contract/IActivePacketSource.h"
 
 namespace inet {
 namespace queueing {
 
-class INET_API EmptyPacketSource : public ActivePacketSourceBase
+class INET_API EmptyPacketSource : public PacketProcessorBase, public virtual IActivePacketSource
 {
+  protected:
+    cGate *outputGate = nullptr;
+    IPassivePacketSink *consumer = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+
   public:
+    virtual IPassivePacketSink *getConsumer(cGate *gate) override { return consumer; }
+
+    virtual bool supportsPacketPushing(cGate *gate) const override { return outputGate == gate; }
+    virtual bool supportsPacketPulling(cGate *gate) const override { return false; }
+
     virtual void handleCanPushPacket(cGate *gate) override;
     virtual void handlePushPacketProcessed(Packet *packet, cGate *gate, bool successful) override;
 };
