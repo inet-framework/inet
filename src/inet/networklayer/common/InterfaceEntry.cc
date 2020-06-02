bbe103db76a3672a7c6b30248ffb71051899b1f5
Levente Mészáros <levy@omnetpp.org>
2020-06-02 10:34:10 +0200

    InterfaceEntry: Reverted back protocolDataSet to use TagSet.
    Shared pointers unnecessarily complicate things.

--- src/inet/networklayer/common/InterfaceEntry.cc
+++ src/inet/networklayer/common/InterfaceEntry.cc
@@ -86,7 +86,7 @@ void InterfaceEntry::clearProtocolDataSet()
     int n = protocolDataSet.getNumTags();
     ids.reserve(n);
     for (int i=0; i < n; i++)
-        ids[i] = staticPtrCast<const InterfaceProtocolData>(protocolDataSet.getTag(i))->id;
+        ids[i] = static_cast<const InterfaceProtocolData *>(protocolDataSet.getTag(i))->id;
     protocolDataSet.clearTags();
     for (int i=0; i < n; i++)
         changed(interfaceConfigChangedSignal, ids[i]);
