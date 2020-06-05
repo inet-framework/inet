6824581fceba36d2553a384c2d51a1ed096686fa
Alfonso <aarizaq_m@hotmail.com>
2020-06-05 11:58:06 +0200

    Fix export problem in windows
    Fix export problem in windows and remove some warnings

--- src/inet/transportlayer/common/L4Tools.h
+++ src/inet/transportlayer/common/L4Tools.h
@@ -24,17 +24,17 @@
 
 namespace inet {
 
-const Protocol *findTransportProtocol(Packet *packet);
-const Protocol& getProtocolId(Packet *packet);
+INET_API const Protocol *findTransportProtocol(Packet *packet);
+INET_API const Protocol& getProtocolId(Packet *packet);
 
-const Ptr<const TransportHeaderBase> findTransportProtocolHeader(Packet *packet);
-const Ptr<const TransportHeaderBase> getTransportProtocolHeader(Packet *packet);
+INET_API const Ptr<const TransportHeaderBase> findTransportProtocolHeader(Packet *packet);
+INET_API const Ptr<const TransportHeaderBase> getTransportProtocolHeader(Packet *packet);
 
-bool isTransportProtocol(const Protocol& protocol);
+INET_API bool isTransportProtocol(const Protocol& protocol);
 
-const Ptr<const TransportHeaderBase> peekTransportProtocolHeader(Packet *packet, const Protocol& protocol, int flags = 0);
+INET_API const Ptr<const TransportHeaderBase> peekTransportProtocolHeader(Packet *packet, const Protocol& protocol, int flags = 0);
 
-void insertTransportProtocolHeader(Packet *packet, const Protocol& protocol, const Ptr<TransportHeaderBase>& header);
+INET_API void insertTransportProtocolHeader(Packet *packet, const Protocol& protocol, const Ptr<TransportHeaderBase>& header);
 
 template <typename T>
 const Ptr<T> removeTransportProtocolHeader(Packet *packet)
@@ -43,7 +43,7 @@ const Ptr<T> removeTransportProtocolHeader(Packet *packet)
     return removeProtocolHeader<T>(packet);
 }
 
-const Ptr<TransportHeaderBase> removeTransportProtocolHeader(Packet *packet, const Protocol& protocol);
+INET_API const Ptr<TransportHeaderBase> removeTransportProtocolHeader(Packet *packet, const Protocol& protocol);
 
 }
 
