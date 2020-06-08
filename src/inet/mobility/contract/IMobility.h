377e0512334c0d90438c1a3d0b92464f35880328
Levente Mészáros <levy@omnetpp.org>
2020-06-08 12:18:27 +0200

    Mobility: Replaced license header in IMobility, this class was completely written in-house.

--- src/inet/mobility/contract/IMobility.h
+++ src/inet/mobility/contract/IMobility.h
@@ -1,23 +1,17 @@
-/* -*- mode:c++ -*- ********************************************************
- * file:        IMobility.h
- *
- * author:      Levente Meszaros
- *
- * copyright:   (C) 2004 Telecommunication Networks Group (TKN) at
- *              Technische Universitaet Berlin, Germany.
- *
- *              (C) 2005 Andras Varga
- *
- *              This program is free software; you can redistribute it
- *              and/or modify it under the terms of the GNU General Public
- *              License as published by the Free Software Foundation; either
- *              version 2 of the License, or (at your option) any later
- *              version.
- *              For further information see file COPYING
- *              in the top level directory
- ***************************************************************************
- * part of:     framework implementation developed by tkn
- *************************************************************************/
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
 
 #ifndef __INET_IMOBILITY_H
 #define __INET_IMOBILITY_H
