d1416328f486c6551a2e46857ab29ffaced43e1c
Levente Mészáros <levy@omnetpp.org>
2020-06-19 14:50:43 +0200

    Protocol: Added StreamerReceiver module which streams up received packets.

---
+++ src/inet/protocol/transceiver/StreamerReceiver.cc
@@ -0,0 +1,111 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#include "inet/protocol/transceiver/StreamerReceiver.h"
+
+namespace inet {
+
+Define_Module(StreamerReceiver);
+
+void StreamerReceiver::initialize(int stage)
+{
+    PacketReceiverBase::initialize(stage);
+    OperationalMixin::initialize(stage);
+    if (stage == INITSTAGE_LOCAL)
+        datarate = bps(par("datarate"));
+}
+
+StreamerReceiver::~StreamerReceiver()
+{
+    delete rxSignal;
+}
+
+void StreamerReceiver::handleMessageWhenUp(cMessage *message)
+{
+    if (message->getArrivalGate() == inputGate)
+        receiveFromMedium(message);
+    else
+        PacketReceiverBase::handleMessage(message);
+}
+
+void StreamerReceiver::handleMessageWhenDown(cMessage *msg)
+{
+    if (!msg->isSelfMessage()) {
+        // received on input gate from another network node
+        EV << "Interface is turned off, dropping message (" << msg->getClassName() << ")" << msg->getName() << "\n";
+        delete msg;
+    }
+    else
+        OperationalMixin::handleMessageWhenDown(msg);
+}
+
+void StreamerReceiver::receivePacketStart(cPacket *cpacket, cGate *gate, double datarate)
+{
+    ASSERT(rxSignal == nullptr);
+    take(cpacket);
+    rxSignal = check_and_cast<Signal *>(cpacket);
+    auto packet = decodePacket(rxSignal);
+    pushOrSendPacketStart(packet, outputGate, consumer, bps(datarate));
+}
+
+void StreamerReceiver::receivePacketProgress(cPacket *cpacket, cGate *gate, double datarate, int bitPosition, simtime_t timePosition, int extraProcessableBitLength, simtime_t extraProcessableDuration)
+{
+    take(cpacket);
+    if (rxSignal) {
+        delete rxSignal;
+        rxSignal = check_and_cast<Signal *>(cpacket);
+    }
+    else {
+        EV_WARN << "Signal start doesn't received, drop it";
+        delete cpacket;
+    }
+}
+
+void StreamerReceiver::receivePacketEnd(cPacket *cpacket, cGate *gate, double datarate)
+{
+    take(cpacket);
+    auto signal = check_and_cast<Signal *>(cpacket);
+    if (rxSignal) {
+        delete rxSignal;
+        rxSignal = nullptr;
+        auto packet = decodePacket(signal);
+        pushOrSendPacketEnd(packet, outputGate, consumer, bps(datarate));
+        delete signal;
+    }
+    else {
+        EV_WARN << "Signal start doesn't received, drop it";
+        //TODO drop signal
+        delete signal;
+    }
+}
+
+void StreamerReceiver::handleStartOperation(LifecycleOperation *operation)
+{
+}
+
+void StreamerReceiver::handleStopOperation(LifecycleOperation *operation)
+{
+    delete rxSignal;
+    rxSignal = nullptr;
+}
+
+void StreamerReceiver::handleCrashOperation(LifecycleOperation *operation)
+{
+    delete rxSignal;
+    rxSignal = nullptr;
+}
+
+} // namespace inet
+
---
+++ src/inet/protocol/transceiver/StreamerReceiver.h
@@ -0,0 +1,64 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#ifndef __INET_STREAMERRECEIVER_H
+#define __INET_STREAMERRECEIVER_H
+
+#include "inet/common/lifecycle/ModuleOperations.h"
+#include "inet/common/lifecycle/OperationalMixin.h"
+#include "inet/physicallayer/common/packetlevel/Signal.h"
+#include "inet/protocol/transceiver/base/PacketReceiverBase.h"
+
+namespace inet {
+
+using namespace inet::units::values;
+using namespace inet::physicallayer;
+
+class INET_API StreamerReceiver : public OperationalMixin<PacketReceiverBase>
+{
+  protected:
+    bps datarate = bps(NaN);
+
+    Signal *rxSignal = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage *msg) override { OperationalMixin::handleMessage(msg); }
+    virtual void handleMessageWhenUp(cMessage *message) override;
+    virtual void handleMessageWhenDown(cMessage *message) override;
+
+    virtual void receivePacketStart(cPacket *packet, cGate *gate, double datarate) override;
+    virtual void receivePacketProgress(cPacket *packet, cGate *gate, double datarate, int bitPosition, simtime_t timePosition, int extraProcessableBitLength, simtime_t extraProcessableDuration) override;
+    virtual void receivePacketEnd(cPacket *packet, cGate *gate, double datarate) override;
+
+    // for lifecycle:
+    virtual bool isInitializeStage(int stage) override { return stage == INITSTAGE_LINK_LAYER; }
+    virtual bool isModuleStartStage(int stage) override { return stage == ModuleStartOperation::STAGE_LINK_LAYER; }
+    virtual bool isModuleStopStage(int stage) override { return stage == ModuleStopOperation::STAGE_LINK_LAYER; }
+    virtual void handleStartOperation(LifecycleOperation *operation) override;
+    virtual void handleStopOperation(LifecycleOperation *operation) override;
+    virtual void handleCrashOperation(LifecycleOperation *operation) override;
+
+  public:
+    StreamerReceiver() {}
+    virtual ~StreamerReceiver();
+
+    virtual bool supportsPacketStreaming(cGate *gate) const override { return true; }
+};
+
+} // namespace inet
+
+#endif // ifndef __INET_STREAMERRECEIVER_H
+
