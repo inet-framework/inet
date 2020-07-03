b4c325d49b1fa48314f71f74cdf3d62a811c9e9a
Levente Mészáros <levy@omnetpp.org>
2020-07-03 10:16:56 +0200

    PacketClassifierBase: Added support for pulling packets.

--- src/inet/queueing/base/PacketClassifierBase.h
+++ src/inet/queueing/base/PacketClassifierBase.h
@@ -19,22 +19,24 @@
 #define __INET_PACKETCLASSIFIERBASE_H
 
 #include "inet/queueing/base/PacketProcessorBase.h"
-#include "inet/queueing/contract/IActivePacketSource.h"
-#include "inet/queueing/contract/IPassivePacketSink.h"
+#include "inet/queueing/contract/IPacketSink.h"
+#include "inet/queueing/contract/IPacketSource.h"
 
 namespace inet {
 namespace queueing {
 
-class INET_API PacketClassifierBase : public PacketProcessorBase, public virtual IPassivePacketSink, public virtual IActivePacketSource
+class INET_API PacketClassifierBase : public PacketProcessorBase, public virtual IPacketSink, public virtual IPacketSource
 {
   protected:
-    bool reverseOrder;
+    bool reverseOrder = false;
 
     cGate *inputGate = nullptr;
     IActivePacketSource *producer = nullptr;
+    IPassivePacketSource *provider = nullptr;
 
     std::vector<cGate *> outputGates;
     std::vector<IPassivePacketSink *> consumers;
+    std::vector<IActivePacketSink *> collectors;
 
     int inProgressStreamId = -1;
     int inProgressGateIndex = -1;
@@ -44,7 +46,7 @@ class INET_API PacketClassifierBase : public PacketProcessorBase, public virtual
     virtual void handleMessage(cMessage *message) override;
 
     virtual int classifyPacket(Packet *packet) = 0;
-    virtual int callClassifyPacket(Packet *packet);
+    virtual int callClassifyPacket(Packet *packet) const;
 
     virtual bool isStreamingPacket() const { return inProgressStreamId != -1; }
     virtual void startPacketStreaming(Packet *packet);
@@ -52,10 +54,11 @@ class INET_API PacketClassifierBase : public PacketProcessorBase, public virtual
     virtual void checkPacketStreaming(Packet *packet);
 
   public:
+    virtual IPassivePacketSource *getProvider(cGate *gate) override { return provider; }
     virtual IPassivePacketSink *getConsumer(cGate *gate) override { return consumers[gate->getIndex()]; }
 
     virtual bool supportsPacketPushing(cGate *gate) const override { return true; }
-    virtual bool supportsPacketPulling(cGate *gate) const override { return false; }
+    virtual bool supportsPacketPulling(cGate *gate) const override { return true; }
     virtual bool supportsPacketStreaming(cGate *gate) const override { return true; }
 
     virtual bool canPushSomePacket(cGate *gate) const override;
@@ -70,6 +73,19 @@ class INET_API PacketClassifierBase : public PacketProcessorBase, public virtual
 
     virtual void handleCanPushPacket(cGate *gate) override;
     virtual void handlePushPacketProcessed(Packet *packet, cGate *gate, bool successful) override;
+
+    virtual bool canPullSomePacket(cGate *gate) const override;
+    virtual Packet *canPullPacket(cGate *gate) const override;
+
+    virtual Packet *pullPacket(cGate *gate) override;
+    virtual Packet *pullPacketStart(cGate *gate, bps datarate) override { throw cRuntimeError("TODO"); }
+    virtual Packet *pullPacketEnd(cGate *gate, bps datarate) override { throw cRuntimeError("TODO"); }
+    virtual Packet *pullPacketProgress(cGate *gate, bps datarate, b position, b extraProcessableLength) override { throw cRuntimeError("TODO"); }
+
+    virtual b getPullPacketProcessedLength(Packet *packet, cGate *gate) override { throw cRuntimeError("TODO"); }
+
+    virtual void handleCanPullPacket(cGate *gate) override;
+    virtual void handlePullPacketProcessed(Packet *packet, cGate *gate, bool successful) override { throw cRuntimeError("TODO"); }
 };
 
 } // namespace queueing
