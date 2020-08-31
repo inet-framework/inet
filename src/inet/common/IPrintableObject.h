156803c7cdaa46588aa5affcd8f2a92688f1447f
Levente Mészáros <levy@omnetpp.org>
2020-08-31 13:03:34 +0200

    IPrintableObject: Added templates to support printing all kinds of object pointers/references either formatted or unformatted.

--- src/inet/common/IPrintableObject.h
+++ src/inet/common/IPrintableObject.h
@@ -50,34 +50,45 @@ class INET_API IPrintableObject
      * Function calls to operator<< with pointers or references either const
      * or not are all forwarded to this function.
      */
-    virtual std::ostream& printToStream(std::ostream& stream, int level, int flags = 0) const {
-        return stream << "<object@" << static_cast<const void *>(this) << ">";
+    virtual std::ostream& printToStream(std::ostream& stream, int level, int evFlags = 0) const {
+        return stream << EV_FAINT << "<object@" << static_cast<const void *>(this) << ">" << EV_NORMAL;
     }
 
-    virtual std::string getInfoStringRepresentation(int flags = 0) const {
-        std::stringstream s; printToStream(s, PRINT_LEVEL_INFO, flags); return s.str();
+    virtual std::string printToString() const {
+        return "";
     }
 
-    virtual std::string getDetailStringRepresentation(int flags = 0) const {
-        std::stringstream s; printToStream(s, PRINT_LEVEL_DETAIL, flags); return s.str();
+    virtual std::string printToString(int level, int evFlags = 0) const {
+        std::stringstream s; printToStream(s, level, evFlags); return s.str();
     }
 
-    virtual std::string getDebugStringRepresentation(int flags = 0) const {
-        std::stringstream s; printToStream(s, PRINT_LEVEL_DEBUG, flags); return s.str();
+    virtual std::string getInfoStringRepresentation(int evFlags = 0) const {
+        std::stringstream s; printToStream(s, PRINT_LEVEL_INFO, evFlags); return s.str();
     }
 
-    virtual std::string getTraceStringRepresentation(int flags = 0) const {
-        std::stringstream s; printToStream(s, PRINT_LEVEL_TRACE, flags); return s.str();
+    virtual std::string getDetailStringRepresentation(int evFlags = 0) const {
+        std::stringstream s; printToStream(s, PRINT_LEVEL_DETAIL, evFlags); return s.str();
     }
 
-    virtual std::string getCompleteStringRepresentation(int flags = 0) const {
-        std::stringstream s; printToStream(s, PRINT_LEVEL_COMPLETE, flags); return s.str();
+    virtual std::string getDebugStringRepresentation(int evFlags = 0) const {
+        std::stringstream s; printToStream(s, PRINT_LEVEL_DEBUG, evFlags); return s.str();
+    }
+
+    virtual std::string getTraceStringRepresentation(int evFlags = 0) const {
+        std::stringstream s; printToStream(s, PRINT_LEVEL_TRACE, evFlags); return s.str();
+    }
+
+    virtual std::string getCompleteStringRepresentation(int evFlags = 0) const {
+        std::stringstream s; printToStream(s, PRINT_LEVEL_COMPLETE, evFlags); return s.str();
     }
 };
 
 inline std::ostream& operator<<(std::ostream& stream, const IPrintableObject *object)
 {
-    return object->printToStream(stream, cLog::logLevel, 0);
+    if (object == nullptr)
+        return stream << EV_FAINT << "<nullptr>" << EV_NORMAL;
+    else
+        return object->printToStream(stream, cLog::logLevel, 0);
 };
 
 inline std::ostream& operator<<(std::ostream& stream, const IPrintableObject& object)
@@ -85,17 +96,154 @@ inline std::ostream& operator<<(std::ostream& stream, const IPrintableObject& ob
     return object.printToStream(stream, cLog::logLevel, 0);
 };
 
-inline std::string printObjectToString(const IPrintableObject *object, int level, int flags = 0)
+inline std::string printFieldToString(const IPrintableObject *object, int level, int evFlags = 0)
 {
-    std::stringstream s;
+    std::stringstream stream;
     if (object == nullptr)
-        return "nullptr";
+        stream << EV_FAINT << "<nullptr>" << EV_NORMAL;
     else {
-        s << "{ ";
-        object->printToStream(s, level, flags);
-        s << " }";
+        stream << "{ ";
+        object->printToStream(stream, level, evFlags);
+        stream << " }";
+    }
+    return stream.str();
+}
+
+template <typename, typename = void>
+struct has_print_to_string : std::false_type { };
+
+template <typename T>
+struct has_print_to_string<T, std::void_t<decltype(std::declval<T>().printToString())>>
+    : std::true_type { };
+
+template <typename, typename = void>
+struct has_str : std::false_type { };
+
+template <typename T>
+struct has_str<T, std::void_t<decltype(std::declval<T>().str())>>
+    : std::true_type { };
+
+template <typename T>
+std::enable_if_t<has_print_to_string<T>::value, std::string> printToStringIfPossible(T *object, int evFlags)
+{
+    if (object == nullptr)
+        return "<nullptr>";
+    else
+        return object->printToString(cLog::logLevel, evFlags);
+}
+
+template <typename T>
+std::enable_if_t<has_print_to_string<T>::value, std::string> printToStringIfPossible(const T *object, int evFlags)
+{
+    if (object == nullptr)
+        return "<nullptr>";
+    else
+        return object->printToString(cLog::logLevel, evFlags);
+}
+
+template <typename T>
+std::enable_if_t<has_print_to_string<T>::value, std::string> printToStringIfPossible(T& object, int evFlags)
+{
+    return object.printToString(cLog::logLevel, evFlags);
+}
+
+template <typename T>
+std::enable_if_t<has_print_to_string<T>::value, std::string> printToStringIfPossible(const T& object, int evFlags)
+{
+    return object.printToString(cLog::logLevel, evFlags);
+}
+
+template <typename T>
+std::enable_if_t<std::is_polymorphic<T>::value && has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(T *object, int evFlags)
+{
+    if (object == nullptr)
+        return "<nullptr>";
+    else if (auto printableObject = dynamic_cast<IPrintableObject *>(object))
+        return printableObject->printToString(cLog::logLevel, evFlags);
+    else
+        return object->str();
+}
+
+template <typename T>
+std::enable_if_t<std::is_polymorphic<T>::value && has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(const T& object, int evFlags)
+{
+    if (auto printableObject = dynamic_cast<const IPrintableObject *>(&object))
+        return printableObject->printToString(cLog::logLevel, evFlags);
+    else
+        return object.str();
+}
+
+template <typename T>
+std::enable_if_t<std::is_polymorphic<T>::value && has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(const T *object, int evFlags)
+{
+    if (object == nullptr)
+        return "<nullptr>";
+    else if (auto printableObject = dynamic_cast<const IPrintableObject *>(object))
+        return printableObject->printToString(cLog::logLevel, evFlags);
+    else
+        return object->str();
+}
+
+template <typename T>
+std::enable_if_t<std::is_polymorphic<T>::value && !has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(T *object, int evFlags)
+{
+    if (object == nullptr)
+        return "<nullptr>";
+    else if (auto printableObject = dynamic_cast<IPrintableObject *>(object))
+        return printableObject->printToString(cLog::logLevel, evFlags);
+    else if (auto cobject = dynamic_cast<cObject *>(object))
+        return cobject->str();
+    else {
+        std::stringstream s;
+        s << object;
         return s.str();
     }
+}
+
+template <typename T>
+std::enable_if_t<std::is_polymorphic<T>::value && !has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(const T *object, int evFlags)
+{
+    if (object == nullptr)
+        return "<nullptr>";
+    else if (auto printableObject = dynamic_cast<const IPrintableObject *>(object))
+        return printableObject->printToString(cLog::logLevel, evFlags);
+    else if (auto cobject = dynamic_cast<const cObject *>(object))
+        return cobject->str();
+    else {
+        std::stringstream s;
+        s << object;
+        return s.str();
+    }
+}
+
+template <typename T>
+std::enable_if_t<std::is_polymorphic<T>::value && !has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(const T& object, int evFlags)
+{
+    if (auto printableObject = dynamic_cast<const IPrintableObject *>(&object))
+        return printableObject->printToString(cLog::logLevel, evFlags);
+    else if (auto cobject = dynamic_cast<const cObject *>(&object))
+        return cobject->str();
+    else {
+        std::stringstream s;
+        s << object;
+        return s.str();
+    }
+}
+
+template <typename T>
+std::enable_if_t<!std::is_polymorphic<T>::value && has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(const T& object, int evFlags)
+{
+    std::stringstream s;
+    s << object;
+    return s.str();
+}
+
+template <typename T>
+std::enable_if_t<!has_str<T>::value && !has_print_to_string<T>::value, std::string> printToStringIfPossible(const T& object, int evFlags)
+{
+    std::stringstream s;
+    s << object;
+    return s.str();
 }
 
 } // namespace inet
