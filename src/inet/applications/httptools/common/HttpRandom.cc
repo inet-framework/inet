a756ad74048dca9e246ff6191bd14dcfe2e34f9b
Levente Mészáros <levy@omnetpp.org>
2020-09-03 17:55:15 +0200

    All: Replaced all GPL license headers with their canonical form from <https://www.gnu.org/licenses/>.
    License versions are kept intact.

--- src/inet/applications/httptools/common/HttpRandom.cc
+++ src/inet/applications/httptools/common/HttpRandom.cc
@@ -1,9 +1,10 @@
 //
 // Copyright (C) 2009 Kristjan V. Jonsson, LDSS (kristjanvj@gmail.com)
 //
-// This program is free software; you can redistribute it and/or
-// modify it under the terms of the GNU General Public License version 3
-// as published by the Free Software Foundation.
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -11,8 +12,7 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
 #include "inet/applications/httptools/common/HttpRandom.h"
--- src/inet/networklayer/arp/ipv4/ArpPacket.msg
+++ src/inet/networklayer/arp/ipv4/ArpPacket.msg
@@ -12,7 +12,8 @@
 // GNU Lesser General Public License for more details.
 //
 // You should have received a copy of the GNU Lesser General Public License
-// along with this program; if not, see <http://www.gnu.org/licenses/>.
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //
 
 
