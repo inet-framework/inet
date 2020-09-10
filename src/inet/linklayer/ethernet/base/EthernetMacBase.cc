408743b9cf245551eb5238b6b51d6afd2d97a5bd
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-09-10 14:08:12 +0200

    eth: EtherMacBase: drop active tx at finish

--- src/inet/linklayer/ethernet/base/EthernetMacBase.cc
+++ src/inet/linklayer/ethernet/base/EthernetMacBase.cc
@@ -470,6 +470,14 @@ void EthernetMacBase::printParameters()
 
 void EthernetMacBase::finish()
 {
+    if (currentTxFrame) {
+        numDroppedPkFromHLIfaceDown++;
+        PacketDropDetails details;
+        details.setReason(INTERFACE_DOWN);
+        dropCurrentTxFrame(details);
+        lastTxFinishTime = -1.0;    // so that it never equals to the current simtime, used for Burst mode detection.
+    }
+
     {
         simtime_t t = simTime();
         recordScalar("simulated time", t);
