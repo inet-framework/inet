c3b02807f5c5ebfe2e4584e4180419d71e52c3be
Levente Mészáros <levy@omnetpp.org>
2020-09-11 13:29:56 +0200

    Packet: Added PacketAPI and ChunkAPI headers.

---
+++ src/inet/common/packet/chunk/ChunkAPI.h
@@ -0,0 +1,31 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_CHUNKAPI_H
+#define __INET_CHUNKAPI_H
+
+#include "inet/common/packet/chunk/BitCountChunk.h"
+#include "inet/common/packet/chunk/BitsChunk.h"
+#include "inet/common/packet/chunk/ByteCountChunk.h"
+#include "inet/common/packet/chunk/BytesChunk.h"
+#include "inet/common/packet/chunk/Chunk.h"
+#include "inet/common/packet/chunk/cPacketChunk.h"
+#include "inet/common/packet/chunk/EmptyChunk.h"
+#include "inet/common/packet/chunk/FieldsChunk.h"
+#include "inet/common/packet/chunk/SequenceChunk.h"
+#include "inet/common/packet/chunk/SliceChunk.h"
+
+#endif
+
