13a902ecd5b42da155bb269265c4263100facc88
Levente Mészáros <levy@omnetpp.org>
2020-09-15 13:48:29 +0200

    MacRelayUnit: Refactored to be based on MacRelayUnitBase module.

--- src/inet/linklayer/ethernet/switch/MacRelayUnit.cc
+++ src/inet/linklayer/ethernet/switch/MacRelayUnit.cc
@@ -15,158 +15,57 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
-
-#include "inet/common/IProtocolRegistrationListener.h"
-#include "inet/common/LayeredProtocolBase.h"
-#include "inet/common/lifecycle/ModuleOperations.h"
-#include "inet/common/ModuleAccess.h"
-#include "inet/common/Protocol.h"
 #include "inet/common/ProtocolTag_m.h"
-#include "inet/common/StringFormat.h"
 #include "inet/linklayer/common/InterfaceTag_m.h"
 #include "inet/linklayer/common/MacAddressTag_m.h"
-#include "inet/linklayer/ethernet/EtherFrame_m.h"
-#include "inet/linklayer/ethernet/EtherMacBase.h"
-#include "inet/linklayer/ethernet/Ethernet.h"
 #include "inet/linklayer/ethernet/switch/MacRelayUnit.h"
 
 namespace inet {
 
 Define_Module(MacRelayUnit);
 
-void MacRelayUnit::initialize(int stage)
+// FIXME: should handle multicast mac addresses correctly
+void MacRelayUnit::handleLowerPacket(Packet *incomingPacket)
 {
-    LayeredProtocolBase::initialize(stage);
-
-    if (stage == INITSTAGE_LOCAL) {
-        numProcessedFrames = numDiscardedFrames = 0;
-
-        macTable = getModuleFromPar<IMacAddressTable>(par("macTableModule"), this);
-        ifTable = getModuleFromPar<IInterfaceTable>(par("interfaceTableModule"), this);
-
-        WATCH(numProcessedFrames);
-        WATCH(numDiscardedFrames);
-    }
-    else if (stage == INITSTAGE_LINK_LAYER) {
-        registerProtocol(Protocol::ethernetMac, gate("ifOut"), gate("ifIn"));
-    }
-}
-
-void MacRelayUnit::handleLowerPacket(Packet *packet)
-{
-    handleAndDispatchFrame(packet);
-    updateDisplayString();
-}
-
-void MacRelayUnit::updateDisplayString() const
-{
-    auto text = StringFormat::formatString(par("displayStringTextFormat"), [&] (char directive) {
-        static std::string result;
-        switch (directive) {
-            case 'p':
-                result = std::to_string(numProcessedFrames);
-                break;
-            case 'd':
-                result = std::to_string(numDiscardedFrames);
-                break;
-            default:
-                throw cRuntimeError("Unknown directive: %c", directive);
-        }
-        return result.c_str();
-    });
-    getDisplayString().setTagArg("t", 0, text);
-}
-
-void MacRelayUnit::broadcast(Packet *packet, int arrivalInterfaceId)
-{
-    EV_DETAIL << "Broadcast frame " << packet << endl;
-
-    int numPorts = ifTable->getNumInterfaces();
-    for (int i = 0; i < numPorts; i++) {
-        NetworkInterface *ie = ifTable->getInterface(i);
-        if (ie->isLoopback() || !ie->isBroadcast())
-            continue;
-        int ifId = ie->getInterfaceId();
-        if (arrivalInterfaceId != ifId) {
-            Packet *dupFrame = packet->dup();
-            dupFrame->addTagIfAbsent<InterfaceReq>()->setInterfaceId(ifId);
-            emit(packetSentToLowerSignal, dupFrame);
-            send(dupFrame, "ifOut");
-        }
-    }
-    delete packet;
-}
-
-void MacRelayUnit::handleAndDispatchFrame(Packet *packet)
-{
-    //FIXME : should handle multicast mac addresses correctly
-
-    auto macAddressInd = packet->getTag<MacAddressInd>();
-    int arrivalInterfaceId = packet->getTag<InterfaceInd>()->getInterfaceId();
-
-    auto& oldPacketProtocolTag = packet->removeTag<PacketProtocolTag>();
-    packet->clearTags();
-    auto newPacketProtocolTag = packet->addTag<PacketProtocolTag>();
-    *newPacketProtocolTag = *oldPacketProtocolTag;
-    auto& macAddressReq = packet->addTag<MacAddressReq>();
-    macAddressReq->setSrcAddress(macAddressInd->getSrcAddress());
-    macAddressReq->setDestAddress(macAddressInd->getDestAddress());
-    packet->trim();
-
-    numProcessedFrames++;
-
-    // update address table
-    learn(macAddressInd->getSrcAddress(), arrivalInterfaceId);
-
-    // handle broadcast frames first
-    if (macAddressInd->getDestAddress().isBroadcast()) {
-        EV << "Broadcasting broadcast frame " << packet->getName() << endl;
-        broadcast(packet, arrivalInterfaceId);
-        return;
-    }
-
-    // Finds output port of destination address and sends to output port
-    // if not found then broadcasts to all other ports instead
-    int outputInterfaceId = macTable->getInterfaceIdForAddress(macAddressInd->getDestAddress());
-    // should not send out the same frame on the same ethernet port
-    // (although wireless ports are ok to receive the same message)
-    if (arrivalInterfaceId == outputInterfaceId) {
-        EV << "Output port is same as input port, " << packet->getFullName()
-           << " dest " << macAddressInd->getDestAddress() << ", discarding frame\n";
-        numDiscardedFrames++;
-        delete packet;
-        return;
-    }
-
-    if (outputInterfaceId >= 0) {
-        EV << "Sending frame " << packet->getName() << " with dest address " << macAddressInd->getDestAddress() << " to port " << outputInterfaceId << endl;
-        packet->addTag<InterfaceReq>()->setInterfaceId(outputInterfaceId);
-        emit(packetSentToLowerSignal, packet);
-        send(packet, "ifOut");
-    }
+    auto protocol = incomingPacket->getTag<PacketProtocolTag>()->getProtocol();
+    auto macAddressInd = incomingPacket->getTag<MacAddressInd>();
+    auto sourceAddress = macAddressInd->getSrcAddress();
+    auto destinationAddress = macAddressInd->getDestAddress();
+    auto interfaceInd = incomingPacket->getTag<InterfaceInd>();
+    int incomingInterfaceId = interfaceInd->getInterfaceId();
+    auto incomingInterface = interfaceTable->getInterfaceById(incomingInterfaceId);
+    EV_INFO << "Processing packet from network" << EV_FIELD(incomingInterface) << EV_FIELD(incomingPacket) << EV_ENDL;
+    updatePeerAddress(incomingInterface, sourceAddress);
+    auto outgoingPacket = new Packet(incomingPacket->getName(), incomingPacket->peekData());
+    outgoingPacket->addTag<PacketProtocolTag>()->setProtocol(protocol);
+    auto& macAddressReq = outgoingPacket->addTag<MacAddressReq>();
+    macAddressReq->setSrcAddress(sourceAddress);
+    macAddressReq->setDestAddress(destinationAddress);
+    if (destinationAddress.isBroadcast())
+        broadcastPacket(outgoingPacket, destinationAddress, incomingInterface);
     else {
-        EV << "Dest address " << macAddressInd->getDestAddress() << " unknown, broadcasting frame " << packet << endl;
-        broadcast(packet, arrivalInterfaceId);
+        // Find output interface of destination address and send packet to output interface
+        // if not found then broadcasts to all other interfaces instead
+        int outgoingInterfaceId = macAddressTable->getInterfaceIdForAddress(destinationAddress);
+        auto outgoingInterface = interfaceTable->getInterfaceById(outgoingInterfaceId);
+        // should not send out the same packet on the same interface
+        // (although wireless interfaces are ok to receive the same message)
+        if (incomingInterfaceId == outgoingInterfaceId) {
+            EV_WARN << "Discarding packet because outgoing interface is the same as incoming interface" << EV_FIELD(destinationAddress) << EV_FIELD(incomingInterface) << EV_FIELD(incomingPacket) << EV_ENDL;
+            numDroppedFrames++;
+            PacketDropDetails details;
+            details.setReason(NO_INTERFACE_FOUND);
+            emit(packetDroppedSignal, outgoingPacket, &details);
+            delete outgoingPacket;
+        }
+        else if (outgoingInterfaceId != -1)
+            sendPacket(outgoingPacket->dup(), destinationAddress, outgoingInterface);
+        else
+            broadcastPacket(outgoingPacket, destinationAddress, incomingInterface);
     }
-}
-
-void MacRelayUnit::start()
-{
-}
-
-void MacRelayUnit::stop()
-{
-}
-
-void MacRelayUnit::learn(MacAddress srcAddr, int arrivalInterfaceId)
-{
-    macTable->updateTableWithAddress(arrivalInterfaceId, srcAddr);
-}
-
-void MacRelayUnit::finish()
-{
-    recordScalar("processed frames", numProcessedFrames);
-    recordScalar("discarded frames", numDiscardedFrames);
+    numProcessedFrames++;
+    updateDisplayString();
+    delete incomingPacket;
 }
 
 } // namespace inet
--- src/inet/linklayer/ethernet/switch/MacRelayUnit.h
+++ src/inet/linklayer/ethernet/switch/MacRelayUnit.h
@@ -16,68 +16,14 @@
 #ifndef __INET_MACRELAYUNIT_H
 #define __INET_MACRELAYUNIT_H
 
-#include "inet/common/INETDefs.h"
-#include "inet/common/LayeredProtocolBase.h"
-#include "inet/common/lifecycle/ModuleOperations.h"
-#include "inet/common/packet/Packet.h"
-#include "inet/linklayer/ethernet/EtherFrame_m.h"
-#include "inet/linklayer/ethernet/switch/IMacAddressTable.h"
-#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/linklayer/base/MacRelayUnitBase.h"
 
 namespace inet {
 
-class INET_API MacRelayUnit : public LayeredProtocolBase
+class INET_API MacRelayUnit : public MacRelayUnitBase
 {
   protected:
-    IInterfaceTable *ifTable = nullptr;
-    IMacAddressTable *macTable = nullptr;
-
-    // Parameters for statistics collection
-    long numProcessedFrames = 0;
-    long numDiscardedFrames = 0;
-
-  protected:
-    virtual void initialize(int stage) override;
-    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
-    virtual void updateDisplayString() const;
-    /**
-     * Updates address table with source address, determines output port
-     * and sends out (or broadcasts) frame on ports. Includes calls to
-     * updateTableWithAddress() and getPortForAddress().
-     *
-     * The message pointer should not be referenced any more after this call.
-     */
-    virtual void handleAndDispatchFrame(Packet *packet);
-
-    void handleLowerPacket(Packet *packet) override;
-
-    /**
-     * Utility function: sends the frame on all ports except inputport.
-     * The message pointer should not be referenced any more after this call.
-     */
-    virtual void broadcast(Packet *frame, int inputport);
-
-    /**
-     * Writes statistics.
-     */
-    virtual void finish() override;
-
-    //@{ for lifecycle:
-    virtual void handleStartOperation(LifecycleOperation *operation) override { start(); }
-    virtual void handleStopOperation(LifecycleOperation *operation) override { stop(); }
-    virtual void handleCrashOperation(LifecycleOperation *operation) override { stop(); }
-    virtual bool isUpperMessage(cMessage *message) override { return message->arrivedOn("upperLayerIn"); }
-    virtual bool isLowerMessage(cMessage *message) override { return message->arrivedOn("ifIn"); }
-
-    virtual bool isInitializeStage(int stage) override { return stage == INITSTAGE_LINK_LAYER; }
-    virtual bool isModuleStartStage(int stage) override { return stage == ModuleStartOperation::STAGE_LINK_LAYER; }
-    virtual bool isModuleStopStage(int stage) override { return stage == ModuleStopOperation::STAGE_LINK_LAYER; }
-
-    virtual void start();
-    virtual void stop();
-    //@}
-
-    virtual void learn(MacAddress srcAddr, int arrivalInterfaceId);
+    virtual void handleLowerPacket(Packet *packet) override;
 };
 
 } // namespace inet
