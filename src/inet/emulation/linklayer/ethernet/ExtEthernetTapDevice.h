c99d59e922a0a0172ead9ff47f5aae4d28eb0013
Levente Mészáros <levy@omnetpp.org>
2020-09-16 11:13:10 +0200

    All: Added missing year (2020) to all Copyright statements where the copyright holder is OpenSim.

--- src/inet/emulation/linklayer/ethernet/ExtEthernetTapDevice.h
+++ src/inet/emulation/linklayer/ethernet/ExtEthernetTapDevice.h
@@ -1,5 +1,5 @@
 //
-// Copyright (C) OpenSimLtd.
+// Copyright (C) 2020 OpenSimLtd.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as published by
