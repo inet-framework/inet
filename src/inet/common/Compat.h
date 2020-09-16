f89b44ddabd267ae71dff019059389f64c13b107
Levente Mészáros <levy@omnetpp.org>
2020-09-16 10:05:47 +0200

    Compat: Removed obsolete compatiblity typedefs.

--- src/inet/common/Compat.h
+++ src/inet/common/Compat.h
@@ -24,14 +24,6 @@
 
 namespace inet {
 
-    typedef uint64_t uint64;
-    typedef int64_t  int64;
-    typedef uint32_t uint32;
-    typedef int32_t  int32;
-    typedef uint16_t uint16;
-    typedef int16_t  int16;
-    typedef uint8_t  uint8;
-
 #  define EVSTREAM                      EV
 
 #if OMNETPP_BUILDNUM < 1503
