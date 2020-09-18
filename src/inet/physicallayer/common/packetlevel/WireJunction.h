215f76750d117efbf7607688042be3eb2d57c25a
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-09-18 18:07:29 +0200

    eth: WireJunction: ported to the OMNeT++ transmission update API (REQUIRES OMNET++ 6.0p9)
    Requires at least OMNeT++ version 6.0preview9 (transmission update
    support).
    
    WireJunction reimplemented. It just forwards all transmissions and
    transmission updates to all ports. This means that there can be
    MULTIPLE CONCURRENT SIGNALS on all connected wires.
    
    WireJunction gates renamed from ethg[] to port[], because WireJunction
    is now generic (not specific to Ethernet in any way.)
    
    EtherHub is reimplemented as trivial subclass of the WireJunction C++
    class.
    
    NOTE: EtherMac will also need to be ported to the OMNeT++ transmission
    update API, and to detect collisions from seeing concurrent transmissions
    on its input. Until this is done, SIMULATIONS CONTAINING EtherHub or
    WireJunction ARE BROKEN!
    
    [review and docu: Andras]

--- src/inet/physicallayer/common/packetlevel/WireJunction.h
+++ src/inet/physicallayer/common/packetlevel/WireJunction.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2003 Andras Varga; CTIE, Monash University, Australia
+ * Copyright (C) 2020 Opensim Ltd.
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public License
@@ -25,26 +25,47 @@ namespace physicallayer {
 
 /**
  * Models a wiring hub. It simply broadcasts the received message
+ * (which can be a transmission update, requiring special attention)
  * on all other ports.
+ *
+ * Note that no special attention is made to appropriately model
+ * frame truncations that occur when the link breaks, or comes
+ * back up while a transmission is underway. This is mainly so
+ * because being a generic model, WireJunction doesn't know how
+ * to produce sliced (truncated) versions of frames. If you need
+ * to precisely model what happens when the link state changes,
+ * you cannot use this class.
  */
 class INET_API WireJunction : public cSimpleModule, protected cListener
 {
   protected:
-    int numPorts;    // sizeof(ethg)
-    int inputGateBaseId;    // gate id of ethg$i[0]
-    int outputGateBaseId;    // gate id of ethg$o[0]
-    bool dataratesDiffer;
+    struct TxInfo {
+        long incomingTxId = -1;
+        long outgoingPort = -1;
+        long outgoingTxId = -1;
+        simtime_t finishTime;
+    };
+
+  protected:
+    std::vector<TxInfo> txList;
+    int numPorts;    // sizeof(port)
+    int inputGateBaseId;  // gate id of port$i[0]
+    int outputGateBaseId; // gate id of port$o[0]
 
     // statistics
     long numMessages;    // number of messages handled
 
   protected:
+    virtual const char *getGateName() const { return "port"; }
     virtual void initialize() override;
     virtual void handleMessage(cMessage *msg) override;
-    virtual void finish() override;
     virtual void receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details) override;
 
-    virtual void checkConnections(bool errorWhenAsymmetric);
+    virtual void setChannelModes();
+    virtual void setGateModes();
+    virtual void addTxInfo(long incomingTxId, int port, long outgoingTxId, simtime_t finishTime);
+    virtual void updateTxInfo(TxInfo *txInfo, simtime_t finishTime) {txInfo->finishTime = finishTime;}
+    virtual TxInfo *findTxInfo(long incomingTxId, int port);
 };
 
 } //namespace physicallayer
