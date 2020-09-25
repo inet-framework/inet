5914ad2f76ebe8b566a1a02b4a062ffc1c493cfe
Levente Mészáros <levy@omnetpp.org>
2020-09-25 10:36:26 +0200

    Ethernet: Added new EthernetMacHeaderInserter and EthernetMacHeaderChecker.

---
+++ src/inet/linklayer/ethernet/layered/EthernetMacHeaderInserter.h
@@ -0,0 +1,41 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_ETHERNETMACHEADERINSERTER_H
+#define __INET_ETHERNETMACHEADERINSERTER_H
+
+#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/queueing/base/PacketFlowBase.h"
+
+namespace inet {
+
+using namespace inet::queueing;
+
+class INET_API EthernetMacHeaderInserter : public PacketFlowBase
+{
+  protected:
+    IInterfaceTable *interfaceTable = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void processPacket(Packet *packet) override;
+};
+
+} // namespace inet
+
+#endif
+
