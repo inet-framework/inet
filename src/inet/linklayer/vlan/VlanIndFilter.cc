749f00be1ba50292529fb380fdb68e040e7d8a18
Levente Mészáros <levy@omnetpp.org>
2020-10-13 13:09:12 +0200

    VLAN: Added VlanIndFilter, VlanReqFilter, VlanReqMapper and VlanPolicyLayer.
    These modules provide VLAN id filtering and mapping policies.

---
+++ src/inet/linklayer/vlan/VlanIndFilter.cc
@@ -0,0 +1,82 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/linklayer/common/InterfaceTag_m.h"
+#include "inet/linklayer/common/VlanTag_m.h"
+#include "inet/linklayer/vlan/VlanIndFilter.h"
+
+namespace inet {
+
+Define_Module(VlanIndFilter);
+
+void VlanIndFilter::initialize(int stage)
+{
+    PacketFilterBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        interfaceTable = getModuleFromPar<IInterfaceTable>(par("interfaceTableModule"), this);
+        cObject *object = par("acceptedVlanIds");
+        acceptedVlanIds = check_and_cast<cValueMap *>(object);
+        WATCH(acceptedVlanIds);
+    }
+}
+
+void VlanIndFilter::processPacket(Packet *packet)
+{
+    if (packet->findTag<DispatchProtocolReq>() == nullptr) {
+        const auto& dispatchProtocolInd = packet->removeTagIfPresent<DispatchProtocolInd>();
+        if (dispatchProtocolInd != nullptr)
+            packet->addTag<DispatchProtocolReq>()->setProtocol(dispatchProtocolInd->getProtocol());
+    }
+}
+
+void VlanIndFilter::dropPacket(Packet *packet)
+{
+    EV_WARN << "Received packet is not accepted, dropping packet" << EV_FIELD(packet) << EV_ENDL;
+    PacketFilterBase::dropPacket(packet, OTHER_PACKET_DROP);
+}
+
+bool VlanIndFilter::matchesPacket(const Packet *packet) const
+{
+    auto interfaceInd = packet->findTag<InterfaceInd>();
+    auto networkInterface = interfaceInd != nullptr ? interfaceTable->getInterfaceById(interfaceInd->getInterfaceId()) : nullptr;
+    auto interfaceName = networkInterface != nullptr ? networkInterface->getInterfaceName() : "";
+    auto key = acceptedVlanIds->containsKey(interfaceName) ? interfaceName : "*";
+    if (acceptedVlanIds->containsKey(key)) {
+        auto vlanInd = packet->findTag<VlanInd>();
+        auto vlanId = vlanInd != nullptr ? vlanInd->getVlanId() : -1;
+        auto interfaceVlanIdList = check_and_cast<cValueArray *>(acceptedVlanIds->get(key).objectValue());
+        for (int i = 0; i < interfaceVlanIdList->size(); i++)
+            if (interfaceVlanIdList->get(i).intValue() == vlanId)
+                return true;
+    }
+    return false;
+}
+
+void VlanIndFilter::handleRegisterService(const Protocol& protocol, cGate *gate, ServicePrimitive servicePrimitive)
+{
+    Enter_Method("handleRegisterService");
+}
+
+void VlanIndFilter::handleRegisterProtocol(const Protocol& protocol, cGate *gate, ServicePrimitive servicePrimitive)
+{
+    Enter_Method("handleRegisterProtocol");
+    if (gate == outputGate)
+        registerProtocol(protocol, inputGate, servicePrimitive);
+}
+
+} // namespace inet
+
---
+++ src/inet/linklayer/vlan/VlanReqMapper.cc
@@ -0,0 +1,74 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/linklayer/common/InterfaceTag_m.h"
+#include "inet/linklayer/common/VlanTag_m.h"
+#include "inet/linklayer/vlan/VlanReqMapper.h"
+
+namespace inet {
+
+Define_Module(VlanReqMapper);
+
+void VlanReqMapper::initialize(int stage)
+{
+    PacketFlowBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        interfaceTable = getModuleFromPar<IInterfaceTable>(par("interfaceTableModule"), this);
+        cObject *object = par("mappedVlanIds");
+        mappedVlanIds = check_and_cast<cValueMap *>(object);
+        WATCH(mappedVlanIds);
+    }
+}
+
+void VlanReqMapper::processPacket(Packet *packet)
+{
+    auto interfaceReq = packet->findTag<InterfaceReq>();
+    auto networkInterface = interfaceReq != nullptr ? interfaceTable->getInterfaceById(interfaceReq->getInterfaceId()) : nullptr;
+    auto interfaceName = networkInterface != nullptr ? networkInterface->getInterfaceName() : "";
+    auto key = mappedVlanIds->containsKey(interfaceName) ? interfaceName : "*";
+    if (mappedVlanIds->containsKey(key)) {
+        auto interfaceMappedVlanIds = check_and_cast<cValueMap *>(mappedVlanIds->get(key).objectValue());
+        auto vlanReq = packet->findTag<VlanReq>();
+        auto oldVlanId = vlanReq != nullptr ? vlanReq->getVlanId() : -1;
+        auto key = std::to_string(oldVlanId);
+        if (interfaceMappedVlanIds->containsKey(key.c_str())) {
+            auto newVlanId = interfaceMappedVlanIds->get(key.c_str()).intValue();
+            if (newVlanId == -1)
+                packet->removeTagIfPresent<VlanReq>();
+            else
+                packet->addTagIfAbsent<VlanReq>()->setVlanId(newVlanId);
+        }
+    }
+    auto dispatchProtocolInd = packet->findTag<DispatchProtocolInd>();
+    if (dispatchProtocolInd != nullptr)
+        packet->addTagIfAbsent<DispatchProtocolReq>()->setProtocol(dispatchProtocolInd->getProtocol());
+}
+
+void VlanReqMapper::handleRegisterService(const Protocol& protocol, cGate *gate, ServicePrimitive servicePrimitive)
+{
+    Enter_Method("handleRegisterService");
+    if (gate == outputGate)
+        registerService(protocol, inputGate, servicePrimitive);
+}
+
+void VlanReqMapper::handleRegisterProtocol(const Protocol& protocol, cGate *gate, ServicePrimitive servicePrimitive)
+{
+    Enter_Method("handleRegisterProtocol");
+}
+
+} // namespace inet
+
