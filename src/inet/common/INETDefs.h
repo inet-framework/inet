c7f8b05ff9007edfd0bb54a3ad87de6a0e73fccf
Rudolf Hornig <rudi@omnetpp.org>
2020-10-20 13:18:30 +0200

    prod: updated to v3.6.8 with OMNeT++ 6.0 preview 9 compatibility

--- src/inet/common/INETDefs.h
+++ src/inet/common/INETDefs.h
@@ -44,7 +44,7 @@ using namespace omnetpp;
 #endif // if OMNETPP_VERSION < 0x0503
 
 #define INET_VERSION  0x0306
-#define INET_PATCH_LEVEL 0x07
+#define INET_PATCH_LEVEL 0x08
 
 #if OMNETPP_VERSION < 0x0600
 #define OMNETPP5_CODE(x) x
