17cf5fdaa4ea946bb5dc6cc4658aa01496e96307
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-10-26 15:05:50 +0100

    common: fixed omnetpp dependency check

--- src/inet/common/INETDefs.h
+++ src/inet/common/INETDefs.h
@@ -41,9 +41,9 @@ namespace inet {
 using namespace omnetpp;
 }
 
-#if OMNETPP_VERSION < 0x0600 || OMNETPP_BUILDNUM < 1505
+#if OMNETPP_VERSION < 0x0600 || OMNETPP_BUILDNUM < 1506
 #  error At least OMNeT++/OMNEST version 6.0pre9 required
-#endif // if OMNETPP_VERSION < 0x0600 || OMNETPP_BUILDNUM < 1505
+#endif // if OMNETPP_VERSION < 0x0600 || OMNETPP_BUILDNUM < 1506
 
 #define INET_VERSION  0x0402
 #define INET_PATCH_LEVEL 0x00
