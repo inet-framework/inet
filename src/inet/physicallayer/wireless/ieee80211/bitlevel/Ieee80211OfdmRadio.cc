3f99db41cbcff8d2105df0e3776e6ca8dd0f03c2
Levente Mészáros <levy@omnetpp.org>
2020-11-24 11:26:05 +0100

    Ieee80211OfdmRadio: Fixed several cases where the decapsulation of the PHY header is not possible.

--- src/inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211OfdmRadio.cc
+++ src/inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211OfdmRadio.cc
@@ -19,6 +19,7 @@
 
 #include "inet/common/ProtocolTag_m.h"
 #include "inet/common/packet/chunk/BitCountChunk.h"
+#include "inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211LayeredOfdmReceiver.h"
 #include "inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211LayeredOfdmTransmitter.h"
 #include "inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader_m.h"
 #include "inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211Tag_m.h"
@@ -50,12 +51,16 @@ void Ieee80211OfdmRadio::encapsulate(Packet *packet) const
 
 void Ieee80211OfdmRadio::decapsulate(Packet *packet) const
 {
-    auto ofdmTransmitter = check_and_cast<const Ieee80211LayeredOfdmTransmitter *>(transmitter);
-    const auto& phyHeader = packet->popAtFront<Ieee80211OfdmPhyHeader>();
-    auto paddingLength = ofdmTransmitter->getPaddingLength(ofdmTransmitter->getMode(packet), B(phyHeader->getLengthField()));
-    // pop padding and 6 tail bits
-    packet->popAtBack(paddingLength + b(6));
-    packet->addTagIfAbsent<PacketProtocolTag>()->setProtocol(&Protocol::ieee80211Mac);
+    if (!packet->hasBitError()) {
+        auto ofdmReceiver = check_and_cast<const Ieee80211LayeredOfdmReceiver *>(receiver);
+        const auto& phyHeader = packet->popAtFront<Ieee80211OfdmPhyHeader>();
+        auto mode = ofdmReceiver->getMode(packet);
+        auto dataLength = B(phyHeader->getLengthField());
+        auto paddingLength = mode->getDataMode()->getPaddingLength(dataLength);
+        packet->popAtBack(std::min(paddingLength + b(6), packet->getDataLength()));
+        packet->setBitError(packet->getDataLength() != dataLength);
+        packet->addTagIfAbsent<PacketProtocolTag>()->setProtocol(&Protocol::ieee80211Mac);
+    }
 }
 
 } // namespace physicallayer
