78a2303cfbb0dd158e504cf3c4b356ea925eaccc
Zoltan Bojthe <zbojthe@omnetpp.org>
2020-12-08 13:32:09 +0100

    DataLinkOsgVisualizer: isLinkStart(), isLinkEnd() should be same as in DataLinkCanvasVisualizer and DataLinkOsgVisualizer

--- src/inet/visualizer/linklayer/DataLinkOsgVisualizer.cc
+++ src/inet/visualizer/linklayer/DataLinkOsgVisualizer.cc
@@ -19,6 +19,10 @@
 
 #include "inet/linklayer/base/MacProtocolBase.h"
 
+#ifdef INET_WITH_IEEE80211
+#include "inet/linklayer/ieee80211/mac/contract/ICoordinationFunction.h"
+#endif // INET_WITH_IEEE80211
+
 namespace inet {
 
 namespace visualizer {
@@ -27,12 +31,20 @@ Define_Module(DataLinkOsgVisualizer);
 
 bool DataLinkOsgVisualizer::isLinkStart(cModule *module) const
 {
-    return dynamic_cast<MacProtocolBase *>(module) != nullptr;
+    return dynamic_cast<MacProtocolBase *>(module) != nullptr
+#ifdef INET_WITH_IEEE80211
+           || dynamic_cast<ieee80211::ICoordinationFunction *>(module) != nullptr
+#endif // INET_WITH_IEEE80211
+        ;
 }
 
 bool DataLinkOsgVisualizer::isLinkEnd(cModule *module) const
 {
-    return dynamic_cast<MacProtocolBase *>(module) != nullptr;
+    return dynamic_cast<MacProtocolBase *>(module) != nullptr
+#ifdef INET_WITH_IEEE80211
+           || dynamic_cast<ieee80211::ICoordinationFunction *>(module) != nullptr
+#endif // INET_WITH_IEEE80211
+        ;
 }
 
 } // namespace visualizer
