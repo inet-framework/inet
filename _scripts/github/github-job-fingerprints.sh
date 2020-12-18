22de19ac1c0052ef4fc3cfbddbf975b0fcf11fac
TÖRÖK Attila <torokati44@gmail.com>
2020-12-19 00:15:33 +0100

    github: Update the tester Docker image to omnetpp-6.0pre10

--- _scripts/github/github-job-fingerprints.sh
+++ _scripts/github/github-job-fingerprints.sh
@@ -12,7 +12,7 @@
 set -e # make the script exit with error if any executed command exits with error
 
 
-export PATH="/root/omnetpp-6.0pre9-linux/bin:$PATH"
+export PATH="/root/omnetpp-6.0pre10-linux/bin:$PATH"
 
 # this is where the cloned INET repo is mounted into the container
 cd $GITHUB_WORKSPACE
