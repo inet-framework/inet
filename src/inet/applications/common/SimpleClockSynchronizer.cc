a3d4e20b865d222f98bb9b7a7315cfd47c45382b
Levente Mészáros <levy@omnetpp.org>
2021-01-22 12:16:22 +0100

    Applications: Added SimpleClockSynchronizer.

---
+++ src/inet/applications/common/SimpleClockSynchronizer.cc
@@ -0,0 +1,62 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/applications/common/SimpleClockSynchronizer.h"
+
+#include "inet/common/ModuleAccess.h"
+
+namespace inet {
+
+Define_Module(SimpleClockSynchronizer);
+
+void SimpleClockSynchronizer::initialize(int stage)
+{
+    ApplicationBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        synhronizationTimer = new cMessage("SynchronizationTimer");
+        masterClock = getModuleFromPar<IClock>(par("masterClockModule"), this);
+        slaveClock = getModuleFromPar<SettableClock>(par("slaveClockModule"), this);
+    }
+}
+
+void SimpleClockSynchronizer::handleMessageWhenUp(cMessage *msg)
+{
+    if (msg == synhronizationTimer) {
+        synchronizeSlaveClock();
+        scheduleSynchronizationTimer();
+    }
+    else
+        throw cRuntimeError("Unknown message");
+}
+
+void SimpleClockSynchronizer::handleStartOperation(LifecycleOperation *operation)
+{
+    scheduleSynchronizationTimer();
+}
+
+void SimpleClockSynchronizer::synchronizeSlaveClock()
+{
+    slaveClock->setClockTime(masterClock->getClockTime() + par("synchronizationAccuracy"), true);
+}
+
+void SimpleClockSynchronizer::scheduleSynchronizationTimer()
+{
+    scheduleAfter(par("synchronizationInterval"), synhronizationTimer);
+}
+
+} // namespace
+
