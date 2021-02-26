e3c1b7f97026391b5ca18beffbc138bde0be9713
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-02-26 10:04:13 +0100

    SettableClock: bugfix: with big numbers, the calculation with doubles lost the precision

--- src/inet/clock/model/SettableClock.cc
+++ src/inet/clock/model/SettableClock.cc
@@ -78,7 +78,7 @@ void SettableClock::setClockTime(clocktime_t newClockTime, bool resetOscillator)
         simtime_t currentSimTime = simTime();
         EV_DEBUG << "Setting clock time from " << oldClockTime << " to " << newClockTime << " at simtime " << currentSimTime << ".\n";
         int64_t numTicks = oscillator->computeTicksForInterval(currentSimTime - oscillator->getComputationOrigin());
-        originClockTick = floor(newClockTime / ClockTime::from(oscillator->getNominalTickLength())) - numTicks;
+        originClockTick = newClockTime.raw() / oscillator->getNominalTickLength().raw() - numTicks;
         ASSERT(newClockTime == getClockTime());
         clocktime_t clockDelta = newClockTime - oldClockTime;
         for (auto event : events) {
