705b5da54e03bf60709d2ae9fb0d9030050832d0
TÖRÖK Attila <torokati44@gmail.com>
2021-03-11 02:15:55 +0100

    github: Split fingerprint tests into 4 parallel jobs

--- _scripts/github/github-job-fingerprints.sh
+++ _scripts/github/github-job-fingerprints.sh
@@ -7,10 +7,11 @@
 #
 #    GITHUB_WORKSPACE       - this is provided by GitHub, most likely has
 #                             the value "/home/runner/work/inet/inet"
-
+# Optionally:
+#    SPLIT_N                - the number of partitions to split the tests into (default 1)
+#    SPLIT_I                - the index of the partition to run in this invocation (default 0)
 
 set -e # make the script exit with error if any executed command exits with error
-
 
 export PATH="/root/omnetpp-6.0pre10-linux/bin:$PATH"
 
@@ -25,11 +26,14 @@ echo "::group::Enable all features"
 opp_featuretool enable all 2>&1 # redirecting stderr so it doesn't get out of sync with stdout
 echo "::endgroup::"
 
+SPLIT_N="${SPLIT_N:-1}"
+SPLIT_I="${SPLIT_I:-0}"
+
 echo "::group::Run fingerprint tests"
 cd tests/fingerprint
 # this indirectly calls the script named simply "inet", which handles the MODE envvar internally
-./fingerprinttest -f 'tplx' | tee fingerprinttest.out
-./fingerprinttest -f '~tND' -a '--**.crcMode=\"computed\" --**.fcsMode=\"computed\"' | tee fingerprinttest.out
+./fingerprinttest -n $SPLIT_N -i $SPLIT_I -f 'tplx' | tee fingerprinttest.out
+./fingerprinttest -n $SPLIT_N -i $SPLIT_I -f '~tND' -a '--**.crcMode=\"computed\" --**.fcsMode=\"computed\"' | tee fingerprinttest.out
 #                ^---- Everything from here on is only needed to make the pretty GitHub annotations. ----v
 EXITCODE="${PIPESTATUS[0]}"
 echo "::endgroup::"
