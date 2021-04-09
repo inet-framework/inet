b09d86fb18f65b775fa9537333b0a335bb569ddc
Levente Mészáros <levy@omnetpp.org>
2021-04-09 10:41:09 +0200

    Test: Followed recent changes in the clock implementation, verified tests manually.

--- tests/unit/lib/ClockTestApp.cc
+++ tests/unit/lib/ClockTestApp.cc
@@ -7,6 +7,9 @@ class ClockTestApp : public ClockUserModuleMixin<cSimpleModule>
 private:
         size_t idx = 0;
         std::vector<clocktime_t> timeVector;
+        ClockEvent *afterClock;
+        clocktime_t after;
+        int repeat;
     public:
        ClockTestApp() : ClockUserModuleMixin() {}
     protected:
@@ -18,6 +21,7 @@ private:
 
     protected:
         void doSchedule(clocktime_t t, ClockEvent *msg);
+        bool doScheduleAfter(clocktime_t t, ClockEvent *msg);
 };
 
 Define_Module(ClockTestApp);
@@ -27,7 +31,9 @@ void ClockTestApp::initialize(int stage)
     ClockUserModuleMixin::initialize(stage);
 
     if (stage == INITSTAGE_LAST) {
-        EV << "start\n";
+        auto c = getClockTime();
+        auto s = computeSimTimeFromClockTime(c);
+        EV << "start" << ": simtime: " << simTime() << ", clock: " << getClockTime() << ", computed simtime: " << computeSimTimeFromClockTime(getClockTime()) << endl;
         timeVector.push_back(1.1);
         timeVector.push_back(2.11);
         timeVector.push_back(2.89);
@@ -39,6 +45,11 @@ void ClockTestApp::initialize(int stage)
             auto msg = new ClockEvent(timerName.c_str());
             doSchedule(ct, msg);
         }
+        after = 1.2;
+        int repeat = 6;
+        std::string timerName = "After_" + after.str();
+        afterClock = new ClockEvent(timerName.c_str(), repeat);
+        doScheduleAfter(after, afterClock);
     }
 }
 
@@ -48,16 +59,36 @@ void ClockTestApp::doSchedule(clocktime_t ct, ClockEvent *msg)
     EV << "schedule " << msg->getName() << " to clock: " << ct << ", scheduled simtime: " << msg->getArrivalTime() << ", scheduled clock: " << getArrivalClockTime(msg) << endl;
 }
 
+bool ClockTestApp::doScheduleAfter(clocktime_t ct, ClockEvent *msg)
+{
+    if (msg->getKind() > 0) {
+        msg->setKind(msg->getKind() - 1);
+        scheduleClockEventAfter(ct, msg);
+        EV << "schedule " << msg->getName() << " after clock: " << ct << ", scheduled simtime: " << msg->getArrivalTime() << ", scheduled clock: " << getArrivalClockTime(msg) << endl;
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 void ClockTestApp::handleMessage(cMessage *msg)
 {
     ASSERT(msg->isSelfMessage());
     EV << "arrived " << msg->getName() << ": simtime: " << simTime() << ", clock: " << getClockTime() << endl;
-    delete msg;
+    if (msg == afterClock) {
+        if (!doScheduleAfter(after, afterClock)) {
+            delete msg;
+            afterClock = nullptr;
+        }
+    }
+    else
+        delete msg;
 }
 
 void ClockTestApp::finish()
 {
-    EV << "finished: simtime: " << simTime() << ", clock: " << getClockTime() << endl;
+    EV << "finished: simtime: " << simTime() << ", clock: " << getClockTime() << ", computed simtime: " << computeSimTimeFromClockTime(getClockTime()) << endl;
 }
 
 } // namespace inet
