d7817379d5d671bcae4667d7e16ecabe72a65906
Levente Mészáros <levy@omnetpp.org>
2021-04-12 13:28:49 +0200

    z3: Initial takeover from project TSNsched at https://github.com/ACassimiro/TSNsched.

---
+++ src/inet/linklayer/configurator/z3/PathNode.java
@@ -0,0 +1,104 @@
+package schedule_generator;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+
+/**
+ * [Class]: PathNode
+ * [Usage]: Contains the data needed in each node of 
+ * a pathTree. Can reference a father, possesses an 
+ * device or switch, a list of children and a flow 
+ * fragment for each children in case of being a switch.
+ * 
+ */
+public class PathNode implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private PathNode parent; // The parent of the current FlowNode
+    private Object node;
+    private ArrayList<PathNode> children; // The children of the current FlowNode
+    private ArrayList<FlowFragment> flowFragments;
+   
+    
+    /**
+     * [Method]: PathNode
+     * [Usage]: Overloaded constructor method of the this class.
+     * Receives an object that must be either a device or a switch.
+     * In case of switch, creates a list of children and flowFragments.
+     * 
+     * @param node      Device or a Switch that represents the node
+     */
+    public PathNode (Object node)
+    {
+        if((node instanceof TSNSwitch) || (node instanceof Switch)) {
+            this.node = node;
+            children = new ArrayList<PathNode>();
+            flowFragments = new ArrayList<FlowFragment>();
+        } else if (node instanceof Device) {
+            this.node = node;
+            children = null;
+        } else {
+            //[TODO]: Throw error
+        }
+        
+        children  = new ArrayList<PathNode>();
+    }
+
+    /**
+     * [Method]: addChild
+     * [Usage]: Adds a child to this node.
+     * 
+     * @param node      Object representing the child device or switch
+     * @return          A reference to the newly created node
+     */
+    public PathNode addChild(Object node)
+    {
+        PathNode pathNode = new PathNode(node);
+        pathNode.setParent(this);
+        children.add(pathNode);
+                
+        return pathNode;
+    }
+    
+    /*
+     * GETTERS AND SETTERS:
+     */
+    
+    public PathNode getParent() {
+        return parent;
+    }
+
+    public void setParent(PathNode parent) {
+        this.parent = parent;
+    }
+
+    public Object getNode() {
+        return node;
+    }
+
+    public void setNode(Object node) {
+        this.node = node;
+    }
+
+    public ArrayList<PathNode> getChildren() {
+        return children;
+    }
+
+    public void setChildren(ArrayList<PathNode> children) {
+        this.children = children;
+    }
+
+    public void addFlowFragment(FlowFragment flowFragment) {
+        this.flowFragments.add(flowFragment);
+    }
+    
+    public ArrayList<FlowFragment> getFlowFragments() {
+        return flowFragments;
+    }
+
+    public void setFlowFragment(ArrayList<FlowFragment>  flowFragments) {
+        this.flowFragments = flowFragments;
+    }
+    
+}
---
+++ src/inet/linklayer/configurator/z3/Switch.java
@@ -0,0 +1,111 @@
+package schedule_generator;
+
+import java.io.Serializable;
+
+/**
+ * [Class]: Switch
+ * [Usage]: Contains most of the properties of a normal
+ * switch that are used to build the schedule. Since this 
+ * scheduler doesn't take in consideration scenarios where
+ * normal switches and TSN switches interact, no z3 properties
+ * had to be specified in this class.
+ * 
+ * Can be used to further extend the usability of this 
+ * project in the future.
+ * 
+ */
+public class Switch implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	protected String name;
+    protected float maxPacketSize;
+    protected float timeToTravel;
+    protected float transmissionTime;
+    protected float portSpeed;
+    
+    //protected RealExpr maxPacketSizeZ3;
+    //protected RealExpr timeToTravelZ3;
+    //protected RealExpr transmissionTimeZ3;
+    //protected RealExpr portSpeedZ3;
+    
+    
+    /**
+     * [Method]: Switch
+     * [Usage]: Default constructor of class Switch. Is used by
+     * TSNSwitch to instantiate new children classes.
+     */
+    public Switch() {
+    
+    }
+    
+    /**
+     * [Method]: Switch
+     * [Usage]: Overloaded constructor of class Switch. Can
+     * instantiate new switches using properties given as 
+     * parameters.
+     * 
+     * @param name                  Name of the switch
+     * @param maxPacketSize         Maximum size of a packet supported by this switch
+     * @param timeToTravel          Time that a packet takes to leave its port and reach the destination
+     * @param transmissionTime      Time taken to process the packet inside the switch
+     * @param portSpeed             Transmission speed of the port
+     */
+    public Switch(String name,
+                  float maxPacketSize,
+                  float timeToTravel,
+                  float transmissionTime,
+                  float portSpeed) {
+        this.name = name;
+        this.maxPacketSize = maxPacketSize;
+        this.timeToTravel = timeToTravel;
+        this.transmissionTime = transmissionTime;
+        this.portSpeed = portSpeed;
+    }
+    
+    
+    /*
+     *  GETTERS AND SETTERS
+     */
+    
+    
+    public float getMaxPacketSize() {
+        return maxPacketSize;
+    }
+
+    public void setMaxPacketSize(float maxPacketSize) {
+        this.maxPacketSize = maxPacketSize;
+    }
+
+    public float getTimeToTravel() {
+        return timeToTravel;
+    }
+
+    public void setTimeToTravel(float timeToTravel) {
+        this.timeToTravel = timeToTravel;
+    }
+
+    public float getTransmissionTime() {
+        return transmissionTime;
+    }
+
+    public void setTransmissionTime(float transmissionTime) {
+        this.transmissionTime = transmissionTime;
+    }
+
+    public float getPortSpeed() {
+        return portSpeed;
+    }
+
+    public void setPortSpeed(float portSpeed) {
+        this.portSpeed = portSpeed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+}
