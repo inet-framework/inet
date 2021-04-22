cbe68280cf4eecb8f6ea4cb61118a16b6f359c69
Levente Mészáros <levy@omnetpp.org>
2021-04-22 10:38:52 +0200

    Network: Added a generic NetworkConfiguratorBase that only deals with extracting the topology and such.

---
+++ src/inet/networklayer/configurator/base/NetworkConfiguratorBase.cc
@@ -0,0 +1,132 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/networklayer/configurator/base/NetworkConfiguratorBase.h"
+
+#include <queue>
+#include <set>
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+#include "inet/networklayer/ipv4/IIpv4RoutingTable.h"
+
+namespace inet {
+
+void NetworkConfiguratorBase::extractTopology(Topology& topology)
+{
+    topology.extractByProperty("networkNode");
+    EV_DEBUG << "Topology found " << topology.getNumNodes() << " nodes\n";
+    if (topology.getNumNodes() == 0)
+        throw cRuntimeError("Empty network!");
+    // extract nodes, fill in interfaceTable and routingTable members in node
+    L3AddressResolver addressResolver;
+    for (int i = 0; i < topology.getNumNodes(); i++) {
+        Node *node = (Node *)topology.getNode(i);
+        node->module = node->getModule();
+        node->interfaceTable = addressResolver.findInterfaceTableOf(node->module);
+        node->routingTable = addressResolver.findIpv4RoutingTableOf(node->module);
+    }
+    // extract links and interfaces
+    std::set<NetworkInterface *> interfacesSeen;
+    std::queue<Node *> unvisited; // unvisited nodes in the graph
+    auto rootNode = (Node *)topology.getNode(0);
+    unvisited.push(rootNode);
+    while (!unvisited.empty()) {
+        Node *node = unvisited.front();
+        unvisited.pop();
+        IInterfaceTable *interfaceTable = node->interfaceTable;
+        if (interfaceTable) {
+            // push neighbors to the queue
+            for (int i = 0; i < interfaceTable->getNumInterfaces(); i++) {
+                NetworkInterface *networkInterface = interfaceTable->getInterface(i);
+                if (interfacesSeen.count(networkInterface) == 0) {
+                    // visiting this interface
+                    interfacesSeen.insert(networkInterface);
+                    Topology::LinkOut *linkOut = findLinkOut(node, networkInterface->getNodeOutputGateId());
+                    Node *childNode = nullptr;
+                    if (linkOut) {
+                        childNode = (Node *)linkOut->getRemoteNode();
+                        unvisited.push(childNode);
+                    }
+                    InterfaceInfo *info = new InterfaceInfo(networkInterface);
+                    node->interfaceInfos.push_back(info);
+                }
+            }
+        }
+    }
+    // annotate links with interfaces
+    for (int i = 0; i < topology.getNumNodes(); i++) {
+        Node *node = (Node *)topology.getNode(i);
+        for (int j = 0; j < node->getNumOutLinks(); j++) {
+            Topology::LinkOut *linkOut = node->getLinkOut(j);
+            Link *link = (Link *)linkOut;
+            Node *localNode = (Node *)linkOut->getLocalNode();
+            if (localNode->interfaceTable)
+                link->sourceInterfaceInfo = findInterfaceInfo(localNode, localNode->interfaceTable->findInterfaceByNodeOutputGateId(linkOut->getLocalGateId()));
+            Node *remoteNode = (Node *)linkOut->getRemoteNode();
+            if (remoteNode->interfaceTable)
+                link->destinationInterfaceInfo = findInterfaceInfo(remoteNode, remoteNode->interfaceTable->findInterfaceByNodeInputGateId(linkOut->getRemoteGateId()));
+        }
+    }
+}
+
+std::vector<NetworkConfiguratorBase::Node *> NetworkConfiguratorBase::computeShortestPath(Node *source, Node *destination) const
+{
+    std::vector<Node *> path;
+    topology.calculateUnweightedSingleShortestPathsTo(destination);
+    auto node = source;
+    while (node != destination) {
+        path.push_back(node);
+        node = (Node *)node->getPath(0)->getRemoteNode();
+    }
+    path.push_back(destination);
+    return path;
+}
+
+bool NetworkConfiguratorBase::isBridgeNode(Node *node) const
+{
+    return !node->routingTable || !node->interfaceTable;
+}
+
+NetworkConfiguratorBase::Link *NetworkConfiguratorBase::findLinkOut(Node *node, const char *neighbor) const
+{
+    for (int i = 0; i < node->getNumOutLinks(); i++)
+        if (!strcmp(node->getLinkOut(i)->getRemoteNode()->getModule()->getFullName(), neighbor))
+            return check_and_cast<Link *>(static_cast<Topology::Link *>(node->getLinkOut(i)));
+    return nullptr;
+}
+
+Topology::LinkOut *NetworkConfiguratorBase::findLinkOut(Node *node, int gateId) const
+{
+    for (int i = 0; i < node->getNumOutLinks(); i++)
+        if (node->getLinkOut(i)->getLocalGateId() == gateId)
+            return node->getLinkOut(i);
+    return nullptr;
+}
+
+NetworkConfiguratorBase::InterfaceInfo *NetworkConfiguratorBase::findInterfaceInfo(Node *node, NetworkInterface *networkInterface) const
+{
+    if (networkInterface == nullptr)
+        return nullptr;
+    for (auto& interfaceInfo : node->interfaceInfos)
+        if (interfaceInfo->networkInterface == networkInterface)
+            return interfaceInfo;
+    return nullptr;
+}
+
+} // namespace inet
+
---
+++ src/inet/networklayer/configurator/base/NetworkConfiguratorBase.h
@@ -0,0 +1,100 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_NETWORKCONFIGURATORBASE_H
+#define __INET_NETWORKCONFIGURATORBASE_H
+
+#include "inet/common/Topology.h"
+#include "inet/networklayer/common/NetworkInterface.h"
+#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/networklayer/contract/IRoutingTable.h"
+
+namespace inet {
+
+class INET_API NetworkConfiguratorBase : public cSimpleModule
+{
+  protected:
+    class InterfaceInfo;
+
+    /**
+     * Represents a node in the network.
+     */
+    class Node : public Topology::Node {
+      public:
+        cModule *module;
+        IInterfaceTable *interfaceTable;
+        IRoutingTable *routingTable = nullptr;
+        std::vector<InterfaceInfo *> interfaceInfos;
+
+      public:
+        Node(cModule *module) : Topology::Node(module->getId()) { this->module = module; interfaceTable = nullptr; }
+        ~Node() { for (size_t i = 0; i < interfaceInfos.size(); i++) delete interfaceInfos[i]; }
+    };
+
+    /**
+     * Represents an interface in the network.
+     */
+    class InterfaceInfo : public cObject {
+      public:
+        NetworkInterface *networkInterface;
+
+      public:
+        InterfaceInfo(NetworkInterface *networkInterface) : networkInterface(networkInterface) {}
+        virtual std::string getFullPath() const override { return networkInterface->getInterfaceFullPath(); }
+    };
+
+    class Link : public Topology::Link {
+      public:
+        InterfaceInfo *sourceInterfaceInfo;
+        InterfaceInfo *destinationInterfaceInfo;
+
+      public:
+        Link() { sourceInterfaceInfo = nullptr; destinationInterfaceInfo = nullptr; }
+    };
+
+    class Topology : public inet::Topology {
+      protected:
+        virtual Node *createNode(cModule *module) override { return new NetworkConfiguratorBase::Node(module); }
+        virtual Link *createLink() override { return new NetworkConfiguratorBase::Link(); }
+    };
+
+  protected:
+    Topology topology;
+
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void handleMessage(cMessage *msg) override { throw cRuntimeError("Invalid operation"); }
+
+    /**
+     * Extracts network topology by walking through the module hierarchy.
+     * Creates nodes from modules having @networkNode property.
+     * Creates links from connections (wired and wireless) between network interfaces.
+     */
+    virtual void extractTopology(Topology& topology);
+
+    virtual std::vector<Node *> computeShortestPath(Node *source, Node *destination) const;
+
+    virtual bool isBridgeNode(Node *node) const;
+    virtual Link *findLinkOut(Node *node, const char *neighbor) const;
+    virtual Topology::LinkOut *findLinkOut(Node *node, int gateId) const;
+    virtual InterfaceInfo *findInterfaceInfo(Node *node, NetworkInterface *networkInterface) const;
+};
+
+} // namespace inet
+
+#endif
+
