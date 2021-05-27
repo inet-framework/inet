d84de8c41ebb5444d6599829e350bd8909ebbd5d
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-05-27 13:40:22 +0200

    DynamicClassifier: follow omnetpp submodule vector api changes

--- src/inet/queueing/classifier/DynamicClassifier.cc
+++ src/inet/queueing/classifier/DynamicClassifier.cc
@@ -31,7 +31,11 @@ void DynamicClassifier::initialize(int stage)
     if (stage == INITSTAGE_LOCAL) {
         submoduleName = par("submoduleName");
         moduleType = cModuleType::get(par("moduleType"));
-    }
+#if OMNETPP_VERSION >= 0x0600 && OMNETPP_BUILDNUM >= 1516
+        if (!getParentModule()->hasSubmoduleVector(submoduleName))
+             throw cRuntimeError("The submodule vector '%s' missing from %s", submoduleName, getParentModule()->getFullPath().c_str());
+#endif
+     }
 }
 
 int DynamicClassifier::classifyPacket(Packet *packet)
@@ -41,7 +45,13 @@ int DynamicClassifier::classifyPacket(Packet *packet)
     if (it == classIndexToGateItMap.end()) {
         auto parentModule = getParentModule();
         int submoduleIndex = gateSize("out");
+#if OMNETPP_VERSION >= 0x0600 && OMNETPP_BUILDNUM >= 1516
+        int origVectorSize = parentModule->getSubmoduleVectorSize(submoduleName);
+        parentModule->setSubmoduleVectorSize(submoduleName, std::max(origVectorSize, submoduleIndex + 1));
+        auto module = moduleType->create(submoduleName, parentModule, submoduleIndex);
+#else
         auto module = moduleType->create(submoduleName, parentModule, submoduleIndex + 1, submoduleIndex);
+#endif
         auto moduleInputGate = module->gate("in");
         auto moduleOutputGate = module->gate("out");
         auto multiplexer = parentModule->getSubmodule("multiplexer");
