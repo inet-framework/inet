4cae819bb2179bef9ab2562eca50b6370ed3e841
Levente Mészáros <levy@omnetpp.org>
2021-06-02 08:16:25 +0200

    applications: Added Ieee8022LlcApp, Ieee8022LlcSink and Ieee8022LlcSocketIo modules.
    These can be used as a LLC layer application the directly uses the IEEE 802.2 LLC protocol.

---
+++ src/inet/applications/ieee8022/Ieee8022LlcSocketIo.cc
@@ -0,0 +1,146 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/applications/ieee8022/Ieee8022LlcSocketIo.h"
+
+#include "inet/common/ModuleAccess.h"
+#include "inet/common/ProtocolTag_m.h"
+#include "inet/common/socket/SocketTag_m.h"
+#include "inet/linklayer/common/MacAddress.h"
+#include "inet/linklayer/common/MacAddressTag_m.h"
+#include "inet/networklayer/common/FragmentationTag_m.h"
+#include "inet/networklayer/common/L3AddressResolver.h"
+
+namespace inet {
+
+Define_Module(Ieee8022LlcSocketIo);
+
+void Ieee8022LlcSocketIo::initialize(int stage)
+{
+    ApplicationBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        localSap = par("localSap");
+        remoteSap = par("remoteSap");
+        numSent = 0;
+        numReceived = 0;
+        WATCH(numSent);
+        WATCH(numReceived);
+    }
+    else if (stage == INITSTAGE_NETWORK_INTERFACE_CONFIGURATION + 1) {
+        const char *localAddressString = par("localAddress");
+        if (*localAddressString != '\0') {
+            L3Address l3Address;
+            L3AddressResolver addressResolver;
+            addressResolver.tryResolve(localAddressString, l3Address, L3AddressResolver::ADDR_MAC);
+            if (l3Address.getType() == L3Address::MAC)
+                localAddress = l3Address.toMac();
+            else
+                localAddress = MacAddress(localAddressString);
+        }
+        const char *remoteAddressString = par("remoteAddress");
+        if (*remoteAddressString != '\0') {
+            L3Address l3Address;
+            L3AddressResolver addressResolver;
+            addressResolver.tryResolve(remoteAddressString, l3Address, L3AddressResolver::ADDR_MAC);
+            if (l3Address.getType() == L3Address::MAC)
+                remoteAddress = l3Address.toMac();
+            else
+                remoteAddress = MacAddress(remoteAddressString);
+        }
+        const char *interface = par("interface");
+        if (interface[0] != '\0') {
+            auto interfaceTable = getModuleFromPar<IInterfaceTable>(par("interfaceTableModule"), this);
+            networkInterface = interfaceTable->findInterfaceByName(interface);
+            if (networkInterface == nullptr)
+                throw cRuntimeError("Cannot find network interface");
+        }
+    }
+}
+
+void Ieee8022LlcSocketIo::handleMessageWhenUp(cMessage *message)
+{
+    if (socket.belongsToSocket(message))
+        socket.processMessage(message);
+    else {
+        auto packet = check_and_cast<Packet *>(message);
+        if (packet->findTag<PacketProtocolTag>() == nullptr) {
+            auto packetProtocolTag = packet->addTag<PacketProtocolTag>();
+            packetProtocolTag->setProtocol(&Protocol::unknown);
+        }
+        auto& macAddressReq = packet->addTag<MacAddressReq>();
+        macAddressReq->setDestAddress(remoteAddress);
+        socket.send(packet);
+        numSent++;
+        emit(packetSentSignal, packet);
+    }
+}
+
+void Ieee8022LlcSocketIo::finish()
+{
+    recordScalar("packets sent", numSent);
+    recordScalar("packets received", numReceived);
+    ApplicationBase::finish();
+}
+
+void Ieee8022LlcSocketIo::refreshDisplay() const
+{
+    ApplicationBase::refreshDisplay();
+    char buf[100];
+    sprintf(buf, "rcvd: %d pks\nsent: %d pks", numReceived, numSent);
+    getDisplayString().setTagArg("t", 0, buf);
+}
+
+void Ieee8022LlcSocketIo::setSocketOptions()
+{
+    socket.setCallback(this);
+}
+
+void Ieee8022LlcSocketIo::socketDataArrived(Ieee8022LlcSocket *socket, Packet *packet)
+{
+    emit(packetReceivedSignal, packet);
+    EV_INFO << "Received packet: " << packet << endl;
+    numReceived++;
+    packet->removeTag<SocketInd>();
+    send(packet, "trafficOut");
+}
+
+void Ieee8022LlcSocketIo::socketClosed(Ieee8022LlcSocket *socket)
+{
+    if (operationalState == State::STOPPING_OPERATION)
+        startActiveOperationExtraTimeOrFinish(par("stopOperationExtraTime"));
+}
+
+void Ieee8022LlcSocketIo::handleStartOperation(LifecycleOperation *operation)
+{
+    setSocketOptions();
+    socket.setOutputGate(gate("socketOut"));
+    socket.open(networkInterface != nullptr ? networkInterface->getInterfaceId() : -1, localSap, remoteSap);
+}
+
+void Ieee8022LlcSocketIo::handleStopOperation(LifecycleOperation *operation)
+{
+    socket.close();
+    delayActiveOperationFinish(par("stopOperationTimeout"));
+}
+
+void Ieee8022LlcSocketIo::handleCrashOperation(LifecycleOperation *operation)
+{
+    socket.destroy();
+}
+
+} // namespace inet
+
