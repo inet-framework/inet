6b0266b1994e90de2dac4026899fa819a9afcd12
Levente Mészáros <levy@omnetpp.org>
2021-06-22 16:49:47 +0200

    protocolelement: Added various eligibility time base modules.
    Added EligiblityTimeMeter, EligibilityTimeFilter, EligiblityTimeGate, and EiligibilityTimeQueue.

---
+++ src/inet/protocolelement/shaper/EligibilityTimeFilter.cc
@@ -0,0 +1,51 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/protocolelement/shaper/EligibilityTimeFilter.h"
+
+#include "inet/protocolelement/shaper/EligibilityTimeTag_m.h"
+
+namespace inet {
+
+Define_Module(EligibilityTimeFilter);
+
+void EligibilityTimeFilter::initialize(int stage)
+{
+    PacketFilterBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL)
+        maxResidenceTime = par("maxResidenceTime");
+}
+
+cGate *EligibilityTimeFilter::getRegistrationForwardingGate(cGate *gate)
+{
+    if (gate == outputGate)
+        return inputGate;
+    else if (gate == inputGate)
+        return outputGate;
+    else
+        throw cRuntimeError("Unknown gate");
+}
+
+bool EligibilityTimeFilter::matchesPacket(const Packet *packet) const
+{
+    simtime_t arrivalTime = simTime();
+    const auto& eligibilityTimeTag = packet->findTag<EligibilityTimeTag>();
+    return eligibilityTimeTag != nullptr && (maxResidenceTime == 0 || eligibilityTimeTag->getEligibilityTime() <= arrivalTime + maxResidenceTime);
+}
+
+} // namespace inet
+
---
+++ src/inet/protocolelement/shaper/EligibilityTimeMeter.cc
@@ -0,0 +1,55 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/protocolelement/shaper/EligibilityTimeMeter.h"
+
+#include "inet/protocolelement/shaper/EligibilityTimeTag_m.h"
+
+namespace inet {
+
+Define_Module(EligibilityTimeMeter);
+
+void EligibilityTimeMeter::initialize(int stage)
+{
+    PacketMeterBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        comittedInformationRate = bps(par("comittedInformationRate"));
+        comittedBurstSize = b(par("comittedBurstSize"));
+        maxResidenceTime = par("maxResidenceTime");
+        WATCH(groupEligibilityTime);
+        WATCH(bucketEmptyTime);
+        WATCH(maxResidenceTime);
+    }
+}
+
+void EligibilityTimeMeter::meterPacket(Packet *packet)
+{
+    simtime_t arrivalTime = simTime();
+    simtime_t lengthRecoveryDuration = s(packet->getDataLength() / comittedInformationRate).get();
+    simtime_t emptyToFullDuration = s(comittedBurstSize / comittedInformationRate).get();
+    simtime_t schedulerEligibilityTime = bucketEmptyTime + lengthRecoveryDuration;
+    simtime_t bucketFullTime = bucketEmptyTime + emptyToFullDuration;
+    simtime_t eligibilityTime = SimTime::fromRaw(std::max(std::max(arrivalTime.raw(), groupEligibilityTime.raw()), schedulerEligibilityTime.raw()));
+    if (eligibilityTime <= arrivalTime + maxResidenceTime) {
+        groupEligibilityTime = eligibilityTime;
+        bucketEmptyTime = eligibilityTime < bucketFullTime ? schedulerEligibilityTime : schedulerEligibilityTime + eligibilityTime - bucketFullTime;
+        packet->addTagIfAbsent<EligibilityTimeTag>()->setEligibilityTime(eligibilityTime);
+    }
+}
+
+} // namespace inet
+
