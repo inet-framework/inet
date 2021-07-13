5df5fa79dc5d8845f3114e24b36311f73de5d7fd
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-07-13 18:40:09 +0200

    eth: added EthernetForker

---
+++ src/inet/linklayer/ethernet/common/EthernetForker.cc
@@ -0,0 +1,94 @@
+//
+// Copyright (C) 2011 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/linklayer/ethernet/common/EthernetForker.h"
+
+#include "inet/common/DirectionTag_m.h"
+#include "inet/linklayer/common/EtherType_m.h"
+#include "inet/linklayer/common/InterfaceTag_m.h"
+#include "inet/linklayer/common/MacAddressTag_m.h"
+#include "inet/linklayer/ethernet/common/EthernetMacHeader_m.h"
+#include "inet/networklayer/common/NetworkInterface.h"
+
+namespace inet {
+
+Define_Module(EthernetForker);
+
+void EthernetForker::initialize(int stage)
+{
+    PacketClassifierBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        interfaceTable.reference(this, "interfaceTableModule", true);
+        if (!par("bridgeAddress").isEmptyString())
+            bridgeAddress.setAddress(par("bridgeAddress").stringValue());
+        auto localDeliveryMacAddresses = check_and_cast<cValueArray*>(par("localDeliveryMacAddresses").objectValue())->asObjectVector<cValueArray>();
+        for (auto elem : localDeliveryMacAddresses) {
+            if (elem->size() == 1) {
+                MacAddress addr(elem->get(0).stringValue());
+                registeredMacAddresses.insert(MacAddressPair(addr, addr));
+            }
+            else if (elem->size() == 2) {
+                MacAddress startAddr(elem->get(0).stringValue());
+                MacAddress endAddr(elem->get(1).stringValue());
+                registeredMacAddresses.insert(MacAddressPair(startAddr, endAddr));
+            }
+            else
+                throw cRuntimeError("Need one or two MAC address in one elem.");
+        }
+
+        WATCH(bridgeAddress);
+    }
+}
+
+int EthernetForker::classifyPacket(Packet *packet)
+{
+    auto directionTag = packet->findTag<DirectionTag>();
+    if (directionTag != nullptr && directionTag->getDirection() != DIRECTION_INBOUND)
+        throw cRuntimeError("Packet must be inbound");
+
+    auto interfaceInd = packet->getTag<InterfaceInd>();
+    auto macAddressInd = packet->getTag<MacAddressInd>();
+    auto incomingInterfaceId = interfaceInd->getInterfaceId();
+    auto incomingInterface = interfaceTable->getInterfaceById(incomingInterfaceId);
+    auto sourceAddress = macAddressInd->getSrcAddress();
+    auto destinationAddress = macAddressInd->getDestAddress();
+
+    if (destinationAddress.isBroadcast())
+        return 1;   // TODO send up + forward?
+    if (destinationAddress == bridgeAddress)
+        return 0;
+    if (in_range(registeredMacAddresses, destinationAddress))
+        return 0;
+    if (incomingInterface->matchesMacAddress(destinationAddress))
+        return 0;
+    return 1;
+}
+
+void EthernetForker::mapRegistrationForwardingGates(cGate *gate, std::function<void(cGate *)> f)
+{
+    if (gate == inputGate) {
+        for (auto g : outputGates)
+            f(g);
+    }
+    else if (contains(outputGates, gate))
+        f(inputGate);
+    else
+        throw cRuntimeError("Unknown gate");
+}
+
+} // namespace inet
+
---
+++ src/inet/linklayer/ethernet/common/EthernetForker.h
@@ -0,0 +1,72 @@
+//
+// Copyright (C) 2011 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_ETHERNETFRAMECLASSIFIER_H
+#define __INET_ETHERNETFRAMECLASSIFIER_H
+
+#include "inet/common/stlutils.h"
+#include "inet/common/IProtocolRegistrationListener.h"
+#include "inet/common/ModuleRefByPar.h"
+#include "inet/networklayer/contract/IInterfaceTable.h"
+#include "inet/queueing/base/PacketClassifierBase.h"
+
+namespace inet {
+
+/**
+ * Ethernet Frame classifier.
+ *
+ * Ethernet frames are classified as:
+ * - PAUSE frames
+ * - others
+ */
+class INET_API EthernetForker : public queueing::PacketClassifierBase
+{
+  protected:
+    ModuleRefByPar<IInterfaceTable> interfaceTable;
+    MacAddress bridgeAddress;
+
+    typedef std::pair<MacAddress, MacAddress> MacAddressPair;
+
+    struct Comp {
+        bool operator()(const MacAddressPair& first, const MacAddressPair& second) const
+        {
+            return first.first < second.first && first.second < second.first;
+        }
+    };
+
+    bool in_range(const std::set<MacAddressPair, Comp>& ranges, MacAddress value)
+    {
+        return contains(ranges, MacAddressPair(value, value));
+    }
+
+    std::set<MacAddressPair, Comp> registeredMacAddresses;
+
+  public:
+    virtual void initialize(int stage) override;
+
+    /**
+     * Sends the incoming packet to either the first or the second gate.
+     */
+    virtual int classifyPacket(Packet *packet) override;
+
+    virtual void mapRegistrationForwardingGates(cGate *gate, std::function<void(cGate *)> f) override;
+};
+
+} // namespace inet
+
+#endif
+
