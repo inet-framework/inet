1effdc2bf55ca08ee7974c566d2a66028b52abad
Levente Mészáros <levy@omnetpp.org>
2021-07-14 10:33:01 +0200

    clock: Added MultiClock module that contains multiple clock submodules.

---
+++ src/inet/clock/model/MultiClock.cc
@@ -0,0 +1,39 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/clock/model/MultiClock.h"
+
+namespace inet {
+
+Define_Module(MultiClock);
+
+void MultiClock::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL)
+        activeClock = check_and_cast<IClock *>(getSubmodule("clock", par("activeClockIndex")));
+}
+
+void MultiClock::handleParameterChange(const char *name)
+{
+    if (name != nullptr) {
+        if (!strcmp(name, "activeClockIndex"))
+            activeClock = check_and_cast<IClock *>(getSubmodule("clock", par("activeClockIndex")));
+    }
+}
+
+} // namespace inet
+
