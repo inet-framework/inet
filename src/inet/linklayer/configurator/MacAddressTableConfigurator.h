b93e7d0754fb5295cb1b5c26be29ec4f88d0745d
Levente Mészáros <levy@omnetpp.org>
2021-07-19 10:23:29 +0200

    Link: Added MacAddressTableConfigurator that can configure the MacAddressTable (forwarding table) of all network nodes.

---
+++ src/inet/linklayer/configurator/MacAddressTableConfigurator.h
@@ -0,0 +1,46 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_MACADDRESSTABLECONFIGURATOR_H
+#define __INET_MACADDRESSTABLECONFIGURATOR_H
+
+#include "inet/networklayer/configurator/base/NetworkConfiguratorBase.h"
+
+namespace inet {
+
+class INET_API MacAddressTableConfigurator : public NetworkConfiguratorBase
+{
+  protected:
+    std::map<int, cValueArray *> configurations;
+
+  protected:
+    virtual void initialize(int stage) override;
+
+    /**
+     * Computes the network configuration for all nodes in the network.
+     * The result of the computation is only stored in the configurator.
+     */
+    virtual void computeConfiguration();
+
+    virtual void computeMacAddressTables();
+    virtual void configureMacAddressTables() const;
+};
+
+} // namespace inet
+
+#endif
+
