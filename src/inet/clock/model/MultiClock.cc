c1b6d11491160f8818497bf7f1139ca8d4bf2e78
Levente Mészáros <levy@omnetpp.org>
2021-07-22 15:51:22 +0200

    MultiClock: Added timeChanged signal for the currently active time of the clock submodules.

--- src/inet/clock/model/MultiClock.cc
+++ src/inet/clock/model/MultiClock.cc
@@ -17,22 +17,39 @@
 
 #include "inet/clock/model/MultiClock.h"
 
+#include "inet/clock/base/ClockBase.h"
+
 namespace inet {
 
 Define_Module(MultiClock);
 
 void MultiClock::initialize(int stage)
 {
-    if (stage == INITSTAGE_LOCAL)
+    if (stage == INITSTAGE_LOCAL) {
         activeClock = check_and_cast<IClock *>(getSubmodule("clock", par("activeClockIndex")));
+        subscribe(ClockBase::timeChangedSignal, this);
+    }
 }
 
 void MultiClock::handleParameterChange(const char *name)
 {
     if (name != nullptr) {
-        if (!strcmp(name, "activeClockIndex"))
+        if (!strcmp(name, "activeClockIndex")) {
+            emit(ClockBase::timeChangedSignal, getClockTime().asSimTime());
             activeClock = check_and_cast<IClock *>(getSubmodule("clock", par("activeClockIndex")));
+            emit(ClockBase::timeChangedSignal, getClockTime().asSimTime());
+        }
     }
+}
+
+void MultiClock::receiveSignal(cComponent *source, int signal, const simtime_t& time, cObject *details)
+{
+    if (signal == ClockBase::timeChangedSignal) {
+        if (check_and_cast<IClock *>(source) == activeClock)
+            emit(ClockBase::timeChangedSignal, time, details);
+    }
+    else
+        throw cRuntimeError("Unknown signal");
 }
 
 } // namespace inet
