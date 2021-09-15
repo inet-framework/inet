f5f44a80b2896b3d8e9b78dcc0c4cf558d6a2754
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-09-15 13:48:18 +0200

    all: move some enum to MSG

--- src/inet/routing/dymo/DYMOdefs.h
+++ src/inet/routing/dymo/DYMOdefs.h
@@ -29,11 +29,6 @@ namespace dymo {
 
 typedef uint32_t DYMOSequenceNumber;
 
-// TODO: metric types are defined in a separate [RFC 6551]
-enum DYMOMetricType {
-    HOP_COUNT = 3,    // Hop Count has Metric Type assignment 3
-};
-
 enum DYMORouteState {
     ACTIVE,
     IDLE,
