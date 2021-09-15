50b3676ae1a7e5aacc0a936ccc663fa0fc172e91
Levente Mészáros <levy@omnetpp.org>
2021-09-15 10:00:44 +0200

    Common: Simplified findConnectedGate to only look at the tynamic type of the owner.
    The original condition was there to prevent issues in PacketProcessorBase::checkPacketOperationSupport().

--- src/inet/common/ModuleAccess.h
+++ src/inet/common/ModuleAccess.h
@@ -106,30 +106,20 @@ template<typename T>
 cGate *findConnectedGate(cGate *gate, int direction = 0)
 {
     if (direction < 0 || (direction == 0 && gate->getType() == cGate::INPUT)) {
-        auto g = gate;
+        auto g = gate->getPreviousGate();
         while (g != nullptr) {
-            auto gateType = g->getType();
-            auto previousGate = g->getPreviousGate();
-            if (dynamic_cast<T *>(g->getOwnerModule()) &&
-                ((gateType == cGate::OUTPUT && previousGate == nullptr) ||
-                 (gateType == cGate::INPUT && g != gate && previousGate != nullptr)))
+            if (dynamic_cast<T *>(g->getOwnerModule()))
                 break;
-            else
-                g = previousGate;
+            g = g->getPreviousGate();
         }
         return g;
     }
     else if (direction > 0 || (direction == 0 && gate->getType() == cGate::OUTPUT)) {
-        auto g = gate;
+        auto g = gate->getNextGate();
         while (g != nullptr) {
-            auto gateType = g->getType();
-            auto nextGate = g->getNextGate();
-            if (dynamic_cast<T *>(g->getOwnerModule()) &&
-                ((gateType == cGate::INPUT && nextGate == nullptr) ||
-                 (gateType == cGate::OUTPUT && g != gate && nextGate != nullptr)))
+            if (dynamic_cast<T *>(g->getOwnerModule()))
                 break;
-            else
-                g = nextGate;
+            g = g->getNextGate();
         }
         return g;
     }
