3b96f6c07cca99560820f26ce09106b8b66652b2
Levente Mészáros <levy@omnetpp.org>
2021-09-24 16:14:30 +0200

    802.1q: Added PCP request handling to Ieee8021qTag*HeaderInserter modules.

--- src/inet/linklayer/ieee8021q/Ieee8021qTagEpdHeaderInserter.cc
+++ src/inet/linklayer/ieee8021q/Ieee8021qTagEpdHeaderInserter.cc
@@ -20,6 +20,7 @@
 #include "inet/common/IProtocolRegistrationListener.h"
 #include "inet/common/ProtocolGroup.h"
 #include "inet/common/ProtocolTag_m.h"
+#include "inet/linklayer/common/PcpTag_m.h"
 #include "inet/linklayer/common/UserPriorityTag_m.h"
 #include "inet/linklayer/common/VlanTag_m.h"
 #include "inet/linklayer/ieee8021q/Ieee8021qTagHeader_m.h"
@@ -43,6 +44,7 @@ void Ieee8021qTagEpdHeaderInserter::initialize(int stage)
         if (*nextProtocolAsString != '\0')
             nextProtocol = Protocol::getProtocol(nextProtocolAsString);
         defaultVlanId = par("defaultVlanId");
+        defaultPcp = par("defaultPcp");
         defaultUserPriority = par("defaultUserPriority");
     }
     else if (stage == INITSTAGE_LINK_LAYER)
@@ -58,6 +60,13 @@ void Ieee8021qTagEpdHeaderInserter::processPacket(Packet *packet)
         EV_INFO << "Setting PCP" << EV_FIELD(pcp, userPriority) << EV_ENDL;
         header->setPcp(userPriority);
         packet->addTagIfAbsent<UserPriorityInd>()->setUserPriority(userPriority);
+    }
+    auto pcpReq = packet->removeTagIfPresent<PcpReq>();
+    auto pcp = pcpReq != nullptr ? pcpReq->getPcp() : defaultPcp;
+    if (pcp != -1) {
+        EV_INFO << "Setting PCP" << EV_FIELD(vid, pcp) << EV_ENDL;
+        header->setPcp(pcp);
+        packet->addTagIfAbsent<PcpInd>()->setPcp(pcp);
     }
     auto vlanReq = packet->removeTagIfPresent<VlanReq>();
     auto vlanId = vlanReq != nullptr ? vlanReq->getVlanId() : defaultVlanId;
