f2005a1191efd858ce11a7f12580da323d90e2a3
Levente Mészáros <levy@omnetpp.org>
2021-09-28 11:10:53 +0200

    TimeTag: Added packetTotalTimes field.

--- src/inet/common/TimeTag.h
+++ src/inet/common/TimeTag.h
@@ -24,20 +24,20 @@
 namespace inet {
 
 template<typename T>
-void increaseTimeTag(const Ptr<Chunk>& chunk, simtime_t duration)
+void increaseTimeTag(const Ptr<Chunk>& chunk, simtime_t bitDuration, simtime_t packetDuration)
 {
     chunk->mapAllTagsForUpdate<T>(b(0), chunk->getChunkLength(), [&] (b offset, b length, T *timeTag) {
         for (int i = 0; i < (int)timeTag->getBitTotalTimesArraySize(); i++)
-            timeTag->setBitTotalTimes(i, timeTag->getBitTotalTimes(i) + duration);
+            timeTag->setBitTotalTimes(i, timeTag->getBitTotalTimes(i) + bitDuration);
     });
 }
 
 template<typename T>
-void increaseTimeTag(Packet *packet, simtime_t duration)
+void increaseTimeTag(Packet *packet, simtime_t bitDuration, simtime_t packetDuration)
 {
     packet->mapAllRegionTagsForUpdate<T>(b(0), packet->getTotalLength(), [&] (b offset, b length, const Ptr<T>& timeTag) {
         for (int i = 0; i < (int)timeTag->getBitTotalTimesArraySize(); i++)
-            timeTag->setBitTotalTimes(i, timeTag->getBitTotalTimes(i) + duration);
+            timeTag->setBitTotalTimes(i, timeTag->getBitTotalTimes(i) + bitDuration);
     });
 }
 
