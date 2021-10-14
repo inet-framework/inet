8f296a406f0ab6411512de9b937da8037a147139
Gyorgy Szaszko <adam.szaszko@gmail.com>
2021-10-14 12:53:24 +0200

    showcases: clockdrift updates

--- showcases/tsn/timesynchronization/clockdrift/run.sh
+++ showcases/tsn/timesynchronization/clockdrift/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 inet_dbg -s -u Cmdenv -c NoClockDrift --sim-time-limit=0.1s --cmdenv-redirect-output=true &
-inet_dbg -s -u Cmdenv -c ConstantClockDrift --sim-time-limit=0.1s --cmdenv-redirect-output=true &
+inet_dbg -s -u Cmdenv -c ConstantClockDriftRate --sim-time-limit=0.1s --cmdenv-redirect-output=true &
 inet_dbg -s -u Cmdenv -c OutOfBandSynchronization --sim-time-limit=0.1s --cmdenv-redirect-output=true &
 inet_dbg -s -u Cmdenv -c GptpSynchronization --sim-time-limit=0.1s --cmdenv-redirect-output=true
