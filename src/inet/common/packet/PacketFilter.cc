b6dead2fdd8139528988e38516f2c0d7a4ea0cc6
Levente Mészáros <levy@omnetpp.org>
2021-10-20 08:30:41 +0200

    PacketFilter: Refactored packet filter expressions to use the new NED expression syntax.

--- src/inet/common/packet/PacketFilter.cc
+++ src/inet/common/packet/PacketFilter.cc
@@ -16,66 +16,166 @@
 //
 
 #include "inet/common/packet/PacketFilter.h"
-
-#include "inet/common/ProtocolTag_m.h"
-#include "inet/common/packet/dissector/PacketDissector.h"
+#include "inet/linklayer/common/MacAddress.h"
+#include "inet/networklayer/common/L3Address.h"
+#include "inet/networklayer/contract/ipv4/Ipv4Address.h"
+#include "inet/networklayer/contract/ipv6/Ipv6Address.h"
 
 namespace inet {
 
 void PacketFilter::setPattern(const char *packetPattern, const char *chunkPattern)
 {
-    packetMatchExpression.setPattern(packetPattern, false, true, true);
-    chunkMatchExpression.setPattern(chunkPattern, false, true, true);
-    matchesAllChunks = !strcmp(chunkPattern, "*");
+    packetDissectorCallback = new PacketDissectorCallback(this);
+    dynamicExpressionResolver = new DynamicExpressionResolver(this);
+    filterExpression.parse(packetPattern, dynamicExpressionResolver);
 }
 
 bool PacketFilter::matches(const cPacket *cpacket) const
 {
-    MatchableObject matchableObject(MatchableObject::ATTRIBUTE_FULLNAME, cpacket);
-    if (!packetMatchExpression.matches(&matchableObject))
-        return false;
-    else if (auto packet = dynamic_cast<const Packet *>(cpacket)) {
-        if (matchesAllChunks)
-            return true;
-        else {
-            PacketDissectorCallback callback(*this);
-            return callback.matches(packet);
-        }
+    this->cpacket = cpacket;
+    protocolToChunkMap.clear();
+    classNameToChunkMap.clear();
+    if (auto packet = dynamic_cast<const Packet *>(cpacket)) {
+        PacketDissector packetDissector(ProtocolDissectorRegistry::globalRegistry, *packetDissectorCallback);
+        packetDissector.dissectPacket(const_cast<Packet *>(packet));
     }
-    else
-        return true;
-}
-
-PacketFilter::PacketDissectorCallback::PacketDissectorCallback(const PacketFilter& packetFilter) :
-    packetFilter(packetFilter)
-{
-}
-
-bool PacketFilter::PacketDissectorCallback::matches(const Packet *packet)
-{
-    PacketDissector packetDissector(ProtocolDissectorRegistry::globalRegistry, *this);
-    packetDissector.dissectPacket(const_cast<Packet *>(packet));
-    return matches_;
-}
-
-void PacketFilter::PacketDissectorCallback::startProtocolDataUnit(const Protocol *protocol)
-{
-}
-
-void PacketFilter::PacketDissectorCallback::endProtocolDataUnit(const Protocol *protocol)
-{
-}
-
-void PacketFilter::PacketDissectorCallback::markIncorrect()
-{
+    return filterExpression.evaluate().boolValue();
 }
 
 void PacketFilter::PacketDissectorCallback::visitChunk(const Ptr<const Chunk>& chunk, const Protocol *protocol)
 {
-    MatchableObject matchableObject(MatchableObject::ATTRIBUTE_CLASSNAME, chunk.get());
-    if (!matches_)
-        // TODO eliminate const_cast when cMatchExpression::matches becomes const
-        matches_ = const_cast<PacketFilter *>(&this->packetFilter)->chunkMatchExpression.matches(&matchableObject);
+    packetFilter->protocolToChunkMap.insert({protocol, const_cast<Chunk *>(chunk.get())});
+    auto className = chunk->getClassName();
+    const char *colon = strrchr(className, ':');
+    if (colon != nullptr)
+        className = colon + 1;
+    packetFilter->classNameToChunkMap.insert({className, const_cast<Chunk *>(chunk.get())});
+}
+
+cValue PacketFilter::DynamicExpressionResolver::readVariable(cExpression::Context *context, const char *name)
+{
+    if (!strcmp(name, "pk"))
+        return const_cast<cPacket *>(packetFilter->cpacket);
+    else {
+        bool isClassName = isupper(name[0]);
+        if (isClassName) {
+            auto it = packetFilter->classNameToChunkMap.find(name);
+            if (it != packetFilter->classNameToChunkMap.end())
+                return it->second;
+        }
+        else {
+            auto protocol = Protocol::findProtocol(name);
+            if (protocol != nullptr) {
+                auto it = packetFilter->protocolToChunkMap.find(protocol);
+                if (it != packetFilter->protocolToChunkMap.end())
+                    return it->second;
+            }
+        }
+        auto classDescriptor = packetFilter->cpacket->getDescriptor();
+        int field = classDescriptor->findField(name);
+        if (field != -1) {
+            auto fieldValue = classDescriptor->getFieldValueAsString(toAnyPtr(packetFilter->cpacket), field, 0);
+            return classDescriptor->getFieldValue(toAnyPtr(packetFilter->cpacket), field, 0);
+        }
+        else
+            return ResolverBase::readVariable(context, name);
+    }
+}
+
+cValue PacketFilter::DynamicExpressionResolver::readVariable(cExpression::Context *context, const char *name, intval_t index)
+{
+    bool isClassName = isupper(name[0]);
+    if (isClassName) {
+        if (index < packetFilter->classNameToChunkMap.count(name)) {
+            auto it = packetFilter->classNameToChunkMap.lower_bound(name);
+            while (index-- > 0 && it != packetFilter->classNameToChunkMap.end())
+                it++;
+            if (it != packetFilter->classNameToChunkMap.end())
+                return it->second;
+        }
+    }
+    else {
+        auto protocol = Protocol::findProtocol(name);
+        if (protocol != nullptr) {
+            if (index != packetFilter->protocolToChunkMap.count(protocol)) {
+                auto it = packetFilter->protocolToChunkMap.lower_bound(protocol);
+                while (index-- > 0 && it != packetFilter->protocolToChunkMap.end())
+                    it++;
+                if (it != packetFilter->protocolToChunkMap.end())
+                    return it->second;
+            }
+        }
+    }
+    return ResolverBase::readVariable(context, name, index);
+}
+
+cValue PacketFilter::DynamicExpressionResolver::readMember(cExpression::Context *context, const cValue &object, const char *name)
+{
+    if (object.getType() == cValue::OBJECT) {
+        if (dynamic_cast<Packet *>(object.objectValue())) {
+            if (cObjectFactory::find(name, "inet", false) != nullptr) {
+                auto it = packetFilter->classNameToChunkMap.find(name);
+                if (it != packetFilter->classNameToChunkMap.end())
+                    return it->second;
+                else
+                    return cValue(any_ptr(nullptr));
+            }
+            else {
+                auto protocol = Protocol::findProtocol(name);
+                if (protocol != nullptr) {
+                    auto it = packetFilter->protocolToChunkMap.find(protocol);
+                    if (it != packetFilter->protocolToChunkMap.end())
+                        return it->second;
+                    else
+                        return cValue(any_ptr(nullptr));
+                }
+            }
+        }
+        auto cobject = object.objectValue();
+        auto classDescriptor = cobject->getDescriptor();
+        int field = classDescriptor->findField(name);
+        if (field != -1) {
+            auto fieldValue = classDescriptor->getFieldValueAsString(toAnyPtr(cobject), field, 0);
+            return classDescriptor->getFieldValue(toAnyPtr(cobject), field, 0);
+        }
+        else
+            return ResolverBase::readMember(context, object, name);
+    }
+    else
+        return ResolverBase::readMember(context, object, name);
+}
+
+cValue PacketFilter::DynamicExpressionResolver::callMethod(cExpression::Context *context, const cValue& object, const char *name, cValue argv[], int argc)
+{
+    if (object.getType() == cValue::POINTER && object.pointerValue() != nullptr) {
+        // TODO this dispatch isn't ideal
+        const auto& type = object.pointerValue().pointerType();
+        if (type == typeid(MacAddress *)) {
+            auto macAddress = object.pointerValue().get<MacAddress>();
+            if (!strcmp("str", name))
+                return macAddress->str();
+            else if (!strcmp("getInt", name))
+                return (intval_t)macAddress->getInt();
+        }
+        else if (type == typeid(Ipv4Address *)) {
+            auto ipv4Address = object.pointerValue().get<Ipv4Address>();
+            if (!strcmp("str", name))
+                return ipv4Address->str();
+            else if (!strcmp("getInt", name))
+                return (intval_t)ipv4Address->getInt();
+        }
+        else if (type == typeid(Ipv6Address *)) {
+            auto ipv6Address = object.pointerValue().get<Ipv6Address>();
+            if (!strcmp("str", name))
+                return ipv6Address->str();
+        }
+        else if (type == typeid(L3Address *)) {
+            auto l3Address = object.pointerValue().get<L3Address>();
+            if (!strcmp("str", name))
+                return l3Address->str();
+        }
+    }
+    return ResolverBase::callMethod(context, object, name, argv, argc);
 }
 
 } // namespace inet
--- src/inet/common/packet/PacketFilter.h
+++ src/inet/common/packet/PacketFilter.h
@@ -18,7 +18,6 @@
 #ifndef __INET_PACKETFILTER_H
 #define __INET_PACKETFILTER_H
 
-#include "inet/common/MatchableObject.h"
 #include "inet/common/packet/dissector/PacketDissector.h"
 
 namespace inet {
@@ -34,27 +33,45 @@ class INET_API PacketFilter
   protected:
     class INET_API PacketDissectorCallback : public PacketDissector::ICallback {
       protected:
-        bool matches_ = false;
-        const PacketFilter& packetFilter;
+        PacketFilter *packetFilter = nullptr;
 
       public:
-        PacketDissectorCallback(const PacketFilter& packetFilter);
-
-        bool matches(const Packet *packet);
+        PacketDissectorCallback(PacketFilter *packetFilter) : packetFilter(packetFilter) { }
+        virtual ~PacketDissectorCallback() { }
 
         virtual bool shouldDissectProtocolDataUnit(const Protocol *protocol) override { return true; }
-        virtual void startProtocolDataUnit(const Protocol *protocol) override;
-        virtual void endProtocolDataUnit(const Protocol *protocol) override;
-        virtual void markIncorrect() override;
+        virtual void startProtocolDataUnit(const Protocol *protocol) override { }
+        virtual void endProtocolDataUnit(const Protocol *protocol) override { }
+        virtual void markIncorrect() override { }
         virtual void visitChunk(const Ptr<const Chunk>& chunk, const Protocol *protocol) override;
     };
 
+    class INET_API DynamicExpressionResolver : public cDynamicExpression::ResolverBase {
+      protected:
+        PacketFilter *packetFilter = nullptr;
+
+      public:
+        DynamicExpressionResolver(PacketFilter *packetFilter) : packetFilter(packetFilter) { }
+
+        virtual IResolver *dup() const override { throw cRuntimeError("Not implemented"); }
+
+        virtual cValue readVariable(cExpression::Context *context, const char *name) override;
+        virtual cValue readVariable(cExpression::Context *context, const char *name, intval_t index) override;
+        virtual cValue readMember(cExpression::Context *context, const cValue &object, const char *name) override;
+        virtual cValue callMethod(cExpression::Context *context, const cValue& object, const char *name, cValue argv[], int argc) override;
+    };
+
   protected:
-    bool matchesAllChunks = false;
-    cMatchExpression packetMatchExpression;
-    cMatchExpression chunkMatchExpression;
+    cDynamicExpression filterExpression;
+    PacketDissectorCallback *packetDissectorCallback = nullptr;
+    DynamicExpressionResolver *dynamicExpressionResolver = nullptr;
+    mutable const cPacket *cpacket = nullptr;
+    mutable std::multimap<const Protocol *, Chunk *> protocolToChunkMap;
+    mutable std::multimap<std::string, Chunk *> classNameToChunkMap;
 
   public:
+    virtual ~PacketFilter() { delete packetDissectorCallback; }
+
     void setPattern(const char *packetPattern, const char *chunkPattern);
 
     bool matches(const cPacket *packet) const;
