f590d484569f6adb9c4f548083c773ed2bcaeedc
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-10-25 13:27:21 +0200

    ~ MovingMobilityBase::handleSelfMessage

--- src/inet/mobility/base/MovingMobilityBase.cc
+++ src/inet/mobility/base/MovingMobilityBase.cc
@@ -73,6 +73,8 @@ void MovingMobilityBase::handleSelfMessage(cMessage *message)
         moveAndUpdate();
         scheduleUpdate();
     }
+    else
+        throw cRuntimeError("Unknown self message %s", message->getClassAndFullName().c_str());
 }
 
 void MovingMobilityBase::scheduleUpdate()
