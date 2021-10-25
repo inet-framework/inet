7193e9093c82fb95f0291e1ccc5f4ac4e9d4aa27
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-10-25 17:10:15 +0200

    MovingMobilityBase: getCurrentAcceleration() and getCurrentAngularAcceleration() returns a default value instead of throws an error

--- src/inet/mobility/base/MovingMobilityBase.h
+++ src/inet/mobility/base/MovingMobilityBase.h
@@ -76,11 +76,11 @@ class INET_API MovingMobilityBase : public MobilityBase
   public:
     virtual const Coord& getCurrentPosition() override;
     virtual const Coord& getCurrentVelocity() override;
-    virtual const Coord& getCurrentAcceleration() override { throw cRuntimeError("Invalid operation"); }
+    virtual const Coord& getCurrentAcceleration() override { return Coord::ZERO; }
 
     virtual const Quaternion& getCurrentAngularPosition() override;
     virtual const Quaternion& getCurrentAngularVelocity() override;
-    virtual const Quaternion& getCurrentAngularAcceleration() override { throw cRuntimeError("Invalid operation"); }
+    virtual const Quaternion& getCurrentAngularAcceleration() override { return Quaternion::IDENTITY; }
 };
 
 } // namespace inet
