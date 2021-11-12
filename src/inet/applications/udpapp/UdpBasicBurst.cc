4ae1ee94d0dfcca3eeae1a18cce75285a2e4be0f
Rudolf Hornig <rudi@omnetpp.org>
2021-11-12 16:18:52 +0100

    All: Followed omnetpp namespace changes.
        Using the internal namespace isn't ideal though.

--- src/inet/applications/udpapp/UdpBasicBurst.cc
+++ src/inet/applications/udpapp/UdpBasicBurst.cc
@@ -33,7 +33,7 @@ namespace inet {
 EXECUTE_ON_STARTUP(
         cEnum * e = cEnum::find("inet::ChooseDestAddrMode");
         if (!e)
-            enums.getInstance()->add(e = new cEnum("inet::ChooseDestAddrMode"));
+            OMNETPP6_CODE(omnetpp::internal::)enums.getInstance()->add(e = new cEnum("inet::ChooseDestAddrMode"));
         e->insert(UdpBasicBurst::ONCE, "once");
         e->insert(UdpBasicBurst::PER_BURST, "perBurst");
         e->insert(UdpBasicBurst::PER_SEND, "perSend");
