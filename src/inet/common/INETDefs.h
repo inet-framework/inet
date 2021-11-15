68122e708f68fd0272f3d7aa1e61d94044210e9e
Rudolf Hornig <rudi@omnetpp.org>
2021-11-15 12:04:40 +0100

    prod: version bump

--- src/inet/common/INETDefs.h
+++ src/inet/common/INETDefs.h
@@ -50,7 +50,7 @@ using namespace omnetpp;
 #endif
 
 #define INET_VERSION  0x0402
-#define INET_PATCH_LEVEL 0x07
+#define INET_PATCH_LEVEL 0x08
 
 #if OMNETPP_VERSION < 0x0600 || OMNETPP_BUILDNUM < 1525
 namespace omnetpp {
