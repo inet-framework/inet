8dd95d72cd3434d19993ada106b201c1e137a7bd
Zoltan Bojthe <zbojthe@omnetpp.org>
2021-11-22 09:59:53 +0100

    linklayer: added MacProtocolBaseExtQ: copy of MacProtocolBase

---
+++ src/inet/linklayer/base/MacProtocolBaseExtQ.cc
@@ -0,0 +1,175 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#include "inet/linklayer/base/MacProtocolBaseExtQ.h"
+
+#include "inet/common/IInterfaceRegistrationListener.h"
+#include "inet/common/ModuleAccess.h"
+
+namespace inet {
+
+MacProtocolBaseExtQ::MacProtocolBaseExtQ()
+{
+}
+
+MacProtocolBaseExtQ::~MacProtocolBaseExtQ()
+{
+    delete currentTxFrame;
+}
+
+MacAddress MacProtocolBaseExtQ::parseMacAddressParameter(const char *addrstr)
+{
+    MacAddress address;
+
+    if (!strcmp(addrstr, "auto"))
+        // assign automatic address
+        address = MacAddress::generateAutoAddress();
+    else
+        address.setAddress(addrstr);
+
+    return address;
+}
+
+void MacProtocolBaseExtQ::initialize(int stage)
+{
+    LayeredProtocolBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        currentTxFrame = nullptr;
+        upperLayerInGateId = findGate("upperLayerIn");
+        upperLayerOutGateId = findGate("upperLayerOut");
+        lowerLayerInGateId = findGate("lowerLayerIn");
+        lowerLayerOutGateId = findGate("lowerLayerOut");
+        hostModule = findContainingNode(this);
+    }
+    else if (stage == INITSTAGE_NETWORK_INTERFACE_CONFIGURATION)
+        registerInterface();
+}
+
+void MacProtocolBaseExtQ::registerInterface()
+{
+    ASSERT(networkInterface == nullptr);
+    networkInterface = getContainingNicModule(this);
+    configureNetworkInterface();
+}
+
+void MacProtocolBaseExtQ::sendUp(cMessage *message)
+{
+    if (message->isPacket())
+        emit(packetSentToUpperSignal, message);
+    send(message, upperLayerOutGateId);
+}
+
+void MacProtocolBaseExtQ::sendDown(cMessage *message)
+{
+    if (message->isPacket())
+        emit(packetSentToLowerSignal, message);
+    send(message, lowerLayerOutGateId);
+}
+
+bool MacProtocolBaseExtQ::isUpperMessage(cMessage *message)
+{
+    return message->getArrivalGateId() == upperLayerInGateId;
+}
+
+bool MacProtocolBaseExtQ::isLowerMessage(cMessage *message)
+{
+    return message->getArrivalGateId() == lowerLayerInGateId;
+}
+
+void MacProtocolBaseExtQ::deleteCurrentTxFrame()
+{
+    delete currentTxFrame;
+    currentTxFrame = nullptr;
+}
+
+void MacProtocolBaseExtQ::dropCurrentTxFrame(PacketDropDetails& details)
+{
+    emit(packetDroppedSignal, currentTxFrame, &details);
+    delete currentTxFrame;
+    currentTxFrame = nullptr;
+}
+
+void MacProtocolBaseExtQ::popTxQueue()
+{
+    if (currentTxFrame != nullptr)
+        throw cRuntimeError("Model error: incomplete transmission exists");
+    ASSERT(txQueue != nullptr);
+    currentTxFrame = txQueue->dequeuePacket();
+    currentTxFrame->setArrival(getId(), upperLayerInGateId, simTime());
+    take(currentTxFrame);
+}
+
+void MacProtocolBaseExtQ::flushQueue(PacketDropDetails& details)
+{
+    // code would look slightly nicer with a pop() function that returns nullptr if empty
+    if (txQueue)
+        while (!txQueue->isEmpty()) {
+            auto packet = txQueue->dequeuePacket();
+            emit(packetDroppedSignal, packet, &details); // FIXME this signal lumps together packets from the network and packets from higher layers! separate them
+            delete packet;
+        }
+}
+
+void MacProtocolBaseExtQ::clearQueue()
+{
+    if (txQueue)
+        while (!txQueue->isEmpty())
+            delete txQueue->dequeuePacket();
+}
+
+void MacProtocolBaseExtQ::handleMessageWhenDown(cMessage *msg)
+{
+    if (!msg->isSelfMessage() && msg->getArrivalGateId() == lowerLayerInGateId) {
+        EV << "Interface is turned off, dropping packet\n";
+        delete msg;
+    }
+    else
+        LayeredProtocolBase::handleMessageWhenDown(msg);
+}
+
+void MacProtocolBaseExtQ::handleStartOperation(LifecycleOperation *operation)
+{
+    networkInterface->setState(NetworkInterface::State::UP);
+    networkInterface->setCarrier(true);
+}
+
+void MacProtocolBaseExtQ::handleStopOperation(LifecycleOperation *operation)
+{
+    PacketDropDetails details;
+    details.setReason(INTERFACE_DOWN);
+    if (currentTxFrame)
+        dropCurrentTxFrame(details);
+    flushQueue(details);
+    networkInterface->setCarrier(false);
+    networkInterface->setState(NetworkInterface::State::DOWN);
+}
+
+void MacProtocolBaseExtQ::handleCrashOperation(LifecycleOperation *operation)
+{
+    deleteCurrentTxFrame();
+    clearQueue();
+    networkInterface->setCarrier(false);
+    networkInterface->setState(NetworkInterface::State::DOWN);
+}
+
+void MacProtocolBaseExtQ::receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details)
+{
+    Enter_Method("%s", cComponent::getSignalName(signalID));
+}
+
+} // namespace inet
+
---
+++ src/inet/linklayer/base/MacProtocolBaseExtQ.h
@@ -0,0 +1,96 @@
+//
+// Copyright (C) 2013 OpenSim Ltd.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+#ifndef __INET_MACPROTOCOLBASE_H
+#define __INET_MACPROTOCOLBASE_H
+
+#include "inet/common/LayeredProtocolBase.h"
+#include "inet/common/lifecycle/ModuleOperations.h"
+#include "inet/common/packet/Packet.h"
+#include "inet/networklayer/common/NetworkInterface.h"
+#include "inet/queueing/contract/IPacketQueue.h"
+
+namespace inet {
+
+class INET_API MacProtocolBaseExtQ : public LayeredProtocolBase, public cListener
+{
+  protected:
+    /** @brief Gate ids */
+    //@{
+    int upperLayerInGateId = -1;
+    int upperLayerOutGateId = -1;
+    int lowerLayerInGateId = -1;
+    int lowerLayerOutGateId = -1;
+    //@}
+
+    opp_component_ptr<NetworkInterface> networkInterface;
+
+    /** Currently transmitted frame if any */
+    Packet *currentTxFrame = nullptr;
+
+    /** Messages received from upper layer and to be transmitted later */
+    opp_component_ptr<queueing::IPacketQueue> txQueue;
+
+    opp_component_ptr<cModule> hostModule;
+
+  protected:
+    MacProtocolBaseExtQ();
+    virtual ~MacProtocolBaseExtQ();
+
+    virtual void initialize(int stage) override;
+
+    virtual void registerInterface();
+    virtual void configureNetworkInterface() = 0;
+
+    virtual MacAddress parseMacAddressParameter(const char *addrstr);
+
+    virtual void sendUp(cMessage *message);
+    virtual void sendDown(cMessage *message);
+
+    virtual bool isUpperMessage(cMessage *message) override;
+    virtual bool isLowerMessage(cMessage *message) override;
+
+    virtual bool isInitializeStage(int stage) override { return stage == INITSTAGE_LINK_LAYER; }
+    virtual bool isModuleStartStage(int stage) override { return stage == ModuleStartOperation::STAGE_LINK_LAYER; }
+    virtual bool isModuleStopStage(int stage) override { return stage == ModuleStopOperation::STAGE_LINK_LAYER; }
+
+    virtual void deleteCurrentTxFrame();
+    virtual void dropCurrentTxFrame(PacketDropDetails& details);
+    virtual void popTxQueue();
+
+    /**
+     * should clear queue and emit signal "packetDropped" with entire packets
+     */
+    virtual void flushQueue(PacketDropDetails& details);
+
+    /**
+     * should clear queue silently
+     */
+    virtual void clearQueue();
+
+    using cListener::receiveSignal;
+    virtual void handleMessageWhenDown(cMessage *msg) override;
+    virtual void receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details) override;
+    virtual void handleStartOperation(LifecycleOperation *operation) override;
+    virtual void handleStopOperation(LifecycleOperation *operation) override;
+    virtual void handleCrashOperation(LifecycleOperation *operation) override;
+};
+
+} // namespace inet
+
+#endif
+
