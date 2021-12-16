3c37c3c091ff37eb27e156e9ed67c91921d2c761
Levente Mészáros <levy@omnetpp.org>
2021-12-16 16:50:37 +0100

    Examples: Removed obsolete TSN traffic shaping examples which are already turned into showcases.

--- examples/tsn/trafficshaping/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-inet_dbg -s -u Cmdenv -c CreditBasedShaper --sim-time-limit=0.1s --cmdenv-redirect-output=true &
-inet_dbg -s -u Cmdenv -c TimeAwareShaper --sim-time-limit=0.1s --cmdenv-redirect-output=true &
-inet_dbg -s -u Cmdenv -c AsynchronousShaper --sim-time-limit=0.1s --cmdenv-redirect-output=true
