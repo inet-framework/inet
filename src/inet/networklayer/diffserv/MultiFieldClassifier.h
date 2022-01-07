5a5334ef83fca3789fa4aa23595a31edd1d7c896
Levente Mészáros <levy@omnetpp.org>
2022-01-07 16:58:13 +0100

    MultiFieldClassifier: Fixed registration forwarding for defaultOut gate.

--- src/inet/networklayer/diffserv/MultiFieldClassifier.h
+++ src/inet/networklayer/diffserv/MultiFieldClassifier.h
@@ -88,6 +88,9 @@ class INET_API MultiFieldClassifier : public queueing::PacketClassifierBase
     virtual void refreshDisplay() const override;
 
     virtual int classifyPacket(Packet *packet) override;
+
+    virtual void mapRegistrationForwardingGates(cGate *gate, std::function<void(cGate *)> f) override;
+
 };
 
 } // namespace inet
