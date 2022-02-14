1e186fafccfa05d36f017c91b6cd89dcca2ae331
Zoltan Bojthe <zbojthe@omnetpp.org>
2022-02-14 16:15:00 +0100

    AckingMac: fixed compiler warnings: added missing "override"

--- src/inet/linklayer/acking/AckingMac.h
+++ src/inet/linklayer/acking/AckingMac.h
@@ -60,9 +60,9 @@ class INET_API AckingMac : public MacProtocolBase, public IMacProtocol, public q
     virtual void handleSelfMessage(cMessage *message) override;
     //@}
 
-    virtual void handleStartOperation(LifecycleOperation *operation);
-    virtual void handleStopOperation(LifecycleOperation *operation);
-    virtual void handleCrashOperation(LifecycleOperation *operation);
+    virtual void handleStartOperation(LifecycleOperation *operation) override;
+    virtual void handleStopOperation(LifecycleOperation *operation) override;
+    virtual void handleCrashOperation(LifecycleOperation *operation) override;
 
   public:
     AckingMac();
