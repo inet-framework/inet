c7d9d1eb6163668d52c63b207dfcdd3fe7ada54f
Levente Mészáros <levy@omnetpp.org>
2022-03-21 16:21:44 +0100

    InterfaceTableVisualizerBase: Fixed #710.
    The order of module connection creation changed in omnet and broke this visualizer.
    This change avoids creating visualizations when the module hierarchy is not completely built yet.

--- src/inet/visualizer/base/InterfaceTableVisualizerBase.cc
+++ src/inet/visualizer/base/InterfaceTableVisualizerBase.cc
@@ -274,7 +274,7 @@ void InterfaceTableVisualizerBase::receiveSignal(cComponent *source, simsignal_t
         auto networkNode = getContainingNode(static_cast<cModule *>(source));
         if (nodeFilter.matches(networkNode)) {
             auto networkInterface = static_cast<NetworkInterface *>(object);
-            if (interfaceFilter.matches(networkInterface)) {
+            if (networkInterface->getInterfaceId() != -1 && interfaceFilter.matches(networkInterface)) {
                 auto interfaceVisualization = createInterfaceVisualization(networkNode, networkInterface);
                 addInterfaceVisualization(interfaceVisualization);
             }
@@ -284,7 +284,7 @@ void InterfaceTableVisualizerBase::receiveSignal(cComponent *source, simsignal_t
         auto networkNode = getContainingNode(static_cast<cModule *>(source));
         if (nodeFilter.matches(networkNode)) {
             auto networkInterface = static_cast<NetworkInterface *>(object);
-            if (interfaceFilter.matches(networkInterface)) {
+            if (networkInterface->getInterfaceId() != -1 && interfaceFilter.matches(networkInterface)) {
                 auto interfaceVisualization = getInterfaceVisualization(networkNode, networkInterface);
                 removeInterfaceVisualization(interfaceVisualization);
                 delete interfaceVisualization;
@@ -303,7 +303,7 @@ void InterfaceTableVisualizerBase::receiveSignal(cComponent *source, simsignal_t
 #endif // INET_WITH_IPv4
                     || (signal == interfaceStateChangedSignal && (fieldId == NetworkInterface::F_STATE || fieldId == NetworkInterface::F_CARRIER)))
             {
-                if (interfaceFilter.matches(networkInterface)) {
+                if (networkInterface->getInterfaceId() != -1 && interfaceFilter.matches(networkInterface)) {
                     auto interfaceVisualization = getInterfaceVisualization(networkNode, networkInterface);
                     if (interfaceVisualization == nullptr) {
                         interfaceVisualization = createInterfaceVisualization(networkNode, networkInterface);
