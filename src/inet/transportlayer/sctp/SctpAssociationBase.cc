735dcfe914328c17cadec4de00101ac869be8e86
Levente Mészáros <levy@omnetpp.org>
2022-04-11 10:57:26 +0200

    SctpPathVariables: Fixed negative upper limit due to C++ stupidity.

--- src/inet/transportlayer/sctp/SctpAssociationBase.cc
+++ src/inet/transportlayer/sctp/SctpAssociationBase.cc
@@ -82,7 +82,7 @@ SctpPathVariables::SctpPathVariables(const L3Address& addr, SctpAssociation *ass
 
     cmtCCGroup = 0;
     lastTransmission = simTime();
-    sendAllRandomizer = RNGCONTEXT uniform(0, (1 << 31));
+    sendAllRandomizer = RNGCONTEXT uniform(0, (1u << 31));
     pseudoCumAck = 0;
     newPseudoCumAck = false;
     findPseudoCumAck = true; // Set findPseudoCumAck to TRUE for new destination.
