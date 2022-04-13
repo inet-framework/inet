1360231669d4de734b75c7f47020c5ac4fba183d
Levente Mészáros <levy@omnetpp.org>
2022-04-13 15:55:57 +0200

    python: Fixed sanitizer test results when the returncode is 0.

--- python/inet/test/sanitizer.py
+++ python/inet/test/sanitizer.py
@@ -9,14 +9,19 @@ class SanitizerTestTask(SimulationTestTask):
     def run_protected(self, output_stream=sys.stdout, **kwargs):
         simulation_task_result = self.simulation_task.run_protected(output_stream=output_stream, **kwargs)
         stderr = simulation_task_result.subprocess_result.stderr.decode("utf-8")
-        task_result = super().check_simulation_task_result(simulation_task_result, **kwargs)
+        test_task_result = super().check_simulation_task_result(simulation_task_result, **kwargs)
         match = re.search("SUMMARY: (.*)", stderr)
         if match:
-            task_result.reason = re.sub(" in", "", match.group(1))
-        return task_result
+            test_task_result.reason = re.sub(" in", "", match.group(1))
+            # TODO isn't there a better way?
+            if test_task_result.result == "PASS":
+                test_task_result.result = "FAIL"
+                test_task_result.expected = False
+                test_task_result.color = COLOR_YELLOW
+        return test_task_result
 
-def get_sanitizer_test_tasks(mode="sanitize", cpu_time_limit="1s", **kwargs):
-    return get_simulation_test_tasks(mode=mode, cpu_time_limit=cpu_time_limit, name="sanitizer test", simulation_test_task_class=SanitizerTestTask, **kwargs)
+def get_sanitizer_test_tasks(mode="sanitize", cpu_time_limit="1s", run=0, **kwargs):
+    return get_simulation_test_tasks(mode=mode, cpu_time_limit=cpu_time_limit, name="sanitizer test", simulation_test_task_class=SanitizerTestTask, run=run, **kwargs)
 
 def run_sanitizer_tests(**kwargs):
     multiple_test_tasks = get_sanitizer_test_tasks(**kwargs)
