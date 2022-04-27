31e62dd2ad6232eaa23af0cb11d8ea43cfebb08f
Levente Mészáros <levy@omnetpp.org>
2022-04-27 09:33:37 +0200

    python: Removed old commented out command line parameters.

--- python/inet/main.py
+++ python/inet/main.py
@@ -11,24 +11,6 @@ from inet.test import *
 def parse_arguments(task_name):
     description = "Runs all " + task_name + " in the enclosing project recursively from the current working directory"
     parser = argparse.ArgumentParser(description=description)
-    # TODO not all of these arguments are meaningful for each cases
-    #      it must be carefully checked and updated accordingly
-    #
-    # parser.add_argument("-f", "--fpfilter", default=list(), action="append", metavar="ingredient", help="fingerprint ingredient filter (expected values after the "/" character)")
-    # parser.add_argument("-F", "--excludefpfilter", default=list(), action="append", metavar="ingredient", help="fingerprint ingredient excluding filter (excluded values after the "/" character)")
-    # parser.add_argument("-m", "--match", action="append", metavar="regex", help="Line filter: a line (more precisely, workingdir+SPACE+args) must match any of the regular expressions in order for that test case to be run")
-    # parser.add_argument("-x", "--exclude", action="append", metavar="regex", help="Negative line filter: a line (more precisely, workingdir+SPACE+args) must NOT match any of the regular expressions in order for that test case to be run")
-    # parser.add_argument("-t", "--threads", type=int, default=defaultNumThreads, help="number of parallel threads (default: number of CPUs, currently "+str(defaultNumThreads)+")")
-    # parser.add_argument("-r", "--repeat", type=int, default=1, help="number of repeating each test (default: 1)")
-    # parser.add_argument("-e", "--executable", default="inet", help="Determines which binary to execute (e.g. opp_run_dbg, opp_run_release) if the command column in the CSV file does not specify one.")
-    # parser.add_argument("-C", "--directory", help="Change to DIRECTORY before executing the tests. Working dirs in the CSV files are relative to this.")
-    # parser.add_argument("-d", "--debug", action="store_true", help="Run debug executables: use the debug version of the executable (appends _dbg to the executable name)")
-    # parser.add_argument("-s", "--release", action="store_true", help="Run release executables: use the release version of the executable (appends _release to the executable name)")
-    # parser.add_argument("-q", "--disable_outfile", dest="writeOutfile", action="store_false", help="disable the writing standard output of simulations to result directory")
-    # parser.add_argument("-c", "--calculator", default=defaultFingerprintCalculator, help="Set the fingerprintcalculator-class. Empty value means the original OmNET++ fingerprint calculator")
-    # parser.add_argument("-a", "--oppargs", action="append", metavar="oppargs", nargs=argparse.REMAINDER, help="extra opp_run arguments until the end of the line")
-    # parser.add_argument("-n", type=int, default=1, help="Split the selected test cases into n sets, and only run one of these sets")
-    # parser.add_argument("-i", type=int, default=0, help="Which set of test cases to run [0..n-1]")
     parser.add_argument("-l", "--log-level", choices=["ERROR", "WARN", "INFO", "DEBUG"], default="WARN", help="Verbose output mode")
     parser.add_argument("--concurrent", default=True, action=argparse.BooleanOptionalAction, help="Concurrent execution")
     parser.add_argument("--dry-run", default=False, action=argparse.BooleanOptionalAction, help="Display what would be done but doesn't actually do anything")
