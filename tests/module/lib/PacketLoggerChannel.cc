e9e541760f1dbea02995e097f38bf1e9a8d77d67
Zoltan Bojthe <zbojthe@omnetpp.org>
2022-04-28 10:41:48 +0200

    tests: module: remove obsolete compatibility code for omnetpp 6.0 pre8 and below

--- tests/module/lib/PacketLoggerChannel.cc
+++ tests/module/lib/PacketLoggerChannel.cc
@@ -22,12 +22,7 @@ class INET_API PacketLoggerChannel : public cDatarateChannel
 
   public:
     explicit PacketLoggerChannel(const char *name = NULL) : cDatarateChannel(name) { counter = 0; }
-
-#if OMNETPP_BUILDNUM < 1504    // OMNETPP_VERSION is 6.0 pre9
-    virtual void processMessage(cMessage *msg, simtime_t t, result_t& result) override;
-#else
     virtual cChannel::Result processMessage(cMessage *msg, const SendOptions& options, simtime_t t) override;
-#endif
 
   protected:
     virtual void initialize() override;
@@ -51,23 +46,12 @@ void PacketLoggerChannel::initialize()
     counter = 0;
 }
 
-#if OMNETPP_BUILDNUM < 1504    // OMNETPP_VERSION is 6.0 pre9
-void PacketLoggerChannel::processMessage(cMessage *msg, simtime_t t, result_t& result)
-#else
 cChannel::Result PacketLoggerChannel::processMessage(cMessage *msg, const SendOptions& options, simtime_t t)
-#endif
 {
     EV << "PacketLogger processMessage()\n";
-#if OMNETPP_BUILDNUM < 1504    // OMNETPP_VERSION is 6.0 pre9
-    cDatarateChannel::processMessage(msg, t, result);
-#else
     cChannel::Result result = cDatarateChannel::processMessage(msg, options, t);
-#endif
 
     const char *status = "";
-#if OMNETPP_BUILDNUM < 1504    // OMNETPP_VERSION is 6.0 pre9
-    counter++;
-#else
     if (options.transmissionId_ == -1) {
         counter++;
     }
@@ -79,7 +63,6 @@ cChannel::Result PacketLoggerChannel::processMessage(cMessage *msg, const SendOp
         status = (options.remainingDuration == SIMTIME_ZERO) ? ":end" : ":update";
     }
 
-#endif
     if (logfile.is_open())
     {
         logfile << '#' << counter << ':' << t.raw() << ": '" << msg->getName() << status << "' (" << msg->getClassName() << ") sent:" << msg->getSendingTime().raw();
@@ -89,9 +72,7 @@ cChannel::Result PacketLoggerChannel::processMessage(cMessage *msg, const SendOp
         logfile << " discard:" << result.discard << ", delay:" << result.delay.raw() << ", duration:" << result.duration.raw();
         logfile << endl;
     }
-#if OMNETPP_BUILDNUM >= 1504    // OMNETPP_VERSION is 6.0 pre9
     return result;
-#endif
 }
 
 void PacketLoggerChannel::finish()
