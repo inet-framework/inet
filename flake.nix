4124308046f925dfb3af29b30798a45b3958432d
Rudolf Hornig <rudi@omnetpp.org>
2022-06-22 12:43:23 +0200

    prod: flake support added

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "INET Framework for OMNeT++ Discrete Event Simulator";
+
+  inputs = {
+    omnetpp.url = "github:omnetpp/omnetpp/flake/omnetpp-6.0.x";
+    flake-utils.follows = "omnetpp/flake-utils";
+  };
+
+  outputs = { self, flake-utils, omnetpp }:
+    flake-utils.lib.eachDefaultSystem(system:
+    let
+      oppPkgs = omnetpp.oppPkgs.${system};
+      pname = "inet";
+      version = "4.4.0.${oppPkgs.lib.substring 0 8 self.lastModifiedDate}.${self.shortRev or "dirty"}";
+    in rec {
+      packages = rec {
+        ${pname} = oppPkgs.callPackage ./releng/inet_mkDerivation.nix {
+          inherit pname version;
+          omnetpp = omnetpp.packages.${system}.omnetpp;
+          src = self;
+        };
+
+        default = packages.${pname};
+      };
+
+      devShells = rec {
+        "${pname}" = oppPkgs.stdenv.mkDerivation {
+          name = "inet";
+          buildInputs = [
+            omnetpp.packages.${system}.default
+            self.packages.${system}.default
+          ];
+          shellHook = ''
+            source ${self.packages.${system}.default}/setenv
+          '';
+        };
+
+        default = devShells."${pname}";
+      };
+    });
+}
---
+++ releng/inet_mkDerivation.nix
@@ -0,0 +1,65 @@
+{ 
+  pname, version, src ? ./.,        # direct parameters
+  stdenv, lib, lld,                 # build environment
+  perl, omnetpp,                    # dependencies
+}:
+let
+in
+stdenv.mkDerivation rec {
+  inherit pname version src;
+
+  enableParallelBuilding = true;
+  strictDeps = true;
+  dontStrip = true;
+
+  buildInputs = [ omnetpp ];
+
+  # tools required for build only (not needed in derivations)
+  nativeBuildInputs = [ omnetpp ];
+
+  # tools required for build only (needed in derivations)
+  propagatedNativeBuildInputs = [  ];
+
+  # we have to patch all shebangs to use NIX versions of the interpreters
+  prePatch = ''
+    patchShebangs bin
+  '';
+
+  buildPhase = ''
+    source setenv
+    opp_featuretool reset
+    make makefiles
+    make MODE=release -j16
+  '';
+
+  installPhase = ''
+    runHook preInstall
+
+    mkdir -p ${placeholder "out"}
+    # get rid of precompiled headers to remove dependency from gcc/clang
+    rm -f src/inet/common/precompiled_release.h.pch* src/inet/common/precompiled_debug.h.pch*
+
+    installFiles=(bin src python Makefile setenv .oppbuildspec .oppfeatures .oppfeaturestate)
+    for f in ''${installFiles[@]}; do
+      cp -r $f ${placeholder "out"}
+    done
+    echo "src" >${placeholder "out"}/.nedfolders
+    grep -E -v 'inet.examples|inet.showcases|inet.tutorials' .nedexclusions >${placeholder "out"}/.nedexclusions
+
+    runHook postInstall
+    '';
+
+  shellHook = ''
+    source $out/setenv
+  '';
+
+  meta = with lib; {
+    homepage= "https://inet.omnetpp.org";
+    description = "INET Framework for OMNeT++ Discrete Event Simulator";
+    longDescription = "An open-source OMNeT++ model suite for wired, wireless and mobile networks. INET evolves via feedback and contributions from the user community.";
+    changelog = "https://github.com/inet-framework/inet/blob/v${version}/WHATSNEW";
+    # license = licenses.lgpl3;
+    maintainers = [ "rudi@omnetpp.org" ];
+    platforms = [ "x86_64-linux" "x86_64-darwin" ];
+  };
+}
