4b2cf0b0aa827fe91b9109a4de3ccbba83e7fdfd
Gyorgy Szaszko <adam.szaszko@gmail.com>
2022-09-01 14:43:44 +0200

    python: feature and fix: modifiedplot: removed some commented lines; added layout options to plot_vectors_separate_grouped(); added function to fix labels in subplots; added function to annotate barchart; added modified version of plot_bars() with optional ordering;

--- python/modifiedplot.py
+++ python/modifiedplot.py
@@ -5,6 +5,7 @@ import pandas as pd
 import ast
 import re
 from matplotlib.lines import Line2D
+import numpy as np
 
 # debug = False
 
@@ -73,10 +74,11 @@ def plot_vectors(df, props, legend_func=utils.make_legend_label):
         style = utils._make_line_args(props, t, df)
 #        if t.propertyname != '':
 #            style[t.propertyname] = t.propertyvalue
-        style_dict = eval(t.additional_style)
-        # print("style_dict:", style_dict)
-        for i in style_dict.items():
-            style[i[0]] = i[1]
+        if 'additional_style' in df.columns:
+            style_dict = eval(t.additional_style)
+            # print("style_dict:", style_dict)
+            for i in style_dict.items():
+                style[i[0]] = i[1]
         # print("style:", style)
         p.plot(t.vectime, t.vecvalue, label=legend_func(legend_cols, t, props), **style)
 
@@ -134,13 +136,18 @@ def plot_vectors_separate(df, props, legend_func=utils.make_legend_label, debug=
     
     return ax_list
     
-def plot_vectors_separate_grouped(df_list, props, legend_func=utils.make_legend_label):
+def plot_vectors_separate_grouped(df_list, props, legend_func=utils.make_legend_label, layout='vertical', columns=0, rows=0, debug=False):
     """
     This is a modified version of the built-in plot_vectors_separate() function. It takes a list of dataframes,
     and plots each dataframe on its own subplot. Useful for plotting multiple lines on a subplot, as the built-in
     function can't do that currently.
     
     The dataframes can also contain 'additional_style' columns.
+    
+    Layout can be:
+    - 'vertical' (default)
+    - 'horizontal'
+    - 'grid': need to specify number of columns and rows
     """
     p = ideplot if chart.is_native_chart() else plt
     
@@ -154,8 +161,43 @@ def plot_vectors_separate_grouped(df_list, props, legend_func=utils.make_legend_
     
         title_cols, legend_cols = utils.extract_label_columns(df, props)
         
+        cols = 0
+        rws = 0
+        
+        shareaxis = None
+        
         ax = None
-        ax = plt.subplot(len(df_list), 1, j+1, sharex=ax)
+        
+        if layout == 'vertical':
+            cols = 1
+            rws = len(df_list)
+            # shareaxis = 'shareax = ax'
+            shareaxis = {'sharex': ax}
+            if len(ax_list) != 0:
+                ax = plt.subplot(len(df_list), 1, j+1, sharex=ax_list[-1])
+            else:
+                ax = plt.subplot(len(df_list), 1, j+1)
+        elif layout == 'horizontal':
+            cols = len(df_list)
+            rws = 1
+            shareaxis = {'sharey': ax}
+            if len(ax_list) != 0:
+                ax = plt.subplot(1, len(df_list), j+1, sharey=ax_list[-1])
+            else:
+                ax = plt.subplot(1, len(df_list), j+1)
+        elif layout == 'grid':
+            cols = columns
+            rws = rows
+            shareaxis = {}
+            num_plots = cols * rws
+            assert num_plots == len(df_list), "Number of plots is not the same as the number of dataframes"
+            ax = plt.subplot(rws, cols, j+1)
+        else:
+            assert False, "layout is either 'horizontal', 'vertical', or 'grid'"
+        
+        if debug: 
+            print("Creating subplots with " + str(layout) + " layout with "+ str(cols) + " column(s) and " + str(rws) + " row(s)\n")
+            print("shareaxis: ", *shareaxis, "type", type(shareaxis))
     
         if 'order' in df.columns:
             df.sort_values(by='order', inplace=True)
@@ -181,7 +223,22 @@ def plot_vectors_separate_grouped(df_list, props, legend_func=utils.make_legend_
     
     return ax_list
 
-def add_to_dataframe(df, style_tuple_list=[], default_dict={}, order={}, debug=False):
+def fix_labels_for_subplots(ax_list, props, layout, debug=False):
+        if debug: print("layout:", layout)
+        if layout == 'vertical':
+            for a in ax_list[0:-1]:
+                a.set_xlabel('')
+            ax_list[-1].set_xlabel(props['xaxis_title'])
+        elif layout == 'horizontal':
+            for a in ax_list[1:]:
+                if debug: print("a:", a)
+                a.set_ylabel('')
+            ax_list[0].set_ylabel(props['yaxis_title'])
+        else:
+            assert False, "somethings wrong"            
+
+
+def add_to_dataframe(df, style_tuple_list=None, default_dict={}, order={}, debug=False):
     """
     Adds 'additional_style' column to dataframe. The concent of this column is added to 'style' object when plotting.
     Can also specify row order in dataframe (e.g. for ordering items in legend).
@@ -207,7 +264,7 @@ def add_to_dataframe(df, style_tuple_list=[], default_dict={}, order={}, debug=F
     
     def add_separator():
         print("")
-    if style_tuple_list:
+    if style_tuple_list is not None:
         df['additional_style'] = None
         for i in style_tuple_list:
             column = i[0]
@@ -308,4 +365,215 @@ def quick_reorder_legend(handles, labels, order):
     """
     plt.legend([handles[i] for i in order], [labels[i] for i in order])
     return handles, labels
+
+
+            
+def annotate_barchart(ax, offset=0, color='white', size=12, zorder=10, prefix='', postfix='', accuracy=2, outliers=None, debug=False):
+    """
+    Add text annotations to barcharts, displaying the bar height.
     
+        Notes on parameters:
+            - ax: axis object of the plot (add ax = plt.gca() before calling)
+            - accuracy: number of decimal places in the annotation
+            - outliers: 
+                some bars might be too small to add the annotation. in this case, can place annotations above, for example.
+                the parameter is a list of dicts, where each dict is like the following:
+                    {'index': 2, 'color': 'black', ...}
+                        ^ index of bar
+                                         ^ style (has the same parameters)
+    """
+    if outliers == None:
+        outliers = [{}]
+        
+    def is_in_outliers(index: int):
+        for i in outliers:
+            if debug: print("index " + str(index) + " is ")
+            if 'index' in i and i['index'] == index:
+                if debug: print("in outliers")
+                return True
+            else:
+                if debug: print("not in outliers")
+                return False
+    
+    for p in range(0, len(ax.patches)):
+        print("p (ax.patches): ", p, ax.patches[p])
+        if is_in_outliers(p):
+            for i in outliers:
+                if i['index'] == p:
+                    i.setdefault('offset', 0)
+                    i.setdefault('color', 'white')
+                    i.setdefault('size', 12)
+                    if debug: print("adding outlier stuff")
+                    ax.annotate(prefix + str(round(ax.patches[p].get_height(), accuracy)) + postfix, (ax.patches[p].get_x() + ax.patches[p].get_width() / 2, ax.patches[p].get_height() - i['offset']),
+                                horizontalalignment='center', verticalalignment='top', color=i['color'], size=i['size'],
+                                zorder=zorder)
+        else:
+            if debug: print('adding default stuff')
+            ax.annotate(prefix + str(round(ax.patches[p].get_height(), accuracy)) + postfix, (ax.patches[p].get_x() + ax.patches[p].get_width() / 2, ax.patches[p].get_height() - offset),
+            horizontalalignment='center', verticalalignment='top', color=color, size=size,
+            zorder=zorder)
+                    
+def plot_bars(df, errors_df=None, meta_df=None, props={}, order=None, zorder=None, rename=None):
+    """
+    Modified version of the built-in plot_bars() function.
+    The bars can be reordered with the optional 'order' argument. Also, can specify a z-order value, and rename bars with 'rename'.
+    
+    order: a list of column names in valuedf (for example, order = ['DefaultConfig', 'BasicConfig', 'AdvancedConfig']
+    zorder: can change the z-order of the bars (0 by default)
+    rename: a dictionary of column names containing what to rename to what (e.g. rename={'BasicConfig': 'Basic config', 'AdvancedConfig': 'Advanced config'}).
+            this is applied after reordering
+    
+    original description:
+    
+        Creates a bar plot from the dataframe, with styling and additional input
+        coming from the properties. Each row in the dataframe defines a series.
+    
+        Group names (displayed on the x axis) are taken from the column index.
+    
+        The name of the variable represented by the values can be passed in as
+        the `variable_name` argument (as it is not present in the dataframe); if so,
+        it will become the y axis label.
+    
+        Error bars can be drawn by providing an extra dataframe of identical
+        dimensions as the main one. Error bars will protrude by the values in the
+        errors dataframe both up and down (i.e. range is 2x error).
+    
+        To make the legend labels customizable, an extra dataframe can be provided,
+        which contains any columns of metadata for each series.
+    
+        Colors are assigned automatically. The `cycle_seed` property allows you
+        to select other combinations if the default one is not suitable.
+    
+        Parameters:
+    
+        - `df`: the dataframe
+        - `props` (dict): the properties
+        - `variable_name` (string): The name of the variable represented by the values.
+        - `errors_df`: dataframe with the errors (in y axis units)
+        - `meta_df`: dataframe with the metadata about each series
+    
+        Notable properties that affect the plot:
+        - `baseline`: The y value at which the x axis is drawn.
+        - `bar_placement`: Selects the arrangement of bars: aligned, overlap, stacked, etc.
+        - `xlabel_rotation`: Amount of counter-clockwise rotation of x axis labels a.k.a. group names, in degrees.
+        - `title`: Plot title (autocomputed if missing).
+        - `cycle_seed`: Alters the sequence in which colors are assigned to series.
+    """
+    p = ideplot if ideplot.is_native_plot() else plt
+
+    def get_prop(k):
+        return props[k] if k in props else None
+
+    group_fill_ratio = 0.8
+    aligned_bar_fill_ratio = 0.9
+    overlap_visible_fraction = 1 / 3
+
+    # used only by the mpl charts
+    xs = np.arange(len(df.columns), dtype=np.float64)  # the x locations for the groups
+    width = group_fill_ratio / len(df.index)  # the width of the bars
+    bottoms = np.zeros_like(xs)
+    stacks = np.zeros_like(xs)
+    group_increment = 0.0
+
+    baseline = get_prop("baseline")
+    if baseline:
+        if ideplot.is_native_plot(): # is this how this should be done?
+            ideplot.set_property("Bars.Baseline", baseline)
+        else:
+            bottoms += float(baseline)
+
+    extra_args = dict()
+
+    placement = get_prop("bar_placement") or "Aligned"
+    if placement:
+        if ideplot.is_native_plot(): # is this how this should be done?
+            ideplot.set_property("Bar.Placement", placement)
+        else:
+            if placement == "InFront":
+                width = group_fill_ratio
+            elif placement == "Stacked":
+                width = group_fill_ratio
+                bottoms *= 0 # doesn't make sense
+            elif placement == "Aligned":
+                width = group_fill_ratio / len(df.index)
+                group_increment = width
+                xs -= width * (len(df.index)-1)/2
+                width *= aligned_bar_fill_ratio
+            elif placement == "Overlap":
+                width = group_fill_ratio / (1 + len(df.index) * overlap_visible_fraction)
+                group_increment = width * overlap_visible_fraction
+                extra_parts = (1.0 / overlap_visible_fraction - 1)
+                xs += width / extra_parts - (len(df.index) + extra_parts) * width * overlap_visible_fraction / 2
+
+    if order is None:
+        df.sort_index(axis="columns", inplace=True)
+        df.sort_index(axis="index", inplace=True)
+
+    if errors_df is not None:
+        if order is None:
+            errors_df.sort_index(axis="columns", inplace=True)
+            errors_df.sort_index(axis="index", inplace=True)
+
+        assert(df.columns.equals(errors_df.columns))
+        assert(df.index.equals(errors_df.index))
+
+    title_cols, legend_cols = utils.extract_label_columns(meta_df.reset_index(), props)
+    
+    if order is not None:
+        df = df.reindex(order, axis='columns')
+        
+    if rename is not None:
+        df.rename(columns=rename, inplace=True)
+
+    for i, ((index, row), meta_row) in enumerate(zip(df.iterrows(), meta_df.reset_index().itertuples(index=False))):
+        style = utils._make_bar_args(props, df)
+
+        if not ideplot.is_native_plot():
+            if zorder is None: # FIXME: noot pretty...
+                extra_args['zorder'] = 1 - (i / len(df.index) / 10)
+            else:
+                extra_args['zorder'] = zorder
+            extra_args['bottom'] = bottoms + stacks
+
+        label = utils.make_legend_label(legend_cols, meta_row, props)
+        ys = row.values
+        p.bar(xs, ys-bottoms, width, label=label, **extra_args, **style)
+
+        if not ideplot.is_native_plot() and errors_df is not None and not errors_df.iloc[i].isna().all():
+            plt.errorbar(xs, ys + stacks, yerr=errors_df.iloc[i], capsize=float(get_prop("cap_size") or 4), **style, linestyle="none", ecolor=mpl.rcParams["axes.edgecolor"])
+
+        xs += group_increment
+        if placement == "Stacked":
+            stacks += row.values
+
+    rotation = get_prop("xlabel_rotation")
+    if rotation:
+        rotation = float(rotation)
+    else:
+        rotation = 0
+    p.xticks(list(range(len(df.columns))), list([utils._to_label(i) for i in df.columns.values]), rotation=rotation)
+
+    # Add some text for labels, title and custom x-axis tick labels, etc.
+    groups = df.columns.names
+
+    p.xlabel(utils._to_label(groups))
+
+    title = get_prop("title")
+    if meta_df is not None:
+        meta_df = meta_df.reset_index()
+        if get_prop("legend_prefer_result_titles") == "true" and "title" in meta_df:
+            series = meta_df["title"]
+        else:
+            series = meta_df["name"]
+
+        title_names = series.unique()
+        ylabel = title_names[0]
+        if len(title_names) > 1:
+            ylabel += ", etc."
+        p.ylabel(ylabel)
+
+        if title is None:
+            title = utils.make_chart_title(meta_df, title_cols)
+
+    if title is not None:
+        utils.set_plot_title(title)
