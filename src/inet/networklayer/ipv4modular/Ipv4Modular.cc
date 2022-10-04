1c5d3dc8850ac9475fce4e1572c1910269eedde2
Zoltan Bojthe <zbojthe@omnetpp.org>
2022-10-04 17:00:19 +0200

    Ipv4Modular: based on INetfilterCompatibleIpv4HookManagerBase

--- src/inet/networklayer/ipv4modular/Ipv4Modular.cc
+++ src/inet/networklayer/ipv4modular/Ipv4Modular.cc
@@ -26,80 +26,10 @@ void Ipv4Modular::initialize(int stage)
     }
 }
 
-static Ipv4Hook::NetfilterResult mapResult(INetfilter::IHook::Result r)
-{
-    switch (r) {
-        case INetfilter::IHook::Result::ACCEPT: return Ipv4Hook::NetfilterResult::ACCEPT;
-        case INetfilter::IHook::Result::DROP: return Ipv4Hook::NetfilterResult::DROP;
-        case INetfilter::IHook::Result::QUEUE: return Ipv4Hook::NetfilterResult::QUEUE;
-        //case INetfilter::IHook::Result::REPEAT: return Ipv4Hook::NetfilterResult::REPEAT;
-        case INetfilter::IHook::Result::STOLEN: return Ipv4Hook::NetfilterResult::STOLEN;
-        //case INetfilter::IHook::Result::STOP: return Ipv4Hook::NetfilterResult::STOP;
-        default: throw cRuntimeError("Unknown result value");
-    }
-}
-
-void Ipv4Modular::registerHook(int priority, IHook *hook)
-{
-    Enter_Method(__FUNCTION__);
-
-    if (hookInfo.find(hook) != hookInfo.end())
-        throw cRuntimeError("Usage error: the hook already registered.");
-
-    IHookHandlers h;
-
-    h.priority = priority;
-    h.prerouting = new Ipv4Hook::NetfilterHandler([=](Packet* packet) { return mapResult(hook->datagramPreRoutingHook(packet)); });
-    h.localIn = new Ipv4Hook::NetfilterHandler([=](Packet* packet) { return mapResult(hook->datagramLocalInHook(packet)); });
-    h.forward = new Ipv4Hook::NetfilterHandler([=](Packet* packet) { return mapResult(hook->datagramForwardHook(packet)); });
-    h.postrouting = new Ipv4Hook::NetfilterHandler([=](Packet* packet) { return mapResult(hook->datagramPostRoutingHook(packet)); });
-    h.localOut = new Ipv4Hook::NetfilterHandler([=](Packet* packet) { return mapResult(hook->datagramLocalOutHook(packet)); });
-
-    registerNetfilterHandler(Ipv4Hook::NetfilterType::PREROUTING, h.priority, h.prerouting);
-    registerNetfilterHandler(Ipv4Hook::NetfilterType::LOCALIN, h.priority, h.localIn);
-    registerNetfilterHandler(Ipv4Hook::NetfilterType::FORWARD, h.priority, h.forward);
-    registerNetfilterHandler(Ipv4Hook::NetfilterType::POSTROUTING, h.priority, h.postrouting);
-    registerNetfilterHandler(Ipv4Hook::NetfilterType::LOCALOUT, h.priority, h.localOut);
-
-    hookInfo.insert(std::pair<IHook*, IHookHandlers>(hook, h));
-}
-
-void Ipv4Modular::unregisterHook(IHook *hook)
-{
-    Enter_Method(__FUNCTION__);
-
-    auto it = hookInfo.find(hook);
-    if (it == hookInfo.end())
-        throw cRuntimeError("Usage error: the hook not found in list of registered hooks.");
-
-    const auto& h = it->second;
-    unregisterNetfilterHandler(Ipv4Hook::NetfilterType::PREROUTING, h.priority, h.prerouting);
-    unregisterNetfilterHandler(Ipv4Hook::NetfilterType::LOCALIN, h.priority, h.localIn);
-    unregisterNetfilterHandler(Ipv4Hook::NetfilterType::FORWARD, h.priority, h.forward);
-    unregisterNetfilterHandler(Ipv4Hook::NetfilterType::POSTROUTING, h.priority, h.postrouting);
-    unregisterNetfilterHandler(Ipv4Hook::NetfilterType::LOCALOUT, h.priority, h.localOut);
-    hookInfo.erase(it);
-}
-
 void Ipv4Modular::chkHookManager()
 {
     if (!hookManager)
         throw cRuntimeError("No hook manager, hooks not supported");
-}
-
-void Ipv4Modular::dropQueuedDatagram(const Packet *datagram)
-{
-    Enter_Method(__FUNCTION__);
-
-    chkHookManager();
-    hookManager->reinjectQueuedDatagram(const_cast<Packet *>(datagram), Ipv4Hook::NetfilterResult::DROP);
-}
-
-void Ipv4Modular::reinjectQueuedDatagram(const Packet *datagram)
-{
-    Enter_Method(__FUNCTION__);
-
-    reinjectQueuedDatagram(const_cast<Packet *>(datagram), Ipv4Hook::NetfilterResult::ACCEPT);
 }
 
 void Ipv4Modular::reinjectDatagram(Packet *datagram, Ipv4Hook::NetfilterResult action)
--- src/inet/networklayer/ipv4modular/Ipv4Modular.h
+++ src/inet/networklayer/ipv4modular/Ipv4Modular.h
@@ -7,27 +7,15 @@
 #ifndef __INET_IPV4MODULAR_H
 #define __INET_IPV4MODULAR_H
 
-#include "inet/networklayer/contract/INetfilter.h"
 #include "inet/networklayer/contract/INetworkProtocol.h"
-#include "inet/networklayer/ipv4modular/IIpv4HookManager.h"
+#include "inet/networklayer/ipv4modular/INetfilterCompatibleIpv4HookManagerBase.h"
 
 namespace inet {
 
-class INET_API Ipv4Modular : public cModule, public INetfilter, public IIpv4HookManager, public INetworkProtocol
+class INET_API Ipv4Modular : public cModule, public INetfilterCompatibleIpv4HookManagerBase, public INetworkProtocol
 {
   protected:
-    class IHookHandlers {
-      public:
-        int priority = -1;
-        Ipv4Hook::NetfilterHandler *prerouting = nullptr;
-        Ipv4Hook::NetfilterHandler *localIn = nullptr;
-        Ipv4Hook::NetfilterHandler *forward = nullptr;
-        Ipv4Hook::NetfilterHandler *postrouting = nullptr;
-        Ipv4Hook::NetfilterHandler *localOut = nullptr;
-    };
     opp_component_ptr<IIpv4HookManager> hookManager;
-    typedef std::map<IHook*, IHookHandlers> IHookInfo;
-    IHookInfo hookInfo;
 
   protected:
     void chkHookManager();
@@ -41,12 +29,6 @@ class INET_API Ipv4Modular : public cModule, public INetfilter, public IIpv4Hook
     virtual void registerNetfilterHandler(Ipv4Hook::NetfilterType type, int priority, Ipv4Hook::NetfilterHandler *handler) override;
     virtual void unregisterNetfilterHandler(Ipv4Hook::NetfilterType type, int priority, Ipv4Hook::NetfilterHandler *handler) override;
     virtual void reinjectDatagram(Packet *datagram, Ipv4Hook::NetfilterResult action) override;
-
-    // INetfilter compatibility:
-    virtual void registerHook(int priority, IHook *hook) override;
-    virtual void unregisterHook(IHook *hook) override;
-    virtual void dropQueuedDatagram(const Packet *datagram) override;
-    virtual void reinjectQueuedDatagram(const Packet *datagram) override;
 };
 
 } // namespace inet
