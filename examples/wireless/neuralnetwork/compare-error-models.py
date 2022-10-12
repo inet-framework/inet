34db45917b0d0673c39c85794b9c90b57ca5a5f6
TÖRÖK Attila <torokati44@gmail.com>
2022-10-12 17:58:32 +0200

    fix up error model comparator script

--- examples/wireless/neuralnetwork/compare-error-models.py
+++ examples/wireless/neuralnetwork/compare-error-models.py
@@ -2,28 +2,44 @@
 
 import os
 import sys
+import time
+import pprint
 import argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-s", "--scalarPacketlevel", default=False)
-parser.add_argument("-sn", "--scalarPacketlevelNeuralNetwork", default=False)
-parser.add_argument("-d", "--dimensionalPacketlevel", default=True)
-parser.add_argument("-dn", "--dimensionalPacketlevelNeuralNetwork", default=True)
-parser.add_argument("-b", "--dimensionalSymbollevel", default=True)
+parser.add_argument("-s", "--scalarPacketlevel", default=False, action="store_true")
+parser.add_argument("-sn", "--scalarPacketlevelNeuralNetwork", default=False, action="store_true")
+parser.add_argument("-d", "--dimensionalPacketlevel", default=True, action="store_true")
+parser.add_argument("-dn", "--dimensionalPacketlevelNeuralNetwork", default=True, action="store_true")
+parser.add_argument("-b", "--dimensionalSymbollevel", default=True, action="store_true")
 parser.add_argument("inifile")
 args = parser.parse_args()
 
-if args.scalarPacketlevel :
+times = {}
+
+if args.scalarPacketlevel:
+    start = time.perf_counter()
     os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevel")
+    times["scalarPacketlevel"] = time.perf_counter() - start
 
-if args.scalarPacketlevelNeuralNetwork :
+if args.scalarPacketlevelNeuralNetwork:
+    start = time.perf_counter()
     os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c ScalarPacketlevelNeuralNetwork")
+    times["scalarPacketlevelNeuralNetwork"] = time.perf_counter() - start
 
-if args.dimensionalPacketlevel :
+if args.dimensionalPacketlevel:
+    start = time.perf_counter()
     os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevel")
+    times["dimensionalPacketlevel"] = time.perf_counter() - start
 
-if args.dimensionalPacketlevelNeuralNetwork :
+if args.dimensionalPacketlevelNeuralNetwork:
+    start = time.perf_counter()
     os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c DimensionalPacketlevelNeuralNetwork")
+    times["dimensionalPacketlevelNeuralNetwork"] = time.perf_counter() - start
 
-if args.dimensionalSymbollevel :
+if args.dimensionalSymbollevel:
+    start = time.perf_counter()
     os.system(f"opp_runall -b 100 inet -f {args.inifile} -u Cmdenv -c DimensionalSymbollevel")
+    times["dimensionalSymbollevel"] = time.perf_counter() - start
+
+pprint.pprint(times)
