f0339d726909fd66ed31a73ddbb4cb8b757b0892
Levente Mészáros <levy@omnetpp.org>
2022-11-23 10:28:00 +0100

    FullPacketSink: Changed to implement both active/passive packet sink interfaces.

--- src/inet/queueing/sink/FullPacketSink.cc
+++ src/inet/queueing/sink/FullPacketSink.cc
@@ -14,6 +14,27 @@ namespace queueing {
 
 Define_Module(FullPacketSink);
 
+void FullPacketSink::initialize(int stage)
+{
+    PacketProcessorBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        inputGate = gate("in");
+        provider = findConnectedModule<IPassivePacketSource>(inputGate);
+    }
+    else if (stage == INITSTAGE_QUEUEING)
+        checkPacketOperationSupport(inputGate);
+}
+
+void FullPacketSink::handleCanPullPacketChanged(cGate *gate)
+{
+    Enter_Method("handleCanPullPacketChanged");
+}
+
+void FullPacketSink::handlePullPacketProcessed(Packet *packet, cGate *gate, bool successful)
+{
+    Enter_Method("handlePullPacketProcessed");
+}
+
 } // namespace queueing
 } // namespace inet
 
--- src/inet/queueing/sink/FullPacketSink.h
+++ src/inet/queueing/sink/FullPacketSink.h
@@ -8,19 +8,37 @@
 #ifndef __INET_FULLPACKETSINK_H
 #define __INET_FULLPACKETSINK_H
 
-#include "inet/queueing/base/PassivePacketSinkBase.h"
-#include "inet/queueing/contract/IPassivePacketSink.h"
+#include "inet/queueing/base/PacketSinkBase.h"
+#include "inet/queueing/contract/IPacketSink.h"
 
 namespace inet {
 namespace queueing {
 
-class INET_API FullPacketSink : public PassivePacketSinkBase, public virtual IPassivePacketSink
+class INET_API FullPacketSink : public PacketProcessorBase, public virtual IPacketSink
 {
+  protected:
+    cGate *inputGate = nullptr;
+    IPassivePacketSource *provider = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+
   public:
+    virtual IPassivePacketSource *getProvider(cGate *gate) override { return provider; }
+
+    virtual bool supportsPacketPushing(cGate *gate) const override { return inputGate == gate; }
+    virtual bool supportsPacketPulling(cGate *gate) const override { return inputGate == gate; }
+
+    virtual void handleCanPullPacketChanged(cGate *gate) override;
+    virtual void handlePullPacketProcessed(Packet *packet, cGate *gate, bool successful) override;
+
     virtual bool canPushSomePacket(cGate *gate) const override { return false; }
     virtual bool canPushPacket(Packet *packet, cGate *gate) const override { return false; }
 
-    virtual void pushPacket(Packet *packet, cGate *gate) override { return throw cRuntimeError("Packet sink is full"); }
+    virtual void pushPacket(Packet *packet, cGate *gate) override { return throw cRuntimeError("Illegal operation: packet sink is full"); }
+    virtual void pushPacketStart(Packet *packet, cGate *gate, bps datarate) override { return throw cRuntimeError("Illegal operation: packet sink is full"); }
+    virtual void pushPacketEnd(Packet *packet, cGate *gate) override { return throw cRuntimeError("Illegal operation: packet sink is full"); }
+    virtual void pushPacketProgress(Packet *packet, cGate *gate, bps datarate, b position, b extraProcessableLength = b(0)) override { return throw cRuntimeError("Illegal operation: packet sink is full"); }
 };
 
 } // namespace queueing
