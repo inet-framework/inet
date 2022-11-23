1f17f63958919ade7635f8a7fc423607e5b5e853
Levente Mészáros <levy@omnetpp.org>
2022-11-23 10:27:19 +0100

    EmptyPacketSource: Changed to implement both active/passive packet source interfaces.

--- src/inet/queueing/source/EmptyPacketSource.h
+++ src/inet/queueing/source/EmptyPacketSource.h
@@ -9,12 +9,12 @@
 #define __INET_EMPTYPACKETSOURCE_H
 
 #include "inet/queueing/base/PacketProcessorBase.h"
-#include "inet/queueing/contract/IActivePacketSource.h"
+#include "inet/queueing/contract/IPacketSource.h"
 
 namespace inet {
 namespace queueing {
 
-class INET_API EmptyPacketSource : public PacketProcessorBase, public virtual IActivePacketSource
+class INET_API EmptyPacketSource : public PacketProcessorBase, public virtual IPacketSource
 {
   protected:
     cGate *outputGate = nullptr;
@@ -27,10 +27,18 @@ class INET_API EmptyPacketSource : public PacketProcessorBase, public virtual IA
     virtual IPassivePacketSink *getConsumer(cGate *gate) override { return consumer; }
 
     virtual bool supportsPacketPushing(cGate *gate) const override { return outputGate == gate; }
-    virtual bool supportsPacketPulling(cGate *gate) const override { return false; }
+    virtual bool supportsPacketPulling(cGate *gate) const override { return outputGate == gate; }
 
     virtual void handleCanPushPacketChanged(cGate *gate) override;
     virtual void handlePushPacketProcessed(Packet *packet, cGate *gate, bool successful) override;
+
+    virtual bool canPullSomePacket(cGate *gate) const override { return false; }
+    virtual Packet *canPullPacket(cGate *gate) const override { return nullptr; }
+
+    virtual Packet *pullPacket(cGate *gate) override { throw cRuntimeError("Illegal operation: packet source is empty"); }
+    virtual Packet *pullPacketStart(cGate *gate, bps datarate)  override { throw cRuntimeError("Illegal operation: packet source is empty"); }
+    virtual Packet *pullPacketEnd(cGate *gate)  override { throw cRuntimeError("Illegal operation: packet source is empty"); }
+    virtual Packet *pullPacketProgress(cGate *gate, bps datarate, b position, b extraProcessableLength)  override { throw cRuntimeError("Illegal operation: packet source is empty"); }
 };
 
 } // namespace queueing
