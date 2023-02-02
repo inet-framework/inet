a8144eb0194e6aae51b465c4ad3f71b58c62fbe5
Gyorgy Szaszko <adam.szaszko@gmail.com>
2023-02-02 15:10:55 +0100

    python modifiedplot

--- python/modifiedplot.py
+++ python/modifiedplot.py
@@ -187,11 +187,10 @@ def add_to_dataframe(df, style_tuple_list=[], default_dict={}, order={}, debug=F
     Note that the value parameter in style_tuple_list and the order can contain regex (e.g. .*foo). Make sure to escape regex characters such as [ and ] with \
     """
     
-    df['additional_style'] = None
-    
     def add_separator():
         print("")
     if style_tuple_list:
+        df['additional_style'] = None
         for i in style_tuple_list:
             column = i[0]
             value = i[1]
