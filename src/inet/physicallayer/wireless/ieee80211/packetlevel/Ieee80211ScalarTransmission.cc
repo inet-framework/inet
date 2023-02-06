3fbf382f487c54c5b5fb1c1a502ceaaf913acb90
Levente Mészáros <levy@omnetpp.org>
2023-02-06 10:50:48 +0100

    Ieee80211ScalarTransmission: Removed ScalarTransmission base class.

--- src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211ScalarTransmission.cc
+++ src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211ScalarTransmission.cc
@@ -12,7 +12,7 @@ namespace inet {
 namespace physicallayer {
 
 Ieee80211ScalarTransmission::Ieee80211ScalarTransmission(const IRadio *transmitter, const Packet *packet, const simtime_t startTime, const simtime_t endTime, const simtime_t preambleDuration, const simtime_t headerDuration, const simtime_t dataDuration, const Coord startPosition, const Coord endPosition, const Quaternion startOrientation, const Quaternion endOrientation, b headerLength, b dataLength, const IModulation *modulation, const simtime_t symbolTime, Hz centerFrequency, Hz bandwidth, bps bitrate, double codeRate, W power, const IIeee80211Mode *mode, const Ieee80211Channel *channel) :
-    ScalarTransmission(transmitter, packet, startTime, endTime, preambleDuration, headerDuration, dataDuration, startPosition, endPosition, startOrientation, endOrientation, headerLength, dataLength, modulation, symbolTime, centerFrequency, bandwidth, bitrate, codeRate, power),
+    FlatTransmissionBase(transmitter, packet, startTime, endTime, preambleDuration, headerDuration, dataDuration, startPosition, endPosition, startOrientation, endOrientation, headerLength, dataLength, bitrate, codeRate, modulation, symbolTime, centerFrequency, bandwidth),
     Ieee80211TransmissionBase(mode, channel)
 {
 }
@@ -21,8 +21,6 @@ std::ostream& Ieee80211ScalarTransmission::printToStream(std::ostream& stream, i
 {
     stream << "Ieee80211ScalarTransmission";
     Ieee80211TransmissionBase::printToStream(stream, level);
-    if (level <= PRINT_LEVEL_DETAIL)
-        stream << EV_FIELD(power);
     return FlatTransmissionBase::printToStream(stream, level);
 }
 
--- src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211ScalarTransmission.h
+++ src/inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211ScalarTransmission.h
@@ -8,19 +8,23 @@
 #ifndef __INET_IEEE80211SCALARTRANSMISSION_H
 #define __INET_IEEE80211SCALARTRANSMISSION_H
 
-#include "inet/physicallayer/wireless/common/analogmodel/packetlevel/ScalarTransmission.h"
+#include "inet/physicallayer/wireless/common/base/packetlevel/FlatTransmissionBase.h"
+#include "inet/physicallayer/wireless/common/contract/packetlevel/IRadio.h"
 #include "inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211TransmissionBase.h"
 
 namespace inet {
 
 namespace physicallayer {
 
-class INET_API Ieee80211ScalarTransmission : public ScalarTransmission, public Ieee80211TransmissionBase
+class INET_API Ieee80211ScalarTransmission : public FlatTransmissionBase, public Ieee80211TransmissionBase
 {
   public:
     Ieee80211ScalarTransmission(const IRadio *transmitter, const Packet *packet, const simtime_t startTime, const simtime_t endTime, const simtime_t preambleDuration, const simtime_t headerDuration, const simtime_t dataDuration, const Coord startPosition, const Coord endPosition, const Quaternion startOrientation, const Quaternion endOrientation, b headerLength, b dataLength, const IModulation *modulation, const simtime_t symbolTime, Hz centerFrequency, Hz bandwidth, bps bitrate, double codeRate, W power, const IIeee80211Mode *mode, const Ieee80211Channel *channel);
 
     virtual std::ostream& printToStream(std::ostream& stream, int level, int evFlags = 0) const override;
+
+    // TODO KLUDGE replace this with analog model
+    virtual W computeMinPower(simtime_t startTime, simtime_t endTime) const override { throw cRuntimeError("KLUDGE"); }
 };
 
 } // namespace physicallayer
