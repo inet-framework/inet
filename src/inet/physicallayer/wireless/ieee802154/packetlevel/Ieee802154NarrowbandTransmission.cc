876cf7bfae1c8f97ceaf76e571c394cf631c26e9
Levente Mészáros <levy@omnetpp.org>
2023-02-13 15:39:52 +0100

    Ieee802154NarrowbandTransmission: Replaced FlatTransmissionBase base class with TransmissionBase.

--- src/inet/physicallayer/wireless/ieee802154/packetlevel/Ieee802154NarrowbandTransmission.cc
+++ src/inet/physicallayer/wireless/ieee802154/packetlevel/Ieee802154NarrowbandTransmission.cc
@@ -11,15 +11,15 @@ namespace inet {
 
 namespace physicallayer {
 
-Ieee802154NarrowbandTransmission::Ieee802154NarrowbandTransmission(const IRadio *transmitter, const Packet *packet, const simtime_t startTime, const simtime_t endTime, const simtime_t preambleDuration, const simtime_t headerDuration, const simtime_t dataDuration, const Coord startPosition, const Coord endPosition, const Quaternion startOrientation, const Quaternion endOrientation, b headerLength, b dataLength, const IModulation *modulation, const simtime_t symbolTime, Hz centerFrequency, Hz bandwidth, bps bitrate, double codeRate, W power) :
-    FlatTransmissionBase(transmitter, packet, startTime, endTime, preambleDuration, headerDuration, dataDuration, startPosition, endPosition, startOrientation, endOrientation, headerLength, dataLength, bitrate, codeRate, modulation, symbolTime, centerFrequency, bandwidth)
+Ieee802154NarrowbandTransmission::Ieee802154NarrowbandTransmission(const IRadio *transmitter, const Packet *packet, const simtime_t startTime, const simtime_t endTime, const simtime_t preambleDuration, const simtime_t headerDuration, const simtime_t dataDuration, const Coord startPosition, const Coord endPosition, const Quaternion startOrientation, const Quaternion endOrientation) :
+    TransmissionBase(transmitter, packet, startTime, endTime, preambleDuration, headerDuration, dataDuration, startPosition, endPosition, startOrientation, endOrientation)
 {
 }
 
 std::ostream& Ieee802154NarrowbandTransmission::printToStream(std::ostream& stream, int level, int evFlags) const
 {
     stream << "Ieee802154NarrowbandTransmission";
-    return FlatTransmissionBase::printToStream(stream, level);
+    return TransmissionBase::printToStream(stream, level);
 }
 
 } // namespace physicallayer
--- src/inet/physicallayer/wireless/ieee802154/packetlevel/Ieee802154NarrowbandTransmission.h
+++ src/inet/physicallayer/wireless/ieee802154/packetlevel/Ieee802154NarrowbandTransmission.h
@@ -8,17 +8,17 @@
 #ifndef __INET_IEEE802154TRANSMISSION_H
 #define __INET_IEEE802154TRANSMISSION_H
 
-#include "inet/physicallayer/wireless/common/base/packetlevel/FlatTransmissionBase.h"
+#include "inet/physicallayer/wireless/common/base/packetlevel/TransmissionBase.h"
 #include "inet/physicallayer/wireless/common/contract/packetlevel/IRadio.h"
 
 namespace inet {
 
 namespace physicallayer {
 
-class INET_API Ieee802154NarrowbandTransmission : public FlatTransmissionBase
+class INET_API Ieee802154NarrowbandTransmission : public TransmissionBase
 {
   public:
-    Ieee802154NarrowbandTransmission(const IRadio *transmitter, const Packet *packet, const simtime_t startTime, const simtime_t endTime, const simtime_t preambleDuration, const simtime_t headerDuration, const simtime_t dataDuration, const Coord startPosition, const Coord endPosition, const Quaternion startOrientation, const Quaternion endOrientation, b headerLength, b dataLength, const IModulation *modulation, const simtime_t symbolTime, Hz centerFrequency, Hz bandwidth, bps bitrate, double codeRate, W power);
+    Ieee802154NarrowbandTransmission(const IRadio *transmitter, const Packet *packet, const simtime_t startTime, const simtime_t endTime, const simtime_t preambleDuration, const simtime_t headerDuration, const simtime_t dataDuration, const Coord startPosition, const Coord endPosition, const Quaternion startOrientation, const Quaternion endOrientation);
 
     virtual std::ostream& printToStream(std::ostream& stream, int level, int evFlags = 0) const override;
 };
