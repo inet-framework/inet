6b82935d273b5d0a43434903a4f0afe82bd840a5
Levente Mészáros <levy@omnetpp.org>
2023-02-21 14:45:09 +0100

    ContentBasedFilter: Added TransparentProtocolRegistrationListener base class to fix #855.

--- src/inet/queueing/filter/ContentBasedFilter.h
+++ src/inet/queueing/filter/ContentBasedFilter.h
@@ -8,19 +8,23 @@
 #ifndef __INET_CONTENTBASEDFILTER_H
 #define __INET_CONTENTBASEDFILTER_H
 
+#include "inet/common/IProtocolRegistrationListener.h"
 #include "inet/common/packet/PacketFilter.h"
 #include "inet/queueing/base/PacketFilterBase.h"
 
 namespace inet {
 namespace queueing {
 
-class INET_API ContentBasedFilter : public PacketFilterBase
+class INET_API ContentBasedFilter : public PacketFilterBase, public TransparentProtocolRegistrationListener
 {
   protected:
     PacketFilter filter;
 
   protected:
     virtual void initialize(int stage) override;
+
+    virtual cGate *getRegistrationForwardingGate(cGate *gate) override;
+
     virtual bool matchesPacket(const Packet *packet) const override;
 };
 
