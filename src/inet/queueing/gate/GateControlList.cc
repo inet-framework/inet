efaad2b1d85dacbef489416da95d492c1965593c
JiashuoLin <linjsh@pcl.ac.cn>
2023-03-06 10:33:55 +0100

    queueing: Added GateControlList, a module which accepts parameters in GCL format and configures the corresponding PeriodicGate modules.

---
+++ src/inet/queueing/gate/GateControlList.cc
@@ -0,0 +1,109 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/queueing/gate/GateControlList.h"
+
+namespace inet {
+namespace queueing {
+
+Define_Module(GateControlList);
+
+void GateControlList::initialize(int stage)
+{
+    if (stage == INITSTAGE_LOCAL) {
+        numGates = par("numGates");
+        offset = par("offset");
+        durations = check_and_cast<cValueArray *>(par("durations").objectValue());
+        gateStates = check_and_cast<cValueArray *>(par("gateStates").objectValue());
+
+        parseGcl();
+
+        for (int index = 0; index < numGates; ++index) {
+            char buf[20];
+            sprintf(buf, "^.transmissionGate[%d]", index);
+            PeriodicGate *mod = check_and_cast<PeriodicGate *>(getModuleByPath(buf));
+            mod->initialOffset = offset;
+            mod->durations = gateDurations.at(index);
+        }
+    }
+}
+
+void GateControlList::handleMessage(cMessage *msg)
+{
+    throw cRuntimeError("Do not handle cMessage");
+}
+
+void GateControlList::parseGcl() {
+    if (durations->size() != gateStates->size()) {
+        throw cRuntimeError("The length of durations is not equal to gateStates.");
+    }
+
+    for (int i = 0; i < numGates; ++i) {
+        gateDurations.emplace_back(new cValueArray());
+    }
+
+    int numEntries = durations->size();
+    std::vector<bool> currentGateStates(numGates, true);
+    std::vector<double> currentDuration(numGates, 0);
+
+    for (int indexEntry = 0; indexEntry < numEntries; ++indexEntry) {
+        const char *curGateStates = gateStates->get(indexEntry).stringValue();
+        double duration = durations->get(indexEntry).doubleValueInUnit("s");
+
+        std::vector<bool> entry = retrieveGateStates(curGateStates, numGates);
+
+        for (int i = 0; i < numGates; ++i) {
+            if (entry.at(i) != currentGateStates.at(i)) {
+                gateDurations.at(i)->add(cValue(currentDuration[i], "s"));
+
+                currentGateStates[i] = !currentGateStates[i];
+                currentDuration[i] = 0;
+            }
+
+            currentDuration[i] += duration;
+        }
+
+    }
+
+    for (int i = 0; i < numGates; ++i) {
+        gateDurations.at(i)->add(cValue(currentDuration[i], "s"));
+
+        if (gateDurations.at(i)->size() % 2 != 0) {
+            gateDurations.at(i)->add(cValue(0.0, "s"));
+        }
+    }
+
+}
+
+std::vector<bool> GateControlList::retrieveGateStates(const char *gateStates, uint numGates) {
+    std::vector<bool> res(numGates);
+
+    if (strlen(gateStates) != numGates) {
+        throw cRuntimeError("The length of the entry is not equal to numGates.");
+    }
+
+    for (int indexGate = 0; indexGate < numGates; ++indexGate) {
+        char ch = *(gateStates + indexGate);
+        if (ch == '1') {
+            res[numGates - indexGate - 1] = true;
+        } else if (ch == '0') {
+            res[numGates - indexGate - 1] = false;
+        } else {
+            throw cRuntimeError("Unknown char");
+        }
+    }
+    return res;
+}
+
+GateControlList::~GateControlList() {
+    for (int i = 0; i < numGates; ++i) {
+        delete gateDurations.at(i);
+    }
+}
+
+} // namespace queueing
+} // namespace inet
---
+++ src/inet/queueing/gate/GateControlList.h
@@ -0,0 +1,43 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#ifndef __INET_GATECONTROLLIST_H_
+#define __INET_GATECONTROLLIST_H_
+
+#include "inet/queueing/gate/PeriodicGate.h"
+
+namespace inet {
+namespace queueing {
+
+class INET_API GateControlList : public cSimpleModule
+{
+  protected:
+    int numGates;
+    clocktime_t offset;
+    cValueArray *durations = nullptr;
+    cValueArray *gateStates = nullptr;
+
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage *msg) override;
+
+  private:
+    // ownership
+    std::vector<cValueArray *> gateDurations;
+
+    void parseGcl();
+    static std::vector<bool> retrieveGateStates(const char *gateStates, uint numGates);
+
+  public:
+    ~GateControlList();
+
+};
+
+} // namespace queueing
+} // namespace inet
+
+#endif
