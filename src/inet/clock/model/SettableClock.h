54f09b944d74288e7ab997263043291f29535e36
Levente Mészáros <levy@omnetpp.org>
2023-03-13 11:29:14 +0100

    SettableClock: Added documentation for the oscillatorCompensationFactor field, changing the field's meaning.
    It's better to store the value in ppm for human consumption.
    The implementation is not updated yet.

--- src/inet/clock/model/SettableClock.h
+++ src/inet/clock/model/SettableClock.h
@@ -17,7 +17,8 @@ class INET_API SettableClock : public OscillatorBasedClock, public IScriptable
 {
   protected:
     OverdueClockEventHandlingMode defaultOverdueClockEventHandlingMode = UNSPECIFIED;
-    double oscillatorCompensationFactor = 1.0;
+    double oscillatorCompensationFactor = 1.0; // measured in ppm, 100 ppm value means the clock runs faster, it compensates 100 microseconds for every second in clock time
+                                               // 100 ppm value means the oscillator tick length is compensated to be smaller by a factor of (1 / (1 + 100 / 1E+6)) than the actual tick length measured in clock time
 
   protected:
     virtual void initialize(int stage) override;
