c4f5c762e0c0e94982bed03321ea7161c4d51386
Levente Mészáros <levy@omnetpp.org>
2023-03-22 11:03:26 +0100

    python cffi: Temporarily commented out OMNeT++ cffi interface.
    The current cffi implementation prevents starting inet_python_repl.

--- python/inet/simulation/cffi/__init__.py
+++ python/inet/simulation/cffi/__init__.py
@@ -5,12 +5,13 @@ from inet.common.util import *
 omnetpp_project_path = get_workspace_path("omnetpp")
 inet_project_path = get_workspace_path("inet")
 
-if omnetpp_project_path is not None and inet_project_path is not None and \
-   os.path.exists(omnetpp_project_path) and os.path.exists(inet_project_path) and \
-   os.path.exists(os.path.join(omnetpp_project_path, "include")) and os.path.exists(os.path.join(inet_project_path, "src")):
-    from inet.simulation.cffi.event import *
-    from inet.simulation.cffi.inet import *
-    from inet.simulation.cffi.inprocess import *
-    from inet.simulation.cffi.omnetpp import *
+# TODO revive this code block when INET is integrated with the new OMNeT++ python API
+# if omnetpp_project_path is not None and inet_project_path is not None and \
+#    os.path.exists(omnetpp_project_path) and os.path.exists(inet_project_path) and \
+#    os.path.exists(os.path.join(omnetpp_project_path, "include")) and os.path.exists(os.path.join(inet_project_path, "src")):
+#     from inet.simulation.cffi.event import *
+#     from inet.simulation.cffi.inet import *
+#     from inet.simulation.cffi.inprocess import *
+#     from inet.simulation.cffi.omnetpp import *
 
 __all__ = [k for k,v in locals().items() if k[0] != "_" and v.__class__.__name__ != "module"]
