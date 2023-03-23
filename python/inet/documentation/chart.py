6e68577596e9020006dfb75ff837532f24c2a75d
TÖRÖK Attila <torokati44@gmail.com>
2023-03-23 16:20:58 +0100

    python: Skip legacy format .anf files because the charttool can't load them

--- python/inet/documentation/chart.py
+++ python/inet/documentation/chart.py
@@ -11,10 +11,13 @@ from inet.simulation.project import *
 
 logger = logging.getLogger(__name__)
 
+def is_anf_v2(filename):
+    return 'version="2"' in open(filename, "rt").read()
+
 def get_analysis_files(simulation_project=default_project, filter=".*", exclude_filter=None, full_match=False, **kwargs):
     simulation_project_path = simulation_project.get_full_path(".")
     analysis_file_names = map(lambda path: os.path.relpath(path, simulation_project_path), glob.glob(simulation_project_path + "/**/*.anf", recursive = True))
-    return builtins.filter(lambda analysis_file_name: matches_filter(analysis_file_name, filter, exclude_filter, full_match), analysis_file_names)
+    return builtins.filter(lambda analysis_file_name: is_anf_v2(simulation_project_path + "/" + analysis_file_name) and matches_filter(analysis_file_name, filter, exclude_filter, full_match), analysis_file_names)
 
 def export_charts(simulation_project=default_project, **kwargs):
     workspace = omnetpp.scave.analysis.Workspace(get_workspace_path("."), [])
