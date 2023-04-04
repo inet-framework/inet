8404ee3728364b04feaec8d59e9057ef1dc3c923
Levente Mészáros <levy@omnetpp.org>
2023-04-04 15:31:07 +0200

    python: Moved fingerprint store under tests and omnetpp python library.

--- python/omnetpp/test/fingerprint/store.py
+++ python/omnetpp/test/fingerprint/store.py
@@ -4,8 +4,8 @@ import os
 import subprocess
 import time
 
-from inet.common import *
-from inet.simulation.project import *
+from omnetpp.common import *
+from omnetpp.simulation.project import *
 
 logger = logging.getLogger(__name__)
 
@@ -26,7 +26,7 @@ class FingerprintStore:
             self.write()
 
     def write(self):
-        self.get_entries().sort(key=lambda element: (element["working_directory"], element["ini_file"], element["config"], element["run"], element["ingredients"], element["sim_time_limit"]))
+        self.get_entries().sort(key=lambda element: (element["working_directory"], element["ini_file"], element["config"], element["run_number"], element["ingredients"], element["sim_time_limit"]))
         logger.info(f"Writing fingerprints to {self.file_name}")
         file = open(self.file_name, "w")
         json.dump(self.entries, file, indent=True)
@@ -55,7 +55,7 @@ class FingerprintStore:
                 if entry["working_directory"] == latest_entry["working_directory"] and \
                    entry["ini_file"] == latest_entry["ini_file"] and \
                    entry["config"] == latest_entry["config"] and \
-                   entry["run"] == latest_entry["run"] and \
+                   entry["run_number"] == latest_entry["run_number"] and \
                    entry["sim_time_limit"] == latest_entry["sim_time_limit"] and \
                    entry["ingredients"] == latest_entry["ingredients"]:
                     if entry["timestamp"] < latest_entry["timestamp"]:
@@ -66,7 +66,7 @@ class FingerprintStore:
             latest_entries = new_latest_entries
             if not found:
                 new_latest_entries.append(entry)
-        latest_entries.sort(key=lambda element: (element["working_directory"], element["ini_file"], element["config"], element["run"], element["ingredients"], element["sim_time_limit"]))
+        latest_entries.sort(key=lambda element: (element["working_directory"], element["ini_file"], element["config"], element["run_number"], element["ingredients"], element["sim_time_limit"]))
         return latest_entries
     
     def find_entry(self, **kwargs):
@@ -83,12 +83,12 @@ class FingerprintStore:
     def remove_entry(self, entry):
         self.get_entries().remove(entry)
 
-    def filter_entries(self, ingredients="tplx", test_result=None, working_directory=os.getcwd(), ini_file="omnetpp.ini", config="General", run=0, sim_time_limit=None, itervars=None):
+    def filter_entries(self, ingredients="tplx", test_result=None, working_directory=os.getcwd(), ini_file="omnetpp.ini", config="General", run_number=0, sim_time_limit=None, itervars=None):
         def f(fingerprint):
             return (working_directory is None or fingerprint["working_directory"] == working_directory) and \
                    (ini_file is None or fingerprint["ini_file"] == ini_file) and \
                    (config is None or fingerprint["config"] == config) and \
-                   (run is None or fingerprint["run"] == run) and \
+                   (run_number is None or fingerprint["run_number"] == run_number) and \
                    (sim_time_limit is None or fingerprint["sim_time_limit"] == sim_time_limit) and \
                    (itervars is None or fingerprint["itervars"] == itervars) and \
                    (test_result is None or fingerprint["test_result"] == test_result) and \
@@ -101,12 +101,12 @@ class FingerprintStore:
     def set_fingerprint(self, fingerprint, **kwargs):
         self.get_entry(**kwargs)["fingerprint"] = fingerprint
 
-    def insert_fingerprint(self, fingerprint, ingredients="tplx", test_result=None, working_directory=os.getcwd(), ini_file="omnetpp.ini", config="General", run=0, sim_time_limit=None, itervars="$repetition==0"):
-        assert test_result == "ERROR" or sim_time_limit is not None
+    def insert_fingerprint(self, fingerprint, ingredients="tplx", test_result=None, working_directory=os.getcwd(), ini_file="omnetpp.ini", config="General", run_number=0, sim_time_limit=None, itervars="$repetition==0"):
+        # assert test_result == "ERROR" or sim_time_limit is not None
         self.get_entries().append({"working_directory": working_directory,
                                    "ini_file": ini_file,
                                    "config": config,
-                                   "run": run,
+                                   "run_number": run_number,
                                    "sim_time_limit": sim_time_limit,
                                    "test_result": test_result,
                                    "ingredients": ingredients,
@@ -128,5 +128,5 @@ correct_fingerprint_stores = dict()
 
 def get_correct_fingerprint_store(simulation_project):
     if not simulation_project in correct_fingerprint_stores:
-        correct_fingerprint_stores[simulation_project] = FingerprintStore(simulation_project, simulation_project.get_full_path("python/inet/test/fingerprint/correct_fingerprints.json"))
+        correct_fingerprint_stores[simulation_project] = FingerprintStore(simulation_project, simulation_project.get_full_path("tests/fingerprint/correct_fingerprints.json"))
     return correct_fingerprint_stores[simulation_project]
