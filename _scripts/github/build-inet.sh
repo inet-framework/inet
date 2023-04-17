62b1300a869467b6c78350c19732371cc8612982
Levente Mészáros <levy@omnetpp.org>
2023-04-17 14:58:57 +0200

    .github: Extracted build-omnetpp.sh and build-inet.sh

---
+++ _scripts/github/build-inet.sh
@@ -0,0 +1,35 @@
+#!/bin/env bash
+
+# This script runs the build job of GitHub Actions Workflows.
+#
+# The following environment variables must be set when invoked:
+#    MODE                   - must be "debug" or "release"
+#
+#    GITHUB_WORKSPACE       - this is provided by GitHub, most likely has
+#                             the value "/home/runner/work/inet/inet"
+
+echo "::group::Configuring ccache"
+export PATH=/usr/lib/ccache:$PATH
+export CCACHE_DIR=/home/runner/work/ccache
+echo "::endgroup::"
+
+echo "::group::Running inet setenv"
+cd $GITHUB_WORKSPACE/inet
+. setenv -f
+echo "::endgroup::"
+
+echo "::group::Enabling all inet features"
+opp_featuretool enable all
+echo "::endgroup::"
+
+echo "::group::Disabling some inet features"
+opp_featuretool disable VisualizationOsg VisualizationOsgShowcases SelfDoc
+echo "::endgroup::"
+
+echo "::group::Making inet makefiles"
+make makefiles
+echo "::endgroup::"
+
+echo "::group::Compiling inet"
+make MODE=$MODE -j $(nproc)
+echo "::endgroup::"
---
+++ _scripts/github/build-omnetpp.sh
@@ -0,0 +1,28 @@
+#!/bin/env bash
+
+# This script runs the build job of GitHub Actions Workflows.
+#
+# The following environment variables must be set when invoked:
+#    MODE                   - must be "debug" or "release"
+#
+#    GITHUB_WORKSPACE       - this is provided by GitHub, most likely has
+#                             the value "/home/runner/work/inet/inet"
+
+echo "::group::Configuring ccache"
+export PATH=/usr/lib/ccache:$PATH
+export CCACHE_DIR=/home/runner/work/ccache
+echo "::endgroup::"
+
+echo "::group::Running omnetpp setenv"
+cd $GITHUB_WORKSPACE/omnetpp
+cp configure.user.dist configure.user
+. setenv -f
+echo "::endgroup::"
+
+echo "::group::Configuring omnetpp"
+./configure WITH_LIBXML=yes WITH_QTENV=no WITH_OSG=no WITH_OSGEARTH=no
+echo "::endgroup::"
+ 
+echo "::group::Compiling omnetpp"
+make MODE=$MODE -j $(nproc) base
+echo "::endgroup::"
