a9a1316ccc7b36c75c93f2bdcc4554697d17d0f2
Levente Mészáros <levy@omnetpp.org>
2023-04-25 12:24:32 +0200

    EthernetFcsInserter: Copy received FCS for cut-through packets.
    The FCS is forwarded as is, see EthernetCutthroughInterface for more details.

--- src/inet/linklayer/ethernet/modular/EthernetFcsInserter.cc
+++ src/inet/linklayer/ethernet/modular/EthernetFcsInserter.cc
@@ -9,6 +9,7 @@
 
 #include "inet/common/ProtocolTag_m.h"
 #include "inet/linklayer/ethernet/common/EthernetMacHeader_m.h"
+#include "inet/protocolelement/cutthrough/CutthroughTag_m.h"
 
 namespace inet {
 
@@ -30,11 +31,17 @@ uint32_t EthernetFcsInserter::computeFcs(const Packet *packet, FcsMode fcsMode) 
 
 void EthernetFcsInserter::processPacket(Packet *packet)
 {
-    const auto& header = makeShared<EthernetFcs>();
-    auto fcs = computeFcs(packet, fcsMode);
-    header->setFcs(fcs);
-    header->setFcsMode(fcsMode);
-    packet->insertAtBack(header);
+    if (auto cutthroughTag = packet->findTag<CutthroughTag>()) {
+        auto header = dynamicPtrCast<const EthernetFcs>(cutthroughTag->getTrailerChunk());
+        packet->insertAtBack(header);
+    }
+    else {
+        const auto& header = makeShared<EthernetFcs>();
+        auto fcs = computeFcs(packet, fcsMode);
+        header->setFcs(fcs);
+        header->setFcsMode(fcsMode);
+        packet->insertAtBack(header);
+    }
     auto packetProtocolTag = packet->addTagIfAbsent<PacketProtocolTag>();
     packetProtocolTag->setProtocol(&Protocol::ethernetMac);
     packetProtocolTag->setFrontOffset(b(0));
