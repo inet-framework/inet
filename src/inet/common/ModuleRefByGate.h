0019bf41e256ce1072b83f3a31f6a21cb6c5b558
Levente Mészáros <levy@omnetpp.org>
2023-05-09 16:28:41 +0200

    ModuleRefByGate: Changed to keep the original gate only in debug mode.
    The gate is only needed for error reporting and usually already stored
    separately and only once for multiple ModuleRefByGate instances.

--- src/inet/common/ModuleRefByGate.h
+++ src/inet/common/ModuleRefByGate.h
@@ -16,7 +16,7 @@ namespace inet {
  * This template class implements a module reference that is parameterized by
  * a gate of the referencing module. The reference can be set by providing the
  * gate of the referencing module. After the reference is set, it can be used
- * similarly to simple a C++ pointer. The pointer is automatically set to nullptr
+ * similarly to a simple C++ pointer. The pointer is automatically set to nullptr
  * when the referenced module is deleted.
  *
  * TODO follow connection
@@ -25,17 +25,24 @@ template<typename T>
 class INET_API ModuleRefByGate
 {
   private:
-    cGate *gate = nullptr;
     cGate *referencedGate = nullptr;
     opp_component_ptr<T> referencedModule;
 
+#ifndef NDEBUG
+    cGate *referencingGate = nullptr;
+#endif
+
     void checkReference() const {
         if (referencedModule.getNullable() == nullptr) {
-            if (gate == nullptr)
+#ifndef NDEBUG
+            if (referencingGate == nullptr)
                 throw cRuntimeError("Dereferencing uninitialized reference of type '%s'", opp_typename(typeid(T)));
             else
                 throw cRuntimeError("Dereferencing nullptr of type '%s' referenced by '(%s)%s' through gate '%s'",
-                                    opp_typename(typeid(T)), gate->getOwnerModule()->getClassName(), gate->getOwnerModule()->getFullPath().c_str(), gate->getFullName());
+                                    opp_typename(typeid(T)), referencingGate->getOwnerModule()->getClassName(), referencingGate->getOwnerModule()->getFullPath().c_str(), referencingGate->getFullName());
+#else
+            throw cRuntimeError("Dereferencing uninitialized reference of type '%s'", opp_typename(typeid(T)));
+#endif
         }
     }
 
@@ -59,22 +66,26 @@ class INET_API ModuleRefByGate
     T *getNullable() { return referencedModule.getNullable(); }
     const T *getNullable() const { return referencedModule.getNullable(); }
 
-    T *get() { checkReference(); return referencedModule; }
-    const T *get() const { checkReference(); return referencedModule; }
+    T *get() { checkReference(); return referencedModule.get(); }
+    const T *get() const { checkReference(); return referencedModule.get(); }
 
-    cGate *getGate() { return gate; }
-    const cGate *getGate() const { return gate; }
+#ifndef NDEBUG
+    cGate *getReferencingGate() { return referencingGate; }
+    const cGate *getReferencingGate() const { return referencingGate; }
+#endif
 
     cGate *getReferencedGate() { return referencedGate; }
     const cGate *getReferencedGate() const { return referencedGate; }
 
-    void reference(cGate *gate, bool mandatory) {
-        if (gate == nullptr)
-            throw cRuntimeError("Gate is nullptr");
-        if (this->gate != nullptr)
+    void reference(cGate *referencingGate, bool mandatory) {
+        if (referencingGate == nullptr)
+            throw cRuntimeError("Referencing gate is nullptr");
+#ifndef NDEBUG
+        if (this->referencingGate != nullptr)
             throw cRuntimeError("Reference is already initialized");
-        this->gate = gate;
-        std::tie(referencedModule, referencedGate) = mandatory ? getConnectedModuleAndGate<T>(gate) : findConnectedModuleAndGate<T>(gate);
+        this->referencingGate = referencingGate;
+#endif
+        std::tie(referencedModule, referencedGate) = mandatory ? getConnectedModuleAndGate<T>(referencingGate) : findConnectedModuleAndGate<T>(referencingGate);
     }
 };
 
