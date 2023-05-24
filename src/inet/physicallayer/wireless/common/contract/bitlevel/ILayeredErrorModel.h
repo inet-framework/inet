93883caa1d9c8438b0956a9536af80ea27a05d23
Levente Mészáros <levy@omnetpp.org>
2023-05-24 10:47:10 +0200

    ILayeredErrorModel: Removed ITransmission parameter from compute*Model methods.
    There's no real degree of freedom for this argument. The transmission is already
    accessible from the snir parameter using snir->getReception()->getTransmission().

--- src/inet/physicallayer/wireless/common/contract/bitlevel/ILayeredErrorModel.h
+++ src/inet/physicallayer/wireless/common/contract/bitlevel/ILayeredErrorModel.h
@@ -19,7 +19,6 @@ namespace physicallayer {
  * The layered error model computes the erroneous bits, symbols, or samples
  * based on the SNIR and a simplified model of the signal processing.
  */
- // REFACTOR TODO: remove transmission parameters, it's accessible from snir through reception
 class INET_API ILayeredErrorModel : public IPrintableObject
 {
   public:
@@ -28,28 +27,28 @@ class INET_API ILayeredErrorModel : public IPrintableObject
      * model for the underlying domains. This result includes all potential
      * errors that were not corrected by the underlying domains.
      */
-    virtual const IReceptionPacketModel *computePacketModel(const ITransmission *transmission, const ISnir *snir) const = 0;
+    virtual const IReceptionPacketModel *computePacketModel(const ISnir *snir) const = 0;
 
     /**
      * Computes the bit domain representation at the receiver using a simplified
      * model for the underlying domains. This result includes all potential
      * errors that were not corrected by the underlying domains.
      */
-    virtual const IReceptionBitModel *computeBitModel(const ITransmission *transmission, const ISnir *snir) const = 0;
+    virtual const IReceptionBitModel *computeBitModel(const ISnir *snir) const = 0;
 
     /**
      * Computes the symbol domain representation at the receiver using a simplified
      * model for the underlying domains. This result includes all potential
      * errors that were not corrected by the underlying domains.
      */
-    virtual const IReceptionSymbolModel *computeSymbolModel(const ITransmission *transmission, const ISnir *snir) const = 0;
+    virtual const IReceptionSymbolModel *computeSymbolModel(const ISnir *snir) const = 0;
 
     /**
      * Computes the sample domain representation at the receiver using a simplified
      * model for the underlying domains. This result includes all potential
      * errors that were not corrected by the underlying domains.
      */
-    virtual const IReceptionSampleModel *computeSampleModel(const ITransmission *transmission, const ISnir *snir) const = 0;
+    virtual const IReceptionSampleModel *computeSampleModel(const ISnir *snir) const = 0;
 };
 
 } // namespace physicallayer
