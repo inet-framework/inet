b02f7bedb7b7168640de465d476190af946ce6ac
Zoltan Bojthe <zbojthe@omnetpp.org>
2023-06-12 13:02:59 +0200

    python: get_inet_relative_path(): using INET_ROOT env var

--- python/inet/common/util.py
+++ python/inet/common/util.py
@@ -57,7 +57,7 @@ def get_omnetpp_relative_path(path):
     return os.path.abspath(os.path.join(os.environ["__omnetpp_root_dir"], path)) if "__omnetpp_root_dir" in os.environ else None
 
 def get_inet_relative_path(path):
-    return os.path.join(get_workspace_path("inet"), path)
+    return os.path.join(os.environ["INET_ROOT"], path)
 
 def get_workspace_path(path):
     return os.path.join(os.path.realpath(get_omnetpp_relative_path("..")), path)
