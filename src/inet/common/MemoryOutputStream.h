73d376c6e781f55984390f6b3e5ede1a2d5594b0
Zoltan Bojthe <zbojthe@omnetpp.org>
2023-07-12 15:29:26 +0200

    MemoryOutputStream: optimize writeBit()

--- src/inet/common/MemoryOutputStream.h
+++ src/inet/common/MemoryOutputStream.h
@@ -116,8 +116,8 @@ class INET_API MemoryOutputStream
         size_t byteIndex = i / 8;
         size_t bitIndex = i % 8;
         if (bitIndex == 0)
-            data.push_back(0);
-        if (value)
+            data.push_back(value ? 0x80 : 0);
+        else if (value)
             data[byteIndex] |= 1 << (7 - bitIndex);
         length += b(1);
     }
