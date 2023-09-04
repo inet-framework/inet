f8ad182cdc9dd249b0631058c96398a044b32741
Levente Mészáros <levy@omnetpp.org>
2023-09-04 11:01:20 +0200

    protocolelement: Added MessageAuthenticationCodeInserter and MessageAuthenticationCodeChecker.

---
+++ src/inet/protocolelement/authentication/MessageAuthenticationCodeInserter.cc
@@ -0,0 +1,33 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/protocolelement/authentication/MessageAuthenticationCodeInserter.h"
+
+#include "inet/common/IProtocolRegistrationListener.h"
+#include "inet/common/ProtocolTag_m.h"
+#include "inet/protocolelement/common/AccessoryProtocol.h"
+
+namespace inet {
+
+Define_Module(MessageAuthenticationCodeInserter);
+
+void MessageAuthenticationCodeInserter::initialize(int stage)
+{
+    PacketFlowBase::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        headerLength = b(par("headerLength"));
+    }
+}
+
+void MessageAuthenticationCodeInserter::processPacket(Packet *packet)
+{
+    const auto& header = makeShared<ByteCountChunk>(headerLength);
+    packet->insertAtFront(header);
+}
+
+} // namespace inet
+
---
+++ src/inet/protocolelement/authentication/MessageAuthenticationCodeInserter.h
@@ -0,0 +1,30 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#ifndef __INET_MESSAGEAUTHENTICATIONCODEINSERTER_H
+#define __INET_MESSAGEAUTHENTICATIONCODEINSERTER_H
+
+#include "inet/queueing/base/PacketFlowBase.h"
+
+namespace inet {
+
+using namespace inet::queueing;
+
+class INET_API MessageAuthenticationCodeInserter : public PacketFlowBase
+{
+  protected:
+    b headerLength = b(-1);
+
+  protected:
+    virtual void initialize(int stage) override;
+    virtual void processPacket(Packet *packet) override;
+};
+
+} // namespace inet
+
+#endif
+
