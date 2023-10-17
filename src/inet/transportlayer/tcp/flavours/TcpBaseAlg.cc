c16a1c668daa2badf96c7e8db1616847b8d4d555
Zoltan Bojthe <zbojthe@omnetpp.org>
2023-10-17 12:55:38 +0200

    Tcp: added maxRexmitCount NED parameter

--- src/inet/transportlayer/tcp/flavours/TcpBaseAlg.cc
+++ src/inet/transportlayer/tcp/flavours/TcpBaseAlg.cc
@@ -30,10 +30,8 @@ namespace tcp {
 // segment."
 
 #define DELAYED_ACK_TIMEOUT    0.2   // 200ms (RFC 1122: MUST be less than 0.5 seconds)
-#define MAX_REXMIT_COUNT       12   // 12 retries
 #define MIN_PERSIST_TIMEOUT    5   // 5s
 #define MAX_PERSIST_TIMEOUT    60   // 60s
-
 std::string TcpBaseAlgStateVariables::str() const
 {
     std::stringstream out;
