6c61da6a4b7300d62c8fcb8dc014ccc84ef9baed
Levente Mészáros <levy@omnetpp.org>
2023-11-06 14:51:56 +0100

    TcpConnectionUtil: Added initialSendSequenceNumber parameter.
    Allows setting the initial sequence number to start from 0.

--- src/inet/transportlayer/tcp/TcpConnectionUtil.cc
+++ src/inet/transportlayer/tcp/TcpConnectionUtil.cc
@@ -494,7 +494,8 @@ void TcpConnection::configureStateVariables()
 void TcpConnection::selectInitialSeqNum()
 {
     // set the initial send sequence number
-    state->iss = (unsigned long)fmod(SIMTIME_DBL(simTime()) * 250000.0, 1.0 + (double)(unsigned)0xffffffffUL) & 0xffffffffUL;
+    int64_t iss = tcpMain->par("initialSendSequenceNumber");
+    state->iss = iss != -1 ? iss : (unsigned long)fmod(SIMTIME_DBL(simTime()) * 250000.0, 1.0 + (double)(unsigned)0xffffffffUL) & 0xffffffffUL;
 
     state->snd_una = state->snd_nxt = state->snd_max = state->iss;
 
