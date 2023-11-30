3f7b6a72dd32b4e111a75d13d916679e8b37cc8e
Levente Mészáros <levy@omnetpp.org>
2023-11-30 11:21:03 +0100

    tcp: Moved contents of TcpConnectionSackUtil into Rfc6675Recovery.

--- src/inet/transportlayer/tcp/flavours/Rfc6675Recovery.h
+++ src/inet/transportlayer/tcp/flavours/Rfc6675Recovery.h
@@ -35,6 +35,51 @@ class INET_API Rfc6675Recovery : public TcpRecovery
     virtual void receivedDataAck(uint32_t firstSeqAcked) override;
 
     virtual void receivedDuplicateAck() override;
+
+    virtual bool processSACKOption(const Ptr<const TcpHeader>& tcpHeader, const TcpOptionSack& option);
+    /**
+     * For SACK TCP. RFC 3517, page 3: "This routine returns whether the given
+     * sequence number is considered to be lost.  The routine returns true when
+     * either DupThresh discontiguous SACKed sequences have arrived above
+     * 'SeqNum' or (DupThresh * SMSS) bytes with sequence numbers greater
+     * than 'SeqNum' have been SACKed.  Otherwise, the routine returns
+     * false."
+     */
+    virtual bool isLost(uint32_t seqNum);
+
+    /**
+     * For SACK TCP. RFC 3517, page 3: "This routine traverses the sequence
+     * space from HighACK to HighData and MUST set the "pipe" variable to an
+     * estimate of the number of octets that are currently in transit between
+     * the TCP sender and the TCP receiver."
+     */
+    virtual void setPipe();
+
+    /**
+     * For SACK TCP. RFC 3517, page 3: "This routine uses the scoreboard data
+     * structure maintained by the Update() function to determine what to transmit
+     * based on the SACK information that has arrived from the data receiver
+     * (and hence been marked in the scoreboard).  NextSeg () MUST return the
+     * sequence number range of the next segment that is to be
+     * transmitted..."
+     * Returns true if a valid sequence number (for the next segment) is found and
+     * returns false if no segment should be send.
+     */
+    virtual bool nextSeg(uint32_t& seqNum);
+
+    /**
+     * Utility: send data during Loss Recovery phase (if SACK is enabled).
+     */
+    virtual void sendDataDuringLossRecoveryPhase(uint32_t congestionWindow);
+
+    /**
+     * Utility: send segment during Loss Recovery phase (if SACK is enabled).
+     * Returns the number of bytes sent.
+     */
+    virtual uint32_t sendSegmentDuringLossRecoveryPhase(uint32_t seqNum);
+
+    /** Utility: adds SACKs to segments header options field */
+    virtual TcpHeader addSacks(const Ptr<TcpHeader>& tcpHeader);
 };
 
 } // namespace tcp
