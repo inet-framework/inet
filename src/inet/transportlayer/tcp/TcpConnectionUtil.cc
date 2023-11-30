5bdd93d66008c69b1aa79447749f9b67e0cb771c
Levente Mészáros <levy@omnetpp.org>
2023-11-30 15:23:20 +0100

    tcp: Renamed dsakSupport to dsackEnabled.

--- src/inet/transportlayer/tcp/TcpConnectionUtil.cc
+++ src/inet/transportlayer/tcp/TcpConnectionUtil.cc
@@ -656,7 +656,7 @@ void TcpConnection::configureStateVariables()
     state->pmtudEnabled = tcpMain->par("pmtudEnabled"); // Path MTU Discovery (RFC 1191, RFC 1981)
     state->pmtudTimeout = tcpMain->par("pmtudTimeout"); // time after which original MSS is restored
     state->pmtudLastMssReduction = -1; // never reduced yet
-    state->dsack_support = tcpMain->par("dsackSupport"); // if set, this means that current host supports SACK (RFC 2018, 2883, 3517)
+    state->dsack_enabled = tcpMain->par("dsackEnabled"); // if set, this means that current host supports SACK (RFC 2018, 2883, 3517)
 
     WATCH_EXPR("snd_nxt", state->snd_nxt);
     WATCH_EXPR("rcv_nxt", state->rcv_nxt);
