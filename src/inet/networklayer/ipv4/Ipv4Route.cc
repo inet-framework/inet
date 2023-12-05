d64af1e2041e067f7698ba588407b8ab3d50ec6c
Zoltan Bojthe <zbojthe@omnetpp.org>
2023-12-05 16:33:02 +0100

    Ipv4MulticastRoute::matches() accepts when the origin is unspecified, too

--- src/inet/networklayer/ipv4/Ipv4Route.cc
+++ src/inet/networklayer/ipv4/Ipv4Route.cc
@@ -276,7 +276,7 @@ void Ipv4MulticastRoute::changed(int fieldCode)
 bool Ipv4MulticastRoute::matches(const Ipv4Address& origin, const Ipv4Address& group) const
 {
     return (this->group.isUnspecified() || this->group == group) &&
-            Ipv4Address::maskedAddrAreEqual(origin, this->origin, this->originNetmask);
+            (this->origin.isUnspecified() || Ipv4Address::maskedAddrAreEqual(origin, this->origin, this->originNetmask));
 }
 
 } // namespace inet
