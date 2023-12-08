798eecae5f0c1047191a39729af3307f498b8d54
Levente Mészáros <levy@omnetpp.org>
2023-12-08 11:09:44 +0100

    tcp: Added quotes around parts copied from RFCs.

--- src/inet/transportlayer/tcp/flavours/Rfc5681Recovery.cc
+++ src/inet/transportlayer/tcp/flavours/Rfc5681Recovery.cc
@@ -21,19 +21,29 @@ void Rfc5681Recovery::receivedAckForUnackedData(uint32_t numBytesAcked)
 
 bool Rfc5681Recovery::isDuplicateAck(const TcpHeader *tcpHeader, uint32_t payloadLength)
 {
+    //"
     // DUPLICATE ACKNOWLEDGMENT: An acknowledgment is considered a
     // "duplicate" in the following algorithms when
     //   (a) the receiver of the ACK has outstanding data,
+    //"
     bool a = state->snd_una != state->snd_max;
+    //"
     //   (b) the incoming acknowledgment carries no data,
+    //"
     bool b = payloadLength == 0;
+    //"
     //   (c) the SYN and FIN bits are both off,
+    //"
     bool c = !tcpHeader->getSynBit() && !tcpHeader->getFinBit();
+    //"
     //   (d) the acknowledgment number is equal to the greatest acknowledgment
     //       received on the given connection (TCP.UNA from [RFC793]) and
+    //"
     bool d = tcpHeader->getAckNo() == state->snd_una;
+    //"
     //   (e) the advertised window in the incoming acknowledgment equals the
     //       advertised window in the last incoming acknowledgment.
+    //"
     uint32_t trueWindow = tcpHeader->getWindow();
     if (state->ws_enabled && !tcpHeader->getSynBit())
         trueWindow = tcpHeader->getWindow() << state->snd_wnd_scale;
@@ -43,6 +53,7 @@ bool Rfc5681Recovery::isDuplicateAck(const TcpHeader *tcpHeader, uint32_t payloa
 
 void Rfc5681Recovery::receivedDuplicateAck()
 {
+    //"
     // 3.2. Fast Retransmit/Fast Recovery
     //
     // ...
@@ -59,13 +70,17 @@ void Rfc5681Recovery::receivedDuplicateAck()
     //    [RFC3042].  Note that a sender using SACK [RFC2018] MUST NOT send
     //    new data unless the incoming duplicate acknowledgment contains
     //    new SACK information.
+    //"
     // TODO how is this implemented?
 
+    //"
     // 2. When the third duplicate ACK is received, a TCP MUST set ssthresh
     //    to no more than the value given in equation (4).  When [RFC3042]
     //    is in use, additional data sent in limited transmit MUST NOT be
     //    included in this calculation.
+    //"
     if (state->dupacks == state->dupthresh) {
+        //"
         // When a TCP sender detects segment loss using the retransmission timer
         // and the given segment has not yet been resent by way of the
         // retransmission timer, the value of ssthresh MUST be set to no more
@@ -75,21 +90,26 @@ void Rfc5681Recovery::receivedDuplicateAck()
         //
         // where, as discussed above, FlightSize is the amount of outstanding
         // data in the network.
+        //"
         state->ssthresh = std::max(conn->getBytesInFlight() / 2, 2 * state->snd_mss);
         conn->emit(ssthreshSignal, state->ssthresh);
 
+        //"
         // 3. The lost segment starting at SND.UNA MUST be retransmitted and
         //    cwnd set to ssthresh plus 3*SMSS.  This artificially "inflates"
         //    the congestion window by the number of segments (three) that have
         //    left the network and which the receiver has buffered.
+        //"
         conn->retransmitOneSegment(false);
         state->snd_cwnd = state->ssthresh + 3 * state->snd_mss;
         conn->emit(cwndSignal, state->snd_cwnd);
     }
+    //"
     // 4. For each additional duplicate ACK received (after the third),
     //    cwnd MUST be incremented by SMSS.  This artificially inflates the
     //    congestion window in order to reflect the additional segment that
     //    has left the network.
+    //"
     else if (state->dupacks > state->dupthresh) {
         state->snd_cwnd += state->snd_mss;
         conn->emit(cwndSignal, state->snd_cwnd);
