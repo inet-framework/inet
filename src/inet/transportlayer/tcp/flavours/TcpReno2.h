3b77954e4e42eb959ebdde8b30c72c150c1d9d53
Levente Mészáros <levy@omnetpp.org>
2023-12-14 16:07:26 +0100

    tcp: Temporarily added TcpReno2 as a replacement for TcpReno using the new architecture.

---
+++ src/inet/transportlayer/tcp/flavours/TcpReno2.h
@@ -0,0 +1,29 @@
+//
+// Copyright (C) 2020 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+#ifndef __INET_TCPRENO2_H
+#define __INET_TCPRENO2_H
+
+#include "inet/transportlayer/tcp/flavours/TcpClassicAlgorithmBase.h"
+
+namespace inet {
+namespace tcp {
+
+/**
+ * Implements RFC 6582: The NewReno Modification to TCP's Fast Recovery Algorithm.
+ */
+class INET_API TcpReno2 : public TcpClassicAlgorithmBase
+{
+  protected:
+    virtual ITcpRecovery *createRecovery() override;
+    virtual ITcpCongestionControl *createCongestionControl() override;
+};
+
+} // namespace tcp
+} // namespace inet
+
+#endif
+
