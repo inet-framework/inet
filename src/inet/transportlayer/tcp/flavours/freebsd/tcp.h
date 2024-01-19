1852fa1f81aa633e5648177d0726aa8d00b25b94
Levente Meszaros <levy@omnetpp.org>
2024-01-19 14:43:22 +0100

    tcp: Fixed freebsd compilation errors.

--- src/inet/transportlayer/tcp/flavours/freebsd/tcp.h
+++ src/inet/transportlayer/tcp/flavours/freebsd/tcp.h
@@ -36,6 +36,14 @@
 
 #include <cstdint>
 
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+typedef unsigned int u_int;
+typedef uint8_t u_int8_t;
+typedef uint16_t u_int16_t;
+typedef uint32_t u_int32_t;
+typedef uint64_t u_int64_t;
+
 #define __BSD_VISIBLE 1
 
 #if __BSD_VISIBLE
