4001e6126ebbd612add7e6c8c03a3cfc9ce94c42
Gyorgy Szaszko <adam.szaszko@gmail.com>
2024-01-22 16:35:13 +0100

    babel refactor

--- showcases/emulation/babel/mininet-tap.py
+++ showcases/emulation/babel/mininet-tap.py
@@ -25,53 +25,36 @@ def checkIntf( intf ):
                'and is probably in use!\n' )
         exit( 1 )
 
+def setupHost(host, netns, tap_intf, ip):
+    "Creates TAP interface in the host OS, assigns IP address to it, creates Mininet host and attaches TAP interface to it."
+    info( '*** Adding host', host, 'to Mininet network', '\n' )
+    python_host = net.addHost(host)
+    info( '*** Adding tap interface', tap_intf, 'in host OS', '\n' )
+    subprocess.run(['bash', '-c', f'sudo tunctl -t {tap_intf} -u $SAVED_USER -g $SAVED_GROUP && sudo ip link set dev {tap_intf} up'])
+    info( '*** Checking', tap_intf, '\n' )
+    checkIntf( tap_intf )
+    info( '*** Adding hardware interface', tap_intf, 'to Mininet host', host, '\n' )
+    _intf = Intf( tap_intf, node=python_host, ip=ip )
+    info( '*** Attaching mininet tap interface to host OS tap interface', tap_intf, '\n' )
+    host_pid = python_host.pid
+    subprocess.run(['bash', '-c', f'sudo ip netns attach {netns} {host_pid}'])
+    return python_host
+
 
 setLogLevel( 'info' )
 
+# delete all network namespaces
 subprocess.run(['sudo', 'ip', '-all', 'netns', 'delete'])
 
 info( '*** Creating network\n' )
 net = Mininet( topo=LinearTopo( k=0, n=3 ), controller=None)
 
-host0 = net.addHost('h1')
-host1 = net.addHost('h2')
-host2 = net.addHost('h3')
+# creating hosts
+host0 = setupHost(host='h0', netns='host0', tap_intf='tap0', ip='192.168.2.1/30')
+host1 = setupHost(host='h1', netns='host1', tap_intf='tap1', ip='192.168.3.1/30')
+host2 = setupHost(host='h2', netns='host2', tap_intf='tap2', ip='192.168.4.1/30')
 
-# add tap interfaces
-subprocess.run(['bash', '-c', 'sudo tunctl -t tap0 -u $SAVED_USER -g $SAVED_GROUP && sudo ip link set dev tap0 up'])
-subprocess.run(['bash', '-c', 'sudo tunctl -t tap1 -u $SAVED_USER -g $SAVED_GROUP && sudo ip link set dev tap1 up'])
-subprocess.run(['bash', '-c', 'sudo tunctl -t tap2 -u $SAVED_USER -g $SAVED_GROUP && sudo ip link set dev tap2 up'])
-
-info( '*** Checking', 'tap0', '\n' )
-checkIntf( 'tap0' )
-
-info( '*** Checking', 'tap1', '\n' )
-checkIntf( 'tap1' )
-
-info( '*** Checking', 'tap2', '\n' )
-checkIntf( 'tap2' )
-
-info( '*** Adding hardware interface', 'tap0', 'to host', host0.name, '\n' )
-_intf = Intf( 'tap0', node=host0, ip='192.168.2.1/30' )
-
-info( '*** Adding hardware interface', 'tap1', 'to host', host1.name, '\n' )
-_intf = Intf( 'tap1', node=host1, ip='192.168.3.1/30' )
-
-info( '*** Adding hardware interface', 'tap2', 'to host', host2.name, '\n' )
-_intf = Intf( 'tap2', node=host2, ip='192.168.4.1/30' )
-
-pid0 = net.hosts[0].pid
-pid1 = net.hosts[1].pid
-pid2 = net.hosts[2].pid
-
-cmd0 = """sudo ip netns attach host0 """ + str(pid0)
-cmd1 = """sudo ip netns attach host1 """ + str(pid1)
-cmd2 = """sudo ip netns attach host2 """ + str(pid2)
-
-subprocess.run(['bash', '-c', str(cmd0)])
-subprocess.run(['bash', '-c', str(cmd1)])
-subprocess.run(['bash', '-c', str(cmd2)])
-
+info( '*** Starting Mininet network', '\n' )
 net.start()
 
 # remove default routes
@@ -79,12 +62,12 @@ host0.cmd("ip route del 192.168.2.0/30")
 host1.cmd("ip route del 192.168.3.0/30")
 host2.cmd("ip route del 192.168.4.0/30")
 
-# start babel daemons on hosts
+info( '*** Starting Babel daemons', '\n' )
 host0.cmd("""babeld -I babel0.pid -S babel-state0 -r -w -h 2 -M 0 -C 'reflect-kernel-metric true' -C 'interface tap0 channel 1 link-quality false' &""")
 host1.cmd("""babeld -I babel1.pid -S babel-state1 -r -w -h 2 -M 0 -C 'reflect-kernel-metric true' -C 'interface tap1 channel 1 link-quality false' &""")
 host2.cmd("""babeld -I babel2.pid -S babel-state2 -r -w -h 2 -M 0 -C 'reflect-kernel-metric true' -C 'interface tap2 channel 1 link-quality false' &""")
 
-# start ping loop
+info( '*** Starting ping loop', '\n' )
 host0.cmd("bash -c 'while ! ping 192.168.4.1; do sleep 1; done' &")
 
 # watch routing tables
@@ -93,11 +76,11 @@ host1.cmd("""xterm -e "watch -n 0.1 'ip route'" &""")
 host2.cmd("""xterm -e "watch -n 0.1 'ip route'" &""")
 
 # uncomment to run wireshark on host
-#host0.cmd("wireshark -i tap0 -k &")
-#host1.cmd("wireshark -i tap1 -k &")
-#host2.cmd("wireshark -i tap2 -k &")
+#python_host0.cmd("wireshark -i tap0 -k &")
+#python_host1.cmd("wireshark -i tap1 -k &")
+#python_host2.cmd("wireshark -i tap2 -k &")
 
-# start inet
+info( '*** Starting INET', '\n' )
 user = os.getenv('SAVED_USER')
 group = os.getenv('SAVED_GROUP')
 saved_path = os.getenv('SAVED_PATH')
@@ -107,8 +90,10 @@ saved_env["PATH"] = saved_path
 # We also need to restore the original PATH which is overwritten by 'sudo'.
 subprocess.run(['sudo -E -u $SAVED_USER bash -c "export PATH=$SAVED_PATH && inet"'], shell=True)
 
+info( '*** Starting Mininet command-line interface', '\n' )
 CLI( net )
 
+# delete all network namespaces
 subprocess.run(['sudo', 'ip', '-all', 'netns', 'delete'])
 
 net.stop()
