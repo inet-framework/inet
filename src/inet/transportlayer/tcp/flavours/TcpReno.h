bc1531c260cdc7e5271c3df742aaea407bdc4644
Levente Meszaros <levy@omnetpp.org>
2024-01-24 16:08:44 +0100

    tcp: Renamed TcpReno2 to TcpReno.

--- src/inet/transportlayer/tcp/flavours/TcpReno.h
+++ src/inet/transportlayer/tcp/flavours/TcpReno.h
@@ -4,8 +4,8 @@
 // SPDX-License-Identifier: LGPL-3.0-or-later
 //
 
-#ifndef __INET_TCPRENO2_H
-#define __INET_TCPRENO2_H
+#ifndef __INET_TCPRENO_H
+#define __INET_TCPRENO_H
 
 #include "inet/transportlayer/tcp/flavours/TcpClassicAlgorithmBase.h"
 
@@ -15,7 +15,7 @@ namespace tcp {
 /**
  * Implements RFC 6582: The NewReno Modification to TCP's Fast Recovery Algorithm.
  */
-class INET_API TcpReno2 : public TcpClassicAlgorithmBase
+class INET_API TcpReno : public TcpClassicAlgorithmBase
 {
   protected:
     virtual ITcpRecovery *createRecovery() override;
