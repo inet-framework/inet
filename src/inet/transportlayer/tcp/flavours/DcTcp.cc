905e0e0b8afc754a31dff05f050c761802dbcaf5
Levente Meszaros <levy@omnetpp.org>
2024-01-25 15:05:32 +0100

    DcTcp: Fixed incorrectly calling TcpClassicAlgorithmBase::receivedAckForUnackedData.
    This caused slow start to be done twice, once in the base class and once in DcTcp.

--- src/inet/transportlayer/tcp/flavours/DcTcp.cc
+++ src/inet/transportlayer/tcp/flavours/DcTcp.cc
@@ -35,7 +35,7 @@ void DcTcp::receivedAckForUnackedData(uint32_t firstSeqAcked)
 {
     uint32_t old_dupacks = state->dupacks;
 
-    TcpClassicAlgorithmBase::receivedAckForUnackedData(firstSeqAcked);
+    TcpAlgorithmBase::receivedAckForUnackedData(firstSeqAcked);
 
     if (old_dupacks >= state->dupthresh) {
         //
