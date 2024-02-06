98d033a97b6ed3ff57b366f1120a32602dc0dbb8
Levente Meszaros <levy@omnetpp.org>
2024-02-06 11:13:07 +0100

    NetworkInterface: Moved isUp/isDown to other is* methods.

--- src/inet/networklayer/common/NetworkInterface.h
+++ src/inet/networklayer/common/NetworkInterface.h
@@ -217,12 +217,6 @@ class INET_API NetworkInterface : public queueing::PacketProcessorBase, public q
     cChannel *getRxTransmissionChannel() { return rxTransmissionChannel; }
     cChannel *getTxTransmissionChannel() { return txTransmissionChannel; }
 
-    /**
-     * Returns the combined state of the carrier and the interface requested state.
-     */
-    bool isUp() const { return getState() == UP; }
-    bool isDown() const { return getState() != UP; }
-
     void setHasModuleIdAddress(bool value) { hasModuleIdAddress = value; }
     void setHasModulePathAddress(bool value) { hasModulePathAddress = value; }
     const ModuleIdAddress getModuleIdAddress() const { return hasModuleIdAddress ? ModuleIdAddress(getId()) : ModuleIdAddress(0); }
@@ -239,6 +233,8 @@ class INET_API NetworkInterface : public queueing::PacketProcessorBase, public q
     int getNodeInputGateId() const { return nodeInputGateId; }
     int getMtu() const { return mtu; }
     bool hasCarrier() const { return carrier; }
+    bool isUp() const { return state == UP; }
+    bool isDown() const { return state != UP; }
     bool isBroadcast() const { return broadcast; }
     bool isMulticast() const { return multicast; }
     bool isPointToPoint() const { return pointToPoint; }
