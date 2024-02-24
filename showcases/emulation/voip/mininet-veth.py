78013ec804092d521d59e83a900bc5bc01e86a67
Andras Varga <andras@omnetpp.org>
2024-02-24 21:26:01 +0100

    showcases/voip: mininet-veth.py: less verbose

--- showcases/emulation/voip/mininet-veth.py
+++ showcases/emulation/voip/mininet-veth.py
@@ -12,25 +12,21 @@ net = Mininet(controller = OVSController, waitConnected=True, intf=TCIntf)
 info('*** Adding controller\n')
 net.addController('c0')
 
-info('*** Adding hosts\n')
+info('*** Creating network topology\n')
 h1 = net.addHost('h1', ip='192.168.2.1/24')
 h2 = net.addHost('h2', ip='192.168.2.2/24')
-
-info('*** Adding switch\n')
 s3 = net.addSwitch('s3')
-
-info('*** Creating links\n')
 net.addLink(h1, s3)
 net.addLink(h2, s3)
 
-info('*** Configuring link bandwidth and packet loss\n')
+info('*** Configuring link characteristics like bandwidth and packet loss\n')
 h1.intf().config(bw=10, loss=10, delay='10ms', jitter='1ms')     # bandwidth in Mpbs, packet loss in %
 
 info('*** Starting network\n')
 net.start()
 
 info('*** Starting INET simulations\n')
-# Note: Mininet runs as root, but we want to run the simulation as the normal user. We use 'sudo' to specify the user.
+# Note: sudo is needed because Mininet runs as root, but we want to run the simulation as the normal user.
 # We also need to restore the original PATH which is overwritten by 'sudo'.
 h1.cmd('sudo -E -u $SAVED_USER bash -c "export PATH=$SAVED_PATH && inet -s -c VoipSenderMininet &"')
 h2.cmd('sudo -E -u $SAVED_USER bash -c "export PATH=$SAVED_PATH && inet -s -c VoipReceiverMininet &"')
