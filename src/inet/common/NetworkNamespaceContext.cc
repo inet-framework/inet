e5a6df7561d96c9f05a89199e0c357c75d46d2cf
Levente Meszaros <levy@omnetpp.org>
2024-03-14 17:10:44 +0100

    NetworkNamespaceContext: Added support for local network namespaces using unshare.
    The old implementation only supported externally created network namespaces that are
    visible through the file system.

--- src/inet/common/NetworkNamespaceContext.cc
+++ src/inet/common/NetworkNamespaceContext.cc
@@ -7,24 +7,97 @@
 
 #include "inet/common/NetworkNamespaceContext.h"
 
-#include <fcntl.h>
+#include "inet/common/IPrintableObject.h"
+#include "inet/common/UnsharedNamespaceInitializer.h"
 
 #include <cstdio>
+#include <fcntl.h>
+#include <sys/mount.h>
 
 namespace inet {
 
-NetworkNamespaceContext::NetworkNamespaceContext(const char *networkNamespace)
+std::map<std::string, int> localNetworkNamespaces; // network namespace which are not bound in the /proc filesystem
+
+void createNetworkNamespace(const char *name, bool global)
 {
-    if (networkNamespace != nullptr && *networkNamespace != '\0') {
+    if (global) {
+        std::string fullPath = std::string("/var/run/netns/") + name;
+        int fd = open(fullPath.c_str(), O_RDONLY | O_CREAT | O_EXCL, 0);
+        if (fd < 0)
+            throw cRuntimeError("Cannot open file: %s", fullPath.c_str());
+        close(fd);
+        if (mount("/proc/self/ns/net", fullPath.c_str(), "none", MS_BIND, nullptr) < 0)
+            throw cRuntimeError("Cannot mount: %s", fullPath.c_str());
+    }
+    else {
+        int oldFd = open("/proc/self/ns/net", O_RDONLY);
+        if (unshare(CLONE_NEWNET) < 0)
+            throw cRuntimeError("Cannot unshare network namespace");
+        int newFd = open("/proc/self/ns/net", O_RDONLY);
+        if (newFd < 0)
+            throw cRuntimeError("Cannot open file: /proc/self/ns/net");
+        localNetworkNamespaces[name] = newFd;
+        // switch back to the original namespace that was used before unshare
+        setns(oldFd, 0);
+    }
+}
+
+bool existsNetworkNamespace(const char *name)
+{
+    auto it = localNetworkNamespaces.find(name);
+    if (it != localNetworkNamespaces.end())
+        return true;
+    else {
+        std::string path = std::string("/var/run/netns/") + name;
+        int fd = open(path.c_str(), O_RDONLY);
+        if (fd >= 0) {
+            close(fd);
+            return true;
+        }
+    }
+    return false;
+}
+
+void deleteNetworkNamespace(const char *name) {
+    auto it = localNetworkNamespaces.find(name);
+    if (it != localNetworkNamespaces.end()) {
+        auto it = localNetworkNamespaces.find(name);
+        close(it->second);
+        localNetworkNamespaces.erase(it);
+    }
+    else {
+        std::string path = std::string("/var/run/netns/") + name;
+        if (umount2(path.c_str(), MNT_DETACH) != 0)
+            throw cRuntimeError("Cannot umount file: %s", path.c_str());
+        if (unlink(path.c_str()) != 0)
+            throw cRuntimeError("Cannot unlink file: %s", path.c_str());
+    }
+}
+
+NetworkNamespaceContext::NetworkNamespaceContext(const char *name)
+{
+    this->name = name;
+    if (!opp_isempty(name)) {
 #ifdef __linux__
-        oldNs = open("/proc/self/ns/net", O_RDONLY);
-        std::string namespaceAsString = "/var/run/netns/";
-        namespaceAsString += networkNamespace;
-        newNs = open(namespaceAsString.c_str(), O_RDONLY);
-        if (newNs == -1)
-            throw cRuntimeError("Cannot open network namespace: errno=%d (%s)", errno, strerror(errno));
-        if (setns(newNs, 0) == -1)
-            throw cRuntimeError("Cannot change network namespace: errno=%d (%s)", errno, strerror(errno));
+        auto it = localNetworkNamespaces.find(name);
+        if (it != localNetworkNamespaces.end()) {
+            oldFd = UnsharedNamespaceInitializer::singleton.originalNetworkNamespaceFd;
+            newFd = it->second;
+            global = false;
+        }
+        else {
+            oldFd = open("/proc/self/ns/net", O_RDONLY);
+            if (oldFd == -1)
+                throw cRuntimeError("Cannot open current network namespace: errno=%d (%s)", errno, strerror(errno));
+            std::string path = std::string("/var/run/netns/") + name;
+            newFd = open(path.c_str(), O_RDONLY);
+            if (newFd == -1)
+                throw cRuntimeError("Cannot open network namespace: %s, errno=%d (%s)", name, errno, strerror(errno));
+            global = true;
+        }
+        EV_INFO << "Switching to network namespace" << EV_FIELD(name) << EV_FIELD(newFd) << std::endl;
+        if (setns(newFd, 0) != 0)
+            throw cRuntimeError("Cannot switch to network namespace: %s, errno=%d (%s)", name, errno, strerror(errno));
 #else
         throw cRuntimeError("Network namespaces are only supported on Linux");
 #endif
@@ -33,13 +106,17 @@ NetworkNamespaceContext::NetworkNamespaceContext(const char *networkNamespace)
 
 NetworkNamespaceContext::~NetworkNamespaceContext()
 {
-    if (newNs != -1) {
+    if (newFd != -1) {
 #ifdef __linux__
-        setns(oldNs, 0);
-        close(oldNs);
-        close(newNs);
-        oldNs = -1;
-        newNs = -1;
+        EV_INFO << "Switching back to network namespace" << EV_FIELD(name) << EV_FIELD(oldFd) << std::endl;
+        if (setns(oldFd, 0) != 0)
+            EV_FATAL << "Cannot switch to network namespace: " << name << ", errno=" << errno << " (" << strerror(errno) << ")" << EV_ENDL;
+        if (global) {
+            close(oldFd);
+            close(newFd);
+        }
+        oldFd = -1;
+        newFd = -1;
 #endif
     }
 }
