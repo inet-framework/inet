85458dff66372e5a1e31f7157be06a6463995b75
Lucas Haug <lucas.haug@ipvs.uni-stuttgart.de>
2024-04-16 22:35:10 +0200

    Changed formatting to match configuration of existing code more closely

--- src/inet/linklayer/ieee8021as/Gptp.h
+++ src/inet/linklayer/ieee8021as/Gptp.h
@@ -22,145 +22,147 @@
 
 namespace inet {
 
-class INET_API Gptp : public ClockUserModuleBase, public cListener {
-  // parameters:
-  ModuleRefByPar<IInterfaceTable> interfaceTable;
+class INET_API Gptp : public ClockUserModuleBase, public cListener
+{
+    // parameters:
+    ModuleRefByPar<IInterfaceTable> interfaceTable;
 
-  // Configuration
-  GptpNodeType gptpNodeType;
-  int domainNumber = -1;
-  int slavePortId = -1;         // interface ID of slave port
-  std::set<int> masterPortIds;  // interface IDs of master ports
-  uint64_t clockIdentity = 0;
-  clocktime_t syncInterval;
-  clocktime_t pdelayInterval;
-  clocktime_t pDelayReqProcessingTime;  // processing time between arrived
-                                        // PDelayReq and send of PDelayResp
+    // Configuration
+    GptpNodeType gptpNodeType;
+    int domainNumber = -1;
+    int slavePortId = -1;        // interface ID of slave port
+    std::set<int> masterPortIds; // interface IDs of master ports
+    uint64_t clockIdentity = 0;
+    clocktime_t syncInterval;
+    clocktime_t pdelayInterval;
+    clocktime_t pDelayReqProcessingTime; // processing time between arrived
+                                         // PDelayReq and send of PDelayResp
 
-  // Rate Ratios
-  double gmRateRatio = 1.0;
-  double receivedRateRatio = 1.0;
-  double neighborRateRatio = 1.0; // the rate ratio to the neighbor
+    // Rate Ratios
+    double gmRateRatio = 1.0;
+    double receivedRateRatio = 1.0;
+    double neighborRateRatio = 1.0; // the rate ratio to the neighbor
 
-  uint16_t sequenceId = 0;
+    uint16_t sequenceId = 0;
 
-  // == Propagation Delay Measurement Procedure ==
-  // Timestamps corresponding to the PDelayRequest and PDelayResponse mechanism
-  clocktime_t pDelayReqEgressTimestamp = -1;  // egress time of pdelay_req at initiator (this node)
-  clocktime_t pDelayReqIngressTimestamp = -1;  // ingress time of pdelay_req at responder
-  clocktime_t pDelayRespEgressTimestamp = -1;  // egress time of pdelay_resp at responder (received in PDelayRespFollowUp)
-  clocktime_t pDelayRespEgressTimestampLast = -1;  // egress time of previous pdelay_resp at responder (received in PDelayRespFollowUp)
-  clocktime_t pDelayRespIngressTimestamp = -1;  // ingress time of pdelay_resp at initiator (this node)
-  clocktime_t pDelayRespIngressTimestampLast = -1;  // ingress time of previous pdelay_resp at initiator (this node)
+    // == Propagation Delay Measurement Procedure ==
+    // Timestamps corresponding to the PDelayRequest and PDelayResponse mechanism
+    clocktime_t pDelayReqEgressTimestamp = -1;       // egress time of pdelay_req at initiator (this node)
+    clocktime_t pDelayReqIngressTimestamp = -1;      // ingress time of pdelay_req at responder
+    clocktime_t pDelayRespEgressTimestamp = -1;      // egress time of pdelay_resp at responder (received in PDelayRespFollowUp)
+    clocktime_t pDelayRespEgressTimestampLast = -1;  // egress time of previous pdelay_resp at responder (received in PDelayRespFollowUp)
+    clocktime_t pDelayRespIngressTimestamp = -1;     // ingress time of pdelay_resp at initiator (this node)
+    clocktime_t pDelayRespIngressTimestampLast = -1; // ingress time of previous pdelay_resp at initiator (this node)
 
-  bool rcvdPdelayResp = false;
-  uint16_t lastSentPdelayReqSequenceId = 0;
+    bool rcvdPdelayResp = false;
+    uint16_t lastSentPdelayReqSequenceId = 0;
 
-  clocktime_t meanLinkDelay = CLOCKTIME_ZERO;  // mean propagation delay between this node and the responder
+    clocktime_t meanLinkDelay = CLOCKTIME_ZERO; // mean propagation delay between this node and the responder
 
+    // == Sync Procedure ==
 
-  // == Sync Procedure ==
+    // Holds the (received) correction field value, used for the next FollowUp message
+    // Unsure why this is also configurable with a parameter (TODO: Check)
+    clocktime_t correctionField = CLOCKTIME_ZERO;
 
-  // Holds the (received) correction field value, used for the next FollowUp message
-  // Unsure why this is also configurable with a parameter (TODO: Check)
-  clocktime_t correctionField = CLOCKTIME_ZERO;
+    clocktime_t preciseOriginTimestamp; // timestamp when the last sync message was generated at the GM
 
-  clocktime_t preciseOriginTimestamp;  // timestamp when the last sync message was generated at the GM
+    clocktime_t syncEgressTimestampMaster = -1;     // egress time of Sync at master
+    clocktime_t syncEgressTimestampMasterLast = -1; // egress time of previous Sync at master
+    clocktime_t syncIngressTimestamp = -1;          // ingress time of Sync at slave (this node)
+    clocktime_t syncIngressTimestampLast = -1;      // ingress time of previous Sync at slave (this node)
 
-  clocktime_t syncEgressTimestampMaster = -1;    // egress time of Sync at master
-  clocktime_t syncEgressTimestampMasterLast = -1;  // egress time of previous Sync at master
-  clocktime_t syncIngressTimestamp = -1;  // ingress time of Sync at slave (this node)
-  clocktime_t syncIngressTimestampLast = -1; // ingress time of previous Sync at slave (this node)
+    bool rcvdGptpSync = false;
+    uint16_t lastReceivedGptpSyncSequenceId = 0xffff;
 
-  bool rcvdGptpSync = false;
-  uint16_t lastReceivedGptpSyncSequenceId = 0xffff;
+    clocktime_t newLocalTimeAtTimeSync;
+    clocktime_t timeDiffAtTimeSync; // new local time - old local time
 
-  clocktime_t newLocalTimeAtTimeSync;
-  clocktime_t timeDiffAtTimeSync;  // new local time - old local time
+    // self timers:
+    ClockEvent *selfMsgSync = nullptr;
+    ClockEvent *selfMsgDelayReq = nullptr;
+    ClockEvent *requestMsg = nullptr;
 
+    // Statistics information: // TODO remove, and replace with emit() calls
+    static simsignal_t localTimeSignal;
+    static simsignal_t timeDifferenceSignal;
+    static simsignal_t rateRatioSignal;
+    static simsignal_t peerDelaySignal;
 
-  // self timers:
-  ClockEvent *selfMsgSync = nullptr;
-  ClockEvent *selfMsgDelayReq = nullptr;
-  ClockEvent *requestMsg = nullptr;
+  public:
+    static const MacAddress GPTP_MULTICAST_ADDRESS;
 
-  // Statistics information: // TODO remove, and replace with emit() calls
-  static simsignal_t localTimeSignal;
-  static simsignal_t timeDifferenceSignal;
-  static simsignal_t rateRatioSignal;
-  static simsignal_t peerDelaySignal;
+  protected:
+    virtual int numInitStages() const
 
- public:
-  static const MacAddress GPTP_MULTICAST_ADDRESS;
+        override
+    {
+        return NUM_INIT_STAGES;
+    }
 
- protected:
-  virtual int numInitStages() const
+    virtual void initialize(int stage)
 
-      override {
-    return NUM_INIT_STAGES;
-  }
+        override;
 
-  virtual void initialize(int stage)
+    virtual void handleMessage(cMessage *msg)
 
-      override;
+        override;
 
-  virtual void handleMessage(cMessage *msg)
+    virtual void handleSelfMessage(cMessage *msg);
 
-      override;
+    void handleDelayOrSendFollowUp(const GptpBase *gptp,
+                                   omnetpp::cComponent *source);
 
-  virtual void handleSelfMessage(cMessage *msg);
+    const GptpBase *extractGptpHeader(Packet *packet);
 
-  void handleDelayOrSendFollowUp(const GptpBase *gptp,
-                                 omnetpp::cComponent *source);
+  public:
+    virtual ~
 
-  const GptpBase *extractGptpHeader(Packet *packet);
+        Gptp();
 
- public:
-  virtual ~
+  protected:
+    void sendPacketToNIC(Packet *packet, int portId);
 
-      Gptp();
+    void sendSync();
 
- protected:
-  void sendPacketToNIC(Packet *packet, int portId);
+    void sendFollowUp(int portId, const GptpSync *sync,
+                      clocktime_t syncEgressTimestampOwn);
 
-  void sendSync();
+    void sendPdelayReq();
 
-  void sendFollowUp(int portId, const GptpSync *sync,
-                    clocktime_t syncEgressTimestampOwn);
+    void sendPdelayResp(GptpReqAnswerEvent *req);
 
-  void sendPdelayReq();
+    void sendPdelayRespFollowUp(int portId, const GptpPdelayResp *resp);
 
-  void sendPdelayResp(GptpReqAnswerEvent *req);
+    void processSync(Packet *packet, const GptpSync *gptp);
 
-  void sendPdelayRespFollowUp(int portId, const GptpPdelayResp *resp);
+    void processFollowUp(Packet *packet, const GptpFollowUp *gptp);
 
-  void processSync(Packet *packet, const GptpSync *gptp);
+    void processPdelayReq(Packet *packet, const GptpPdelayReq *gptp);
 
-  void processFollowUp(Packet *packet, const GptpFollowUp *gptp);
+    void processPdelayResp(Packet *packet, const GptpPdelayResp *gptp);
 
-  void processPdelayReq(Packet *packet, const GptpPdelayReq *gptp);
+    void processPdelayRespFollowUp(Packet *packet,
+                                   const GptpPdelayRespFollowUp *gptp);
 
-  void processPdelayResp(Packet *packet, const GptpPdelayResp *gptp);
+    clocktime_t getCalculatedDrift(IClock *clock, clocktime_t value)
+    {
+        return CLOCKTIME_ZERO;
+    }
 
-  void processPdelayRespFollowUp(Packet *packet,
-                                 const GptpPdelayRespFollowUp *gptp);
+    void synchronize();
 
-  clocktime_t getCalculatedDrift(IClock *clock, clocktime_t value) {
-    return CLOCKTIME_ZERO;
-  }
+    inline void adjustLocalTimestamp(clocktime_t &time)
+    {
+        time += timeDiffAtTimeSync;
+    }
 
-  void synchronize();
+    virtual void receiveSignal(cComponent *source, simsignal_t signal,
+                               cObject *obj, cObject *details)
 
-  inline void adjustLocalTimestamp(clocktime_t &time) {
-    time += timeDiffAtTimeSync;
-  }
-
-  virtual void receiveSignal(cComponent *source, simsignal_t signal,
-                             cObject *obj, cObject *details)
-
-      override;
+        override;
 };
 
-}  // namespace inet
+} // namespace inet
 
 #endif
