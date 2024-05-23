a5cebf5f225864b4c4e46323997c5e32c4f46412
Levente Meszaros <levy@omnetpp.org>
2024-05-23 15:53:57 +0200

    EthernetLayer

---
+++ src/inet/linklayer/ethernet/modular/EthernetLayer.cc
@@ -0,0 +1,42 @@
+//
+// Copyright (C) 2005 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/linklayer/ethernet/modular/EthernetLayer.h"
+
+#include "inet/queueing/contract/IPassivePacketSink.h"
+
+namespace inet {
+
+using namespace inet::queueing;
+
+Define_Module(EthernetLayer);
+
+cGate *EthernetLayer::lookupModuleInterface(cGate *gate, const std::type_info &type, const cObject *arguments, int direction)
+{
+    if (gate->isName("upperLayerIn")) {
+        if (type == typeid(IPassivePacketSink)) {
+            auto dispatchProtocolReq = dynamic_cast<const DispatchProtocolReq *>(arguments);
+            if (dispatchProtocolReq != nullptr && dispatchProtocolReq->getProtocol() == &Protocol::ethernetMac && dispatchProtocolReq->getServicePrimitive() == SP_REQUEST)
+                return findModuleInterface(gate, type, nullptr, 1);
+        }
+    }
+    else if (gate->isName("upperLayerOut"))
+        return findModuleInterface(gate, type, arguments); // forward all other interfaces
+    else if (gate->isName("lowerLayerIn")) {
+        if (type == typeid(IPassivePacketSink)) {
+            auto dispatchProtocolReq = dynamic_cast<const DispatchProtocolReq *>(arguments);
+            if (dispatchProtocolReq != nullptr && dispatchProtocolReq->getProtocol() == &Protocol::ethernetMac && dispatchProtocolReq->getServicePrimitive() == SP_INDICATION)
+                return findModuleInterface(gate, type, nullptr, 1);
+        }
+    }
+    else if (gate->isName("lowerLayerOut"))
+        return findModuleInterface(gate, type, arguments); // forward all other interfaces
+    return nullptr;
+}
+
+} // namespace inet
+
