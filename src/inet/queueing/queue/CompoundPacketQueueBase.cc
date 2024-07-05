933d6fd0de43f831945fa109d2338743f2d9991f
Levente Meszaros <levy@omnetpp.org>
2024-07-05 10:23:12 +0200

    CompoundPacketQueueBase: Changed to enqueuing similarly to PacketQueue.

--- src/inet/queueing/queue/CompoundPacketQueueBase.cc
+++ src/inet/queueing/queue/CompoundPacketQueueBase.cc
@@ -57,7 +57,7 @@ void CompoundPacketQueueBase::pushPacket(Packet *packet, const cGate *gate)
     cNamedObject packetPushStartedDetails("atomicOperationStarted");
     emit(packetPushStartedSignal, packet, &packetPushStartedDetails);
     animatePushPacket(packet, inputGate, consumer.getReferencedGate());
-    EV_INFO << "Pushing packet" << EV_FIELD(packet) << EV_ENDL;
+    EV_INFO << "Enqueuing packet" << EV_FIELD(packet) << EV_ENDL;
     consumer.pushPacket(packet);
     if (packetDropperFunction != nullptr) {
         while (isOverloaded()) {
