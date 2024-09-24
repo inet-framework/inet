16670d57a1f28a4661172a154fd6ff578f383acc
Levente Meszaros <levy@omnetpp.org>
2024-09-24 17:27:22 +0200

    common: Added support for spawning child simulation tasks that run concurrently in simulation time.
    For example:
    auto t = new SimulationTask();
    t->spawnChild([] () { printf("Hello"); });
    t->spawnChild([] () { printf("World"); });
    t->joinChildren();

---
+++ src/inet/common/SimulationTask.cc
@@ -0,0 +1,47 @@
+//
+// Copyright (C) 2024 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/common/FunctionalEvent.h"
+#include "inet/common/SimulationTask.h"
+
+namespace inet {
+
+SimulationTask::~SimulationTask()
+{
+    delete joinChildrenContinuation;
+}
+
+bool SimulationTask::isChildrenFinished()
+{
+    return std::all_of(children.begin(), children.end(), [] (SimulationTask *task) { return task->isFinished; });
+}
+
+void SimulationTask::spawnChild(std::function<void()> f)
+{
+    SimulationTask *childTask = new SimulationTask();
+    children.push_back(childTask);
+    schedule("TaskStartEvent", simTime(), [=] () {
+        f();
+        childTask->isFinished = true;
+        if (isChildrenFinished() && joinChildrenContinuation != nullptr)
+            joinChildrenContinuation->resume();
+    });
+}
+
+void SimulationTask::joinChildren()
+{
+    if (!isChildrenFinished()) {
+        joinChildrenContinuation = new SimulationContinuation();
+        joinChildrenContinuation->suspend();
+    }
+    for (auto childTask : children)
+        delete childTask;
+    children.clear();
+}
+
+} // namespace inet
+
