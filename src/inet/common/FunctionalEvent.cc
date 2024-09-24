58060a97695d4ce0278d8e5c7ddfc0f80dfa0019
Levente Meszaros <levy@omnetpp.org>
2024-09-24 17:27:22 +0200

    common: Added global scheduleAt() and scheduleAfter() functions that execute a lambda function at a specific simulation time.
    For example:
    scheduleAt("mytask", simTime() + 0.1, [] () { std::cout << "Doing my task" << std::endl; });
    scheduleAfter("mytask", 0.1, [] () { std::cout << "Doing my task" << std::endl; });

---
+++ src/inet/common/FunctionalEvent.cc
@@ -0,0 +1,38 @@
+//
+// Copyright (C) 2024 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/common/FunctionalEvent.h"
+
+namespace inet {
+
+void scheduleAt(const char *name, simtime_t time, std::function<void()> f)
+{
+    if (time < simTime())
+        throw cRuntimeError("Cannot schedule function to the past");
+    auto event = new FunctionalEvent(name, f);
+    event->setArrivalTime(time);
+    getSimulation()->insertEvent(event);
+}
+
+void scheduleAfter(const char *name, simtime_t delay, std::function<void()> f)
+{
+    if (delay < 0)
+        throw cRuntimeError("Cannot schedule function to the past");
+    scheduleAt(name, simTime() + delay, f);
+}
+
+void FunctionalEvent::execute()
+{
+    f();
+    // TODO: when f() calls swapcontext it doesn't return, so how should this event be deleted?
+    // TODO: it cannot be deleted before calling f() in such a case because that would corrupt the closure's data
+    // TODO: perhaps we need a list of objects that should be eventually deleted (in the event loop)
+    delete this;
+}
+
+} // namespace inet
+
