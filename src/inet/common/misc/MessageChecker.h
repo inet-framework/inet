ed0f3a33314eb99c753ada3ce7ca8ee2e2047312
Levente Meszaros <levy@omnetpp.org>
2024-09-26 11:31:39 +0200

    MessageChecker: todo

--- src/inet/common/misc/MessageChecker.h
+++ src/inet/common/misc/MessageChecker.h
@@ -7,16 +7,28 @@
 #ifndef __INET_MESSAGECHECKER_H
 #define __INET_MESSAGECHECKER_H
 
-#include "inet/common/INETDefs.h"
+#include "inet/common/IModuleInterfaceLookup.h"
+#include "inet/queueing/common/PassivePacketSinkRef.h"
 
 namespace inet {
 
+using namespace inet::queueing;
+
 #define BUFSIZE    4096
 
-class INET_API MessageChecker : public cSimpleModule
+class INET_API MessageChecker : public cSimpleModule, public IPassivePacketSink, public IModuleInterfaceLookup
 {
   public:
     MessageChecker();
+
+    virtual bool canPushSomePacket(const cGate *gate) const override { return gate->isName("in"); }
+    virtual bool canPushPacket(Packet *packet, const cGate *gate) const override { return gate->isName("in"); }
+    virtual void pushPacket(Packet *packet, const cGate *gate) override;
+    virtual void pushPacketStart(Packet *packet, const cGate *gate, bps datarate) override { throw cRuntimeError("TODO"); }
+    virtual void pushPacketEnd(Packet *packet, const cGate *gate) override { throw cRuntimeError("TODO"); }
+    virtual void pushPacketProgress(Packet *packet, const cGate *gate, bps datarate, b position, b extraProcessableLength = b(0)) override { throw cRuntimeError("TODO"); }
+
+    virtual cGate *lookupModuleInterface(cGate *gate, const std::type_info& type, const cObject *arguments, int direction) override;
 
   protected:
     void initialize() override;
@@ -36,6 +48,7 @@ class INET_API MessageChecker : public cSimpleModule
     void checkFieldType(any_ptr object, cClassDescriptor *descriptor, int field, cXMLAttributeMap& attrList, int i = 0) const;
     int findFieldIndex(any_ptr object, cClassDescriptor *descriptor, const std::string& fieldName) const;
 
+    PassivePacketSinkRef outSink;
     cXMLElementList m_checkingInfo; // List of checking information
     cXMLElementList::iterator m_iterChk; // Interator of the list of chacking information
     unsigned forwardedMsg; // Number of received and forwarded messages
