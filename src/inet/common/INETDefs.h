299257609c5a94a6bfce88d7a1089dbf33cf7d24
Rudolf Hornig <rudi@omnetpp.org>
2024-10-10 21:22:33 +0200

    prod: version umped to 4.5.4

--- src/inet/common/INETDefs.h
+++ src/inet/common/INETDefs.h
@@ -34,7 +34,7 @@ using namespace omnetpp;
 #endif
 
 #define INET_VERSION        0x0405
-#define INET_PATCH_LEVEL    0x03
+#define INET_PATCH_LEVEL    0x04
 
 #if defined(INET_EXPORT)
 #define INET_API          OPP_DLLEXPORT
