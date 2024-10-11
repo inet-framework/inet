8717e19d282827fe9df26e6ed83ead9c32eabad3
Levente Meszaros <levy@omnetpp.org>
2024-10-11 14:16:44 +0200

    comments

--- src/inet/linklayer/ethernet/basic/EthernetMac.cc
+++ src/inet/linklayer/ethernet/basic/EthernetMac.cc
@@ -213,7 +213,7 @@ void EthernetMac::processMsgFromNetwork(Signal *signal)
     bool hasBitError = signal->hasBitError();
     auto packet = check_and_cast<Packet *>(signal->decapsulate());
     delete signal;
-    if (!hasBitError)
+    if (!hasBitError) // needed to run examples/ospfv2/simpletest/ -f omnetpp.ini -c CrashAndReboot -r 0 EthernetMac Ipv4 ... : PASS
         decapsulate(packet);
     emit(packetReceivedFromLowerSignal, packet);
 
