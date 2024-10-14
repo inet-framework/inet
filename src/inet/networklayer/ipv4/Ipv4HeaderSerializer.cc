inet-framework/inet

commit ccdc34a50f3a7fa5b87471a9d08dfd572f699304

all: Fixed 'warning: variable length arrays in C++ are a Clang extension'.

src/inet/networklayer/ipv4/Ipv4HeaderSerializer.cc
@@ -152,7 +152,7 @@ const Ptr<Chunk> Ipv4HeaderSerializer::deserialize(MemoryInputStream& stream) co
 {
     auto position = stream.getPosition();
     B bufsize = stream.getRemainingLength();
-    uint8_t buffer[B(IPv4_MIN_HEADER_LENGTH).get()];
+    uint8_t *buffer = new uint8_t[B(IPv4_MIN_HEADER_LENGTH).get()];
     stream.readBytes(buffer, IPv4_MIN_HEADER_LENGTH);
     auto ipv4Header = makeShared<Ipv4Header>();
     const struct ip& iphdr = *static_cast<const struct ip *>((void *)&buffer);
@@ -198,7 +198,7 @@ const Ptr<Chunk> Ipv4HeaderSerializer::deserialize(MemoryInputStream& stream) co
 
     ipv4Header->setCrc(ntohs(iphdr.ip_sum));
     ipv4Header->setCrcMode(CRC_COMPUTED);
-
+    delete [] buffer;
     return ipv4Header;
 }
 

src/inet/transportlayer/sctp/SctpAssociationUtil.cc
@@ -1437,7 +1437,8 @@ inline static bool writeCompressedValue(uint8_t *outputBuffer,
 
 static uint32_t compressGaps(const SctpGapList *gapList, const SctpGapList::GapType type, size_t& space)
 {
-    uint8_t compressedDataBuffer[1 + 6 * gapList->getNumGaps(type)]; // Worst-case size
+    size_t compressedDataBufferSize = 1 + 6 * gapList->getNumGaps(type);
+    uint8_t *compressedDataBuffer = new uint8_t[compressedDataBufferSize]; // Worst-case size
 
     size_t outputPos = 0;
     unsigned int entriesWritten = 0;
@@ -1448,8 +1449,8 @@ static uint32_t compressGaps(const SctpGapList *gapList, const SctpGapList::GapT
         const uint16_t startOffset = gapList->getGapStart(type, i) - last;
         const uint16_t stopOffset = gapList->getGapStop(type, i) - gapList->getGapStart(type, i);
         const size_t lastOutputPos = outputPos;
-        if ((writeCompressedValue((uint8_t *)&compressedDataBuffer, sizeof(compressedDataBuffer), outputPos, startOffset) == false) ||
-            (writeCompressedValue((uint8_t *)&compressedDataBuffer, sizeof(compressedDataBuffer), outputPos, stopOffset) == false) ||
+        if ((writeCompressedValue((uint8_t *)&compressedDataBuffer, compressedDataBufferSize, outputPos, startOffset) == false) ||
+            (writeCompressedValue((uint8_t *)&compressedDataBuffer, compressedDataBufferSize, outputPos, stopOffset) == false) ||
             (outputPos + 1 > space))
         {
             outputPos = lastOutputPos;
@@ -1458,9 +1459,10 @@ static uint32_t compressGaps(const SctpGapList *gapList, const SctpGapList::GapT
         entriesWritten++;
         last = gapList->getGapStop(type, i);
     }
-    ASSERT(writeCompressedValue((uint8_t *)&compressedDataBuffer, sizeof(compressedDataBuffer), outputPos, 0x00) == true);
+    ASSERT(writeCompressedValue((uint8_t *)&compressedDataBuffer, compressedDataBufferSize, outputPos, 0x00) == true);
     space = outputPos;
 
+    delete [] compressedDataBuffer;
     return entriesWritten;
 }
 

src/inet/transportlayer/sctp/SctpHeaderSerializer.cc
@@ -1124,7 +1124,7 @@ const Ptr<Chunk> SctpHeaderSerializer::deserialize(MemoryInputStream& stream) co
 
 //    auto position = stream.getPosition();
     int bufsize = B(stream.getRemainingLength()).get();
-    uint8_t buffer[bufsize];
+    uint8_t *buffer = new uint8_t[bufsize];
     stream.readBytes(buffer, B(bufsize));
     auto dest = makeShared<SctpHeader>();
 
@@ -1156,6 +1156,7 @@ const Ptr<Chunk> SctpHeaderSerializer::deserialize(MemoryInputStream& stream) co
         int32_t chunkType = chunk->type;
         woPadding = ntohs(chunk->length);
         if (woPadding == 0) {
+            delete [] buffer;
             return dest;
         }
         cLen = ADD_PADDING(woPadding);
@@ -2147,6 +2148,7 @@ const Ptr<Chunk> SctpHeaderSerializer::deserialize(MemoryInputStream& stream) co
         chunkPtr += cLen;
     } // end of while()
     EV_INFO << "SctpSerializer - pkt info - " << B(dest->getChunkLength()).get() << " bytes" << endl;
+    delete [] buffer;
     return dest;
 }
 

src/inet/transportlayer/tcp_common/TcpHeaderSerializer.cc
@@ -157,7 +157,7 @@ void TcpHeaderSerializer::serializeOption(MemoryOutputStream& stream, const TcpO
 const Ptr<Chunk> TcpHeaderSerializer::deserialize(MemoryInputStream& stream) const
 {
     auto position = stream.getPosition();
-    uint8_t buffer[B(TCP_MIN_HEADER_LENGTH).get()];
+    uint8_t *buffer = new uint8_t[B(TCP_MIN_HEADER_LENGTH).get()];
     stream.readBytes(buffer, TCP_MIN_HEADER_LENGTH);
     auto tcpHeader = makeShared<TcpHeader>();
     const struct tcphdr& tcp = *static_cast<const struct tcphdr *>((void *)&buffer);
@@ -194,6 +194,7 @@ const Ptr<Chunk> TcpHeaderSerializer::deserialize(MemoryInputStream& stream) con
     tcpHeader->setHeaderLength(headerLength);
     tcpHeader->setCrc(ntohs(tcp.th_sum));
     tcpHeader->setCrcMode(CRC_COMPUTED);
+    delete [] buffer;
     return tcpHeader;
 }
 

src/inet/applications/netperfmeter/NetPerfMeter.cc
@@ -1032,7 +1032,7 @@ void NetPerfMeter::sendDataOfSaturatedStreams(const unsigned long long bytesAvai
     if (SendingAllowed) {
         // ====== SCTP tells current queue occupation for each stream =========
         unsigned long long contingent;
-        unsigned long long queued[ActualOutboundStreams];
+        unsigned long long *queued = new unsigned long long[ActualOutboundStreams];
         if (sendQueueAbatedIndication == nullptr) {
             // At the moment, the actual queue size is unknown.
             // => Assume it to be bytesAvailableInQueue.
@@ -1075,6 +1075,7 @@ void NetPerfMeter::sendDataOfSaturatedStreams(const unsigned long long bytesAvai
                 LastStreamID = (LastStreamID + 1) % ActualOutboundStreams;
             } while (LastStreamID != startStreamID);
         } while ((newlyQueuedBytes < bytesAvailableInQueue) && (progress == true));
+        delete [] queued;
     }
 }
 

src/inet/applications/sctpapp/SctpNatPeer.cc
@@ -354,7 +354,7 @@ void SctpNatPeer::handleMessage(cMessage *msg)
                 if (rendezvous) {
                     const auto& smsg = message->peekDataAsBytes();
                     int bufferlen = B(smsg->getChunkLength()).get();
-                    uint8_t buffer[bufferlen];
+                    uint8_t *buffer = new uint8_t[bufferlen];
                     std::vector<uint8_t> vec = smsg->getBytes();
                     for (int i = 0; i < bufferlen; i++) {
                         buffer[i] = vec[i];
@@ -372,6 +372,7 @@ void SctpNatPeer::handleMessage(cMessage *msg)
                     }
                     peerPort = nat->portPeer2;
                     delete msg;
+                    delete [] buffer;
                 }
                 else {
                     auto j = rcvdBytesPerAssoc.find(id);

src/inet/applications/sctpapp/SctpNatServer.cc
@@ -266,7 +266,7 @@ void SctpNatServer::handleMessage(cMessage *msg)
                 id = ind->getSocketId();
                 const auto& smsg = message->peekDataAsBytes();
                 int bufferlen = B(smsg->getChunkLength()).get();
-                uint8_t buffer[bufferlen];
+                uint8_t *buffer = new uint8_t[bufferlen];
                 std::vector<uint8_t> vec = smsg->getBytes();
                 for (int i = 0; i < bufferlen; i++) {
                     buffer[i] = vec[i];
@@ -358,6 +358,7 @@ void SctpNatServer::handleMessage(cMessage *msg)
                 printNatVector();
 
                 delete msg;
+                delete [] buffer;
                 break;
             }
 

src/inet/applications/voipstream/VoipStreamReceiver.cc
@@ -119,9 +119,10 @@ void VoipStreamReceiver::Connection::writeLostSamples(int sampleCount)
 {
     int pktBytes = sampleCount * av_get_bytes_per_sample(decCtx->sample_fmt);
     if (outFile.isOpen()) {
-        uint8_t decBuf[pktBytes];
+        uint8_t *decBuf = new uint8_t[pktBytes];
         memset(decBuf, 0, pktBytes);
         outFile.write(decBuf, pktBytes);
+        delete [] decBuf;
     }
 }
 

commit 42f894451e8e11d8677831174db8f74645be4106

all: Fixed incorrect dereference introduced with ccdc34a50f3a7fa5b87471a9d08dfd572f699304.

src/inet/networklayer/ipv4/Ipv4HeaderSerializer.cc
@@ -155,7 +155,7 @@ const Ptr<Chunk> Ipv4HeaderSerializer::deserialize(MemoryInputStream& stream) co
     uint8_t *buffer = new uint8_t[B(IPv4_MIN_HEADER_LENGTH).get()];
     stream.readBytes(buffer, IPv4_MIN_HEADER_LENGTH);
     auto ipv4Header = makeShared<Ipv4Header>();
-    const struct ip& iphdr = *static_cast<const struct ip *>((void *)&buffer);
+    const struct ip& iphdr = *static_cast<const struct ip *>((void *)buffer);
     B totalLength, headerLength;
 
     ipv4Header->setVersion(iphdr.ip_v);

src/inet/transportlayer/sctp/SctpAssociationUtil.cc
@@ -1449,8 +1449,8 @@ static uint32_t compressGaps(const SctpGapList *gapList, const SctpGapList::GapT
         const uint16_t startOffset = gapList->getGapStart(type, i) - last;
         const uint16_t stopOffset = gapList->getGapStop(type, i) - gapList->getGapStart(type, i);
         const size_t lastOutputPos = outputPos;
-        if ((writeCompressedValue((uint8_t *)&compressedDataBuffer, compressedDataBufferSize, outputPos, startOffset) == false) ||
-            (writeCompressedValue((uint8_t *)&compressedDataBuffer, compressedDataBufferSize, outputPos, stopOffset) == false) ||
+        if ((writeCompressedValue(compressedDataBuffer, compressedDataBufferSize, outputPos, startOffset) == false) ||
+            (writeCompressedValue(compressedDataBuffer, compressedDataBufferSize, outputPos, stopOffset) == false) ||
             (outputPos + 1 > space))
         {
             outputPos = lastOutputPos;
@@ -1459,7 +1459,7 @@ static uint32_t compressGaps(const SctpGapList *gapList, const SctpGapList::GapT
         entriesWritten++;
         last = gapList->getGapStop(type, i);
     }
-    ASSERT(writeCompressedValue((uint8_t *)&compressedDataBuffer, compressedDataBufferSize, outputPos, 0x00) == true);
+    ASSERT(writeCompressedValue(compressedDataBuffer, compressedDataBufferSize, outputPos, 0x00) == true);
     space = outputPos;
 
     delete [] compressedDataBuffer;

src/inet/transportlayer/sctp/SctpHeaderSerializer.cc
@@ -1128,7 +1128,7 @@ const Ptr<Chunk> SctpHeaderSerializer::deserialize(MemoryInputStream& stream) co
     stream.readBytes(buffer, B(bufsize));
     auto dest = makeShared<SctpHeader>();
 
-    struct common_header *common_header = (struct common_header *)((void *)&buffer);
+    struct common_header *common_header = (struct common_header *)((void *)buffer);
     int32_t tempChecksum = common_header->checksum;
     common_header->checksum = 0;
     int32_t chksum = SctpChecksum::checksum((unsigned char *)common_header, bufsize);

src/inet/transportlayer/tcp_common/TcpHeaderSerializer.cc
@@ -160,7 +160,7 @@ const Ptr<Chunk> TcpHeaderSerializer::deserialize(MemoryInputStream& stream) con
     uint8_t *buffer = new uint8_t[B(TCP_MIN_HEADER_LENGTH).get()];
     stream.readBytes(buffer, TCP_MIN_HEADER_LENGTH);
     auto tcpHeader = makeShared<TcpHeader>();
-    const struct tcphdr& tcp = *static_cast<const struct tcphdr *>((void *)&buffer);
+    const struct tcphdr& tcp = *static_cast<const struct tcphdr *>((void *)buffer);
     ASSERT(B(sizeof(tcp)) == TCP_MIN_HEADER_LENGTH);
 
     // fill Tcp header structure