ab9cb37d10d896f18320499008582c0c0e6685cd
Levente Meszaros <levy@omnetpp.org>
2024-10-14 17:33:34 +0200

    python: Refactored opp tests to avoid using the runtest shell script.
    This allows the parallel build and execution of individual test cases,
    because each one will have its own executable.

--- python/inet/test/opp.py
+++ python/inet/test/opp.py
@@ -10,7 +10,7 @@ from inet.test.task import *
 logger = logging.getLogger(__name__)
 
 class OppTestTask(TestTask):
-    def __init__(self, simulation_project, working_directory, test_file_name, **kwargs):
+    def __init__(self, simulation_project, working_directory, test_file_name, mode="debug", **kwargs):
         super().__init__(**kwargs)
         self.locals = locals()
         self.locals.pop("self")
@@ -18,21 +18,39 @@ class OppTestTask(TestTask):
         self.simulation_project = simulation_project
         self.working_directory = working_directory
         self.test_file_name = test_file_name
+        self.mode = mode
 
     def get_parameters_string(self, **kwargs):
         return self.test_file_name
 
     def run_protected(self, capture_output=True, **kwargs):
-        executable = "sh"
-        args = [executable, "runtest", self.test_file_name]
+        binary_suffix = "_dbg" if self.mode == "debug" else ""
+        test_binary_name = re.sub(r"\.test", "", self.test_file_name)
+        test_directory = os.path.join(self.working_directory, f"work/{test_binary_name}")
+        has_lib = os.path.exists(os.path.join(self.working_directory, "lib"))
+        os.makedirs(test_directory, exist_ok=True)
+        args = ["opp_test", "gen", "-v", self.test_file_name]
+        subprocess_result = subprocess.run(args, cwd=self.working_directory, capture_output=capture_output, env=self.simulation_project.get_env())
+        if subprocess_result.returncode != 0:
+            return self.task_result_class(self, result="ERROR", stderr=subprocess_result.stderr.decode("utf-8"))
+        args = ["opp_makemake", "-f", "--deep", f"-lINET{binary_suffix}", "-L../../../../src", *([f"-ltest{binary_suffix}", "-L../../lib"] if has_lib else []), "-P", test_directory, "-I../../../../src", "-I../../lib"]
+        subprocess_result = subprocess.run(args, cwd=test_directory, capture_output=capture_output, env=self.simulation_project.get_env())
+        if subprocess_result.returncode != 0:
+            return self.task_result_class(self, result="ERROR", stderr=subprocess_result.stderr.decode("utf-8"))
+        args = ["make", f"MODE={self.mode}"]
+        subprocess_result = subprocess.run(args, cwd=test_directory, capture_output=capture_output, env=self.simulation_project.get_env())
+        if subprocess_result.returncode != 0:
+            return self.task_result_class(self, result="ERROR", stderr=subprocess_result.stderr.decode("utf-8"))
+        args = ["opp_test", "run", "-v", "-p", f"{test_binary_name}/{test_binary_name}{binary_suffix}", self.test_file_name, "-a", "--check-signals=false", "-lINET", "-n", f"../../../../src:.:{'../../lib' if has_lib else ''}"]
         subprocess_result = subprocess.run(args, cwd=self.working_directory, capture_output=capture_output, env=self.simulation_project.get_env())
         stdout = subprocess_result.stdout.decode("utf-8")
         stderr = subprocess_result.stderr.decode("utf-8")
         match = re.search(r"Aggregate result: (\w+)", stdout)
-        if subprocess_result.returncode == signal.SIGINT.value or subprocess_result.returncode == -signal.SIGINT.value:
+        if match:
+            result = match.group(1)
+            return self.task_result_class(self, result=match.group(1), stdout=stdout, stderr=stderr)
+        elif subprocess_result.returncode == signal.SIGINT.value or subprocess_result.returncode == -signal.SIGINT.value:
             return self.task_result_class(self, result="CANCEL", reason="Cancel by user")
-        elif match and subprocess_result.returncode == 0:
-            return self.task_result_class(self, result=match.group(1))
         else:
             return self.task_result_class(self, result="FAIL", reason=f"Non-zero exit code: {subprocess_result.returncode}", stdout=stdout, stderr=stderr)
 
