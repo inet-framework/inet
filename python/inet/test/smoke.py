19a9eecd98a400e687dc92430146319c082e0b54
Levente Meszaros <levy@omnetpp.org>
2024-10-22 14:45:40 +0200

    python: Changed to run smoke tests only for run 0 by default.

--- python/inet/test/smoke.py
+++ python/inet/test/smoke.py
@@ -13,8 +13,8 @@ class SmokeTestTask(SimulationTestTask):
         # TODO check simulation_task_result.elapsed_cpu_time
         return super().check_simulation_task_result(simulation_task_result, **kwargs)
 
-def get_smoke_test_tasks(cpu_time_limit="1s", **kwargs):
-    return get_simulation_test_tasks(cpu_time_limit=cpu_time_limit, name="smoke test", simulation_test_task_class=SmokeTestTask, **kwargs)
+def get_smoke_test_tasks(cpu_time_limit="1s", run=0, **kwargs):
+    return get_simulation_test_tasks(cpu_time_limit=cpu_time_limit, run=run, name="smoke test", simulation_test_task_class=SmokeTestTask, **kwargs)
 
 def run_smoke_tests(**kwargs):
     multiple_test_tasks = get_smoke_test_tasks(**kwargs)
