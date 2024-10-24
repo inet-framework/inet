f22835def51805aa9880d713d5ae5a0ccc5e33c6
Levente Meszaros <levy@omnetpp.org>
2024-10-24 15:25:58 +0200

    python: Added 'make makefiles' to build_project to make sure the Makefiles are up to date.

--- python/inet/simulation/build.py
+++ python/inet/simulation/build.py
@@ -8,6 +8,10 @@ logger = logging.getLogger(__name__)
 
 def build_project(simulation_project, mode="debug", capture_output=True, **kwargs):
     logger.info(f"Building {simulation_project.get_name()} started")
+    args = ["make", "makefiles"]
+    subprocess_result = subprocess.run(args, cwd=simulation_project.get_full_path("."), capture_output=capture_output)
+    if subprocess_result.returncode != 0:
+        raise Exception(f"Build {simulation_project.get_name()} failed")
     args = ["make", "MODE=" + mode, "-j", str(multiprocessing.cpu_count())]
     subprocess_result = subprocess.run(args, cwd=simulation_project.get_full_path("."), capture_output=capture_output)
     if subprocess_result.returncode != 0:
