98c68b75f17343d4bd1e9df48717088c235ed6e7
Levente Meszaros <levy@omnetpp.org>
2024-11-26 16:05:10 +0100

    python: Fixed hotspot symbol resolution in perf.data.
    Previously hotspot incorrectly resolved symbols, mixing up them and making the performance analysis incorrect.

--- python/inet/test/profile.py
+++ python/inet/test/profile.py
@@ -11,7 +11,7 @@ _logger = logging.getLogger(__name__)
 def generate_profile_report(simulation_project=None, output_file="perf.data", **kwargs):
     if simulation_project is None:
         simulation_project = get_default_simulation_project()
-    simulation_task_results = run_simulations(simulation_project=simulation_project, mode="profile", prepend_args=["perf", "record", "-g", "-o", output_file], **kwargs)
+    simulation_task_results = run_simulations(simulation_project=simulation_project, mode="profile", prepend_args=["perf", "record", "-g", "--call-graph", "dwarf", "-o", output_file], **kwargs)
     working_directory = simulation_task_results.results[0].task.simulation_config.working_directory
     return simulation_project.get_relative_path(os.path.join(working_directory, output_file))
 
