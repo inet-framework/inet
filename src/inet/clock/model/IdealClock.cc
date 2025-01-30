3b182f28dcbb57bb638206d650ae8aa6480f703c
Levente Meszaros <levy@omnetpp.org>
2025-01-30 16:42:03 +0100

    IdealClock: Removed overridden methods which already have the implemented behavior.

--- src/inet/clock/model/IdealClock.cc
+++ src/inet/clock/model/IdealClock.cc
@@ -11,19 +11,6 @@ namespace inet {
 
 Define_Module(IdealClock);
 
-void IdealClock::initialize(int stage)
-{
-    ClockBase::initialize(stage);
-    if (stage == INITSTAGE_LOCAL)
-        emit(timeChangedSignal, simTime());
-}
-
-void IdealClock::finish()
-{
-    ClockBase::finish();
-    emit(timeChangedSignal, simTime());
-}
-
 clocktime_t IdealClock::computeClockTimeFromSimTime(simtime_t t) const
 {
     return ClockTime::from(t);
