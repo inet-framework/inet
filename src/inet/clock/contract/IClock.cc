ff6e477ce5a8091db973c8cbe5b751d55a7e29b0
Levente Meszaros <levy@omnetpp.org>
2025-02-27 15:15:34 +0100

    clock: Added macros to check/log the oscillator/clock computation in detail if requested.

---
+++ src/inet/clock/contract/IClock.cc
@@ -0,0 +1,17 @@
+//
+// Copyright (C) 2024 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#include "inet/clock/contract/IClock.h"
+
+namespace inet {
+
+bool clockCoutEnabled = true;
+int clockCoutIndentLevel = 0;
+std::ostringstream devnull;
+
+} // namespace inet
+
