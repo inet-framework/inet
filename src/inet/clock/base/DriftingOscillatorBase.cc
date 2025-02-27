9f8644ef49908cda01a8e5f3ea1e5bbd4ec29c4f
Levente Meszaros <levy@omnetpp.org>
2025-02-27 16:43:40 +0100

    DriftingOscillatorBase: Added several new ASSERTs.
    Added ASSERTs for checking if computeTicksForInterval/computeIntervalForTicks are reverse of each other.

--- src/inet/clock/base/DriftingOscillatorBase.cc
+++ src/inet/clock/base/DriftingOscillatorBase.cc
@@ -36,6 +36,7 @@ void DriftingOscillatorBase::initialize(int stage)
 void DriftingOscillatorBase::setOrigin(simtime_t origin)
 {
     ASSERTCMP(<=, origin, simTime());
+    ASSERTCMP(<=, origin, newOrigin);
     this->origin = origin;
 }
 
@@ -53,6 +54,10 @@ void DriftingOscillatorBase::setDriftRate(ppm newDriftRate)
         nextTickFromOrigin = SimTime::fromRaw((currentTickLength.raw() - elapsedTickTime.raw()) * driftFactor / newDriftFactor);
         driftRate = newDriftRate;
         driftFactor = newDriftFactor;
+        if (driftRate != ppm(0)) {
+            ASSERTCMP(!=, nominalTickLength, currentTickLength);
+            ASSERTCMP(==, nominalTickLength, currentTickLength * driftFactor);
+        }
         setOrigin(currentSimTime);
         emit(driftRateChangedSignal, driftRate.get<ppm>());
         emit(postOscillatorStateChangedSignal, this);
@@ -77,7 +82,6 @@ void DriftingOscillatorBase::setTickOffset(simtime_t newTickOffset)
 
 int64_t DriftingOscillatorBase::doComputeTicksForInterval(simtime_t timeInterval) const
 {
-    ASSERT(timeInterval >= 0);
     int64_t result = floor((timeInterval - nextTickFromOrigin).raw() * driftFactor / nominalTickLength.raw()) + 1;
     return result;
 }
@@ -92,13 +96,19 @@ simtime_t DriftingOscillatorBase::doComputeIntervalForTicks(int64_t numTicks) co
 
 int64_t DriftingOscillatorBase::computeTicksForInterval(simtime_t timeInterval) const
 {
+    ASSERTCMP(>=, timeInterval, 0);
     int64_t result = doComputeTicksForInterval(timeInterval);
+    ASSERTCMP(>=, result, 0);
+    ASSERTCMP(<=, doComputeIntervalForTicks(result), timeInterval);
     return result;
 }
 
 simtime_t DriftingOscillatorBase::computeIntervalForTicks(int64_t numTicks) const
 {
+    ASSERTCMP(>=, numTicks, 0);
     simtime_t result = doComputeIntervalForTicks(numTicks);
+    ASSERTCMP(>=, result, 0);
+    ASSERTCMP(==, doComputeTicksForInterval(result), numTicks);
     return result;
 }
 
