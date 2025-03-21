aaae4d6485c832258a25276ea98754d214da6350
Levente Meszaros <levy@omnetpp.org>
2025-03-21 20:58:14 +0100

    python: Extracted a separate file for fingerprint related abstractions from fingerprint tests.

--- python/inet/test/fingerprint/task.py
+++ python/inet/test/fingerprint/task.py
@@ -38,33 +38,6 @@ def get_ingredients_append_args(ingredients):
     append_args = ["--fingerprintcalculator-class", "inet::FingerprintCalculator"] if "~" in ingredients else []
     return append_args + list(all_fingerprint_ingredients_append_args[ingredients]) if ingredients in all_fingerprint_ingredients_append_args else []
 
-class Fingerprint:
-    def __init__(self, fingerprint, ingredients):
-        self.fingerprint = fingerprint
-        self.ingredients = ingredients
-
-    def __repr__(self):
-        return repr(self)
-    
-    def __str__(self):
-        return self.fingerprint + "/" + self.ingredients
-
-    def __eq__(self, other):
-        return other and self.fingerprint == other.fingerprint and self.ingredients == other.ingredients
-
-    def __ne__(self, other):
-        return not self.__eq__(other)
-
-    def __composite_values__(self):
-        return self.fingerprint, self.ingredients
-
-    @classmethod
-    def parse(self, text):
-        match = re.match(r"(.*)/(.*)", text)
-        fingerprint = match.groups()[0]
-        ingredients = match.groups()[1]
-        return Fingerprint(fingerprint, ingredients)
-
 class FingerprintTestTaskResult(SimulationTestTaskResult):
     def __init__(self, expected_fingerprint=None, calculated_fingerprint=None, **kwargs):
         super().__init__(**kwargs)
@@ -239,37 +212,6 @@ class FingerprintTrajectoryTestTaskResult(TestTaskResult):
     def __init__(self):
         # TODO
         pass
-
-class FingerprintTrajectory:
-    def __init__(self, simulation_result, ingredients, fingerprints, event_numbers):
-        self.simulation_result = simulation_result
-        self.ingredients = ingredients
-        self.fingerprints = fingerprints
-        self.event_numbers = event_numbers
-
-    def get_unique(self):
-        i = 0
-        unique_fingerprints = []
-        event_numbers = []
-        while i < len(self.fingerprints):
-            fingerprint = self.fingerprints[i]
-            j = i
-            while (j < len(self.fingerprints)) and (fingerprint == self.fingerprints[j]):
-                j = j + 1
-            unique_fingerprints.append(fingerprint)
-            event_numbers.append(j - 1)
-            i = j
-        return FingerprintTrajectory(self.simulation_result, self.ingredients, unique_fingerprints, event_numbers)
-
-    def find_divergence_position(self, other):
-        min_size = min(len(self.fingerprints), len(other.fingerprints))
-        for i in range(0, min_size):
-            self_fingerprint = self.fingerprints[i]
-            other_fingerprint = other.fingerprints[i]
-            if self_fingerprint.fingerprint != other_fingerprint.fingerprint:
-                return FingerprintTrajectoryDivergencePosition(SimulationEvent(self.simulation_result.task, self.event_numbers[i]),
-                                                               SimulationEvent(other.simulation_result.task, other.event_numbers[i]))
-        return None
 
 class FingerprintTrajectoryDivergencePosition:
     def __init__(self, simulation_event_1, simulation_event_2):
