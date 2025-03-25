3836b8a6ca2977d2c9d4709c460cdba3a99d9b3d
Levente Meszaros <levy@omnetpp.org>
2025-03-25 09:38:45 +0100

    clock: Added emitNumTicksSignal to OscillatorBase.
    This allows simulating the oscillator using individual events for each tick.
    It makes possible comparing the more efficient implementation to the correct behavior.

--- src/inet/clock/base/DriftingOscillatorBase.cc
+++ src/inet/clock/base/DriftingOscillatorBase.cc
@@ -33,6 +33,18 @@ void DriftingOscillatorBase::initialize(int stage)
     }
 }
 
+void DriftingOscillatorBase::handleTickTimer()
+{
+    setOrigin(simTime());
+    nextTickFromOrigin = getCurrentTickLength();
+    OscillatorBase::handleTickTimer();
+}
+
+void DriftingOscillatorBase::scheduleTickTimer()
+{
+    scheduleAfter(nextTickFromOrigin, tickTimer);
+}
+
 void DriftingOscillatorBase::setOrigin(simtime_t origin)
 {
     ASSERTCMP(<=, origin, simTime());
