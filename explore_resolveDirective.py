10e2e0498453369998093146953ad794c5713223
Rudolf Hornig <rudolf.hornig@gmail.com>
2025-04-01 10:45:57 +0200

    added file name

--- explore_resolveDirective.py
+++ explore_resolveDirective.py
@@ -1,7 +1,3 @@
-import code
-from sys import modules
-
-from proto import module
 from omnetpp.cppindex import CodeBase
 
 codebase = CodeBase()
@@ -26,23 +22,29 @@ def is_module(clazz):
     b = get_all_base_class_names(clazz)
     return "cSimpleModule" in b or "omnetpp::cSimpleModule" in b or "cModule" in b or "omnetpp::cModule" in b
 
-
+def print_clazz_list(clazz_list):
+    for clazz in clazz_list:
+        print(clazz.name, clazz.get_location().file)    
 
 modules = [clazz for clazz in codebase.get_classes() if is_module(clazz)]
-print("Modules:", modules, len(modules))
-print()
+# print("Modules:", modules, len(modules))
+# print()
 
 defines_it = [clazz for clazz in modules if has_method(clazz, "resolveDirective")]
-print("Defines resolveDirective():", defines_it, len(defines_it))
+print("Defines resolveDirective():")
+print_clazz_list(defines_it)
 print()
 
 needs_override = [clazz for clazz in modules
                   if not has_method(clazz, "resolveDirective")
                   and has_inherited_method(clazz, "resolveDirective")]
-print("Supports resolveDirective() but does not override it:", needs_override, len(needs_override))
+print("Supports resolveDirective() but does not override it:")
+print_clazz_list(needs_override)
 print()
 
 no_support = [clazz for clazz in modules
               if not has_method(clazz, "resolveDirective")
               and not has_inherited_method(clazz, "resolveDirective")]
-print("No resolveDirective() in it or base classes:", no_support, len(no_support))
+print("No resolveDirective() in it or base classes:")
+print_clazz_list(no_support)
+print() 
