9c042c4d0e2284804bdac39c2fbc894901d9e28a
Rudolf Hornig <rudolf.hornig@gmail.com>
2025-04-04 15:27:52 +0200

    common: created a new SimpleModule to be used as a base for all simple modules in INET
    - added support for displaying above the module any
      directives (%d) or any parameter or WATCH {name}
      if the module has a
      string displayStringTextFormat @mutable = default("");
      defined.
    - also adds the necessary base implementation of refreshDisplay()

---
+++ src/inet/common/SimpleModule.cc
@@ -0,0 +1,37 @@
+//
+// Copyright (C) 2025 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+#include "inet/common/SimpleModule.h"
+
+namespace inet {
+
+void SimpleModule::refreshDisplay() const
+{
+    if (hasPar("displayStringTextFormat")) {
+        auto displayStringTextFormat = par("displayStringTextFormat").stringValue();
+        if (!opp_isempty(displayStringTextFormat)) {
+            auto text = StringFormat::formatString(displayStringTextFormat, this);
+            getDisplayString().setTagArg("t", 0, text.c_str());
+        }
+    }
+}
+
+std::string SimpleModule::resolveDirective(char directive) const
+{
+    throw cRuntimeError("Unknown directive: %c", directive);
+}
+
+std::string SimpleModule::resolveExpression(const char *expression) const
+{
+    cModule *module = const_cast<SimpleModule*>(this);
+    cObject *obj = module->findObject(expression, false);
+    if (obj)
+        return obj->str();
+    else    
+        throw cRuntimeError("Unknown expression: %s", expression);
+}
+
+} // namespace inet
--- src/inet/common/StringFormat.h
+++ src/inet/common/StringFormat.h
@@ -20,6 +20,7 @@ class INET_API StringFormat
     class INET_API IDirectiveResolver {
       public:
         virtual std::string resolveDirective(char directive) const = 0;
+        virtual std::string resolveExpression(const char *expression) const { return ""; }
     };
 
   protected:
@@ -30,9 +31,10 @@ class INET_API StringFormat
 
     std::string formatString(IDirectiveResolver *resolver) const;
     std::string formatString(std::function<std::string(char)>& resolver) const;
-
+    
     static std::string formatString(const char *format, const IDirectiveResolver *resolver);
     static std::string formatString(const char *format, const std::function<std::string(char)> resolver);
+    static std::string formatString(const char *format, const std::function<std::string(char)> resolver, const std::function<std::string(const char *)> expressionResolver);
 };
 
 } // namespace inet
