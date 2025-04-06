995b39dfb83fbecec7d54e36a1f1ffe8ed59c666
Andras Varga <andras@omnetpp.org>
2025-04-06 21:38:41 +0200

    checksum: ethernetCRC: added overload for std::vector input

--- src/inet/common/checksum/EthernetCRC.h
+++ src/inet/common/checksum/EthernetCRC.h
@@ -16,10 +16,14 @@ extern const uint32_t crc32_tab[];
 
 uint32_t ethernetCRC(const unsigned char *buf, unsigned int bufsize, uint32_t crc = 0);
 
+inline uint32_t ethernetCRC(const std::vector<uint8_t>& vec, uint32_t crc = 0) {
+    return ethernetCRC(vec.data(), vec.size(), crc);
+}
+
 } // namespace inet
 
 /*
- * Ethernet CRC32 polynomials (big- and little-endian verions).
+ * Ethernet CRC32 polynomials (big- and little-endian versions).
  */
 #define ETHER_CRC_POLY_LE    0xedb88320
 #define ETHER_CRC_POLY_BE    0x04c11db6
