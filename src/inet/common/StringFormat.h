6db6c82a9ba2f18c9f1d88709236a3c9387aeece
Rudolf Hornig <rudolf.hornig@gmail.com>
2025-04-14 09:51:38 +0200

    StringFormat: Added support for formatting expressions.
    This will be used, for example, to use named objects such as vars and watches in display string text formats.
    
    Besides single char directives like (%d) names can be specified in {} (like: {name_of_watch})

--- src/inet/common/StringFormat.h
+++ src/inet/common/StringFormat.h
@@ -17,9 +17,10 @@ namespace inet {
 class INET_API StringFormat
 {
   public:
-    class INET_API IDirectiveResolver {
+    class INET_API IResolver {
       public:
         virtual std::string resolveDirective(char directive) const = 0;
+        virtual std::string resolveExpression(const char *expression) const { return ""; } // TODO: make this = 0 when derived classes are updated
     };
 
   protected:
@@ -28,11 +29,15 @@ class INET_API StringFormat
   public:
     void parseFormat(const char *format);
 
-    std::string formatString(IDirectiveResolver *resolver) const;
-    std::string formatString(std::function<std::string(char)>& resolver) const;
+    std::string formatString(const IResolver *resolver) const;
+    std::string formatString(std::function<std::string(char)>& directiveResolver) const;
+    std::string formatString(std::function<std::string(const char*)>& expressionResolver) const;
+    std::string formatString(std::function<std::string(char)>& directiveResolver, std::function<std::string(const char*)>& expressionResolver) const;
 
-    static std::string formatString(const char *format, const IDirectiveResolver *resolver);
-    static std::string formatString(const char *format, const std::function<std::string(char)> resolver);
+    static std::string formatString(const char *format, const IResolver *resolver);
+    static std::string formatString(const char *format, const std::function<std::string(char)> directiveResolver);
+    static std::string formatString(const char *format, const std::function<std::string(const char *)> expressionResolver);
+    static std::string formatString(const char *format, const std::function<std::string(char)> directiveResolver, const std::function<std::string(const char *)> expressionResolver);
 };
 
 } // namespace inet
