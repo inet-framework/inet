e71795b4b8566724192ad9f9a411ce83f55f78e8
Rudolf Hornig <rudolf.hornig@gmail.com>
2025-04-14 09:49:57 +0200

    refactor: added a SimpleModule to be a base class for all modules in INET

---
+++ src/inet/common/SimpleModule.cc
@@ -0,0 +1,50 @@
+//
+// Copyright (C) 2025 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+#include "inet/common/SimpleModule.h"
+
+namespace inet {
+
+Define_Module(SimpleModule);
+
+void SimpleModule::initialize()
+{
+    cSimpleModule::initialize();
+    if (typeid(*this) == typeid(SimpleModule))
+        throw cRuntimeError("%s is missing @class property - modules that extend SimpleModule must have a @class property to specify the C++ implementation class", getComponentType()->getFullName());
+}
+
+void SimpleModule::initialize(int stage)
+{
+    cSimpleModule::initialize(stage);
+}
+
+// NOTE: We intentionally NOT registering SimpleModule here to prevent the user from instantiating it by mistake
+void SimpleModule::refreshDisplay() const
+{
+    auto displayStringTextFormat = par("displayStringTextFormat").stringValue();
+    if (!opp_isempty(displayStringTextFormat)) {
+        auto text = StringFormat::formatString(displayStringTextFormat, this);
+        getDisplayString().setTagArg("t", 0, text.c_str());
+    }
+}
+
+std::string SimpleModule::resolveDirective(char directive) const
+{
+    throw cRuntimeError("Unknown directive in 'displayStringTextFormat' parameter: %c", directive);
+}
+
+std::string SimpleModule::resolveExpression(const char *expression) const
+{
+    cModule *module = const_cast<SimpleModule*>(this);
+    cObject *obj = module->findObject(expression, false);
+    if (obj)
+        return obj->str();
+    else    
+        throw cRuntimeError("Unknown expression in 'displayStringTextFormat' parameter: %s", expression);
+}
+
+} // namespace inet
---
+++ src/inet/common/SimpleModule.h
@@ -0,0 +1,41 @@
+//
+// Copyright (C) 2025 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+#ifndef __INET_SIMPLEMODULE_H
+#define __INET_SIMPLEMODULE_H
+
+#include "inet/common/INETDefs.h"
+#include "inet/common/StringFormat.h"
+
+namespace inet {
+
+/**
+ * A base module for all INET simple modules that implements
+ * behavior common to all modules.
+ *
+ * Derived classes can implement resolveDirective() to support
+ * %c style directives that can be used in displayStringTextFormat.
+ *
+ * refreshDisplay() implements displaying the displayStringTextFormat
+ * parameter above the module in the t tag.
+ *
+ */
+class INET_API SimpleModule : public cSimpleModule, public StringFormat::IDirectiveResolver
+{
+  protected:
+    virtual void refreshDisplay() const override;
+
+  public:
+    virtual void initialize() override;
+    virtual void initialize(int stage) override;
+  // Implementation of StringFormat::IDirectiveResolver interface
+    virtual std::string resolveDirective(char directive) const override;
+    virtual std::string resolveExpression(const char *expression) const override;
+};
+
+} // namespace inet
+
+#endif // __INET_SIMPLEMODULE_H
