5b8be377a879173a033f12579410e1b1d9181350
Levente Meszaros <levy@omnetpp.org>
2025-06-27 10:47:18 +0200

    IInterfaceRegistrationListener: Fixed propagating interface registration if the connected module is the same for both the output and input gate.
    In a special bridge module the incoming direction was connected without using
    intermediate modules while the outgoing direction was connected with multiple
    intermediate modules. This caused propagating the interface registration with
    incorrect gates.

--- src/inet/common/IInterfaceRegistrationListener.cc
+++ src/inet/common/IInterfaceRegistrationListener.cc
@@ -14,9 +14,10 @@ void registerInterface(const NetworkInterface& interface, cGate *in, cGate *out)
     EV_INFO << "Registering network interface" << EV_FIELD(interface) << EV_FIELD(in) << EV_FIELD(out) << EV_ENDL;
     auto outPathEnd = out->getPathEndGate();
     auto inPathStart = in->getPathStartGate();
-    IInterfaceRegistrationListener *interfaceRegistration = dynamic_cast<IInterfaceRegistrationListener *>(outPathEnd->getOwner());
-    if (interfaceRegistration != nullptr)
-        interfaceRegistration->handleRegisterInterface(interface, inPathStart, outPathEnd);
+    IInterfaceRegistrationListener *outInterfaceRegistration = dynamic_cast<IInterfaceRegistrationListener *>(outPathEnd->getOwner());
+    IInterfaceRegistrationListener *inInterfaceRegistration = dynamic_cast<IInterfaceRegistrationListener *>(inPathStart->getOwner());
+    if (outInterfaceRegistration != nullptr && inInterfaceRegistration != nullptr && outInterfaceRegistration == inInterfaceRegistration)
+        outInterfaceRegistration->handleRegisterInterface(interface, inPathStart, outPathEnd);
 }
 
 } // namespace inet
