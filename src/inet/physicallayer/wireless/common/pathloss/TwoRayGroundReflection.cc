c9a5551b3831d76a3cb85c32b9f058feda983b3a
Rudolf Hornig <rudi@omnetpp.org>
2025-09-05 12:12:35 +0200

    TwoRayGroundReflection: throw error if mandatory <ground> element is missing in physical environment.
    The simulation was crashing if the element was missing.

--- src/inet/physicallayer/wireless/common/pathloss/TwoRayGroundReflection.cc
+++ src/inet/physicallayer/wireless/common/pathloss/TwoRayGroundReflection.cc
@@ -29,6 +29,10 @@ void TwoRayGroundReflection::initialize(int stage)
 {
     if (stage == INITSTAGE_LOCAL) {
         physicalEnvironment = getModuleFromPar<IPhysicalEnvironment>(par("physicalEnvironmentModule"), this);
+        if (physicalEnvironment == nullptr)
+            throw cRuntimeError("Physical environment module '%s' not found.", par("physicalEnvironmentModule").stringValue());
+        if (physicalEnvironment->getGround() == nullptr)
+            throw cRuntimeError("Ground model is missing in the physical environment.");
     }
 }
 
