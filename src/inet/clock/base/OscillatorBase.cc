2655e58bc25ec693c80142164f302b03e25ad821
Levente Meszaros <levy@omnetpp.org>
2025-11-20 16:24:30 +0100

    clock/base: TODO

--- src/inet/clock/base/OscillatorBase.cc
+++ src/inet/clock/base/OscillatorBase.cc
@@ -16,10 +16,17 @@ simsignal_t OscillatorBase::driftRateChangedSignal = cComponent::registerSignal(
 void OscillatorBase::initialize(int stage)
 {
     if (stage == INITSTAGE_LOCAL) {
-        displayStringTextFormat = par("displayStringTextFormat");
         if (par("emitNumTicksSignal")) {
-            tickTimer = new cMessage("TickTimer");
-            scheduleAfter(0, tickTimer);
+            tickTimer = new cMessage("OscillatorTickTimer");
+            tickTimer->setSchedulingPriority(par("oscillatorTickEventSchedulingPriority"));
+        }
+    }
+    else if (stage == INITSTAGE_CLOCK) {
+        if (tickTimer != nullptr) {
+            numTicks = 0;
+            EV_DEBUG << "Initialized oscillator" << EV_FIELD(numTicks) << EV_ENDL;
+            emit(numTicksChangedSignal, numTicks);
+            scheduleTickTimer();
         }
     }
 }
@@ -34,10 +41,10 @@ void OscillatorBase::handleMessage(cMessage *msg)
 
 void OscillatorBase::handleTickTimer()
 {
-    EV_INFO << "Handling tick" << EV_FIELD(numTicks) << EV_ENDL;
+    numTicks++;
+    EV_DEBUG << "Handling oscillator tick event" << EV_FIELD(numTicks) << EV_ENDL;
     emit(numTicksChangedSignal, numTicks);
     scheduleTickTimer();
-    numTicks++;
 }
 
 std::string OscillatorBase::resolveDirective(char directive) const
