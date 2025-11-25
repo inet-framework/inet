862e88063cc9f873c8c0ef1922f5f8501efd3d8e
Levente Meszaros <levy@omnetpp.org>
2025-11-25 11:38:44 +0100

    IdealOscillator: Implemented getNumTicksAtOrigin() method to return the total number of ticks at the oscillator computation origin.

--- src/inet/clock/oscillator/IdealOscillator.h
+++ src/inet/clock/oscillator/IdealOscillator.h
@@ -26,6 +26,7 @@ class INET_API IdealOscillator : public OscillatorBase
   public:
     virtual simtime_t getComputationOrigin() const override { return origin; }
     virtual simtime_t getNominalTickLength() const override { return tickLength; }
+    virtual int64_t getNumTicksAtOrigin() const override { return 0; }
 
     virtual int64_t computeTicksForInterval(simtime_t timeInterval) const override;
     virtual simtime_t computeIntervalForTicks(int64_t numTicks) const override;
