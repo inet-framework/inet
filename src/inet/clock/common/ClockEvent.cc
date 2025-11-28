63f7da55a8b39fabe06dd03ac0509580559ed9a7
Levente Meszaros <levy@omnetpp.org>
2025-11-28 14:33:30 +0100

    ClockEvent::execute(): Added assert for current clock time being equal to the event arrival clock time.

--- src/inet/clock/common/ClockEvent.cc
+++ src/inet/clock/common/ClockEvent.cc
@@ -8,6 +8,7 @@
 #include "inet/clock/common/ClockEvent.h"
 
 #include "inet/clock/contract/IClock.h"
+#include "inet/common/IPrintableObject.h"
 
 namespace inet {
 
@@ -15,8 +16,13 @@ Register_Class(ClockEvent)
 
 void ClockEvent::execute()
 {
-    if (clock != nullptr)
+    if (clock != nullptr) {
+        clocktime_t clockTime = clock->getClockTime();
+        EV_DEBUG << "Executing clock event" << EV_FIELD(clockTime) << EV_FIELD(event, this) << EV_ENDL;
+        // NOTE: IClock interface 2. invariant
+        ASSERT(getArrivalClockTime() == clockTime);
         clock->handleClockEvent(this);
+    }
     else {
         // TODO: this should be part of setArrival if clock is nullptr
         arrivalClockTime = SIMTIME_AS_CLOCKTIME(getArrivalTime());
