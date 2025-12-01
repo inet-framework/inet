91fdeeeb5685615caf770c8b97315976643fdceb
Levente Meszaros <levy@omnetpp.org>
2025-12-01 16:57:00 +0100

    ClockBase::getTargetModule(): Moved to cc file.

--- src/inet/clock/base/ClockBase.cc
+++ src/inet/clock/base/ClockBase.cc
@@ -60,6 +60,14 @@ void ClockBase::checkScheduledClockEvent(const ClockEvent *event) const
     DEBUG_LEAVE();
 }
 
+cSimpleModule *ClockBase::getTargetModule() const
+{
+    cSimpleModule *target = getSimulation()->getContextSimpleModule();
+    if (target == nullptr)
+        throw cRuntimeError("scheduleAt()/cancelEvent() must be called with a simple module in context");
+    return target;
+}
+
 clocktime_t ClockBase::getClockTime() const
 {
     return clockEventTime != -1 ? clockEventTime : computeClockTimeFromSimTime(simTime());
