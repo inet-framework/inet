cc41265322679a5915565b6f4520987882c4635f
Levente Meszaros <levy@omnetpp.org>
2025-12-01 15:52:25 +0100

    OscillatorBase: Added periodic tick timer to numTicksChanged signal if requested.

--- src/inet/clock/base/OscillatorBase.cc
+++ src/inet/clock/base/OscillatorBase.cc
@@ -7,13 +7,43 @@
 
 #include "inet/clock/base/OscillatorBase.h"
 
-namespace inet {
+#include "inet/common/IPrintableObject.h"
 
 simsignal_t OscillatorBase::driftRateChangedSignal = cComponent::registerSignal("driftRateChanged");
+namespace inet {
 
 void OscillatorBase::initialize(int stage)
 {
-    SimpleModule::initialize(stage);
+    if (stage == INITSTAGE_LOCAL) {
+        if (par("emitNumTicksSignal")) {
+            tickTimer = new cMessage("OscillatorTickTimer");
+            tickTimer->setSchedulingPriority(par("oscillatorTickEventSchedulingPriority"));
+        }
+    }
+    else if (stage == INITSTAGE_CLOCK) {
+        if (tickTimer != nullptr) {
+            numTicks = 0;
+            EV_DEBUG << "Initialized oscillator" << EV_FIELD(numTicks) << EV_ENDL;
+            emit(numTicksChangedSignal, numTicks);
+            scheduleTickTimer();
+        }
+    }
+}
+
+void OscillatorBase::handleMessage(cMessage *msg)
+{
+    if (msg == tickTimer)
+        handleTickTimer();
+    else
+        throw cRuntimeError("Unknown message");
+}
+
+void OscillatorBase::handleTickTimer()
+{
+    numTicks++;
+    EV_DEBUG << "Handling oscillator tick event" << EV_FIELD(numTicks) << EV_ENDL;
+    emit(numTicksChangedSignal, numTicks);
+    scheduleTickTimer();
 }
 
 std::string OscillatorBase::resolveDirective(char directive) const
--- src/inet/clock/base/OscillatorBase.h
+++ src/inet/clock/base/OscillatorBase.h
@@ -8,6 +8,7 @@
 #ifndef __INET_OSCILLATORBASE_H
 #define __INET_OSCILLATORBASE_H
 
+#include "inet/clock/contract/IClock.h"
 #include "inet/clock/contract/IOscillator.h"
 #include "inet/common/SimpleModule.h"
 
@@ -17,11 +18,21 @@ class INET_API OscillatorBase : public SimpleModule, public IOscillator
 {
   public:
     static simsignal_t driftRateChangedSignal;
+  protected:
+    cMessage *tickTimer = nullptr;
+
+    uint64_t numTicks = -1;
 
   protected:
     virtual int numInitStages() const override { return NUM_INIT_STAGES; }
     virtual void initialize(int stage) override;
+    virtual void handleMessage(cMessage *msg) override;
+
+    virtual void handleTickTimer();
+    virtual void scheduleTickTimer() = 0;
+
   public:
+    virtual ~OscillatorBase() { cancelAndDelete(tickTimer); }
     virtual std::string resolveDirective(char directive) const override;
 };
 
