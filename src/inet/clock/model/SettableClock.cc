b364ef3c98b48313fec9040e97a3f25173984274
Levente Meszaros <levy@omnetpp.org>
2025-12-18 16:23:39 +0100

    SettableClock: Refactored setClockTime to immediately execute/skip overdue clock events in clock time order.
    Previously, they were scheduled for the same simulation time, but it could terminate the simulation because
    the clock events and the corresponding clock/oscillator was left in inconsistent state. The clock was already
    set to the new clock time even though the overdue clock events were not yet executed.
    When the events were executed they set the clock time back.
    This also fixes #844.

--- src/inet/clock/model/SettableClock.cc
+++ src/inet/clock/model/SettableClock.cc
@@ -74,28 +74,12 @@ OverdueClockEventHandlingMode SettableClock::getOverdueClockEventHandlingMode(Cl
         return mode;
 }
 
-simtime_t SettableClock::handleOverdueClockEvent(ClockEvent *event, simtime_t t)
-{
-    switch (getOverdueClockEventHandlingMode(event)) {
-        case EXECUTE:
-            EV_WARN << "Scheduling overdue clock event " << event->getName() << " to current simulation time.\n";
-            return t;
-        case SKIP:
-            EV_WARN << "Skipping overdue clock event " << event->getName() << ".\n";
-            cancelClockEvent(event);
-            return -1;
-        case ERROR:
-            throw cRuntimeError("Clock event is overdue");
-        default:
-            throw cRuntimeError("Unknown overdue clock event handling mode");
-    }
-}
-
 void SettableClock::setClockTime(clocktime_t newClockTime, ppm oscillatorCompensation, bool resetOscillator)
 {
     Enter_Method("setClockTime");
     clocktime_t oldClockTime = getClockTime();
     if (newClockTime != oldClockTime) {
+        clocktime_t clockDelta = newClockTime - oldClockTime;
         emit(timeChangedSignal, oldClockTime.asSimTime());
         if (resetOscillator) {
             if (auto driftingOscillator = dynamic_cast<DriftingOscillatorBase *>(oscillator))
@@ -103,28 +87,49 @@ void SettableClock::setClockTime(clocktime_t newClockTime, ppm oscillatorCompens
         }
         simtime_t currentSimTime = simTime();
         EV_INFO << "Setting clock time from " << oldClockTime << " to " << newClockTime << " at simtime " << currentSimTime << ".\n";
-        originSimulationTime = simTime();
-        originClockTime = newClockTime;
-        this->oscillatorCompensation = oscillatorCompensation;
-        ASSERT(newClockTime == getClockTime());
-        clocktime_t clockDelta = newClockTime - oldClockTime;
-        std::sort(events.begin(), events.end(), [] (auto &a, auto &b) { return compareClockEvents(b, a); });
-        for (auto event : events) {
+        for (auto event : events)
             if (event->getRelative())
-                // NOTE: the simulation time of event execution is not affected
                 event->setArrivalClockTime(event->getArrivalClockTime() + clockDelta);
-            else {
-                clocktime_t arrivalClockTime = event->getArrivalClockTime();
-                bool isOverdue = arrivalClockTime < newClockTime;
-                simtime_t arrivalSimTime = isOverdue ? -1 : computeSimTimeFromClockTime(arrivalClockTime);
-                if (isOverdue || arrivalSimTime < currentSimTime)
-                    arrivalSimTime = handleOverdueClockEvent(event, currentSimTime);
-                if (event->isScheduled()) {
+        std::make_heap(events.begin(), events.end(), compareClockEvents);
+        while (!events.empty() && events.front()->getArrivalClockTime() < newClockTime)
+        {
+            std::pop_heap(events.begin(), events.end(), compareClockEvents);
+            auto event = events.back();
+            ASSERT(!event->getRelative());
+            events.erase(std::remove(events.begin(), events.end(), event), events.end());
+            switch (getOverdueClockEventHandlingMode(event)) {
+                case EXECUTE: {
+                    EV_WARN << "Executing overdue clock event " << event->getName() << ".\n";
                     cSimpleModule *targetModule = check_and_cast<cSimpleModule *>(event->getArrivalModule());
                     cContextSwitcher contextSwitcher(targetModule);
-                    targetModule->rescheduleAt(arrivalSimTime, event);
+                    this->originSimulationTime = currentSimTime;
+                    this->originClockTime = event->getArrivalClockTime();
+                    targetModule->cancelEvent(event);
+                    event->setClock(nullptr);
+                    event->execute();
+                    break;
                 }
+                case SKIP: {
+                    EV_WARN << "Skipping overdue clock event " << event->getName() << ".\n";
+                    cSimpleModule *targetModule = check_and_cast<cSimpleModule *>(event->getArrivalModule());
+                    cContextSwitcher contextSwitcher(targetModule);
+                    targetModule->cancelEvent(event);
+                    event->setClock(nullptr);
+                    break;
+                }
+                case ERROR:
+                    throw cRuntimeError("Clock event is overdue");
+                default:
+                    throw cRuntimeError("Unknown overdue clock event handling mode");
             }
+        }
+        this->oscillatorCompensation = oscillatorCompensation;
+        this->originSimulationTime = currentSimTime;
+        this->originClockTime = newClockTime;
+        for (auto event : events) {
+            cSimpleModule *targetModule = check_and_cast<cSimpleModule *>(event->getArrivalModule());
+            cContextSwitcher contextSwitcher(targetModule);
+            targetModule->rescheduleAt(computeScheduleTime(event->getArrivalClockTime()), event);
         }
         emit(timeChangedSignal, newClockTime.asSimTime());
     }
--- src/inet/clock/model/SettableClock.h
+++ src/inet/clock/model/SettableClock.h
@@ -24,7 +24,6 @@ class INET_API SettableClock : public OscillatorBasedClock, public IScriptable
     virtual void initialize(int stage) override;
 
     virtual OverdueClockEventHandlingMode getOverdueClockEventHandlingMode(ClockEvent *event) const;
-    virtual simtime_t handleOverdueClockEvent(ClockEvent *event, simtime_t t);
 
     // IScriptable implementation
     virtual void processCommand(const cXMLElement& node) override;
