6d5f2b0cf538a34a25342a1446385188e43bba0f
Levente Meszaros <levy@omnetpp.org>
2026-03-26 14:12:11 +0100

    EthernetPauseCommandProcessor: Replaced send() calls with pushPacket() calls using PassivePacketSinkRef.

--- src/inet/linklayer/ethernet/modular/EthernetPauseCommandProcessor.h
+++ src/inet/linklayer/ethernet/modular/EthernetPauseCommandProcessor.h
@@ -13,14 +13,18 @@
 #include "inet/common/packet/Packet.h"
 #include "inet/linklayer/common/Ieee802Ctrl_m.h"
 #include "inet/linklayer/ethernet/common/EthernetControlFrame_m.h"
+#include "inet/queueing/common/PassivePacketSinkRef.h"
 
 namespace inet {
 
-class INET_API EthernetPauseCommandProcessor : public SimpleModule
+using namespace inet::queueing;
+
+class INET_API EthernetPauseCommandProcessor : public SimpleModule, public IPassivePacketSink
 {
   protected:
     int seqNum = 0;
     static simsignal_t pauseSentSignal;
+    PassivePacketSinkRef outSink;
 
   protected:
     virtual void initialize() override;
@@ -28,6 +32,14 @@ class INET_API EthernetPauseCommandProcessor : public SimpleModule
 
   protected:
     void handleSendPause(Request *msg, Ieee802PauseCommand *etherctrl);
+
+  public:
+    virtual bool canPushSomePacket(const cGate *gate) const override { return true; }
+    virtual bool canPushPacket(Packet *packet, const cGate *gate) const override { return true; }
+    virtual void pushPacket(Packet *packet, const cGate *gate) override;
+    virtual void pushPacketStart(Packet *packet, const cGate *gate, bps datarate) override { throw cRuntimeError("TODO"); }
+    virtual void pushPacketEnd(Packet *packet, const cGate *gate) override { throw cRuntimeError("TODO"); }
+    virtual void pushPacketProgress(Packet *packet, const cGate *gate, bps datarate, b position, b extraProcessableLength = b(0)) override { throw cRuntimeError("TODO"); }
 };
 
 } // namespace inet
