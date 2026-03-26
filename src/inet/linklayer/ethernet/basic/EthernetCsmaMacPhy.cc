5c633f359a7aead91b41897fb79358d29b6f2cb1
Levente Meszaros <levy@omnetpp.org>
2026-03-26 14:22:36 +0100

    EthernetCsmaMacPhy: Replaced send() with upperLayerSink.pushPacket() in processReceivedDataFrame.

--- src/inet/linklayer/ethernet/basic/EthernetCsmaMacPhy.cc
+++ src/inet/linklayer/ethernet/basic/EthernetCsmaMacPhy.cc
@@ -752,7 +752,7 @@ void EthernetCsmaMacPhy::processReceivedDataFrame(Packet *packet)
     emit(packetSentToUpperSignal, packet);
     // pass up to upper layer
     EV_INFO << "Sending " << packet << " to upper layer.\n";
-    send(packet, "upperLayerOut");
+    upperLayerSink.pushPacket(packet);
 }
 
 void EthernetCsmaMacPhy::processReceivedControlFrame(Packet *packet)
