0e51d2d95f8c7dc05b649eecec861a077a1e9fb9
Andras Varga <andras@omnetpp.org>
2026-06-16 16:11:29 +0200

    IPv6: implement MLDv1 host listener join/leave behavior and signal wiring
    - Subscribe to ipv6MulticastGroupJoined/LeftSignal and interface signals in
      initialize() at INITSTAGE_NETWORK_LAYER; replay already-joined groups
    - receiveSignal() dispatches to multicastGroupJoined/Left (join/leave signals)
      and deleteHostInterfaceData (interfaceDeletedSignal)
    - multicastGroupJoined: create HostGroupData, send unsolicited Report, arm
      unsolicited-report timer, enter MLD_HGS_DELAYING_LISTENER (HOST-01)
    - multicastGroupLeft: cancel timer if delaying, send Done to ff02::2 when flag
      set (last reporter), delete HostGroupData (HOST-04)
    - sendReport: builds MldReport (setMulticastAddress, B(24)), dest = group
    - sendDone: builds MldDone (setMulticastAddress, B(24)), dest = ALL_ROUTERS_2
    - startHostTimer / startTimer: arm MLD_HOSTGROUP_TIMER with uniform(0, maxResp)
    - processHostGroupTimer: re-send Report on expiry, set flag, IDLE_LISTENER
    - processMldMessage: dispatch switch for Query/Report/Done (stubs for 03-02)
    - handleStopOperation / handleCrashOperation: drain hostData + unsubscribe
    - HostGroupData / HostInterfaceData ctors/dtors; destructor cleans hostData
    - Add DispatchProtocolInd(&Protocol::mld) in sendToIPv6 for Igmpv2 parity
    - Both MODE=release and MODE=debug build clean; MLD_smoke.test still passes

--- src/inet/networklayer/icmpv6/Mldv1.cc
+++ src/inet/networklayer/icmpv6/Mldv1.cc
@@ -10,6 +10,7 @@
 #include "inet/common/ModuleAccess.h"
 #include "inet/common/Protocol.h"
 #include "inet/common/ProtocolTag_m.h"
+#include "inet/common/Simsignals.h"
 #include "inet/common/checksum/ChecksumMode_m.h"
 #include "inet/common/packet/Message.h"
 #include "inet/common/packet/Packet.h"
@@ -19,11 +20,59 @@
 #include "inet/networklayer/contract/IInterfaceTable.h"
 #include "inet/networklayer/icmpv6/Icmpv6.h"
 #include "inet/networklayer/icmpv6/MldMessage_m.h"
+#include "inet/networklayer/ipv6/Ipv6InterfaceData.h"
 #include "inet/networklayer/ipv6/Ipv6RoutingTable.h"
 
 namespace inet {
 
 Define_Module(Mldv1);
+
+// --- HostGroupData ---
+
+Mldv1::HostGroupData::HostGroupData(Mldv1 *owner, const Ipv6Address& group)
+    : owner(owner), groupAddr(group)
+{
+    ASSERT(owner);
+    ASSERT(groupAddr.isMulticast());
+
+    state = MLD_HGS_NON_LISTENER;
+    flag = false;
+    timer = nullptr;
+}
+
+Mldv1::HostGroupData::~HostGroupData()
+{
+    if (timer) {
+        delete (MldHostTimerContext *)timer->getContextPointer();
+        owner->cancelAndDelete(timer);
+    }
+}
+
+// --- HostInterfaceData ---
+
+Mldv1::HostInterfaceData::HostInterfaceData(Mldv1 *owner)
+    : owner(owner)
+{
+    ASSERT(owner);
+}
+
+Mldv1::HostInterfaceData::~HostInterfaceData()
+{
+    for (auto& elem : groups)
+        delete elem.second;
+}
+
+// --- Destructor ---
+
+Mldv1::~Mldv1()
+{
+    cancelAndDelete(testTimer);
+    while (!hostData.empty())
+        deleteHostInterfaceData(hostData.begin()->first);
+    // Router teardown added in Phase 4
+}
+
+// --- Initialize ---
 
 void Mldv1::initialize(int stage)
 {
@@ -45,8 +94,31 @@ void Mldv1::initialize(int stage)
 
         // Register Protocol::mld so the lp dispatcher delivers MLD ICMPv6 messages to us
         registerProtocol(Protocol::mld, gate("ipOut"), gate("ipIn"));
+
+        // Read NED parameter
+        unsolicitedReportInterval = par("unsolicitedReportInterval");
+
+        // Replay already-joined groups on each interface (e.g. after restart)
+        for (int i = 0; i < ift->getNumInterfaces(); ++i) {
+            NetworkInterface *ie = ift->getInterface(i);
+            if (ie->isMulticast()) {
+                if (auto *ipv6data = ie->findProtocolData<Ipv6InterfaceData>()) {
+                    for (const auto& groupAddr : ipv6data->getJoinedMulticastGroups())
+                        multicastGroupJoined(ie, groupAddr);
+                }
+            }
+        }
+
+        // Subscribe to interface and multicast group signals at the containing node
+        cModule *host = getContainingNode(this);
+        host->subscribe(interfaceCreatedSignal, this);
+        host->subscribe(interfaceDeletedSignal, this);
+        host->subscribe(ipv6MulticastGroupJoinedSignal, this);
+        host->subscribe(ipv6MulticastGroupLeftSignal, this);
     }
 }
+
+// --- handleMessageWhenUp ---
 
 void Mldv1::handleMessageWhenUp(cMessage *msg)
 {
@@ -67,8 +139,15 @@ void Mldv1::handleMessageWhenUp(cMessage *msg)
                 }
             }
         }
-        else
-            throw cRuntimeError("Unexpected self-message: %s", msg->getName());
+        else {
+            switch (msg->getKind()) {
+                case MLD_HOSTGROUP_TIMER:
+                    processHostGroupTimer(msg);
+                    break;
+                default:
+                    throw cRuntimeError("Unknown self-message kind %d: %s", msg->getKind(), msg->getName());
+            }
+        }
     }
     else if (auto packet = dynamic_cast<Packet *>(msg)) {
         if (packet->getTag<PacketProtocolTag>()->getProtocol() == &Protocol::mld) {
@@ -88,13 +167,187 @@ void Mldv1::handleMessageWhenUp(cMessage *msg)
         throw cRuntimeError("Unknown message '%s' (%s) received", msg->getName(), msg->getClassName());
 }
 
+// --- Signal handler ---
+
+void Mldv1::receiveSignal(cComponent *source, simsignal_t signalID, cObject *obj, cObject *details)
+{
+    Enter_Method("%s", cComponent::getSignalName(signalID));
+
+    NetworkInterface *ie;
+    int interfaceId;
+    const Ipv6MulticastGroupInfo *info;
+
+    if (signalID == ipv6MulticastGroupJoinedSignal) {
+        info = check_and_cast<const Ipv6MulticastGroupInfo *>(obj);
+        multicastGroupJoined(info->ie, info->groupAddress);
+    }
+    else if (signalID == ipv6MulticastGroupLeftSignal) {
+        info = check_and_cast<const Ipv6MulticastGroupInfo *>(obj);
+        multicastGroupLeft(info->ie, info->groupAddress);
+    }
+    else if (signalID == interfaceCreatedSignal) {
+        ie = check_and_cast<NetworkInterface *>(obj);
+        if (ie->isMulticast())
+            ; // Phase 4: configureInterface(ie) for router role
+    }
+    else if (signalID == interfaceDeletedSignal) {
+        ie = check_and_cast<NetworkInterface *>(obj);
+        if (ie->isMulticast()) {
+            interfaceId = ie->getInterfaceId();
+            deleteHostInterfaceData(interfaceId);
+        }
+    }
+}
+
+// --- Host join/leave handlers ---
+
+void Mldv1::multicastGroupJoined(NetworkInterface *ie, const Ipv6Address& groupAddr)
+{
+    ASSERT(ie && ie->isMulticast());
+    ASSERT(groupAddr.isMulticast());
+
+    // RFC 2710 §5: "An MLD message is never sent for the link-scope all-nodes address (FF02::1)"
+    if (enabled && groupAddr != Ipv6Address::ALL_NODES_2) {
+        HostGroupData *groupData = createHostGroupData(ie, groupAddr);
+        numGroups++;
+        numHostGroups++;
+
+        sendReport(ie, groupData);
+        groupData->flag = true;
+        startHostTimer(ie, groupData, unsolicitedReportInterval);
+        groupData->state = MLD_HGS_DELAYING_LISTENER;
+    }
+}
+
+void Mldv1::multicastGroupLeft(NetworkInterface *ie, const Ipv6Address& groupAddr)
+{
+    ASSERT(ie && ie->isMulticast());
+    ASSERT(groupAddr.isMulticast());
+
+    if (enabled && groupAddr != Ipv6Address::ALL_NODES_2) {
+        HostGroupData *groupData = getHostGroupData(ie, groupAddr);
+        if (groupData) {
+            if (groupData->state == MLD_HGS_DELAYING_LISTENER)
+                cancelEvent(groupData->timer);
+
+            if (groupData->flag)
+                sendDone(ie, groupData);
+        }
+
+        deleteHostGroupData(ie, groupAddr);
+        numHostGroups--;
+        numGroups--;
+    }
+}
+
+// --- Host timer handlers ---
+
+void Mldv1::processHostGroupTimer(cMessage *msg)
+{
+    MldHostTimerContext *ctx = (MldHostTimerContext *)msg->getContextPointer();
+    EV_DEBUG << "Mldv1: Host Timer expired for group=" << ctx->hostGroup->groupAddr
+             << " iface=" << ctx->ie->getInterfaceName() << "\n";
+    sendReport(ctx->ie, ctx->hostGroup);
+    ctx->hostGroup->flag = true;
+    ctx->hostGroup->state = MLD_HGS_IDLE_LISTENER;
+}
+
+void Mldv1::startTimer(cMessage *timer, double interval)
+{
+    ASSERT(timer);
+    rescheduleAfter(interval, timer);
+}
+
+void Mldv1::startHostTimer(NetworkInterface *ie, HostGroupData *group, double maxRespTime)
+{
+    if (!group->timer) {
+        group->timer = new cMessage("Mldv1 group timer", MLD_HOSTGROUP_TIMER);
+        group->timer->setContextPointer(new MldHostTimerContext(ie, group));
+    }
+
+    double delay = uniform(0.0, maxRespTime);
+    EV_DEBUG << "setting host timer for " << ie->getInterfaceName() << " and group "
+             << group->groupAddr.str() << " to " << delay << "\n";
+    startTimer(group->timer, delay);
+}
+
+// --- Inbound MLD message processing ---
+
 void Mldv1::processMldMessage(Packet *packet)
 {
     const auto& mldMsg = packet->peekAtFront<MldMessage>();
-    EV_INFO << "Received MLD message, type=" << (int)mldMsg->getType() << endl;
-    // Phase 3/4: dispatch to host/router state machines
+    NetworkInterface *ie = ift->getInterfaceById(packet->getTag<InterfaceInd>()->getInterfaceId());
+    EV_INFO << "Received MLD message, type=" << (int)mldMsg->getType() << endl;  // keep for MLD_smoke.test
+    switch (mldMsg->getType()) {
+        case ICMPv6_MLD_QUERY:
+            processQuery(ie, packet);
+            break;
+        case ICMPv6_MLD_REPORT:
+            processReport(ie, packet);
+            break;
+        case ICMPv6_MLD_DONE:
+            // Host side: Done is sent by us, not processed (ignored on receive).
+            delete packet;
+            break;
+        default:
+            throw cRuntimeError("Mldv1: Unknown MLD message type %d", (int)mldMsg->getType());
+    }
+}
+
+// Plan 03-02: processQuery and processGroupQuery bodies are implemented there.
+void Mldv1::processQuery(NetworkInterface *ie, Packet *packet)
+{
+    // TODO (Plan 03-02): implement general and group-specific query handling
     delete packet;
 }
+
+void Mldv1::processGroupQuery(NetworkInterface *ie, HostGroupData *group, simtime_t maxRespTime)
+{
+    // TODO (Plan 03-02): implement query-driven timer scheduling
+}
+
+// Plan 03-02: processReport body (report suppression) is implemented there.
+void Mldv1::processReport(NetworkInterface *ie, Packet *packet)
+{
+    // TODO (Plan 03-02): implement report suppression
+    delete packet;
+}
+
+// --- Outbound send helpers ---
+
+void Mldv1::sendReport(NetworkInterface *ie, HostGroupData *group)
+{
+    ASSERT(group->groupAddr.isMulticast());
+    ASSERT(group->groupAddr != Ipv6Address::ALL_NODES_2);
+    EV_INFO << "Mldv1: sending Multicast Listener Report for group=" << group->groupAddr
+            << " on iface=" << ie->getInterfaceName() << "\n";
+    Packet *packet = new Packet("Mldv1 report");
+    const auto& msg = makeShared<MldReport>();
+    msg->setMulticastAddress(group->groupAddr);
+    msg->setChunkLength(B(24));
+    Icmpv6::insertChecksum(checksumMode, msg, packet);
+    packet->insertAtFront(msg);
+    sendToIPv6(packet, ie, group->groupAddr); // dest = group address (RFC 2710 §5)
+    numReportsSent++;
+}
+
+void Mldv1::sendDone(NetworkInterface *ie, HostGroupData *group)
+{
+    ASSERT(group->groupAddr.isMulticast());
+    ASSERT(group->groupAddr != Ipv6Address::ALL_NODES_2);
+    EV_INFO << "Mldv1: sending Multicast Listener Done for group=" << group->groupAddr
+            << " on iface=" << ie->getInterfaceName() << "\n";
+    Packet *packet = new Packet("Mldv1 done");
+    const auto& msg = makeShared<MldDone>();
+    msg->setMulticastAddress(group->groupAddr);
+    msg->setChunkLength(B(24));
+    Icmpv6::insertChecksum(checksumMode, msg, packet);
+    packet->insertAtFront(msg);
+    sendToIPv6(packet, ie, Ipv6Address::ALL_ROUTERS_2); // Done goes to ff02::2
+    numDonesSent++;
+}
+
+// --- sendToIPv6 ---
 
 void Mldv1::sendToIPv6(Packet *msg, NetworkInterface *ie, const Ipv6Address& dest)
 {
@@ -104,11 +357,100 @@ void Mldv1::sendToIPv6(Packet *msg, NetworkInterface *ie, const Ipv6Address& des
     // that the IPv6 module encodes protocol number 58 in the IPv6 header, and
     // Protocol::ipv6 in DispatchProtocolReq so the lp dispatcher routes to Ipv6.
     msg->addTagIfAbsent<PacketProtocolTag>()->setProtocol(&Protocol::icmpv6);
+    msg->addTagIfAbsent<DispatchProtocolInd>()->setProtocol(&Protocol::mld);
     msg->addTagIfAbsent<DispatchProtocolReq>()->setProtocol(&Protocol::ipv6);
     msg->addTagIfAbsent<InterfaceReq>()->setInterfaceId(ie->getInterfaceId());
     msg->addTagIfAbsent<L3AddressReq>()->setDestAddress(dest);
     msg->addTagIfAbsent<HopLimitReq>()->setHopLimit(1);   // RFC 2710 §3: hop limit = 1
     send(msg, "ipOut");
+}
+
+// --- Lifecycle ---
+
+void Mldv1::handleStopOperation(LifecycleOperation *operation)
+{
+    while (!hostData.empty())
+        deleteHostInterfaceData(hostData.begin()->first);
+    cModule *host = getContainingNode(this);
+    host->unsubscribe(interfaceCreatedSignal, this);
+    host->unsubscribe(interfaceDeletedSignal, this);
+    host->unsubscribe(ipv6MulticastGroupJoinedSignal, this);
+    host->unsubscribe(ipv6MulticastGroupLeftSignal, this);
+}
+
+void Mldv1::handleCrashOperation(LifecycleOperation *operation)
+{
+    while (!hostData.empty())
+        deleteHostInterfaceData(hostData.begin()->first);
+    // On crash: signals may already be torn down; attempt unsubscribe defensively
+    cModule *host = getContainingNode(this);
+    if (host->isSubscribed(interfaceCreatedSignal, this))
+        host->unsubscribe(interfaceCreatedSignal, this);
+    if (host->isSubscribed(interfaceDeletedSignal, this))
+        host->unsubscribe(interfaceDeletedSignal, this);
+    if (host->isSubscribed(ipv6MulticastGroupJoinedSignal, this))
+        host->unsubscribe(ipv6MulticastGroupJoinedSignal, this);
+    if (host->isSubscribed(ipv6MulticastGroupLeftSignal, this))
+        host->unsubscribe(ipv6MulticastGroupLeftSignal, this);
+}
+
+// --- Host group-data CRUD helpers ---
+
+Mldv1::HostGroupData *Mldv1::createHostGroupData(NetworkInterface *ie, const Ipv6Address& group)
+{
+    HostInterfaceData *interfaceData = getHostInterfaceData(ie);
+    ASSERT(interfaceData->groups.find(group) == interfaceData->groups.end());
+    HostGroupData *data = new HostGroupData(this, group);
+    interfaceData->groups[group] = data;
+    return data;
+}
+
+Mldv1::HostGroupData *Mldv1::getHostGroupData(NetworkInterface *ie, const Ipv6Address& group)
+{
+    HostInterfaceData *interfaceData = getHostInterfaceData(ie);
+    auto it = interfaceData->groups.find(group);
+    return it != interfaceData->groups.end() ? it->second : nullptr;
+}
+
+void Mldv1::deleteHostGroupData(NetworkInterface *ie, const Ipv6Address& group)
+{
+    HostInterfaceData *interfaceData = getHostInterfaceData(ie);
+    auto it = interfaceData->groups.find(group);
+    if (it != interfaceData->groups.end()) {
+        HostGroupData *data = it->second;
+        interfaceData->groups.erase(it);
+        delete data;
+    }
+}
+
+// --- Host interface-data CRUD helpers ---
+
+Mldv1::HostInterfaceData *Mldv1::getHostInterfaceData(NetworkInterface *ie)
+{
+    int interfaceId = ie->getInterfaceId();
+    auto it = hostData.find(interfaceId);
+    if (it != hostData.end())
+        return it->second;
+
+    // Create on demand
+    HostInterfaceData *data = createHostInterfaceData();
+    hostData[interfaceId] = data;
+    return data;
+}
+
+Mldv1::HostInterfaceData *Mldv1::createHostInterfaceData()
+{
+    return new HostInterfaceData(this);
+}
+
+void Mldv1::deleteHostInterfaceData(int interfaceId)
+{
+    auto interfaceIt = hostData.find(interfaceId);
+    if (interfaceIt != hostData.end()) {
+        HostInterfaceData *data = interfaceIt->second;
+        hostData.erase(interfaceIt);
+        delete data;
+    }
 }
 
 } // namespace inet
