0b7d6549745da18c441c4d41cd4921cc3568f9da
Giovanni Nardini <giovanni.nardini@unipi.it>
2026-06-19 10:54:01 +0200

    BGP: make the module lifecycle-aware (start/stop/crash)
    Convert Bgp from SimpleModule+LifecycleUnsupported to RoutingProtocolBase.
    Startup logic moves into startBgp(), messages are processed via
    handleMessageWhenUp(), and node start/stop/crash are handled through the
    standard lifecycle operation hooks.

--- src/inet/routing/bgpv4/Bgp.h
+++ src/inet/routing/bgpv4/Bgp.h
@@ -7,10 +7,9 @@
 #ifndef __INET_BGP_H
 #define __INET_BGP_H
 
-#include "inet/common/SimpleModule.h"
-#include "inet/common/lifecycle/LifecycleUnsupported.h"
 #include "inet/networklayer/contract/ipv4/Ipv4Address.h"
 #include "inet/networklayer/ipv4/Ipv4InterfaceData.h"
+#include "inet/routing/base/RoutingProtocolBase.h"
 #include "inet/routing/bgpv4/BgpCommon.h"
 #include "inet/routing/bgpv4/BgpRouter.h"
 #include "inet/routing/bgpv4/bgpmessage/BgpHeader_m.h"
@@ -19,12 +18,11 @@ namespace inet {
 
 namespace bgp {
 
-class INET_API Bgp : public SimpleModule, protected cListener, public LifecycleUnsupported
+class INET_API Bgp : public RoutingProtocolBase, protected cListener
 {
   private:
     ModuleRefByPar<IIpv4RoutingTable> rt;
     ModuleRefByPar<IInterfaceTable> ift;
-    bool isUp = false;
     BgpRouter *bgpRouter = nullptr; // data structure to fill in
     cMessage *startupTimer = nullptr; // timer for delayed startup
 
@@ -35,7 +33,12 @@ class INET_API Bgp : public SimpleModule, protected cListener, public LifecycleU
   protected:
     virtual int numInitStages() const override { return NUM_INIT_STAGES; }
     virtual void initialize(int stage) override;
-    virtual void handleMessage(cMessage *msg) override;
+    virtual void handleMessageWhenUp(cMessage *msg) override;
+    virtual void handleStartOperation(LifecycleOperation *operation) override;
+    virtual void handleStopOperation(LifecycleOperation *operation) override;
+    virtual void handleCrashOperation(LifecycleOperation *operation) override;
+    void startBgp();
+    void stopBgp();
     void createBgpRouter();
     void handleTimer(cMessage *timer);
     virtual void finish() override;
@@ -46,4 +49,3 @@ class INET_API Bgp : public SimpleModule, protected cListener, public LifecycleU
 } // namespace inet
 
 #endif
-
