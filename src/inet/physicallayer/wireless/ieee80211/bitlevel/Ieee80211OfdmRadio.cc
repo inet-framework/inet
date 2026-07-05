5014277e8b466e9d8479017201b5a2952b1d1f10
Miguel González López <miguel.gonzalez.lopez@udc.es>
2026-07-05 13:20:48 +0200

    fix(ieee80211): decode layered OFDM signal bytes

--- src/inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211OfdmRadio.cc
+++ src/inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211OfdmRadio.cc
@@ -9,6 +9,7 @@
 
 #include "inet/common/ProtocolTag_m.h"
 #include "inet/common/packet/chunk/BitCountChunk.h"
+#include "inet/common/packet/chunk/BytesChunk.h"
 #include "inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211LayeredOfdmReceiver.h"
 #include "inet/physicallayer/wireless/ieee80211/bitlevel/Ieee80211LayeredOfdmTransmitter.h"
 #include "inet/physicallayer/wireless/ieee80211/packetlevel/Ieee80211PhyHeader_m.h"
@@ -43,12 +44,24 @@ void Ieee80211OfdmRadio::decapsulate(Packet *packet) const
 {
     if (!packet->hasBitError()) {
         auto ofdmReceiver = check_and_cast<const Ieee80211LayeredOfdmReceiver *>(receiver);
-        const auto& phyHeader = packet->popAtFront<Ieee80211OfdmPhyHeader>();
+        const auto& phyHeaderBytes = packet->peekDataAt<BytesChunk>(b(0), B(5), Chunk::PF_ALLOW_IMPROPERLY_REPRESENTED);
+        auto signal = phyHeaderBytes->getByte(0) | (phyHeaderBytes->getByte(1) << 8) | (phyHeaderBytes->getByte(2) << 16);
+        const auto& phyHeader = makeShared<Ieee80211OfdmPhyHeader>();
+        phyHeader->setRate(signal & 0xF);
+        phyHeader->setReserved((signal & 0x10) != 0);
+        phyHeader->setLengthField(B((signal >> 5) & 0xFFF));
+        phyHeader->setParity((signal & 0x20000) != 0);
+        phyHeader->setTail((signal >> 18) & 0x3F);
+        phyHeader->setService(phyHeaderBytes->getByte(3) | (phyHeaderBytes->getByte(4) << 8));
+        packet->eraseAtFront(B(5));
         auto mode = ofdmReceiver->getMode(packet);
         auto dataLength = B(phyHeader->getLengthField());
         auto paddingLength = mode->getDataMode()->getPaddingLength(dataLength);
-        packet->popAtBack(std::min(paddingLength + b(6), packet->getDataLength()));
-        packet->setBitError(packet->getDataLength() != dataLength);
+        auto trailerLength = std::min(paddingLength + b(6), packet->getDataLength());
+        if (trailerLength > b(0))
+            packet->popAtBack(trailerLength,
+                    Chunk::PF_ALLOW_INCORRECT | Chunk::PF_ALLOW_INCOMPLETE | Chunk::PF_ALLOW_IMPROPERLY_REPRESENTED);
+        packet->setBitError(packet->hasBitError() || packet->getDataLength() != dataLength);
         packet->addTagIfAbsent<PacketProtocolTag>()->setProtocol(&Protocol::ieee80211Mac);
     }
 }
@@ -56,4 +69,3 @@ void Ieee80211OfdmRadio::decapsulate(Packet *packet) const
 } // namespace physicallayer
 
 } // namespace inet
-
