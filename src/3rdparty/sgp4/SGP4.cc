a14037b989ecbaad9fcd0aefe7d5b390d04f290e
Rudolf Hornig <rudi@omnetpp.org>
2026-07-06 21:16:38 +0200

    sgp4: fixed a warning in sgp4 third party lib

--- src/3rdparty/sgp4/SGP4.cc
+++ src/3rdparty/sgp4/SGP4.cc
@@ -338,11 +338,12 @@ namespace SGP4Funcs
 				// nodep used without a trigonometric function ahead
 				if ((nodep < 0.0) && (opsmode == 'a'))
 					nodep = nodep + twopi;
-				if (fabs(xnoh - nodep) > pi)
+				if (fabs(xnoh - nodep) > pi) {
 					if (nodep < xnoh)
 						nodep = nodep + twopi;
 					else
 						nodep = nodep - twopi;
+				}
 				mp = mp + pl;
 				argpp = xls - mp - cosip * nodep;
 			}
