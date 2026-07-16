126b4179297bddc29444ba9adb7cc4c2a36af094
Zoltan Bojthe <zbojthe@omnetpp.org>
2026-07-16 14:49:47 +0200

    aodv: add a protocol dissector and route UDP port 654
    AODV has serializers (AodvControlPacketsSerializer, dispatching on the packet type
    to Rreq/Rrep/Rerr/RrepAck) but nothing let the generic PacketDissector reach them:
    an AODV datagram on UDP port 654 fell back to raw bytes. Add an AodvProtocolDissector
    (popAtFront<aodv::AodvControlPacket>) and map UDP port 654 to Protocol::aodv in the
    ProtocolGroup, so a captured or emulated AODV packet is dissected into its concrete
    control-packet type. Fingerprint-neutral (verified on the 11 /examples/aodv/ sims).

---
+++ src/inet/routing/aodv/AodvProtocolDissector.h
@@ -0,0 +1,23 @@
+//
+// Copyright (C) 2026 OpenSim Ltd.
+//
+// SPDX-License-Identifier: LGPL-3.0-or-later
+//
+
+
+#ifndef __INET_AODVPROTOCOLDISSECTOR_H
+#define __INET_AODVPROTOCOLDISSECTOR_H
+
+#include "inet/common/packet/dissector/ProtocolDissector.h"
+
+namespace inet {
+
+class INET_API AodvProtocolDissector : public ProtocolDissector
+{
+  public:
+    virtual void dissect(Packet *packet, const Protocol *protocol, ICallback& callback) const override;
+};
+
+} // namespace inet
+
+#endif
